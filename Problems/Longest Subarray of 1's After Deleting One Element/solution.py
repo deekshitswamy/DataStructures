e2d8f1993a7d2277ca1c58d2becfa38bb17075c9
Deeku-Zeus <deekuzeus21@gmail.com>
2023-07-05 21:02:14 +0530

    Longest Subarray of 1's After Deleting One Element

---
+++ Problems/Longest Subarray of 1's After Deleting One Element/solution.py
@@ -0,0 +1,25 @@
+import io
+from typing import List
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        prev = None
+        curr = 0
+        longest = 0
+        for n in nums:
+            if n:
+                curr += 1
+            else:
+                if prev == None:
+                    prev = 0
+                longest = max(longest, prev + curr)
+                prev = curr
+                curr = 0
+        if prev is None:
+            return curr - 1
+        return max(longest, prev + curr)
+
+obj = Solution()
+#data = obj.longestSubarray(nums = [1,1,0,1])
+data = obj.longestSubarray(nums = [0,1,1,1,0,1,1,0,1])
+#data = obj.longestSubarray(nums = [1,1,1])
+print(data)
