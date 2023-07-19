13f4660745cf9c82936f54bd0344ee515ffab0fc
Deeku-Zeus <deekuzeus21@gmail.com>
2023-07-19 21:59:01 +0530

    Non-overlapping Intervals

---
+++ Problems/Non-overlapping Intervals/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+data = obj.eraseOverlapIntervals(intervals = [[1,2],[2,3],[3,4],[1,3]])
+#data = obj.eraseOverlapIntervals(intervals = [[1,2],[1,2],[1,2]])
+#data = obj.eraseOverlapIntervals(intervals = [[1,2],[2,3]])
+print(data)
