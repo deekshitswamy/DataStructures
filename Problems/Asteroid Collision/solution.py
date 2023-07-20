bfa8c03ccc700cc8a0c9192f2bde87d05760169a
Deeku-Zeus <deekuzeus21@gmail.com>
2023-07-20 22:37:23 +0530

    Asteroid Collision

---
+++ Problems/Asteroid Collision/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        pass
+            
+obj = Solution()
+# data = obj.asteroidCollision(asteroids = [5,10,-5])
+data = obj.asteroidCollision(asteroids = [8,-8])
+#data = obj.asteroidCollision(asteroids = [10,2,-5])
+print(data)
