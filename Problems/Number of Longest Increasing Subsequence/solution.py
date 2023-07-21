e5c984591f073e696578276348fa7e6cb935f244
Deeku-Zeus <deekuzeus21@gmail.com>
2023-07-21 21:25:03 +0530

    Number of Longest Increasing Subsequence

---
+++ Problems/Number of Longest Increasing Subsequence/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+data = obj.findNumberOfLIS(nums = [1,3,5,4,7])
+#data = obj.findNumberOfLIS(nums = [2,2,2,2,2])
+print(data)
