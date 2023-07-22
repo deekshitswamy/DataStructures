fd47e637a30f3a9f9b7ee39ab91316e6418b40c5
Deeku-Zeus <deekuzeus21@gmail.com>
2023-07-22 19:40:30 +0530

    Knight Probability in Chessboard

---
+++ Problems/Knight Probability in Chessboard/solution.py
@@ -0,0 +1,9 @@
+import io
+class Solution:
+    def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
+        pass
+
+obj = Solution()
+#data = obj.knightProbability(n = 3, k = 2, row = 0, column = 0)
+data = obj.knightProbability(n = 1, k = 0, row = 0, column = 0)
+print(data)
