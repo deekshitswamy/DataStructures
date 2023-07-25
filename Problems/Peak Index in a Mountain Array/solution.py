3d7d2fd1bffb454ae7e5e4bc0fb8debad5b5d79b
Deeku-Zeus <deekuzeus21@gmail.com>
2023-07-25 22:39:42 +0530

    changes

--- Problems/Peak Index in a Mountain Array/solution.py
+++ Problems/Peak Index in a Mountain Array/solution.py
@@ -2,7 +2,22 @@ import io
 from typing import List
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        pass
+        left, right = 0, len(arr) - 1
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if arr[mid] < arr[mid + 1]:
+                # We are on the ascending slope of the mountain.
+                # Move the left pointer to mid + 1.
+                left = mid + 1
+            else:
+                # We are on the descending slope of the mountain.
+                # Move the right pointer to mid.
+                right = mid
+
+        # The left pointer will be at the peak of the mountain.
+        return left
 
 obj = Solution()
 data = obj.peakIndexInMountainArray(arr = [0,1,0])
