0fec44d7244f5ea33c3799c85b6dcca7cda56363
Deeku-Zeus <deekuzeus21@gmail.com>
2023-07-25 22:38:56 +0530

    Peak Index in a Mountain Array

---
+++ Problems/Peak Index in a Mountain Array/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        pass
+
+obj = Solution()
+data = obj.peakIndexInMountainArray(arr = [0,1,0])
+#data = obj.peakIndexInMountainArray(arr = [0,2,1,0])
+#data = obj.peakIndexInMountainArray(arr = [0,10,5,2])
+print(data)
