1ef726b8d8084d03c489ba40d6bfa2c737aa9a24
Deeku-Zeus <deekuzeus21@gmail.com>
2023-07-26 20:52:49 +0530

    Minimum Speed to Arrive on Time

---
+++ Problems/Minimum Speed to Arrive on Time/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+        pass
+obj = Solution()
+#data = obj.minSpeedOnTime(dist = [1,3,2], hour = 6)
+#data = obj.minSpeedOnTime(dist = [1,3,2], hour = 2.7)
+data = obj.minSpeedOnTime(dist = [1,3,2], hour = 1.9)
+print(data)
