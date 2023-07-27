99223c614355d842870a771ff3a9887a69d6c82f
Deeku-Zeus <deekuzeus21@gmail.com>
2023-07-27 20:28:02 +0530

    changes

--- Problems/Maximum Running Time of N Computers/solution.py
+++ Problems/Maximum Running Time of N Computers/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def maxRunTime(self, n: int, batteries: List[int]) -> int:
-        pass
+        batteries.sort()
+        total_runtime = sum(batteries)
+        max_runtime = total_runtime // n
+        
+        for battery in reversed(batteries):
+            if battery <= max_runtime:
+                break
+            total_runtime -= battery
+            n -= 1
+            max_runtime = total_runtime // n
+            
+        return max_runtime
 
 obj = Solution()
 #data = obj.maxRunTime(n = 2, batteries = [3,3,3])
