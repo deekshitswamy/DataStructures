f71736148c17af55670470b1abaf22df4cd8210f
Deeku-Zeus <deekuzeus21@gmail.com>
2023-07-27 19:56:44 +0530

    Maximum Running Time of N Computers

---
+++ Problems/Maximum Running Time of N Computers/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxRunTime(self, n: int, batteries: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxRunTime(n = 2, batteries = [3,3,3])
+data = obj.maxRunTime(n = 2, batteries = [1,1,1,1])
+print(data)
