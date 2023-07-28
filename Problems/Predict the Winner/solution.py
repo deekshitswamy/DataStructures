655b5e6d373322859b9eda1fc384f0e0af714198
Deeku-Zeus <deekuzeus21@gmail.com>
2023-07-28 19:50:41 +0530

    changes

--- Problems/Predict the Winner/solution.py
+++ Problems/Predict the Winner/solution.py
@@ -2,7 +2,14 @@ import io
 from typing import List
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        pass
+        n = len(nums)
+        def maxDiff(left, right):
+            if left == right:
+                return nums[left]
+            score_by_left = nums[left] - maxDiff(left + 1, right)
+            score_by_right = nums[right] - maxDiff(left, right - 1)
+            return max(score_by_left, score_by_right)
+        return maxDiff(0, n - 1) >= 0
 
 obj = Solution()
 data = obj.PredictTheWinner(nums = [1,5,2])
