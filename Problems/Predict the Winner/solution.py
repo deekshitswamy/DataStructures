7df4c45a7dbc414bee1d9926865786ae323e9310
Deeku-Zeus <deekuzeus21@gmail.com>
2023-07-28 19:39:01 +0530

    Predict the Winner

---
+++ Problems/Predict the Winner/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        pass
+
+obj = Solution()
+data = obj.PredictTheWinner(nums = [1,5,2])
+#data = obj.PredictTheWinner(nums = [1,5,233,7])
+print(data)
