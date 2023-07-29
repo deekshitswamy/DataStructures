7b111874bbcd0620ec0d9a4cf1555bfaeedc0f9f
Deeku-Zeus <deekuzeus21@gmail.com>
2023-07-29 19:47:16 +0530

    Soup Servings

---
+++ Problems/Soup Servings/solution.py
@@ -0,0 +1,9 @@
+import io
+class Solution:
+    def soupServings(self, n: int) -> float:
+        pass
+
+obj = Solution()
+#data = obj.soupServings(n = 50)
+data = obj.soupServings(n = 100)
+print(data)
