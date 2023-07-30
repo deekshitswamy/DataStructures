a2ca43250df3373c39b3a0ecb499a9cd743c6612
Deeku-Zeus <deekuzeus21@gmail.com>
2023-07-30 19:21:48 +0530

    Strange Printer

---
+++ Problems/Strange Printer/solution.py
@@ -0,0 +1,9 @@
+import io
+class Solution:
+    def strangePrinter(self, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.strangePrinter(s = "aaabbb")
+data = obj.strangePrinter(s = "aba")
+print(data)
