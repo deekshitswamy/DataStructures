4c904fe1999f730164e5d3ecab5a7455dfd28855
Deeku-Zeus <deekuzeus21@gmail.com>
2023-07-31 20:12:07 +0530

    Minimum ASCII Delete Sum for Two Strings

---
+++ Problems/Minimum ASCII Delete Sum for Two Strings/solution.py
@@ -0,0 +1,10 @@
+import io
+
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minimumDeleteSum(s1 = "sea", s2 = "eat")
+data = obj.minimumDeleteSum(s1 = "delete", s2 = "leet")
+print(data)
