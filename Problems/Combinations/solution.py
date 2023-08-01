b44bb7dbeb876d18b28333bfdff3f309f232583c
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-01 21:44:37 +0530

    changes

--- Problems/Combinations/solution.py
+++ Problems/Combinations/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        pass
+        def backtrack(first = 1,curr = []):
+            if len(curr) == k :
+                output.append(curr[:])
+                return 
+            for i in range(first,n+1):
+                curr.append(i)
+                backtrack(i+1,curr)
+                curr.pop()
+        output = []
+        backtrack()
+        return output
 
 obj = Solution()
 #data = obj.combine(n = 4, k = 2)
