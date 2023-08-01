a668ae35dc7e9972200bef9d7f18432ab9a85e6c
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-01 21:42:16 +0530

    Combinations

---
+++ Problems/Combinations/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        pass
+
+obj = Solution()
+#data = obj.combine(n = 4, k = 2)
+data = obj.combine(n = 1, k = 1)
+print(data)
