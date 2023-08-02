23a54d35cf1665c435524c306bbdae7d821f88c4
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-02 18:57:50 +0530

    Permutations

---
+++ Problems/Permutations/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        pass
+
+obj = Solution()
+data = obj.permute(nums = [1,2,3])
+#data = obj.permute(nums = [0,1])
+#data = obj.permute(nums = [1])
+print(data)
