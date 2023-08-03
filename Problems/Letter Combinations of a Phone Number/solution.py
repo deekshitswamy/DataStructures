3af596e79fffaf71fe9c92f35eac051f95b039ef
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-03 20:05:40 +0530

    Letter Combinations of a Phone Number

---
+++ Problems/Letter Combinations of a Phone Number/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        pass
+
+obj = Solution()
+#data = obj.letterCombinations(digits = "23")
+#data = obj.letterCombinations(digits = "")
+data = obj.letterCombinations(digits = "2")
+print(data)
