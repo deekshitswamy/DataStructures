66007e182f68d39f4ac7d251f7e3880d8f9c89b9
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-04 19:27:08 +0530

    changes

--- Problems/Word Break/solution.py
+++ Problems/Word Break/solution.py
@@ -3,7 +3,17 @@ from collections import defaultdict
 from typing import List
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        pass
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True
+        max_len = max(map(len, wordDict)) 
+        for i in range(1, n + 1):
+            for j in range(i - 1, max(i - max_len - 1, -1), -1): 
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+                    break
+
+        return dp[n]
 
 obj = Solution()
 #data = obj.wordBreak(s = "leetcode", wordDict = ["leet","code"])
