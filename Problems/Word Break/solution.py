bfaf5419ded059002fee8e759f5f4c2160cb4ab5
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-04 19:24:40 +0530

    Word Break

---
+++ Problems/Word Break/solution.py
@@ -0,0 +1,12 @@
+import io
+from collections import defaultdict
+from typing import List
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.wordBreak(s = "leetcode", wordDict = ["leet","code"])
+#data = obj.wordBreak(s = "applepenapple", wordDict = ["apple","pen"])
+data = obj.wordBreak(s = "catsandog", wordDict = ["cats","dog","sand","and","cat"])
+print(data)
