49f8fb02a4c34acab37fe6040b83c35c8f81ccff
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-06 18:02:54 +0530

    Number of Music Playlists

---
+++ Problems/Number of Music Playlists/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numMusicPlaylists(n = 3, goal = 3, k = 1)
+#data = obj.numMusicPlaylists(n = 2, goal = 3, k = 0)
+data = obj.numMusicPlaylists(n = 2, goal = 3, k = 1)
+print(data)
