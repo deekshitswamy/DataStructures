a678d0dcaacab5eb9f02910a463cea9a5fb12ea8
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-08 21:02:54 +0530

    Search in Rotated Sorted Array

---
+++ Problems/Search in Rotated Sorted Array/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        pass
+
+obj = Solution()
+data = obj.search(nums = [4,5,6,7,0,1,2], target = 0)
+#data = obj.search(nums = [4,5,6,7,0,1,2], target = 3)
+#data = obj.search(nums = [1], target = 0)
+print(data)
