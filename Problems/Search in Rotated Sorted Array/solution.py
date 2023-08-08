1edc3b1bf0eef7ce9d46c0cf41d4bd503ba1f0a6
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-08 21:05:44 +0530

    changes

--- Problems/Search in Rotated Sorted Array/solution.py
+++ Problems/Search in Rotated Sorted Array/solution.py
@@ -8,7 +8,7 @@ class Solution:
         return -1
 
 obj = Solution()
-data = obj.search(nums = [4,5,6,7,0,1,2], target = 0)
+#data = obj.search(nums = [4,5,6,7,0,1,2], target = 0)
 #data = obj.search(nums = [4,5,6,7,0,1,2], target = 3)
-#data = obj.search(nums = [1], target = 0)
+data = obj.search(nums = [1], target = 0)
 print(data)
