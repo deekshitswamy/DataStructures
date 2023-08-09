4e895c41241787d323d489834efc4fb64cfed5cd
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-09 20:22:44 +0530

    changes

--- Problems/Minimize the Maximum Difference of Pairs/solution.py
+++ Problems/Minimize the Maximum Difference of Pairs/solution.py
@@ -2,7 +2,23 @@ import io
 from typing import List
 class Solution:
     def minimizeMax(self, nums: List[int], p: int) -> int:
-        pass
+        nums.sort()
+        n = len(nums)
+        left, right = 0, nums[-1] - nums[0]
+        while left < right:
+            mid = (left + right) // 2
+            k = 0
+            i = 1
+            while i < n:
+                if nums[i] - nums[i - 1] <= mid:
+                    k += 1
+                    i += 1
+                i += 1
+            if k >= p:
+                right = mid
+            else:
+                left = mid + 1
+        return left
 
 obj = Solution()
 #data = obj.minimizeMax(nums = [10,1,2,7,1,3], p = 2)
