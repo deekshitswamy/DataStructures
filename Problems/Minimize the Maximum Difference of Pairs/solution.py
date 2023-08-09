7c46fbac5b7571164feb10914ba5e95aaaa5eabf
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-09 20:21:05 +0530

    Minimize the Maximum Difference of Pairs

---
+++ Problems/Minimize the Maximum Difference of Pairs/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minimizeMax(self, nums: List[int], p: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minimizeMax(nums = [10,1,2,7,1,3], p = 2)
+data = obj.minimizeMax(nums = [4,2,1,2], p = 1)
+print(data)
