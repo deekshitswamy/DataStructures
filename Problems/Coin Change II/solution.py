5ca314cf348cc6dd473e6aa928b2350049fe17ed
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-11 23:50:09 +0530

    Coin Change II

---
+++ Problems/Coin Change II/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+     def change(self, amount: int, coins: List[int]) -> int:
+          pass
+
+obj = Solution()
+#data = obj.change(amount = 5, coins = [1,2,5])
+#data = obj.change(amount = 3, coins = [2])
+data = obj.change(amount = 10, coins = [10])
+print(data)
