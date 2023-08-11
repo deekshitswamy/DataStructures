c540a456e578a03f019126dcb550f6a7371a0408
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-11 23:51:30 +0530

    changes

--- Problems/Coin Change II/solution.py
+++ Problems/Coin Change II/solution.py
@@ -2,7 +2,13 @@ import io
 from typing import List
 class Solution:
      def change(self, amount: int, coins: List[int]) -> int:
-          pass
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] += dp[i - coin]
+
+        return dp[-1]
 
 obj = Solution()
 #data = obj.change(amount = 5, coins = [1,2,5])
