263de8eb37c4ae61aae0660513a7c32f4f159f6b
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-12 18:33:57 +0530

    Unique Paths II

---
+++ Problems/Unique Paths II/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        pass
+
+
+obj = Solution()
+data = obj.uniquePathsWithObstacles(obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]])
+#data = obj.uniquePathsWithObstacles(obstacleGrid = [[0,1],[0,0]])
+print(data)
