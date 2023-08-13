25a43e7bd574d99a3785edcfb1f98bf136fc1039
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-13 10:46:06 +0530

    changes

--- Problems/Check if There is a Valid Partition For The Array/solution.py
+++ Problems/Check if There is a Valid Partition For The Array/solution.py
@@ -1,8 +1,21 @@
 import io
+from functools import cache
 from typing import List
 class Solution:
     def validPartition(self, nums: List[int]) -> bool:
-        pass
+        n=len(nums)
+        @cache
+        def fn(index):
+            if index==n:
+                return True
+            if index+1<n and nums[index]==nums[index+1] and fn(index+2):
+                return True
+            if index+2<n and nums[index]==nums[index+1]==nums[index+2] and fn(index+3):
+                return True
+            if index+2<n and nums[index]==nums[index+1]-1 and nums[index+1]==nums[index+2]-1 and fn(index+3):
+                return True
+            return False
+        return fn(0)
 
 obj = Solution()
 #data = obj.validPartition(nums = [4,4,4,5,6])
