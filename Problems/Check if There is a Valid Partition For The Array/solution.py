e820815e91edc6dd7ae0fa8ccef1a211f276eb82
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-13 10:42:50 +0530

    Check if There is a Valid Partition For The Array

---
+++ Problems/Check if There is a Valid Partition For The Array/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def validPartition(self, nums: List[int]) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.validPartition(nums = [4,4,4,5,6])
+data = obj.validPartition(nums = [1,1,1,2])
+print(data)
