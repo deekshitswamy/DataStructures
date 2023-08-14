23affcf41397c1221c1e662be2a19e78be3eddc4
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-14 23:46:00 +0530

    changes

--- Problems/Kth Largest Element in an Array/solution.py
+++ Problems/Kth Largest Element in an Array/solution.py
@@ -1,8 +1,22 @@
 import io
+import random
 from typing import List
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        pass 
+        pivot=random.choice(nums)
+        left=[x for x in nums if x>pivot]
+        mid=[x for x in nums if x==pivot]
+        right=[x for x in nums if x<pivot]
+        n=len(left)
+        m=len(mid)
+        if k<=n:
+            return self.findKthLargest(left,k)
+
+        elif k>(n+m):
+            return self.findKthLargest(right,k-(n+m))
+
+        else:
+            return mid[0] 
 
 obj = Solution()
 #data = obj.findKthLargest(nums = [3,2,1,5,6,4], k = 2)
