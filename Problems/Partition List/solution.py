0a184b938b1587792ba9a37382bed4197ac6ae98
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-15 21:38:14 +0530

    changes

--- Problems/Partition List/solution.py
+++ Problems/Partition List/solution.py
@@ -26,20 +26,20 @@ class Solution:
         return slist.next
 
 
-node5 = ListNode(2, None)
-node4 = ListNode(5, node5)
-node3 = ListNode(2, node4)
-node2 = ListNode(3, node3)
-node1 = ListNode(4, node2)
-root = ListNode(1, node1)
+# node5 = ListNode(2, None)
+# node4 = ListNode(5, node5)
+# node3 = ListNode(2, node4)
+# node2 = ListNode(3, node3)
+# node1 = ListNode(4, node2)
+# root = ListNode(1, node1)
 
 # example 2
-# node1 = ListNode(1, None)
-# root = ListNode(2, node1)
+node1 = ListNode(1, None)
+root = ListNode(2, node1)
 
 obj = Solution()
-data = obj.partition(head = root, x = 3)
-#data = obj.partition(head = root, x = 2)
+#data = obj.partition(head = root, x = 3)
+data = obj.partition(head = root, x = 2)
 ans = []
 while data:
     ans.append(data.val)
