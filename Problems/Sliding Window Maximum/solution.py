0dcb862ba90ccebb83edfcc54e997f0fd4b47b05
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-16 18:12:34 +0530

    changes

--- Problems/Sliding Window Maximum/solution.py
+++ Problems/Sliding Window Maximum/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        pass
+        n=len(nums)
+        seen=[]
+        ans=[]   
+        for i in range(n):
+            if seen and seen[0]==i-k:
+                seen.pop(0)
+
+            while seen and nums[seen[-1]]<nums[i]:
+                seen.pop()
+
+            seen.append(i)
+            if i>=k-1:
+                ans.append(nums[seen[0]])
+        return ans
 
 obj = Solution()
 data = obj.maxSlidingWindow(nums = [1,3,-1,-3,5,3,6,7], k = 3)
