64a77e3d5680de2508bf309281e4ddc22ade3d9c
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-17 20:13:16 +0530

    changes

---
+++ Problems/01 Matrix/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        pass
+
+
+obj = Solution()
+data = obj.updateMatrix(mat = [[0,0,0],[0,1,0],[0,0,0]])
+data = obj.updateMatrix(mat = [[0,0,0],[0,1,0],[1,1,1]])
+print(data)
