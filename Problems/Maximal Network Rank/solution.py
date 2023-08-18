35a8d7e44717afec03feb6b6843975479041ea06
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-18 22:46:44 +0530

    changes

--- Problems/Maximal Network Rank/solution.py
+++ Problems/Maximal Network Rank/solution.py
@@ -2,7 +2,31 @@ import io
 from typing import List
 class Solution:
     def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
-        pass
+        g=[[False]*n for _ in range(n)]
+        for x,y in roads:
+            g[x][y]=g[y][x]=True
+
+        ans=0
+        for i in range(n):
+            for j in range(n):
+                if i==j:
+                    continue
+
+                cur=0
+                for k in range(n):
+                    if k!=i and k!=j:
+                        if g[i][k]:
+                            cur+=1
+
+                        if g[j][k]:
+                             cur+=1
+
+                if g[i][j]:
+                    cur+=1
+
+                ans=max(cur,ans)
+
+        return ans 
 obj = Solution()
 #data = obj.maximalNetworkRank(n = 4, roads = [[0,1],[0,3],[1,2],[1,3]])
 #data = obj.maximalNetworkRank(n = 5, roads = [[0,1],[0,3],[1,2],[1,3],[2,3],[2,4]])
