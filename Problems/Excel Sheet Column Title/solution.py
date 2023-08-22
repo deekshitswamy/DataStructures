ccff7e143d1f332ad6d63c636ab557cbc3a95340
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-22 15:16:52 +0530

    Excel Sheet Column Title

---
+++ Problems/Excel Sheet Column Title/solution.py
@@ -0,0 +1,11 @@
+import io
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        pass
+
+
+obj = Solution()
+#data = obj.convertToTitle(columnNumber = 1)
+#data = obj.convertToTitle(columnNumber = 28)
+data = obj.convertToTitle(columnNumber = 701)
+print(data)
