3a5a88bd38192475a84bc85c68722399b6620aef
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-22 15:29:02 +0530

    Sort Items by Groups Respecting Dependencies

---
+++ Problems/Sort Items by Groups Respecting Dependencies/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+
+class Solution:
+    def sortItems(self, n: int, m: int, group: List[int], beforeItems: List[List[int]]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.sortItems(n = 8, m = 2, group = [-1,-1,1,0,0,1,0,-1], beforeItems = [[],[6],[5],[6],[3,6],[],[],[]])
+data = obj.sortItems(n = 8, m = 2, group = [-1,-1,1,0,0,1,0,-1], beforeItems = [[],[6],[5],[6],[3],[],[4],[]])
+print(data)
