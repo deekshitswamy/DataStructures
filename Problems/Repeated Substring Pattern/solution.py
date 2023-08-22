a7a51578d8e301de2dcd4f7bfaf7af6fd2217d2d
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-22 15:37:15 +0530

    Repeated Substring Pattern

---
+++ Problems/Repeated Substring Pattern/solution.py
@@ -0,0 +1,10 @@
+import io
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.repeatedSubstringPattern(s = "abab")
+#data = obj.repeatedSubstringPattern(s = "aba")
+data = obj.repeatedSubstringPattern(s = "abcabcabcabc")
+print(data)
