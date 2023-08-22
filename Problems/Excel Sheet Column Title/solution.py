c7657f1ed1e3645bb2177b66fa13640d35d2aa51
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-22 15:18:18 +0530

    changes

--- Problems/Excel Sheet Column Title/solution.py
+++ Problems/Excel Sheet Column Title/solution.py
@@ -1,7 +1,12 @@
 import io
 class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
-        pass
+        ans=""        
+        while columnNumber>0:
+            c=chr(ord('A')+(columnNumber-1)%26)
+            ans=c+ans
+            columnNumber=(columnNumber-1)//26            
+        return ans         
 
 
 obj = Solution()
