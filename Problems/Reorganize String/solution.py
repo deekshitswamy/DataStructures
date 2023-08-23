55364646b281095a0937204e12aae3027c31bd84
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-23 19:23:05 +0530

    changes

--- Problems/Reorganize String/solution.py
+++ Problems/Reorganize String/solution.py
@@ -1,7 +1,28 @@
 import io
+from heapq import heapify, heappop
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        pass
+        n=len(s)
+        freq=dict()
+        for ch in s:
+            freq[ch]=1+freq.get(ch,0)
+
+        hp=[(-v,k) for k,v in freq.items()]  
+        heapify(hp)
+
+        i=0
+        ans=[""]*n  
+        while hp:
+            v,k=heappop(hp)
+            v=-v
+            if 2*v-1>n:
+                return ""
+
+            for _ in range(v):
+                ans[i]=k
+                i=i+2 if i+2<n else 1  
+
+        return "".join(ans)
 
 obj = Solution()
 #data = obj.reorganizeString(s = "aab")
