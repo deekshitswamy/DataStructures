ad470250dc4b7f748bb10afc0542afba8c3c288b
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-23 19:18:41 +0530

    Reorganize String

---
+++ Problems/Reorganize String/solution.py
@@ -0,0 +1,9 @@
+import io
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.reorganizeString(s = "aab")
+data = obj.reorganizeString(s = "aaab")
+print(data)
