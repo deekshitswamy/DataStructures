ce0c2c294d57d3fe552ccb14bd436d0fcf855468
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-24 16:40:09 +0530

    Text Justification

---
+++ Problems/Text Justification/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        pass
+
+obj = Solution()
+#data = obj.fullJustify(words = ["This", "is", "an", "example", "of", "text", "justification."], maxWidth = 16)
+#data = obj.fullJustify(words = ["What","must","be","acknowledgment","shall","be"], maxWidth = 16)
+data = obj.fullJustify(words = ["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"], maxWidth = 20)
+print(data)
