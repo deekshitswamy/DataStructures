7526172960952623f8aace98e94ff8a32eb0b22a
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-25 14:24:11 +0530

    Interleaving String

---
+++ Problems/Interleaving String/solution.py
@@ -0,0 +1,11 @@
+import io
+import collections
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.isInterleave(s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac")
+#data = obj.isInterleave(s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc")
+data = obj.isInterleave(s1 = "", s2 = "", s3 = "")
+print(data)
