fc083ad6110d6e1ad40263454798630cfbae126b
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-26 17:49:50 +0530

    Maximum Length of Pair Chain

---
+++ Problems/Maximum Length of Pair Chain/solution.py
@@ -0,0 +1,9 @@
+import io
+from typing import List
+class Solution:
+    def findLongestChain(self, pairs: List[List[int]]) -> int:
+        pass
+obj = Solution()
+#data = obj.findLongestChain(pairs = [[1,2],[2,3],[3,4]])
+data = obj.findLongestChain(pairs = [[1,2],[7,8],[4,5]])
+print(data)
