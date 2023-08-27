5a6fd6a94130ee8640cdcde13eb337d8e3e2c81f
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-27 20:24:44 +0530

    changes

---
+++ Problems/Frog Jump/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.canCross(stones = [0,1,3,5,6,8,12,17])
+data = obj.canCross(stones = [0,1,2,3,4,8,9,11])
+print(data)
