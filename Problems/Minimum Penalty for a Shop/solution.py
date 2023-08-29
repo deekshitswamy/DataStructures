0a679bf9271f2b03b55a8ed31fa46cff179d872f
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-29 18:38:08 +0530

    Minimum Penalty for a Shop

---
+++ Problems/Minimum Penalty for a Shop/solution.py
@@ -0,0 +1,9 @@
+import io
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        pass
+obj = Solution()
+#data = obj.bestClosingTime(customers = "YYNY")
+#data = obj.bestClosingTime(customers = "NNNNN")
+data = obj.bestClosingTime(customers = "YYYY")
+print(data)
