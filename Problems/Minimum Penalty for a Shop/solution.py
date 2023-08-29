4a928447f816b9239366282a30e9f5767c07f92b
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-29 18:39:43 +0530

    changes

--- Problems/Minimum Penalty for a Shop/solution.py
+++ Problems/Minimum Penalty for a Shop/solution.py
@@ -1,7 +1,21 @@
 import io
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-        pass
+        penalty = len(customers)
+        min_penalty = penalty
+        min_penalty_index = 0
+
+        for i in range(1, len(customers)+1):
+            if customers[i-1] == 'Y':
+                penalty -= 1
+            else:
+                penalty += 1
+
+            if penalty < min_penalty:
+                min_penalty = penalty
+                min_penalty_index = i
+
+        return min_penalty_index
 obj = Solution()
 #data = obj.bestClosingTime(customers = "YYNY")
 #data = obj.bestClosingTime(customers = "NNNNN")
