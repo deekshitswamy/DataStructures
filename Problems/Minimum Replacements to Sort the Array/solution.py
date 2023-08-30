11a1d66ce707e4c68780ade0e99d5f5937339229
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-30 18:23:14 +0530

    Minimum Replacements to Sort the Array

---
+++ Problems/Minimum Replacements to Sort the Array/solution.py
@@ -0,0 +1,9 @@
+import io
+from typing import List
+class Solution:
+    def minimumReplacement(self, nums: List[int]) -> int:
+        pass
+obj = Solution()
+#data = obj.minimumReplacement(nums = [3,9,3])
+data = obj.minimumReplacement(nums = [1,2,3,4,5])
+print(data)
