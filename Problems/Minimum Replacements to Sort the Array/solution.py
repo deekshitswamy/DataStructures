c2110f08de89f327eacaf962009297bc2a700c18
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-30 18:23:43 +0530

    changes

--- Problems/Minimum Replacements to Sort the Array/solution.py
+++ Problems/Minimum Replacements to Sort the Array/solution.py
@@ -1,8 +1,16 @@
 import io
+from math import ceil
 from typing import List
 class Solution:
     def minimumReplacement(self, nums: List[int]) -> int:
-        pass
+        ans=0
+        prev=10**9+1
+        for x in nums[::-1]:
+            d=ceil(x/prev)
+            ans+=d-1
+            prev=x//d
+
+        return ans 
 obj = Solution()
 #data = obj.minimumReplacement(nums = [3,9,3])
 data = obj.minimumReplacement(nums = [1,2,3,4,5])
