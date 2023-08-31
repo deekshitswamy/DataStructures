a8c4f9e170ac5de9cbb3f16faecee485b8004c00
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-31 05:40:55 +0530

    changes

--- Problems/Minimum Number of Taps to Open to Water a Garden/solution.py
+++ Problems/Minimum Number of Taps to Open to Water a Garden/solution.py
@@ -2,7 +2,24 @@ import io
 from typing import List
 class Solution:
     def minTaps(self, n: int, ranges: List[int]) -> int:
-        pass
+        for i in range(n + 1):
+            ranges[i] = [i - ranges[i], i + ranges[i]]
+        ranges.sort(key = lambda x: x[0])
+        ans = 1
+        curr = prev = 0
+        maxval = -1
+        while prev < n + 1:
+            while prev < n + 1 and ranges[prev][0] <= curr:
+                maxval = max(maxval, ranges[prev][1])
+                prev += 1
+            if maxval >= n:
+                return ans
+            elif maxval == curr:
+                return -1
+            else:
+                curr = maxval
+                ans += 1
+        return -1
 obj = Solution()
 #data = obj.minTaps(n = 5, ranges = [3,4,1,1,0,0])
 data = obj.minTaps(n = 3, ranges = [0,0,0,0])
