b787bb815d419f07ada4e231b792bf3b2d96518f
Deeku-Zeus <deekuzeus21@gmail.com>
2023-08-31 05:39:58 +0530

    Minimum Number of Taps to Open to Water a Garden

---
+++ Problems/Minimum Number of Taps to Open to Water a Garden/solution.py
@@ -0,0 +1,9 @@
+import io
+from typing import List
+class Solution:
+    def minTaps(self, n: int, ranges: List[int]) -> int:
+        pass
+obj = Solution()
+#data = obj.minTaps(n = 5, ranges = [3,4,1,1,0,0])
+data = obj.minTaps(n = 3, ranges = [0,0,0,0])
+print(data)
