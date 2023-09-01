c1884c35c7e47751c230aaff83e6ef96d51f2f70
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-01 11:39:56 +0530

    changes

--- Problems/Counting Bits/solution.py
+++ Problems/Counting Bits/solution.py
@@ -2,7 +2,13 @@ import io
 from typing import List
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        pass
+        dp=[0]
+        for i in range(1,n+1):
+            if i%2==1:
+                dp.append(dp[i-1]+1)
+            else:
+                dp.append(dp[i//2])
+        return dp 
 
 obj = Solution()
 #data = obj.countBits(n = 2)
