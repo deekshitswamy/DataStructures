d90e111ab6e31889ed6b0bb3eba5e66ad58974e4
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-01 11:38:28 +0530

    Counting Bits

---
+++ Problems/Counting Bits/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.countBits(n = 2)
+data = obj.countBits(n = 5)
+print(data)
