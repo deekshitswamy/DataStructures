5be4c3adad5f4642deb192def1f927afff64b5cf
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-02 15:39:26 +0530

    changes

--- Problems/Extra Characters in a String/solution.py
+++ Problems/Extra Characters in a String/solution.py
@@ -1,9 +1,21 @@
 import io
 from typing import List
+from functools import cache
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
-        pass
+        n=len(s)
+        @cache
+        def dp(i:int,extra:int)->int:
+            if i>=n:
+                return extra
 
+            min_extra=dp(i+1,extra)
+            for word in dictionary:
+                if s[i:].startswith(word):
+                    min_extra=min(min_extra,dp(i+len(word),extra-len(word)))
+
+            return min_extra
+        return dp(0,n)
 
 obj = Solution()
 #data = obj.minExtraChar(s = "leetscode", dictionary = ["leet","code","leetcode"])
