ce91dd04c0d90120169a4a855dd19db442b1663a
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-02 15:38:07 +0530

    Extra Characters in a String

---
+++ Problems/Extra Characters in a String/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        pass
+
+
+obj = Solution()
+#data = obj.minExtraChar(s = "leetscode", dictionary = ["leet","code","leetcode"])
+data = obj.minExtraChar(s = "sayhelloworld", dictionary = ["hello","world"])
+print(data)
