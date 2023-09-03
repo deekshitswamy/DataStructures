fb44238c3232d048e2f06992cc50eec143e54f85
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-03 15:25:08 +0530

    Unique Paths

---
+++ Problems/Unique Paths/solution.py
@@ -0,0 +1,9 @@
+import io
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.uniquePaths(m = 3, n = 7)
+data = obj.uniquePaths(m = 3, n = 2)
+print(data)
