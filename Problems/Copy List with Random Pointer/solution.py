d14829c8a6bbec9e041e2a72e044b6d0d488c89c
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-05 18:31:12 +0530

    changes

--- Problems/Copy List with Random Pointer/solution.py
+++ Problems/Copy List with Random Pointer/solution.py
@@ -9,7 +9,21 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: Node) -> Node:
-        pass
+        old_to_copy={None:None}
+        cur=head
+        while cur:
+            copy=Node(cur.val)
+            old_to_copy[cur]=copy
+            cur=cur.next
+
+        cur=head
+        while cur:
+            copy=old_to_copy[cur]
+            copy.next=old_to_copy[cur.next]
+            copy.random=old_to_copy[cur.random]
+            cur=cur.next
+
+        return old_to_copy[head]
 
 obj = Solution()
 #data = obj.copyRandomList(head = [[7,None],[13,0],[11,4],[10,2],[1,0]])
