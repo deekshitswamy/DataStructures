028895e8d32022af3a8f214e8f2788984d90983f
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-08 17:07:20 +0530

    Pascal's Triangle

---
+++ Problems/Pascal's Triangle/solution.py
@@ -0,0 +1,13 @@
+import io
+from typing import List
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+
+obj = Solution()
+#data = obj.generate(numRows = 5)
+data = obj.generate(numRows = 1)
+print(data)
