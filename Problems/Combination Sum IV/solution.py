47517d29bbe5084a38ef5837ae6c0e95b751de23
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-09 20:14:30 +0530

    Combination Sum IV

---
+++ Problems/Combination Sum IV/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.combinationSum4(nums = [1,2,3], target = 4)
+data = obj.combinationSum4(nums = [9], target = 3)
+print(data)
