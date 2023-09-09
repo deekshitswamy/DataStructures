f589839ebecc55f2c054ac17ad17c493850e94af
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-09 20:16:15 +0530

    changes

--- Problems/Combination Sum IV/solution.py
+++ Problems/Combination Sum IV/solution.py
@@ -2,7 +2,14 @@ import io
 from typing import List
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        pass
+        dp=[0]*(target+1)
+        dp[0]=1
+        for t in range(target+1):
+            for num in nums:
+                if num<=t:
+                    dp[t]+=dp[t-num]
+
+        return dp[target]
 
 obj = Solution()
 #data = obj.combinationSum4(nums = [1,2,3], target = 4)
