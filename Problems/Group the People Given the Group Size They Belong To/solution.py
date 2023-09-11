f07c34b8d0c7136dfad97593a8aeb2977db39cc2
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-11 17:01:07 +0530

    changes

--- Problems/Group the People Given the Group Size They Belong To/solution.py
+++ Problems/Group the People Given the Group Size They Belong To/solution.py
@@ -1,8 +1,16 @@
 import io
+import collections
 from typing import List
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
-        pass
+        ans=[]
+        group_map=collections.defaultdict(list)
+        for i,group_size in enumerate(groupSizes):
+            group_map[group_size].append(i)
+
+        return [ids[i:i+k]
+        for k,ids in group_map.items()
+        for i in range(0,len(ids),k)]
 
 obj = Solution()
 data = obj.groupThePeople(groupSizes = [3,3,3,3,3,1,3])
