dbab5b2d731ce535094017717d5ace82a4ec1eca
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-11 16:53:25 +0530

    Group the People Given the Group Size They Belong To

---
+++ Problems/Group the People Given the Group Size They Belong To/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        pass
+
+obj = Solution()
+data = obj.groupThePeople(groupSizes = [3,3,3,3,3,1,3])
+#data = obj.groupThePeople(groupSizes = [2,1,3,3,3,2])
+print(data)
