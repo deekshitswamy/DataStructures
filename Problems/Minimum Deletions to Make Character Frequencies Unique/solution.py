014c6ba1055078f4275900e6e522eefc4baaedd0
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-12 21:20:04 +0530

    Minimum Deletions to Make Character Frequencies Unique

---
+++ Problems/Minimum Deletions to Make Character Frequencies Unique/solution.py
@@ -0,0 +1,10 @@
+import io
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        pass
+    
+obj = Solution()
+#data = obj.minDeletions(s = "aab")
+#data = obj.minDeletions(s = "aaabbbcc")
+data = obj.minDeletions(s = "ceabaacb")
+print(data)
