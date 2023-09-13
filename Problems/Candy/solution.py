fa1c6bba8d9068f62a5ec94e8036c52e38fe785d
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-13 23:18:06 +0530

    changes

--- Problems/Candy/solution.py
+++ Problems/Candy/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        pass
+        n=len(ratings)
+        dp=[1]*n
+        for i in range(1,n):
+            if ratings[i]>ratings[i-1]:
+                dp[i]=dp[i-1]+1
+
+        for i in range(n-2,-1,-1):
+            if ratings[i]>ratings[i+1]:
+                dp[i]=max(dp[i],dp[i+1]+1)
+
+        return sum(dp) 
 
 obj = Solution()
 #data = obj.candy(ratings = [1,0,2])
