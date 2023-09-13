0fe46e11624cbf97c10d4786f3c45ccf427e86d0
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-13 23:17:03 +0530

    candy

---
+++ Problems/Candy/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.candy(ratings = [1,0,2])
+data = obj.candy(ratings = [1,2,2])
+print(data)
