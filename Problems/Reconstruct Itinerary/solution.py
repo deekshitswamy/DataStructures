5ceb3e6636a53dd787577b8d21d3098a54a928d0
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-14 18:48:08 +0530

    changes

--- Problems/Reconstruct Itinerary/solution.py
+++ Problems/Reconstruct Itinerary/solution.py
@@ -2,7 +2,25 @@ import io
 from typing import List
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        pass
+        adj={}
+        for ticket in tickets:
+            adj[ticket[0]]=[]
+            adj[ticket[1]]=[]
+
+        for ticket in tickets:
+            adj[ticket[0]].append(ticket[1])
+
+        for l in adj.values():
+            l.sort()
+
+        ans=[]
+        def helper(node):
+            while adj[node]:
+                helper(adj[node].pop(0))
+
+            ans.append(node)
+        helper("JFK")
+        return reversed(ans) 
 
 obj = Solution()
 data = obj.findItinerary(tickets = [["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]])
