0556fa229185174dc7e55ec64d11befb8e0bd4b2
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-15 19:41:28 +0530

    Min cost to connect All Points

---
+++ Problems/Min Cost to Connect All Points/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minCostConnectPoints(points = [[0,0],[2,2],[3,10],[5,2],[7,0]])
+data = obj.minCostConnectPoints(points = [[3,12],[-2,5],[-4,1]])
+print(data)
