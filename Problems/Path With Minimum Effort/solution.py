733a4dd179ba3ae8926759b231c970a154f2d5a1
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-16 19:07:26 +0530

    Path With Minimum Effort

---
+++ Problems/Path With Minimum Effort/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minimumEffortPath(heights = [[1,2,2],[3,8,2],[5,3,5]])
+#data = obj.minimumEffortPath(heights = [[1,2,3],[3,8,4],[5,3,5]])
+data = obj.minimumEffortPath(heights = [[1,2,1,1,1],[1,2,1,2,1],[1,2,1,2,1],[1,2,1,2,1],[1,1,1,2,1]])
+print(data)
