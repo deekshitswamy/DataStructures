c820a16dcaa499840c7f6d71857e83417d54b6a7
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-17 12:16:39 +0530

    Shortest Path Visiting All Nodes

---
+++ Problems/Shortest Path Visiting All Nodes/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def shortestPathLength(self, graph: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.shortestPathLength(graph = [[1,2,3],[0],[0],[0]])
+data = obj.shortestPathLength(graph = [[1],[0,2,4],[1,3,4],[2],[1,2]])
+print(data)
