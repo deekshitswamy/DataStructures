e861f7fe4f63556608c7549ce4e044b30b233c23
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-18 17:44:39 +0530

    changes

--- Problems/The K Weakest Rows in a Matrix/solution.py
+++ Problems/The K Weakest Rows in a Matrix/solution.py
@@ -2,7 +2,13 @@ import io
 from typing import List
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        pass
+        ans=[]
+        n=len(mat)
+        for i in range(n):
+            ans.append([mat[i].count(1),i])
+
+        ans.sort()    
+        return [i[1] for i in ans[:k]]
 
 obj = Solution()
 #data = obj.kWeakestRows(mat = [[1,1,0,0,0],[1,1,1,1,0],[1,0,0,0,0],[1,1,0,0,0],[1,1,1,1,1]], k = 3)
