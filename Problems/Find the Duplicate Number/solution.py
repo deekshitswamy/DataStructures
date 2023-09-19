837f69e624bb24f0b9dd6998a0bcd633c8259b74
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-19 18:17:44 +0530

    Find the Duplicate Number

---
+++ Problems/Find the Duplicate Number/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        pass 
+
+obj = Solution()
+#data = obj.findDuplicate(nums = [1,3,4,2,2])
+data = obj.findDuplicate(nums = [3,1,3,4,2])
+print(data)
