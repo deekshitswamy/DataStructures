a7312ecda08b211aab8a344fa9caf00b64c5dce6
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-20 20:41:32 +0530

    Minimum Operations to Reduce X to Zero

---
+++ Problems/Minimum Operations to Reduce X to Zero/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minOperations(nums = [1,1,4,2,3], x = 5)
+#data = obj.minOperations(nums = [5,6,7,8,9], x = 4)
+data = obj.minOperations(nums = [3,2,20,1,1,3], x = 10)
+print(data)
