8c77f33819a720aa0c8cfa01c6b3fd6b0ae3555a
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-21 21:11:32 +0530

    changes

--- Problems/Median of Two Sorted Arrays/solution.py
+++ Problems/Median of Two Sorted Arrays/solution.py
@@ -2,7 +2,13 @@ import io
 from typing import List
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        pass                            
+        list1=nums1+nums2
+        list1.sort()
+        length=len(list1)
+        if length%2==1:
+            return float(list1[((length+1)//2)-1])
+        else:
+            return (list1[length//2-1]+list1[(length//2)])/2                             
 
 obj = Solution()
 #data = obj.findMedianSortedArrays(nums1 = [1,3], nums2 = [2])
