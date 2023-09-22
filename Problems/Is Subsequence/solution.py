24664968c2f39d079977d42ef64b5a3415fd3d95
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-22 20:19:24 +0530

    Is Subsequence

---
+++ Problems/Is Subsequence/solution.py
@@ -0,0 +1,10 @@
+import io
+from functools import cache
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        pass
+
+obj = Solution()
+data = obj.isSubsequence(s = "abc", t = "ahbgdc")
+#data = obj.isSubsequence(s = "axc", t = "ahbgdc")
+print(data)
