291575e26649849a3d90657446caaa1ddb7a822a
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-23 15:38:02 +0530

    Longest String Chain

---
+++ Problems/Longest String Chain/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.longestStrChain(words = ["a","b","ba","bca","bda","bdca"])
+data = obj.longestStrChain(words = ["xbc","pcxbcf","xb","cxbc","pcxbc"])
+#data = obj.longestStrChain(words = ["abcd","dbqca"])
+print(data)
