fd8e33e4ee05bdd996a5c512a8e8ee09d1707efc
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-24 14:06:30 +0530

    changes

--- Problems/Champagne Tower/solution.py
+++ Problems/Champagne Tower/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        pass
+        q = [poured]
+        for r in range(query_row):
+            q2 = [0] * (len(q) + 1)
+            for i, amount in enumerate(q):
+                if amount <= 1:
+                    continue
+                tmp = (amount - 1) / 2
+                q2[i] += tmp
+                q2[i + 1] += tmp
+            q = q2
+        return min(q[query_glass], 1)
 
 obj = Solution()
 #data = obj.champagneTower(poured = 1, query_row = 1, query_glass = 1)
