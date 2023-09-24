d67d9237f6edd91efd6b4b91844e5c03673f836f
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-24 14:05:15 +0530

    Champagne Tower

---
+++ Problems/Champagne Tower/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        pass
+
+obj = Solution()
+#data = obj.champagneTower(poured = 1, query_row = 1, query_glass = 1)
+#data = obj.champagneTower(poured = 2, query_row = 1, query_glass = 1)
+data = obj.champagneTower(poured = 100000009, query_row = 33, query_glass = 17)
+print(data)
