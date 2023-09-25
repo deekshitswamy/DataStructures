d373f0ff8823d3c532248d0019c01a3781563bb4
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-25 15:29:59 +0530

    Find the Difference

---
+++ Problems/Find the Difference/solution.py
@@ -0,0 +1,9 @@
+import io
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.findTheDifference(s = "abcd", t = "abcde")
+data = obj.findTheDifference(s = "", t = "y")
+print(data)
