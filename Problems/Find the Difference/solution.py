56674d65814e8427058271df0a07ed4369732974
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-25 15:30:41 +0530

    changes

--- Problems/Find the Difference/solution.py
+++ Problems/Find the Difference/solution.py
@@ -1,7 +1,11 @@
 import io
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        pass
+        b = 0
+        s = s+t
+        for c in s:
+            b = b ^ ord(c)
+        return chr(b)
 
 obj = Solution()
 #data = obj.findTheDifference(s = "abcd", t = "abcde")
