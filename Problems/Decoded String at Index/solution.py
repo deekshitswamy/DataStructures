5decc5e4894bb72d90344fa05ef4441de47aaba3
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-27 19:51:53 +0530

    changes

--- Problems/Decoded String at Index/solution.py
+++ Problems/Decoded String at Index/solution.py
@@ -1,7 +1,24 @@
 import io
 class Solution:
     def decodeAtIndex(self, s: str, k: int) -> str:
-        pass
+        n=len(s)
+        i=0
+        count=0
+        while i<n:
+            if s[i].isnumeric():
+                if (int(s[i])-1)*count>=k:
+                    i=0
+                    count=0
+                    continue
+                else:
+                    k-=(int(s[i])-1)*count
+                    count+=(int(s[i])-1)*count
+            else:
+                k-=1
+                count+=1
+                if k==0:
+                    return s[i]
+            i+=1 
 
 obj = Solution()
 #data = obj.decodeAtIndex(s = "leet2code3", k = 10)
