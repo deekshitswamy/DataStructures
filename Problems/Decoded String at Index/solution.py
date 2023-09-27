cbbcc9cefad373d0954e3044941968affa74a22d
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-27 19:50:53 +0530

    Decoded String at Index

---
+++ Problems/Decoded String at Index/solution.py
@@ -0,0 +1,10 @@
+import io
+class Solution:
+    def decodeAtIndex(self, s: str, k: int) -> str:
+        pass
+
+obj = Solution()
+#data = obj.decodeAtIndex(s = "leet2code3", k = 10)
+#data = obj.decodeAtIndex(s = "ha22", k = 5)
+data = obj.decodeAtIndex(s = "a2345678999999999999999", k = 1)
+print(data)
