4ef299bfa16ffa349bf42dda22b8112d99d83c1c
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-28 21:03:16 +0530

    changes

--- Problems/Sort Array By Parity/solution.py
+++ Problems/Sort Array By Parity/solution.py
@@ -2,7 +2,14 @@ import io
 from typing import List
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        pass
+        left = 0
+
+        for i in range(len(nums)):
+            if nums[i] % 2 == 0:
+                nums[left], nums[i] = nums[i], nums[left]
+                left += 1
+        
+        return nums
 
 obj = Solution()
 #data = obj.sortArrayByParity(nums = [3,1,2,4])
