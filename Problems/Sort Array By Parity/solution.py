1b23057c2aa86a61099845380697f0037b4999e4
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-28 20:56:11 +0530

    Sort Array By Parity

---
+++ Problems/Sort Array By Parity/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+data = obj.sortArrayByParity(nums = [3,1,2,4])
+#data = obj.sortArrayByParity(nums = [0])
+print(data)
