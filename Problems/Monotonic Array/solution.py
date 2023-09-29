89340d1d5daf339df62c0e175b7604604128f6e9
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-29 17:11:47 +0530

    changes

--- Problems/Monotonic Array/solution.py
+++ Problems/Monotonic Array/solution.py
@@ -2,11 +2,23 @@ import io
 from typing import List
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
-        pass
+        # If last elemnt is greater than first
+        # Conisdeirng it as monotonically increasing
+        if nums[-1]>nums[0]:
+            for i in range(len(nums)-1):
+                if nums[i+1]<nums[i]:
+                    return False
+            return True
+        # Else monotonically decreasing
+        else:
+            for i in range(len(nums)-1):
+                if nums[i+1]>nums[i]:
+                    return False
+            return True
 
 
 obj = Solution()
-data = obj.isMonotonic(nums = [1,2,2,3])
-data = obj.isMonotonic(nums = [6,5,4,4])
+#data = obj.isMonotonic(nums = [1,2,2,3])
+#data = obj.isMonotonic(nums = [6,5,4,4])
 data = obj.isMonotonic(nums = [1,3,2])
 print(data)
