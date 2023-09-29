1361c053e4a5b9517b934ae39ca91adb52744454
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-29 17:11:10 +0530

    Monotonic Array

---
+++ Problems/Monotonic Array/solution.py
@@ -0,0 +1,12 @@
+import io
+from typing import List
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        pass
+
+
+obj = Solution()
+data = obj.isMonotonic(nums = [1,2,2,3])
+data = obj.isMonotonic(nums = [6,5,4,4])
+data = obj.isMonotonic(nums = [1,3,2])
+print(data)
