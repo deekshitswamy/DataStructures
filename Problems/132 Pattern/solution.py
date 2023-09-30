6498bc8716fc12259034dd0f57ab65e766d0debc
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-30 17:28:17 +0530

    132 Pattern

---
+++ Problems/132 Pattern/solution.py
@@ -0,0 +1,12 @@
+import io
+from typing import List
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        pass
+
+
+obj = Solution()
+#data = obj.find132pattern(nums = [1,2,3,4])
+#data = obj.find132pattern(nums = [3,1,4,2])
+data = obj.find132pattern(nums = [-1,3,2,0])
+print(data)
