f76e63c0a8bc3f62370a6be108ada6c1c2fde4ba
Deeku-Zeus <deekuzeus21@gmail.com>
2023-09-30 17:29:09 +0530

    changes

--- Problems/132 Pattern/solution.py
+++ Problems/132 Pattern/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        pass
+        stack=[]
+        curMin=nums[0]
+        for num in nums:
+            while stack and num>=stack[-1][0]:
+                stack.pop()
+
+            if stack and num>stack[-1][1]:
+                return True
+
+            stack.append([num,curMin])
+            curMin=min(curMin,num)
+        return False 
 
 
 obj = Solution()
