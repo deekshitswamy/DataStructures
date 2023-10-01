5e285224bf56ec489b06801cac5d37cba21da75d
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-01 11:04:41 +0530

    Reverse Words in a String III

---
+++ Problems/Reverse Words in a String III/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.reverseWords(s = "Let's take LeetCode contest")
+data = obj.reverseWords(s = "God Ding")
+print(data)
