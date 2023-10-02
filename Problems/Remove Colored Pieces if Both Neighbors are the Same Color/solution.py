fefbd8d66342d4a11ed0900a2fb59784f77782a3
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-02 13:23:12 +0530

    Remove Colored Pieces if Both Neighbors are the Same Color

---
+++ Problems/Remove Colored Pieces if Both Neighbors are the Same Color/solution.py
@@ -0,0 +1,10 @@
+import io
+class Solution:
+    def winnerOfGame(self, colors: str) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.winnerOfGame(colors = "AAABABB")
+#data = obj.winnerOfGame(colors = "AA")
+data = obj.winnerOfGame(colors = "ABBBBBBBAAA")
+print(data)
