6345ebc70af83b1202b9a887aea4a3de8a0a9efb
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-02 13:24:36 +0530

    changes

--- Problems/Remove Colored Pieces if Both Neighbors are the Same Color/solution.py
+++ Problems/Remove Colored Pieces if Both Neighbors are the Same Color/solution.py
@@ -1,7 +1,14 @@
 import io
+import collections
+from itertools import groupby
 class Solution:
     def winnerOfGame(self, colors: str) -> bool:
-        pass
+        c=collections.Counter()
+        for x,t in groupby(colors):
+            c[x]+=max(len(list(t))-2,0)
+        if c['A']>c['B']:
+            return True
+        return False
 
 obj = Solution()
 #data = obj.winnerOfGame(colors = "AAABABB")
