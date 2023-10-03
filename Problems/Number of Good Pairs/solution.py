383d9442f3136193d4c2199006ade8a28ce5a369
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-03 18:30:57 +0530

    changes

--- Problems/Number of Good Pairs/solution.py
+++ Problems/Number of Good Pairs/solution.py
@@ -2,7 +2,13 @@ import io
 from typing import List
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        pass
+        count=0
+        n=len(nums)
+        for i in range(n):
+            for j in range(i+1,n):
+                if nums[i]==nums[j]:
+                    count+=1
+        return count
 
 obj = Solution()
 #data = obj.numIdenticalPairs(nums = [1,2,3,1,1,3])
