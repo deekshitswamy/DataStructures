974348fbeeb33c57124ce00e50d52a83b6724e72
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-05 18:20:56 +0530

    changes

--- Problems/Majority Element II/solution.py
+++ Problems/Majority Element II/solution.py
@@ -3,7 +3,15 @@ from typing import List
 
 class Solution:
         def majorityElement(self, nums: List[int]) -> List[int]:
-            pass
+            n = list(set(nums))
+            l = len(nums)//3
+            res = []
+
+            for i in n:
+                if nums.count(i) > l:
+                    res.append(i)
+            
+            return res
 
 obj = Solution()
 #data = obj.majorityElement(nums = [3,2,3])
