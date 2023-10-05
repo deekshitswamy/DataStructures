e36563f8af61afa362e3e426c468e70d46178786
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-05 18:14:01 +0530

    Majority Element II

---
+++ Problems/Majority Element II/solution.py
@@ -0,0 +1,12 @@
+import io
+from typing import List
+
+class Solution:
+        def majorityElement(self, nums: List[int]) -> List[int]:
+            pass
+
+obj = Solution()
+#data = obj.majorityElement(nums = [3,2,3])
+#data = obj.majorityElement(nums = [1])
+data = obj.majorityElement(nums = [1,2])
+print(data)
