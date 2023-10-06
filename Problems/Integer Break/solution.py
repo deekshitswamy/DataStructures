928b4508d1686021b11f3ad0389f47d10a9db0a1
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-06 19:12:34 +0530

    changes

--- Problems/Integer Break/solution.py
+++ Problems/Integer Break/solution.py
@@ -1,7 +1,15 @@
 import io
 class Solution:
     def integerBreak(self, n: int) -> int:
-        pass
+        if n==2:
+            return 1
+        if n==3:
+            return 2
+        ans=1
+        while n>4:
+            ans=ans*3
+            n-=3
+        return ans*n 
 
 obj = Solution()
 #data = obj.integerBreak(n = 2)
