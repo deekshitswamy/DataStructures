477b32f7b81f4a68309aa85c444aba778282a4ac
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-06 19:10:06 +0530

    Integer Break

---
+++ Problems/Integer Break/solution.py
@@ -0,0 +1,9 @@
+import io
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.integerBreak(n = 2)
+data = obj.integerBreak(n = 10)
+print(data)
