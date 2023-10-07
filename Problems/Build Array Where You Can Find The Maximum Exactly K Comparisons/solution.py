4a22d969ad2f5f9d17496d116b1a40e1bcc99d02
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-07 18:12:34 +0530

    changes

--- Problems/Build Array Where You Can Find The Maximum Exactly K Comparisons/solution.py
+++ Problems/Build Array Where You Can Find The Maximum Exactly K Comparisons/solution.py
@@ -1,7 +1,16 @@
 import io
+from functools import lru_cache
 class Solution:
     def numOfArrays(self, n: int, m: int, k: int) -> int:
-        pass
+        mod=10**9+7
+        @lru_cache(None)
+        def dp(i,h,k):
+            if i==n and k==0:
+                return 1
+            if i==n or k<0:
+                return 0
+            return sum(dp(i+1,max(h,j),k-(j>h)) for j in range(1,m+1))%mod
+        return dp(0,-1,k)
 
 obj = Solution()
 #data = obj.numOfArrays(n = 2, m = 3, k = 1)
