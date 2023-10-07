aeefa321afe4f3fc57951cbe1b5b0e39acbd6364
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-07 18:11:51 +0530

    Build Array Where You Can Find The Maximum Exactly K Comparisons

---
+++ Problems/Build Array Where You Can Find The Maximum Exactly K Comparisons/solution.py
@@ -0,0 +1,10 @@
+import io
+class Solution:
+    def numOfArrays(self, n: int, m: int, k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numOfArrays(n = 2, m = 3, k = 1)
+#data = obj.numOfArrays(n = 5, m = 2, k = 3)
+data = obj.numOfArrays(n = 9, m = 1, k = 1)
+print(data)
