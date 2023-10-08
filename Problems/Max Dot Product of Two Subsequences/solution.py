fe8018766f194243e29c7b1e207026f1a6f8474c
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-08 13:00:03 +0530

    Max Dot Product of Two Subsequences

---
+++ Problems/Max Dot Product of Two Subsequences/solution.py
@@ -0,0 +1,12 @@
+import io
+from typing import List
+
+class Solution:
+    def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxDotProduct(nums1 = [2,1,-2,5], nums2 = [3,0,-6])
+#data = obj.maxDotProduct(nums1 = [3,-2], nums2 = [2,-6,7])
+data = obj.maxDotProduct(nums1 = [-1,-1], nums2 = [1,1])
+print(data)
