b9c08aa748d5e5b841c8a606419aeed99d283c20
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-09 20:45:37 +0530

    changes

--- Problems/Find First and Last Position of Element in Sorted Array/solution.py
+++ Problems/Find First and Last Position of Element in Sorted Array/solution.py
@@ -2,7 +2,19 @@ import io
 from typing import List
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        pass
+        n=len(nums)
+        left=0
+        right=n-1
+        if target in nums:
+            for i in range(0,n):
+                if nums[left]==nums[right]:
+                    return [left,right] 
+                elif nums[left]<target:
+                    left+=1
+                else:
+                    right-=1
+        else:
+            return [-1,-1]
 
 obj = Solution()
 #data = obj.searchRange(nums = [5,7,7,8,8,10], target = 8)
