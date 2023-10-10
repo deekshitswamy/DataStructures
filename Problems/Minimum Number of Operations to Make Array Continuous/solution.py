8ae7f8256e2423e18d14db5fd54989bb798d44f2
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-10 19:21:24 +0530

    changes

--- Problems/Minimum Number of Operations to Make Array Continuous/solution.py
+++ Problems/Minimum Number of Operations to Make Array Continuous/solution.py
@@ -1,8 +1,17 @@
 import io
+import sys
 from typing import List
+from bisect import bisect_right
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        pass
+        n=len(nums)
+        nums=sorted(set(nums))
+        ans=sys.maxsize
+        for i,s in enumerate(nums):
+            e=s+n-1
+            idx=bisect_right(nums,e)
+            ans=min(ans,n-idx+i)
+        return ans    
 
 obj = Solution()
 #data = obj.minOperations(nums = [4,2,5,3])
