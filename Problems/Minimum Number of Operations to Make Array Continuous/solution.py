63b8ef56ed846e9f15cf07445144843cfe45e9fb
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-10 19:19:41 +0530

    Minimum Number of Operations to Make Array Continuous

---
+++ Problems/Minimum Number of Operations to Make Array Continuous/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minOperations(nums = [4,2,5,3])
+#data = obj.minOperations(nums = [1,2,3,5,6])
+data = obj.minOperations(nums = [1,10,100,1000])
+print(data)
