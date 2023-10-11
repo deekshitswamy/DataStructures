7c12db1d36e677c6cc0e941ee928911712e05836
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-11 18:34:10 +0530

    Number of Flowers in Full Bloom

---
+++ Problems/Number of Flowers in Full Bloom/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.fullBloomFlowers(flowers = [[1,6],[3,7],[9,12],[4,13]], poeple = [2,3,7,11])
+data = obj.fullBloomFlowers(flowers = [[1,10],[3,3]], poeple = [3,3,2])
+print(data)
