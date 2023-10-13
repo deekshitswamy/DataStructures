880fb47dc21dc608616230abd84872f6b0ac0b64
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-13 19:10:27 +0530

    changes

--- Problems/Min Cost Climbing Stairs/solution.py
+++ Problems/Min Cost Climbing Stairs/solution.py
@@ -2,7 +2,14 @@ import io
 from typing import List
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        pass
+        n=len(cost)
+        prev2=cost[0]
+        prev1=cost[1]
+        for i in range(2,n):
+            temp=cost[i]+min(prev1,prev2)
+            prev2=prev1
+            prev1=temp
+        return min(prev1,prev2)
 
 obj = Solution()
 #data = obj.minCostClimbingStairs(cost = [10,15,20])
