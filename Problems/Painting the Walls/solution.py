71e482113571b78f2a904523d74e67def8b0b47a
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-14 18:55:37 +0530

    changes

--- Problems/Painting the Walls/solution.py
+++ Problems/Painting the Walls/solution.py
@@ -1,9 +1,15 @@
 import io
+from math import inf
 from typing import List
 
 class Solution:
     def paintWalls(self, cost: List[int], time: List[int]) -> int:
-        pass
+        n = len(cost)
+        dp = [0] + [inf] * n
+        for c, t in zip(cost, time):
+            for j in range(n, 0, -1):
+                dp[j] = min(dp[j], dp[max(j - t - 1, 0)] + c)
+        return dp[n]
 
 obj = Solution()
 #data = obj.paintWalls(cost = [1,2,3,2], time = [1,2,3,2])
