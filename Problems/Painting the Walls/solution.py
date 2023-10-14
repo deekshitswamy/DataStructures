31d7744aff4240f1023d52bc54cae404eae63a6d
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-14 18:54:25 +0530

    Painting the Walls

---
+++ Problems/Painting the Walls/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+
+class Solution:
+    def paintWalls(self, cost: List[int], time: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.paintWalls(cost = [1,2,3,2], time = [1,2,3,2])
+data = obj.paintWalls(cost = [2,3,4,2], time = [1,1,1,1])
+print(data)
