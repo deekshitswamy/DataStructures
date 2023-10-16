60d1f7ed819289014efcdc5180976776b028e21c
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-16 21:00:00 +0530

    ' '

---
+++ Problems/Pascal's Triangle II/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.getRow(rowIndex = 3)
+data = obj.getRow(rowIndex = 0)
+data = obj.getRow(rowIndex = 1)
+print(data)
