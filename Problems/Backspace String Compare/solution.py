13a892ed0becf7de24cd4de3ec5e4d1cf68717a1
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-19 19:16:09 +0530

    changes

--- Problems/Backspace String Compare/solution.py
+++ Problems/Backspace String Compare/solution.py
@@ -2,7 +2,22 @@ import io
 
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        pass
+        ans1=[]
+        ans2=[]
+        for i in s:
+            if i=='#':
+                if len(ans1)>0:
+                    ans1.pop()
+            else:
+                ans1.append(i)
+        for  i in t:
+            if i=='#':
+                if len(ans2)>0:
+                    ans2.pop()
+            else:
+                ans2.append(i)
+
+        return ans1==ans2
 
 obj = Solution()
 #data = obj.backspaceCompare(s = "ab#c", t = "ad#c")
