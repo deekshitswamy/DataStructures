60a2e09b7d7e4b586bb7a850dbf895db9d371d90
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-21 14:45:23 +0530

    status

--- Problems/Constrained Subsequence Sum/solution.py
+++ Problems/Constrained Subsequence Sum/solution.py
@@ -3,7 +3,17 @@ from typing import List
 
 class Solution:
     def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
-        pass
+        deque=[]
+        for i,num in enumerate(nums):
+            while deque and deque[0]<i-k:
+                deque.pop(0)
+            if deque:
+                nums[i]=nums[deque[0]]+num
+            while deque and nums[deque[-1]]<nums[i]:
+                deque.pop()
+            if nums[i]>0:
+                deque.append(i)
+        return max(nums) 
 
 obj = Solution()
 #data = obj.constrainedSubsetSum(nums = [10,2,-10,5,20], k = 2)
