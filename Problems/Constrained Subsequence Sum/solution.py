1a6f71637beafefd0b16f90763a5ef1a5164c219
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-21 14:44:37 +0530

    Constrained Subsequence Sum

---
+++ Problems/Constrained Subsequence Sum/solution.py
@@ -0,0 +1,12 @@
+import io
+from typing import List
+
+class Solution:
+    def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.constrainedSubsetSum(nums = [10,2,-10,5,20], k = 2)
+#data = obj.constrainedSubsetSum(nums = [-1,-2,-3], k = 1)
+data = obj.constrainedSubsetSum(nums = [10,-2,-10,-5,20], k = 2)
+print(data)
