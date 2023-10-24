1df7711264217fb83102c748cfa815e7065770c0
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-25 01:04:40 +0530

    Power of Four

--- Problems/Power of Four/solution.py
+++ Problems/Power of Four/solution.py
@@ -1,1 +1,14 @@
-Sample content for file 2 
+import io
+from cmath import log
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n<=0:
+            return False
+        return n>0 and log(n,4).is_integer()
+
+obj = Solution()
+#data = obj.isPowerOfFour(n = 16)
+#data = obj.isPowerOfFour(n = 5)
+data = obj.isPowerOfFour(n = 1)
+print(data)
