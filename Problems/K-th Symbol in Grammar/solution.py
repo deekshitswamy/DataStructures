064599dcd45dbf5fd57fa7b90baaa2ee7906ac9a
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-25 19:08:50 +0530

    changes

--- Problems/K-th Symbol in Grammar/solution.py
+++ Problems/K-th Symbol in Grammar/solution.py
@@ -2,7 +2,14 @@ import io
 
 class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
-        pass
+        if n==1 or n==0:
+            return 0
+        length=1<<n-1
+        mid=length //2
+        if k<=mid:
+            return self.kthGrammar(n-1,k)
+        else:
+            return  (int(not(self.kthGrammar(n-1,k-mid))))
 
 obj = Solution()
 #data = obj.kthGrammar(n = 1, k = 1)
