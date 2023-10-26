c2f2619a04962464cef34537c7d47fdaa0e29ef4
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-26 18:58:22 +0530

    changes

--- Problems/Binary Trees With Factors/solution.py
+++ Problems/Binary Trees With Factors/solution.py
@@ -3,7 +3,17 @@ from typing import List
 
 class Solution:
     def numFactoredBinaryTrees(self, arr: List[int]) -> int:
-        pass
+        mod=10**9+7
+        arr.sort()
+        ans=defaultdict(int)
+        for a in arr:
+            tmp=1
+            for b in arr:
+                if b>a:
+                    break
+                tmp+=(ans[b]*ans[a/b])
+            ans[a]=tmp
+        return sum(ans.values())%mod 
 
 obj = Solution()
 #data = obj.numFactoredBinaryTrees(arr = [2,4])
