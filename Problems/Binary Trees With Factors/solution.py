5e055cd2e6e2334f9927c9147ac35f41c9149215
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-26 18:57:28 +0530

    Binary Trees With Factors

---
+++ Problems/Binary Trees With Factors/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+
+class Solution:
+    def numFactoredBinaryTrees(self, arr: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numFactoredBinaryTrees(arr = [2,4])
+data = obj.numFactoredBinaryTrees(arr = [2,4,5,10])
+print(data)
