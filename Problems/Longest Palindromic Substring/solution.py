bebd8e7c4ac25d02070a9dd44680dc20ed1449d8
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-27 21:24:22 +0530

    changes

--- Problems/Longest Palindromic Substring/solution.py
+++ Problems/Longest Palindromic Substring/solution.py
@@ -2,7 +2,14 @@ import io
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        pass
+        n=len(s)
+        c=n
+        while c>=1:
+            for k in range(n-c+1):
+                ans=s[k:c+k]
+                if ans==ans[::-1]:
+                    return ans
+            c=c-1
 
 obj = Solution()
 #data = obj.longestPalindrome(s = "babad")
