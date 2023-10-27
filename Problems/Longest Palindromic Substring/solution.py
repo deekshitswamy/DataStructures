52e188d38d299faecb5339524c797998b403c423
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-27 21:23:23 +0530

    Longest Palindromic Substring

---
+++ Problems/Longest Palindromic Substring/solution.py
@@ -0,0 +1,10 @@
+import io
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.longestPalindrome(s = "babad")
+data = obj.longestPalindrome(s = "cbbd")
+print(data)
