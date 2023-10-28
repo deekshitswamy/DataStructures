a8bc70780f3af6ff436530457187bec6788ff448
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-28 20:35:46 +0530

    Count Vowels Permutation

---
+++ Problems/Count Vowels Permutation/solution.py
@@ -0,0 +1,10 @@
+import io
+class Solution:
+    def countVowelPermutation(self, n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countVowelPermutation(n = 1)
+data = obj.countVowelPermutation(n = 2)
+data = obj.countVowelPermutation(n = 5)
+print(data)
