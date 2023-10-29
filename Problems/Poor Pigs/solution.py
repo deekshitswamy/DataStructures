208822ac2587ac788e170847ea7672b27c71294a
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-29 14:42:21 +0530

    Poor Pigs

---
+++ Problems/Poor Pigs/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.poorPigs(buckets = 4, minutesToDie = 15, minutesToTest = 15)
+data = obj.poorPigs(buckets = 4, minutesToDie = 15, minutesToTest = 30)
+print(data)
