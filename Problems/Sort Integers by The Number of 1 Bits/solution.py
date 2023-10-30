384ccb0e2839dae3becb265c7590dc5ef9e1b094
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-30 15:25:50 +0530

    Sort Integers by The Number of 1 Bits

---
+++ Problems/Sort Integers by The Number of 1 Bits/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.sortByBits(arr = [0,1,2,3,4,5,6,7,8])
+data = obj.sortByBits(arr = [1024,512,256,128,64,32,16,8,4,2,1])
+print(data)
