e55fb49259c66dfdcc92ac1d219a9ee60610555a
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-31 06:31:35 +0530

    changes

--- Problems/Find The Original Array of Prefix Xor/solution.py
+++ Problems/Find The Original Array of Prefix Xor/solution.py
@@ -2,7 +2,12 @@ import io
 from typing import List
 class Solution:
     def findArray(self, pref: List[int]) -> List[int]:
-        pass
+        if len(pref)==1:
+            return pref
+        ans=[pref[0]]
+        for i in range(len(pref)-1):
+            ans.append(pref[i]^pref[i+1])
+        return ans
 
 obj = Solution()
 #data = obj.findArray(pref = [5,2,0,3,1])
