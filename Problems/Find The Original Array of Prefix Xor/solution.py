232b1d8668eac9072525711cbed1c3603228314b
Deeku-Zeus <deekuzeus21@gmail.com>
2023-10-31 06:30:15 +0530

    Find The Original Array of Prefix Xor

---
+++ Problems/Find The Original Array of Prefix Xor/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.findArray(pref = [5,2,0,3,1])
+data = obj.findArray(pref = [13])
+print(data)
