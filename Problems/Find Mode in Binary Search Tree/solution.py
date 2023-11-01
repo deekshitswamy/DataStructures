4f5b37b09ee8549cdbdb6eaffdaecb822b3ba6bd
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-01 20:28:51 +0530

    Find Mode in Binary Search Tree

---
+++ Problems/Find Mode in Binary Search Tree/solution.py
@@ -0,0 +1,17 @@
+import io
+from typing import List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def findMode(self, root: TreeNode) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.findMode(root = [1,null,2,2])
+data = obj.findMode(root = [0])
+print(data)
