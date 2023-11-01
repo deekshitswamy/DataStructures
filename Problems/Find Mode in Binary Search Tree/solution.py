c71071435d10ddc368ea451f490146dc0e47560a
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-01 20:30:04 +0530

    changes

--- Problems/Find Mode in Binary Search Tree/solution.py
+++ Problems/Find Mode in Binary Search Tree/solution.py
@@ -1,5 +1,6 @@
 import io
 from typing import List
+from collections import defaultdict
 
 # Definition for a binary tree node.
 class TreeNode:
@@ -9,7 +10,22 @@ class TreeNode:
         self.right = right
 class Solution:
     def findMode(self, root: TreeNode) -> List[int]:
-        pass
+        D = defaultdict(int)
+
+        def dfs(tree):
+
+            if not tree:
+                return
+
+            else:
+                D[tree.val] += 1
+                dfs(tree.left)
+                dfs(tree.right)
+
+        dfs(root)
+
+        M = max(list(D.values()))
+        return [x for x in list(D.keys()) if D[x] == M]
 
 obj = Solution()
 #data = obj.findMode(root = [1,null,2,2])
