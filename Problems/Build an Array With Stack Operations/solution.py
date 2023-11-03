5b5734d3f97dc6cbe066fe767258756c108b3882
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-03 18:57:30 +0530

    Build an Array With Stack Operations

---
+++ Problems/Build an Array With Stack Operations/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        pass
+
+obj = Solution()
+#data = obj.buildArray(target = [1,3], n = 3)
+#data = obj.buildArray(target = [1,2,3], n = 3)
+data = obj.buildArray(target = [1,2], n = 4)
+print(data)
