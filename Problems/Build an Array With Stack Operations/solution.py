004a39db3a9903e920f3768752f817e8d3d6a2f3
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-03 18:58:13 +0530

    changes

--- Problems/Build an Array With Stack Operations/solution.py
+++ Problems/Build an Array With Stack Operations/solution.py
@@ -2,7 +2,16 @@ import io
 from typing import List
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
-        pass
+        ans=[]
+        best=0
+        for i in range(1,n+1):
+            if i in target:
+                ans.append("Push")
+                best=max(len(ans),best)
+            else:
+                ans.append("Push")
+                ans.append("Pop")
+        return ans[:best]
 
 obj = Solution()
 #data = obj.buildArray(target = [1,3], n = 3)
