026c020e71111a9f321a513eb0e071a25d497fd3
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-04 10:54:53 +0530

    changes

--- Problems/Last Moment Before All Ants Fall Out of a Plank/solution.py
+++ Problems/Last Moment Before All Ants Fall Out of a Plank/solution.py
@@ -6,6 +6,6 @@ class Solution:
 
 obj = Solution()
 #data = obj.getLastMoment(n = 4, left = [4,3], right = [0,1])
-data = obj.getLastMoment(n = 7, left = [], right = [0,1,2,3,4,5,6,7])
+#data = obj.getLastMoment(n = 7, left = [], right = [0,1,2,3,4,5,6,7])
 data = obj.getLastMoment(n = 7, left = [0,1,2,3,4,5,6,7], right = [])
 print(data)
