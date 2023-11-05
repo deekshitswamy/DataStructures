573ab77a38bb54b999066e6151a45e5501e350f7
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-05 18:18:26 +0530

    Find the Winner of an Array Game

---
+++ Problems/Find the Winner of an Array Game/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def getWinner(self, arr: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.getWinner(arr = [2,1,3,5,4,6,7], k = 2)
+data = obj.getWinner(arr = [3,2,1], k = 10)
+print(data)
