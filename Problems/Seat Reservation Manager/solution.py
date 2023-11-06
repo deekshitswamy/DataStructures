70202067d206428e8b17f519b0db1b13ae517bc6
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-06 18:36:26 +0530

    Seat Reservation Manager

---
+++ Problems/Seat Reservation Manager/solution.py
@@ -0,0 +1,31 @@
+import io
+from typing import List
+class SeatManager:
+
+    def __init__(self, n: int):
+        pass
+        
+
+    def reserve(self) -> int:
+        pass
+        
+
+    def unreserve(self, seatNumber: int) -> None:
+        pass
+        
+
+
+# Your SeatManager object will be instantiated and called as such:
+seatNumber = 3
+n = 5
+obj = SeatManager(n)
+param_1 = obj.reserve()
+obj.unreserve(seatNumber)
+
+
+
+# obj = Solution()
+# #data = obj.testfunc(n = 1)
+# data = obj.testfunc(n = 2)
+# data = obj.testfunc(n = 2)
+# print(data)
