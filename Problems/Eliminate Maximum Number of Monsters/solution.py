ec93ecc8f5fa7c150616140dc8ba3fab8cd1359c
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-07 19:00:08 +0530

    changes

--- Problems/Eliminate Maximum Number of Monsters/solution.py
+++ Problems/Eliminate Maximum Number of Monsters/solution.py
@@ -2,7 +2,16 @@ import io
 from typing import List
 class Solution:
     def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
-        pass
+        n=len(dist)
+        time=[]
+        for i in range(n):
+            time.append((dist[i]/speed[i]))
+        time.sort()
+        for i in range(n):
+            if i>=time[i]:
+                i-=1
+                break
+        return i+1  
 
 obj = Solution()
 #data = obj.eliminateMaximum(dist = [1,3,4], speed = [1,1,1])
