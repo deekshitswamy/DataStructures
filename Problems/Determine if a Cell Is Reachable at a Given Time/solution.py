e0535de8d05c7723a4ba0c285df82f047d087403
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-08 19:15:17 +0530

    Determine if a Cell Is Reachable at a Given Time

---
+++ Problems/Determine if a Cell Is Reachable at a Given Time/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+
+class Solution:
+    def isReachableAtTime(self, sx: int, sy: int, fx: int, fy: int, t: int) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.isReachableAtTime(sx = 2, sy = 4, fx = 7, fy = 7, t = 6)
+data = obj.isReachableAtTime(sx = 3, sy = 1, fx = 7, fy = 3, t = 3)
+print(data)
