87048a9482ae1ab8dd4dec23236e1f1365846393
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-09 19:27:33 +0530

    Count Number of Homogenous Substrings

---
+++ Problems/Count Number of Homogenous Substrings/solution.py
@@ -0,0 +1,12 @@
+import io
+from typing import List
+
+class Solution:
+    def countHomogenous(self, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countHomogenous(s = "abbcccaa")
+#data = obj.countHomogenous(s = "xy")
+data = obj.countHomogenous(s = "zzzzz")
+print(data)
