a18b400731955d78af5aa0c67abfeb9b9dd6af53
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-09 19:28:22 +0530

    changes

--- Problems/Count Number of Homogenous Substrings/solution.py
+++ Problems/Count Number of Homogenous Substrings/solution.py
@@ -1,9 +1,15 @@
 import io
 from typing import List
+from itertools import groupby
 
 class Solution:
     def countHomogenous(self, s: str) -> int:
-        pass
+        mod=10**9+7
+        ans=0
+        for x,y in groupby(s):
+            n=len(list(y))
+            ans+=n*(n+1)//2
+        return ans%mod
 
 obj = Solution()
 #data = obj.countHomogenous(s = "abbcccaa")
