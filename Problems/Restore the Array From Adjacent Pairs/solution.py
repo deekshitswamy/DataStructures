66a10b23b4000dcbee8ef985df4d89ddcda42e66
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-10 18:32:37 +0530

    changes

--- Problems/Restore the Array From Adjacent Pairs/solution.py
+++ Problems/Restore the Array From Adjacent Pairs/solution.py
@@ -2,7 +2,21 @@ import io
 from typing import List
 class Solution:
     def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
-        pass
+        adjs=collections.defaultdict(set)
+        for i,j in adjacentPairs:
+            adjs[i].add(j)
+            adjs[j].add(i)
+
+        for node,adj in adjs.items():
+            if len(adj)==1:
+                break
+        ans=[node]
+        while adjs[node]:
+            new=adjs[node].pop()
+            ans.append(new)
+            adjs[new].remove(node)
+            node=new
+        return ans
 
 obj = Solution()
 #data = obj.restoreArray(adjacentPairs = [[2,1],[3,4],[3,2]])
