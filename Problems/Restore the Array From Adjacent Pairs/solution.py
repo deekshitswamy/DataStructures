b23aaaf7a4a0803ca32eec701f0c029ac95d8b2c
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-10 18:31:48 +0530

    Restore the Array From Adjacent Pairs

---
+++ Problems/Restore the Array From Adjacent Pairs/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.restoreArray(adjacentPairs = [[2,1],[3,4],[3,2]])
+#data = obj.restoreArray(adjacentPairs = [[4,-2],[1,4],[-3,1]])
+data = obj.restoreArray(adjacentPairs = [[100000,-100000]])
+print(data)
