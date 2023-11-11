fb41cb7ee6f0366b82faf97fda2c8d007db6eeff
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-11 19:18:59 +0530

    changes

--- Problems/Design Graph With Shortest Path Calculator/solution.py
+++ Problems/Design Graph With Shortest Path Calculator/solution.py
@@ -4,15 +4,34 @@ from typing import List
 class Graph:
 
     def __init__(self, n: int, edges: List[List[int]]):
-        pass
+        self.adj_list=[[] for _ in range(n)]
+        for u,v,w in edges:
+            self.adj_list[u].append((v,w))
         
 
     def addEdge(self, edge: List[int]) -> None:
-        pass
+        u,v,w=edge
+        self.adj_list[u].append((v,w))
         
 
     def shortestPath(self, node1: int, node2: int) -> int:
-        pass
+        n=len(self.adj_list)
+        pq=[(0,node1)]
+        dist=[float('inf') for _ in range(n)]
+        dist[node1]=0
+        while pq:
+            d,u=heapq.heappop(pq)
+            if u==node2:
+                return d
+            if d>dist[u]:
+                continue
+                
+            for v,w in self.adj_list[u]:
+                if d+w<dist[v]:
+                    dist[v]=d+w
+                    heapq.heappush(pq,(dist[v],v))
+                          
+        return -1  
         
 
 
