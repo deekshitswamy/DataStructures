5adc441f46a62c040d488c5a60059f9d6860d5f2
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-11 19:17:51 +0530

    Design Graph With Shortest Path Calculator

---
+++ Problems/Design Graph With Shortest Path Calculator/solution.py
@@ -0,0 +1,22 @@
+import io
+from typing import List
+
+class Graph:
+
+    def __init__(self, n: int, edges: List[List[int]]):
+        pass
+        
+
+    def addEdge(self, edge: List[int]) -> None:
+        pass
+        
+
+    def shortestPath(self, node1: int, node2: int) -> int:
+        pass
+        
+
+
+# Your Graph object will be instantiated and called as such:
+# obj = Graph(n, edges)
+# obj.addEdge(edge)
+# param_2 = obj.shortestPath(node1,node2)
