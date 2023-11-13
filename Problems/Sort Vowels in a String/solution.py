5589172454f92ce14114614f69e2bb8aca27daa5
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-13 12:20:55 +0530

    Sort Vowels in a String

---
+++ Problems/Sort Vowels in a String/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def sortVowels(self, s: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.sortVowels(s = "lEetcOde")
+data = obj.sortVowels(s = "lYmpH")
+print(data)
