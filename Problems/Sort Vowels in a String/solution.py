4e8af01fcc3a46c903af680aa26bd89e31c4a21f
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-13 12:22:06 +0530

    changes

--- Problems/Sort Vowels in a String/solution.py
+++ Problems/Sort Vowels in a String/solution.py
@@ -2,7 +2,16 @@ import io
 
 class Solution:
     def sortVowels(self, s: str) -> str:
-        pass
+        ans=""
+        ans1=[i for i in s if i not in "aeiouAEIOU"]
+        ans2=sorted([ i for i in s if i in "aeiouAEIOU"])
+
+        for i in s:
+            if i in ans1:
+                ans+=i
+            else:
+                ans+=ans2.pop(0)
+        return ans 
 
 obj = Solution()
 #data = obj.sortVowels(s = "lEetcOde")
