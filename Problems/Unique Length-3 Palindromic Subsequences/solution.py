13de1b7b36519c3889451df18ac4c0657714c6d0
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-14 19:49:33 +0530

    Unique Length-3 Palindromic Subsequences

---
+++ Problems/Unique Length-3 Palindromic Subsequences/solution.py
@@ -0,0 +1,11 @@
+import io
+
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countPalindromicSubsequence(s = "aabca")
+#data = obj.countPalindromicSubsequence(s = "adc")
+data = obj.countPalindromicSubsequence(s = "bbcbaba")
+print(data)
