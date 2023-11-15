f9ba79a64f07a6a3446f8f0042a962c3b4c87e29
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-15 18:26:28 +0530

    Maximum Element After Decreasing and Rearranging

---
+++ Problems/Maximum Element After Decreasing and Rearranging/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maximumElementAfterDecrementingAndRearranging(arr = [2,2,1,2,1])
+#data = obj.maximumElementAfterDecrementingAndRearranging(arr = [100,1,1000])
+data = obj.maximumElementAfterDecrementingAndRearranging(arr = [1,2,3,4,5])
+print(data)
