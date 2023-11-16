50e93ab6ffd3cf8a104f9cf23f70a317ef96dac6
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-16 19:02:27 +0530

    changes

--- Problems/Find Unique Binary String/solution.py
+++ Problems/Find Unique Binary String/solution.py
@@ -2,7 +2,15 @@ import io
 from typing import List
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        pass
+        n=len(nums)
+        s=set(int(x,2) for x in nums)
+        def pod(x,n):
+            n1=len(x)
+            return ("0"*(n-n1))+x
+        for x in range(1<<n):
+            if x not in s:
+                return pod(bin(x)[2:],n)
+        return "0"*n
 
 obj = Solution()
 #data = obj.findDifferentBinaryString(n = 1)
