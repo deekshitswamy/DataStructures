d7420dd95618099a0d59ee20d44f9a280c11f673
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-17 20:32:12 +0530

    Minimize Maximum Pair Sum in Array

---
+++ Problems/Minimize Maximum Pair Sum in Array/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minPairSum(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minPairSum(nums = [3,5,2,3])
+data = obj.minPairSum(nums = [3,5,4,2,4,6])
+print(data)
