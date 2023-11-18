20766e753a974f214a039f9538de31fdc0b88ab4
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-18 19:47:26 +0530

    Frequency of the Most Frequent Element

---
+++ Problems/Frequency of the Most Frequent Element/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxFrequency(nums = [1,2,4], k = 5)
+#data = obj.maxFrequency(nums = [1,4,8,13], k = 5)
+data = obj.maxFrequency(nums = [3,9,6], k = 2)
+print(data)
