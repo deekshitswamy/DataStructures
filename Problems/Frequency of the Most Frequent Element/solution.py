d7707753b3a859b7935600d320b5fd81d7f3dd97
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-18 19:48:03 +0530

    changes

--- Problems/Frequency of the Most Frequent Element/solution.py
+++ Problems/Frequency of the Most Frequent Element/solution.py
@@ -2,7 +2,22 @@ import io
 from typing import List
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
-        pass
+        n=len(nums)
+        nums.sort()
+        left=0
+        right=0
+        dist=0
+        ans=0
+        while right<n:
+            if right>0 and nums[right]!=nums[right-1]:
+                dist+=(right-left)*(nums[right]-nums[right-1])
+            right+=1
+            while dist>k:
+                dist-=nums[right-1]-nums[left]
+                left+=1
+            if right-left>ans:
+                ans=right-left
+        return ans
 
 obj = Solution()
 #data = obj.maxFrequency(nums = [1,2,4], k = 5)
