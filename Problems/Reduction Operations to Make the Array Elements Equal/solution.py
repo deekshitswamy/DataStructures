c1bca1a1bdbd3de919afc004cb2d07807b8b5418
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-19 17:46:48 +0530

    Reduction Operations to Make the Array Elements Equal

---
+++ Problems/Reduction Operations to Make the Array Elements Equal/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def reductionOperations(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.reductionOperations(nums = [5,1,3])
+#data = obj.reductionOperations(nums = [1,1,1])
+data = obj.reductionOperations(nums = [1,1,2,2,3])
+print(data)
