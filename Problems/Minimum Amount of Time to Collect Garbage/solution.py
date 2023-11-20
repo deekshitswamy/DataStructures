35a825d748a754db740ced00df42d72d4de81e20
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-20 17:24:48 +0530

    Minimum Amount of Time to Collect Garbage

---
+++ Problems/Minimum Amount of Time to Collect Garbage/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.garbageCollection(garbage = ["G","P","GP","GG"], travel = [2,4,3])
+data = obj.garbageCollection(garbage = ["MMM","PGM","GP"], travel = [3,10])
+print(data)
