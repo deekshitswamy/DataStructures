6761c38c9449fc0b1c17ef63775dee31d0cba1af
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-20 17:26:07 +0530

    change

--- Problems/Minimum Amount of Time to Collect Garbage/solution.py
+++ Problems/Minimum Amount of Time to Collect Garbage/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
-        pass
+        ans=0
+        n=len(garbage)
+        for c in "MPG":
+            best=0
+            cur=0
+            cur+=garbage[0].count(c)
+            best=max(cur,best)
+            for i in range(1,n):
+                cur+=travel[i-1]
+                if c in garbage[i]:
+                    cur+=garbage[i].count(c)
+                    best=max(cur,best)
+            ans+=best
+        return ans
 
 obj = Solution()
 #data = obj.garbageCollection(garbage = ["G","P","GP","GG"], travel = [2,4,3])
