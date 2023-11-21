81bceaa508a38206e3648b85a703845d72aaf080
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-21 19:05:17 +0530

    changes

--- Problems/Count Nice Pairs in an Array/solution.py
+++ Problems/Count Nice Pairs in an Array/solution.py
@@ -2,7 +2,14 @@ import io
 from typing import List
 class Solution:
     def countNicePairs(self, nums: List[int]) -> int:
-        pass
+        mod=10**9+7
+        ans=0
+        count=collections.Counter()
+        for num in nums:
+            num-=int(str(num)[::-1])
+            ans+=count[num]
+            count[num]+=1
+        return ans%mod
 
 obj = Solution()
 #data = obj.countNicePairs(nums = [42,11,1,97])
