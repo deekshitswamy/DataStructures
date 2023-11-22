fd452ba5b7ecfb6add389ee2aea1ebf01d52afc5
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-22 18:57:58 +0530

    Diagonal Traverse II

---
+++ Problems/Diagonal Traverse II/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.findDiagonalOrder(nums = [[1,2,3],[4,5,6],[7,8,9]])
+data = obj.findDiagonalOrder(nums = [[1,2,3,4,5],[6,7],[8],[9,10,11],[12,13,14,15,16]])
+print(data)
