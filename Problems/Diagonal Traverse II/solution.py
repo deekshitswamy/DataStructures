7f5a1fe8fa8cecffa86d7e8e4a54e33c776aee47
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-22 18:59:00 +0530

    changes

--- Problems/Diagonal Traverse II/solution.py
+++ Problems/Diagonal Traverse II/solution.py
@@ -2,7 +2,16 @@ import io
 from typing import List
 class Solution:
     def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
-        pass
+        ans=[]
+        for i,r in enumerate(nums):
+            for j ,val in enumerate(r):
+                if len(ans)<=i+j:
+                    ans.append([])
+                ans[i+j].append(val)
+        seen=[]
+        for r in ans:
+            seen+=reversed(r)
+        return seen
 
 obj = Solution()
 #data = obj.findDiagonalOrder(nums = [[1,2,3],[4,5,6],[7,8,9]])
