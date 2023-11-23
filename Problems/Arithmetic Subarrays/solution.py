fe3f38b7445d7ae29bfeaa5ba34391ec89a01562
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-23 18:55:47 +0530

    changes

--- Problems/Arithmetic Subarrays/solution.py
+++ Problems/Arithmetic Subarrays/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
-        pass
+        n=len(nums)
+        def good(left,right):
+            k=list(sorted(nums[left:right+1]))
+            if len(k)==1:
+                return True
+            delta=k[1]-k[0]
+            for i,j in zip(k,k[1:]):
+                if (abs(i-j))!=delta:
+                    return False
+            return True
+        ans=[]
+        for left,right in zip(l,r):
+            ans+=[good(left,right)]
+        return ans
 
 obj = Solution()
 #data = obj.checkArithmeticSubarrays(nums = [4,6,5,9,3,7], l = [0,0,2], r = [2,3,5])
