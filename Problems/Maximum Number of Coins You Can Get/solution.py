173233eff2a5d83753ad22f03f72119eeb8f84c5
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-24 17:55:08 +0530

    Maximum Number of Coins You Can Get

---
+++ Problems/Maximum Number of Coins You Can Get/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxCoins(piles = [2,4,1,2,7,8])
+#data = obj.maxCoins(piles = [2,4,5])
+data = obj.maxCoins(piles = [9,8,7,6,5,1,2,3,4])
+print(data)
