1f10ea978bf4f75407a9cb3fc2612fc9441f14f6
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-25 19:20:12 +0530

    Sum of Absolute Differences in a Sorted Array

---
+++ Problems/Sum of Absolute Differences in a Sorted Array/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def getSumAbsoluteDifferences(self, nums: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.getSumAbsoluteDifferences(nums = [2,3,5])
+data = obj.getSumAbsoluteDifferences(nums = [1,4,6,8,10])
+print(data)
