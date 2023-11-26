482ef49bff39e095b05ad73eeb7bdb1149cc117b
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-26 17:26:45 +0530

    Largest Submatrix With Rearrangements

---
+++ Problems/Largest Submatrix With Rearrangements/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def largestSubmatrix(self, matrix: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.largestSubmatrix(matrix = [[0,0,1],[1,1,1],[1,0,1]])
+#data = obj.largestSubmatrix(matrix = [[1,0,1,0,1]])
+data = obj.largestSubmatrix(matrix = [[1,1,0],[1,0,1]])
+print(data)
