8023ebdc0932e0c842fa0954198b6c31fffe2e27
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-27 14:42:55 +0530

    Knight Dailer

---
+++ Problems/Knight Dialer/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def knightDialer(self, n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.knightDialer(n = 1)
+#data = obj.knightDialer(n = 2)
+data = obj.knightDialer(n = 3131)
+print(data)
