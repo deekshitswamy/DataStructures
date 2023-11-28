610a8a6a5990eb5971936201179868fbd1515290
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-28 19:44:17 +0530

    changes

--- Problems/Number of Ways to Divide a Long Corridor/solution.py
+++ Problems/Number of Ways to Divide a Long Corridor/solution.py
@@ -2,7 +2,14 @@ import io
 from typing import List
 class Solution:
     def numberOfWays(self, corridor: str) -> int:
-        pass
+        mod=10**9+7
+        a,b,c=1,0,0
+        for ch in corridor:
+            if ch=="S":
+                a,b,c=0,a+c,b
+            else:
+                a,b,c=a+c,b,c
+        return c%mod
 
 obj = Solution()
 #data = obj.numberOfWays(corridor = "SSPPSPS")
