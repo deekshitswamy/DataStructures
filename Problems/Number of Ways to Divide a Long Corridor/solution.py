fead18e999bb4d2bb2f216a2477778a3b9543e3a
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-28 19:43:52 +0530

    Number of Ways to Divide a Long Corridor

---
+++ Problems/Number of Ways to Divide a Long Corridor/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def numberOfWays(self, corridor: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numberOfWays(corridor = "SSPPSPS")
+#data = obj.numberOfWays(corridor = "PPSPSP")
+data = obj.numberOfWays(corridor = "S")
+print(data)
