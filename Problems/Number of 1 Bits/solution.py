b882f138a7b6cc2722e1c56c4aa4633074a12284
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-29 19:45:18 +0530

    Number of 1 Bits

---
+++ Problems/Number of 1 Bits/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.hammingWeight(n = 00000000000000000000000000001011)
+#data = obj.hammingWeight(n = 00000000000000000000000010000000)
+data = obj.hammingWeight(n = 11111111111111111111111111111101)
+print(data)
