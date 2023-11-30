bac21284541f16abf63b756e9bdb3ebfcfab7b8e
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-30 19:26:00 +0530

    changes

--- Problems/Minimum One Bit Operations to Make Integers Zero/solution.py
+++ Problems/Minimum One Bit Operations to Make Integers Zero/solution.py
@@ -2,6 +2,16 @@ import io
 from typing import List
 class Solution:
     def minimumOneBitOperations(self, n: int) -> int:
+        """
+        ans = n
+        ans ^= ans >> 16
+        ans ^= ans >> 8
+        ans ^= ans >> 4
+        ans ^= ans >> 2
+        ans ^= ans >> 1
+        return ans
+        
+        """
         ans=0
         while n:
             ans=-ans-(n^(n-1))
