7413ac57ff77246da2fae51f9101241d009df9c4
Deeku-Zeus <deekuzeus21@gmail.com>
2023-11-30 19:23:41 +0530

    Minimum One Bit Operations to Make Integers Zero

---
+++ Problems/Minimum One Bit Operations to Make Integers Zero/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minimumOneBitOperations(self, n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minimumOneBitOperations(n = 3)
+data = obj.minimumOneBitOperations(n = 6)
+print(data)
