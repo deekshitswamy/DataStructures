3f6b0146a62d02db5eae32fb03b3fd9c50c319d0
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-01 20:13:19 +0530

    Check If Two String Arrays are Equivalent

---
+++ Problems/Check If Two String Arrays are Equivalent/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.arrayStringsAreEqual(word1 = ["ab", "c"], word2 = ["a", "bc"])
+#data = obj.arrayStringsAreEqual(word1 = ["a", "cb"], word2 = ["ab", "c"])
+data = obj.arrayStringsAreEqual(word1  = ["abc", "d", "defg"], word2 = ["abcddefg"])
+print(data)
