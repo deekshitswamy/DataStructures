13e60ee5c3b37aefaca61f452e104c118d9a536c
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-02 11:21:27 +0530

    Find Words That Can Be Formed by Characters

---
+++ Problems/Find Words That Can Be Formed by Characters/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countCharacters(words = ["cat","bt","hat","tree"], chars = "atach")
+data = obj.countCharacters(words = ["hello","world","leetcode"], chars = "welldonehoneyr")
+print(data)
