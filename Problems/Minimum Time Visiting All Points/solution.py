a2287ce65e58e49778b0636ca91bf621ec1a4902
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-03 13:21:41 +0530

    Minimum Time Visiting All Points

---
+++ Problems/Minimum Time Visiting All Points/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minTimeToVisitAllPoints(points = [[1,1],[3,4],[-1,0]])
+data = obj.minTimeToVisitAllPoints(points = [[3,2],[-2,2]])
+print(data)
