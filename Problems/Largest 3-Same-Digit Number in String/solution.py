14aa24d477fe3af05cbe99a9a4a5e654579eab3a
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-04 10:47:55 +0530

    Largest 3-Same-Digit Number in String

---
+++ Problems/Largest 3-Same-Digit Number in String/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def largestGoodInteger(self, num: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.largestGoodInteger(num = "6777133339")
+#data = obj.largestGoodInteger(num = "2300019")
+data = obj.largestGoodInteger(num = "42352338")
+print(data)
