b5aca27def3ed8f5e898f2346adcff01c1b606cc
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-05 18:27:51 +0530

    Count of Matches in Tournament

---
+++ Problems/Count of Matches in Tournament/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numberOfMatches(n = 7)
+data = obj.numberOfMatches(n = 14)
+print(data)
