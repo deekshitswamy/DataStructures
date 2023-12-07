6aabcff35c60e7ee40d62693ac14c2c1368a79a8
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-07 07:41:41 +0530

    changes

--- Problems/Largest Odd Number in String/solution.py
+++ Problems/Largest Odd Number in String/solution.py
@@ -2,7 +2,16 @@ import io
 from typing import List
 class Solution:
     def largestOddNumber(self, num: str) -> str:
-        pass
+        i=len(num)-1
+        while i>=0:
+            check=int(num[i])
+            if check%2!=0:
+                return(num[:i+1])
+                break
+            else:
+                num=num[:i]
+            i-=1
+        return("")
 
 obj = Solution()
 #data = obj.largestOddNumber(num = "52")
