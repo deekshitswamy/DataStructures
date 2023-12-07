39c8a661509ffead02c44e50dd5f795882070779
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-07 07:36:28 +0530

    Calculate Money in Leetcode Bank

---
+++ Problems/Calculate Money in Leetcode Bank/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.totalMoney(n = 4)
+#data = obj.totalMoney(n = 10)
+data = obj.totalMoney(n = 20)
+print(data)
