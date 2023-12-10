19f854e15591ca6d16f04babccf3cb37c45dd81f
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-10 23:15:28 +0530

    changes

--- Problems/Transpose Matrix/solution.py
+++ Problems/Transpose Matrix/solution.py
@@ -2,7 +2,13 @@ import io
 from typing import List
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        pass
+        n=len(matrix)
+        m=len(matrix[0])
+        ans=[[0]*n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                ans[i][j]=matrix[j][i]
+        return ans
 
 obj = Solution()
 #data = obj.transpose(matrix = [[1,2,3],[4,5,6],[7,8,9]])
