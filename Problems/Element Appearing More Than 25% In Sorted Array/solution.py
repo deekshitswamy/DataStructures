6af660e1ea0eac57b7f0e614bc809e6c3a541240
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-11 11:40:01 +0530

    Element Appearing More Than 25% In Sorted Array

---
+++ Problems/Element Appearing More Than 25% In Sorted Array/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findSpecialInteger(arr = [1,2,2,6,6,6,6,7,10])
+data = obj.findSpecialInteger(arr = [1,1])
+print(data)
