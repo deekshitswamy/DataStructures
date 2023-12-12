174ec0b1300fda90b4ce9b61d6053fc3848fa314
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-12 18:26:23 +0530

    Maximum Product of Two Elements in an Array

---
+++ Problems/Maximum Product of Two Elements in an Array/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxProduct(nums = [3,4,5,2])
+#data = obj.maxProduct(nums = [1,5,4,5])
+data = obj.maxProduct(nums = [3,7])
+print(data)
