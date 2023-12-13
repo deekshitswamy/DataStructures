39bdabe1371433c725fbe8871d1e886b5d588aec
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-13 07:06:54 +0530

    Special Positions in a Binary Matrix

---
+++ Problems/Special Positions in a Binary Matrix/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def numSpecial(self, mat: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numSpecial(mat = [[1,0,0],[0,0,1],[1,0,0]])
+data = obj.numSpecial(mat = [[1,0,0],[0,1,0],[0,0,1]])
+print(data)
