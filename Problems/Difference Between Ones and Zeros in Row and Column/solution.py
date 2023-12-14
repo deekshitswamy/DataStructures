8edb0829d2988c9f9a71ef14a3c92750d260604f
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-14 19:21:06 +0530

    Difference Between Ones and Zeros in Row and Column

---
+++ Problems/Difference Between Ones and Zeros in Row and Column/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
+        pass
+
+obj = Solution()
+#data = obj.onesMinusZeros(grid = [[0,1,1],[1,0,1],[0,0,1]])
+data = obj.onesMinusZeros(grid = [[1,1,1],[1,1,1]])
+print(data)
