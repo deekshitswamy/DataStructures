64877adaf28273aa9e3b7a76525e72cd405f77b3
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-15 19:57:39 +0530

    Destination City

---
+++ Problems/Destination City/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        pass
+
+obj = Solution()
+#data = obj.destCity(paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]])
+#data = obj.destCity(paths = [["B","C"],["D","B"],["C","A"]])
+data = obj.destCity(paths = [["A","Z"]])
+print(data)
