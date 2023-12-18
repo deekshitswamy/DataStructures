be52d36f3ed59c78a6f83323747b2bb50b09ebb2
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-18 18:58:11 +0530

    Maximum Product Difference Between Two Pairs

---
+++ Problems/Maximum Product Difference Between Two Pairs/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxProductDifference(nums = [5,6,2,7,4])
+data = obj.maxProductDifference(nums = [4,2,5,9,7,4,8])
+print(data)
