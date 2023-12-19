a59e06b8af3ff08c38492b70a0401891e5fc29da
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-19 07:26:56 +0530

    Image Smoother

---
+++ Problems/Image Smoother/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+        pass
+
+obj = Solution()
+#data = obj.imageSmoother(img = [[1,1,1],[1,0,1],[1,1,1]])
+data = obj.imageSmoother(img = [[100,200,100],[200,50,200],[100,200,100]])
+print(data)
