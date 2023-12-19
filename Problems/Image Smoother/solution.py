050f259d3912e74169f047b0a2edb3f9cdd4b0cf
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-19 07:29:12 +0530

    changes

--- Problems/Image Smoother/solution.py
+++ Problems/Image Smoother/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
-        pass
+        m, n = len(img), len(img[0])
+        img_np = np.zeros((m+2, n+2), dtype=np.int32)
+        img_np[1:-1, 1:-1] = img
+        cnt_np = np.zeros((m+2, n+2), dtype=np.int32)
+        cnt_np[1:-1, 1:-1] = 1
+
+        reduce_img = lambda x: sum([x[o1:m+o1, o2:n+o2] for o1, o2 in product(range(3), range(3))])
+
+
+        ans = reduce_img(img_np) // reduce_img(cnt_np)
+        return ans.tolist()
 
 obj = Solution()
 #data = obj.imageSmoother(img = [[1,1,1],[1,0,1],[1,1,1]])
