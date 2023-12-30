097710b313e75afd6aeb2ab39e1a5baa78c24d30
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-30 06:14:56 +0530

    Widest Vertical Area Between Two Points Containing No Points

---
+++ Problems/Widest Vertical Area Between Two Points Containing No Points/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxWidthOfVerticalArea(points = [[8,7],[9,9],[7,4],[9,7]])
+data = obj.maxWidthOfVerticalArea(points = [[3,1],[9,0],[1,0],[1,4],[5,3],[8,8]])
+print(data)
