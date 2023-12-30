904adb8a4c900b65c933db69dd592c96d6196af2
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-30 06:11:17 +0530

    Buy Two Chocolates

---
+++ Problems/Buy Two Chocolates/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.buyChoco(prices = [1,2,2], money = 3)
+data = obj.buyChoco(prices = [3,2,3], money = 3)
+print(data)
