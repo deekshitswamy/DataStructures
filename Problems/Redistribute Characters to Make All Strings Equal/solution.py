aeb538a837682c64e829aeb00cc1556aa0152f6d
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-30 06:03:23 +0530

    Redistribute Characters to Make All Strings Equal

---
+++ Problems/Redistribute Characters to Make All Strings Equal/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.makeEqual(words = ["abc","aabc","bc"])
+data = obj.makeEqual(words = ["ab","a"])
+print(data)
