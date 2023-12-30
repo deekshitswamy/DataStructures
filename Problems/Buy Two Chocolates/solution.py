b45f23c7ab805f0811d3f80457386248d36e0a9e
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-30 06:12:26 +0530

    changes

--- Problems/Buy Two Chocolates/solution.py
+++ Problems/Buy Two Chocolates/solution.py
@@ -2,7 +2,11 @@ import io
 from typing import List
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        pass
+        prices.sort()
+        if (prices[0]+prices[1])<=money:
+            return (money-(prices[0]+prices[1]))
+        else:
+            return money
 
 obj = Solution()
 #data = obj.buyChoco(prices = [1,2,2], money = 3)
