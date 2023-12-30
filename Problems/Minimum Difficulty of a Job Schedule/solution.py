c19a97b689979916ba4a65e7f67f6dc05b13999c
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-30 06:07:16 +0530

    Minimum Difficulty of a Job Schedule

---
+++ Problems/Minimum Difficulty of a Job Schedule/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minDifficulty(jobDifficulty = [6,5,4,3,2,1], d = 2)
+#data = obj.minDifficulty(jobDifficulty = [9,9,9], d = 4)
+data = obj.minDifficulty(jobDifficulty = [1,1,1], d = 3)
+print(data)
