d8fe05808b50370151a6bf6d18fdd6be9269b8b6
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-31 22:09:14 +0530

    Path Crossing

---
+++ Problems/Path Crossing/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.isPathCrossing(path = "NES")
+data = obj.isPathCrossing(path = "NESWW")
+print(data)
