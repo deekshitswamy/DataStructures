89281a99de926e2aaa2426b242a8ff61307e2146
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-31 22:19:14 +0530

    changes

---
+++ Problems/Number of Dice Rolls With Target Sum/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numRollsToTarget(n = 1, k = 6, target = 3)
+#data = obj.numRollsToTarget(n = 2, k = 6, target = 7)
+data = obj.numRollsToTarget(n = 30, k = 30, target = 500)
+print(data)
