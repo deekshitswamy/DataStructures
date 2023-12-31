c381d60b6687303e86569f1f5a776bda3486421d
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-31 22:22:14 +0530

    Decode Ways

---
+++ Problems/Decode Ways/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numDecodings(s = "12")
+#data = obj.numDecodings(s = "226")
+data = obj.numDecodings(s = "06")
+print(data)
