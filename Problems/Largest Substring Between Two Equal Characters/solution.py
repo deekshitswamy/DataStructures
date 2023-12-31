9e9e8ad90e5093d4e5cafc59a4e0c5e2547647d4
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-31 22:03:19 +0530

    Largest Substring Between Two Equal Characters

---
+++ Problems/Largest Substring Between Two Equal Characters/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maxLengthBetweenEqualCharacters(self, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxLengthBetweenEqualCharacters(s = "aa")
+#data = obj.maxLengthBetweenEqualCharacters(s = "abca")
+data = obj.maxLengthBetweenEqualCharacters(s = "cbzxy")
+print(data)
