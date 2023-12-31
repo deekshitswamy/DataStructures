c1a8549704b2c6eb7c2f0aa0307fa0b58feb00da
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-31 22:24:47 +0530

    Minimum Changes To Make Alternating Binary String

---
+++ Problems/Minimum Changes To Make Alternating Binary String/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minOperations(self, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minOperations(s = "0100")
+#data = obj.minOperations(s = "10")
+data = obj.minOperations(s = "1111")
+print(data)
