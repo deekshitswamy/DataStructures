66ffbd21378ff9d1dc76eee4f9d3a19e6ffbd51f
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-31 22:11:51 +0530

    String Compression II

---
+++ Problems/String Compression II/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def getLengthOfOptimalCompression(self, s: str, k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.getLengthOfOptimalCompression(s = "aaabcccd", k = 2)
+#data = obj.getLengthOfOptimalCompression(s = "aabbaa", k = 2)
+data = obj.getLengthOfOptimalCompression(s = "aaaaaaaaaaa", k = 0)
+print(data)
