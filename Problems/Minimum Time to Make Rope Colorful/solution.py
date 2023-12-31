cd29b4742048d039f2d81673c18b46948f6498bf
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-31 22:17:31 +0530

    changes

--- Problems/Minimum Time to Make Rope Colorful/solution.py
+++ Problems/Minimum Time to Make Rope Colorful/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
-        pass
+        time = 0
+        l= 0
+
+        for r in range(1, len(colors)):
+            if colors[l] == colors[r]:
+                time += min(neededTime[l] , neededTime[r])
+                if neededTime[l] < neededTime[r]:
+                    l = r
+            else:
+                l = r
+        return time
 
 obj = Solution()
 #data = obj.minCost(colors = "abaac", neededTime = [1,2,3,4,5])
