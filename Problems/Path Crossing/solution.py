a69a71ab33d5c4eca1ef8edb54672c201f6428bd
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-31 22:10:06 +0530

    changes

--- Problems/Path Crossing/solution.py
+++ Problems/Path Crossing/solution.py
@@ -2,7 +2,24 @@ import io
 from typing import List
 class Solution:
     def isPathCrossing(self, path: str) -> bool:
-        pass
+        h=set()
+        x=0
+        y=0
+        h.add((0,0))
+        for p in path:
+            if p=="N":
+                x+=1
+            if p=="S":
+                x-=1
+            if p=="E":
+                y-=1
+            if p=="W":
+                y+=1
+            if (x,y) in h:
+                return True
+            h.add((x,y))
+            
+        return False
 
 obj = Solution()
 #data = obj.isPathCrossing(path = "NES")
