5f060ed568bcd8bf80582e641ddaf47867d59613
Deeku-Zeus <deekuzeus21@gmail.com>
2023-12-31 22:07:05 +0530

    Maximum Score After Splitting a String

---
+++ Problems/Maximum Score After Splitting a String/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maxScore(self, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxScore(s = "011101")
+#data = obj.maxScore(s = "00111")
+data = obj.maxScore(s = "1111")
+print(data)
