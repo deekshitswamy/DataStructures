0054ee6250274224060bebba2b71bc7176c5eea6
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-01 18:09:00 +0530

    Assign Cookies

---
+++ Problems/Assign Cookies/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findContentChildren(g = [1,2,3], s = [1,1])
+data = obj.findContentChildren(g = [1,2], s = [1,2,3])
+print(data)
