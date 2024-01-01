c690bde0ee152fcf566735ba441b28cfa3c779fa
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-01 18:09:29 +0530

    changes

--- Problems/Assign Cookies/solution.py
+++ Problems/Assign Cookies/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        pass
+        n=len(g)-1
+        m=len(s)-1
+        g=sorted(g)
+        s=sorted(s)
+        ans=0
+        while min(n,m)>=0:
+            if g[n]<=s[m]:
+                ans+=1
+                m-=1
+            n-=1
+        return ans
 
 obj = Solution()
 #data = obj.findContentChildren(g = [1,2,3], s = [1,1])
