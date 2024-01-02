cf2e191c5b8ceb0bc996e11d6bd6c1dd6118c277
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-02 19:49:41 +0530

    changes

--- Problems/Convert an Array Into a 2D Array With Conditions/solution.py
+++ Problems/Convert an Array Into a 2D Array With Conditions/solution.py
@@ -2,7 +2,23 @@ import io
 from typing import List
 class Solution:
     def findMatrix(self, nums: List[int]) -> List[List[int]]:
-        pass
+        ans=[]
+        count=Counter(nums)
+        while count:
+            val=[]
+            drop=[]
+            for k,v in count.items():
+                val.append(k)
+                count[k]-=1
+                if count[k]==0:
+                    drop.append(k)
+
+            for k in drop:
+                count.pop(k)
+
+            ans.append(val)
+
+        return ans
 
 obj = Solution()
 #data = obj.findMatrix(nums = [1,3,4,1,2,3,1])
