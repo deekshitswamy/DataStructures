e4e1bd79c222e950a1ec32256d9530b4b439fa3a
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-03 19:06:25 +0530

    changes

--- Problems/Number of Laser Beams in a Bank/solution.py
+++ Problems/Number of Laser Beams in a Bank/solution.py
@@ -2,7 +2,14 @@ import io
 from typing import List
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
-        pass
+        ans=0
+        prev=0
+        for i in bank:
+            res=i.count('1')
+            if res:
+                ans+=prev*res
+                prev=res
+        return ans
 
 obj = Solution()
 #data = obj.numberOfBeams(bank = ["011001","000000","010100","001000"])
