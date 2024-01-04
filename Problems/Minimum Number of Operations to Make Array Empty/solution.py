b8394e69a0023586e8c8554513ac40ef4ffa73e7
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-04 19:06:27 +0530

    changes

--- Problems/Minimum Number of Operations to Make Array Empty/solution.py
+++ Problems/Minimum Number of Operations to Make Array Empty/solution.py
@@ -2,7 +2,14 @@ import io
 from typing import List
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        pass
+        count=Counter(nums).values()
+        ans=0
+        for i in count:
+            if i==1:
+                return -1
+            else:
+                ans+=i//3 + (i%3!=0)
+        return ans
 
 obj = Solution()
 #data = obj.minOperations(nums = [2,3,3,2,2,4,2,3,4])
