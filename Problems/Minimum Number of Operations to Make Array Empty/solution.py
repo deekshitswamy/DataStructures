e574b2886f0607bc8f1700f95cc33421447f2b19
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-04 19:06:08 +0530

    Minimum Number of Operations to Make Array Empty

---
+++ Problems/Minimum Number of Operations to Make Array Empty/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minOperations(nums = [2,3,3,2,2,4,2,3,4])
+data = obj.minOperations(nums = [2,1,2,2,3,3])
+print(data)
