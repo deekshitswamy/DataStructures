74afb552fc299445b2380cd663507f7d01b481c5
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-05 20:55:21 +0530

    Longest Increasing Subsequence

---
+++ Problems/Longest Increasing Subsequence/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.lengthOfLIS(nums = [10,9,2,5,3,7,101,18])
+#data = obj.lengthOfLIS(nums = [0,1,0,3,2,3])
+data = obj.lengthOfLIS(nums = [7,7,7,7,7,7,7])
+print(data)
