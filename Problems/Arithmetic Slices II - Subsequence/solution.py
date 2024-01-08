edfe6837252f6a7ea16a7bae1df2f9d42113f82e
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-08 07:19:24 +0530

    Arithmetic Slices II - Subsequence

---
+++ Problems/Arithmetic Slices II - Subsequence/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numberOfArithmeticSlices(nums = [2,4,6,8,10])
+data = obj.numberOfArithmeticSlices(nums = [7,7,7,7,7])
+print(data)
