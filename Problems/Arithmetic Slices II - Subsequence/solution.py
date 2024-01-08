9475cc25c6b5cdb6cbf89c41217f4c278f184b01
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-08 07:20:50 +0530

    changes

--- Problems/Arithmetic Slices II - Subsequence/solution.py
+++ Problems/Arithmetic Slices II - Subsequence/solution.py
@@ -2,7 +2,15 @@ import io
 from typing import List
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        pass
+        res,n=0,len(nums)
+        dp=[defaultdict(int) for _ in range (n)]
+        for i in range(n):
+            for j in range(i):
+                diff=nums[i]-nums[j]
+                dp[i][diff]+=1+dp[j][diff]
+                res+=dp[j][diff]
+
+        return res
 
 obj = Solution()
 #data = obj.numberOfArithmeticSlices(nums = [2,4,6,8,10])
