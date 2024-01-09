c0fbfffcdfe61c74837354301af89eeb031d2a2d
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-09 19:00:02 +0530

    changes

---
+++ Problems/Leaf-Similar Trees/solution.py
@@ -0,0 +1,16 @@
+import io
+from typing import List
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def leafSimilar(self, root1: TreeNode, root2: TreeNode) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.leafSimilar(n = 1)
+#data = obj.leafSimilar(n = 2)
+data = obj.leafSimilar(n = 2)
+print(data)
