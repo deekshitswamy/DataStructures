feb773a9da213db7553866aad329f121c901652f
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-10 17:56:09 +0530

    changes

--- Problems/Amount of Time for Binary Tree to Be Infected/solution.py
+++ Problems/Amount of Time for Binary Tree to Be Infected/solution.py
@@ -8,7 +8,29 @@ class TreeNode:
         self.right = right
 class Solution:
     def amountOfTime(self, root: TreeNode, start: int) -> int:
-        pass
+        graph=defaultdict(list)
+        stack=[(root,None)]
+        while stack:
+            n,p=stack.pop()
+            if p:
+                graph[p.val].append(n.val)
+                graph[n.val].append(p.val)
+            if n.left:
+                stack.append((n.left,n))
+            if n.right:
+                stack.append((n.right,n))
+        ans=-1
+        seen={start}
+        queue=deque([start])
+        while queue:
+            for _ in range(len(queue)):
+                u=queue.popleft()
+                for v in graph[u]:
+                    if v not in seen:
+                        seen.add(v)
+                        queue.append(v)
+            ans+=1
+        return ans 
 
 obj = Solution()
 #data = obj.amountOfTime(root = [1], start = 1)
