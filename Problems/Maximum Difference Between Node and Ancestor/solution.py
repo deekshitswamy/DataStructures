1a5aea4e5b433bbfb0262db16add3d94504cdf56
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-11 19:07:10 +0530

    changes

--- Problems/Maximum Difference Between Node and Ancestor/solution.py
+++ Problems/Maximum Difference Between Node and Ancestor/solution.py
@@ -8,7 +8,17 @@ class TreeNode:
         self.right = right
 class Solution:
     def maxAncestorDiff(self, root: TreeNode) -> int:
-        pass
+        INF=10**10
+        def go(node,mn,mx):
+            ans=0
+            ans=max(ans,node.val-mn)
+            ans=max(ans,mx-node.val)
+            if node.left:
+                ans=max(ans,go(node.left,min(mn,node.val),max(mx,node.val)))
+            if node.right:
+                ans=max(ans,go(node.right,min(mn,node.val),max(mx,node.val)))
+            return ans
+        return go(root,INF,-INF)
 
 obj = Solution()
 #data = obj.maxAncestorDiff(n = 1)
