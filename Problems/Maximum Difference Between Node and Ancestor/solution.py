2fdb6d2458b8e002150a58f7d8d1e9604f9a8f43
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-11 19:05:29 +0530

    Maximum Difference Between Node and Ancestor

---
+++ Problems/Maximum Difference Between Node and Ancestor/solution.py
@@ -0,0 +1,17 @@
+import io
+from typing import List
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def maxAncestorDiff(self, root: TreeNode) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxAncestorDiff(n = 1)
+#data = obj.maxAncestorDiff(n = 2)
+data = obj.maxAncestorDiff(n = 2)
+print(data)
