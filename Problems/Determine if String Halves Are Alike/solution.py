8430bbd1f82281ee443c0aaa09ff6998f965778c
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-12 18:23:03 +0530

    changes

--- Problems/Determine if String Halves Are Alike/solution.py
+++ Problems/Determine if String Halves Are Alike/solution.py
@@ -2,7 +2,22 @@ import io
 from typing import List
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
-        pass
+        vowels = ['a', 'e', 'i', 'o', 'u','A', 'E', 'I', 'O', 'U']
+        l = len(s)
+        a = s[0:(l//2)]
+        b = s[(l//2):l]
+        counta = 0
+        countb = 0
+        
+        for i in range(l//2):
+            if a[i] in vowels:
+                counta += 1
+            if b[i] in vowels:
+                countb += 1
+        if counta == countb:
+            return True
+        else:
+            return False
 
 obj = Solution()
 #data = obj.halvesAreAlike(s = "book")
