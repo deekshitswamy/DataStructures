dfa81f73b3ee41e1f9308a1a3ad3af30f1cc7427
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-12 18:21:04 +0530

    changes

---
+++ Problems/Determine if String Halves Are Alike/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.halvesAreAlike(s = "book")
+data = obj.halvesAreAlike(s = "textbook")
+print(data)
