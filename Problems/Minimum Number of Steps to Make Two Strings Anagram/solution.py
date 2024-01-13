1694705bd6d748382c6b3fe51591ecc666621a89
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-13 18:02:58 +0530

    Minimum Number of Steps to Make Two Strings Anagram

---
+++ Problems/Minimum Number of Steps to Make Two Strings Anagram/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minSteps(s = "bab", t = "aba")
+#data = obj.minSteps(s = "leetcode", t = "practice")
+data = obj.minSteps(s = "anagram", t = "mangaar")
+print(data)
