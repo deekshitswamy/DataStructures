b6241ec6a9a7faae4fa49d43acaa99fe41f72469
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-14 08:57:39 +0530

    Determine if Two Strings Are Close

---
+++ Problems/Determine if Two Strings Are Close/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.closeStrings(word1 = "abc", word2 = "bca")
+#data = obj.closeStrings(word1 = "a", word2 = "aa")
+data = obj.closeStrings(word1 = "cabbba", word2 = "abbccc")
+print(data)
