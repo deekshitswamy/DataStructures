8b31d570f1da17bd5f5c25cb11435ae6c311f08a
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-15 11:40:27 +0530

    Find Players With Zero or One Losses

---
+++ Problems/Find Players With Zero or One Losses/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        pass
+
+obj = Solution()
+#data = obj.findWinners(matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]])
+data = obj.findWinners(matches = [[2,3],[1,3],[5,4],[6,4]])
+print(data)
