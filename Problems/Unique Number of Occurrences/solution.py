821540d2b4907f0f999832986a5cdd3591981ad8
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-17 19:36:24 +0530

    changes

--- Problems/Unique Number of Occurrences/solution.py
+++ Problems/Unique Number of Occurrences/solution.py
@@ -2,7 +2,15 @@ import io
 from typing import List
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        pass
+        hash={}
+
+        for i in range (len(arr)):
+            if arr[i] not in hash:    
+                hash[arr[i]]=1
+            else:
+                hash[arr[i]]=hash[arr[i]]+1
+        
+        return len(set(hash.values()))== len(hash.values())
 
 obj = Solution()
 #data = obj.uniqueOccurrences(arr = [1,2,2,1,1,3])
