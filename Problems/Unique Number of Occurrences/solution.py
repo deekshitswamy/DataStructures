c91bffc2969d985f892900543c92c56b0e369023
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-17 19:35:07 +0530

    Unique Number of Occurrences

---
+++ Problems/Unique Number of Occurrences/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.uniqueOccurrences(arr = [1,2,2,1,1,3])
+#data = obj.uniqueOccurrences(arr = [1,2])
+data = obj.uniqueOccurrences(arr = [-3,0,1,-3,1,1,1,-3,10,0])
+print(data)
