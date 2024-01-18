47ac8922dce1a5573b308267aa2e6f8721b8f08a
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-18 18:17:02 +0530

    changes

--- Problems/Climbing Stairs/solution.py
+++ Problems/Climbing Stairs/solution.py
@@ -2,7 +2,10 @@ import io
 from typing import List
 class Solution:
     def climbStairs(self, n: int) -> int:
-        pass
+        z = [1, 1, 2]
+        for i in range(n - 2):
+            z.append(z[-1] + z[-2])
+        return z[n]
 
 obj = Solution()
 #data = obj.climbStairs(n = 2)
