a50ef6e91d11850faeeea248e5a39f810bc85a40
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-18 18:16:39 +0530

    Climbing Stairs

---
+++ Problems/Climbing Stairs/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.climbStairs(n = 2)
+data = obj.climbStairs(n = 2)
+print(data)
