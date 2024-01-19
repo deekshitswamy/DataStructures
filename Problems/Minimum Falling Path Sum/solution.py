47ce2424ddd9f4e03498d06a3464b3c580350e7c
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-19 19:01:08 +0530

    Minimum Falling Path Sum

---
+++ Problems/Minimum Falling Path Sum/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minFallingPathSum(matrix = [[2,1,3],[6,5,4],[7,8,9]])
+data = obj.minFallingPathSum(matrix = [[-19,57],[-40,-5]])
+print(data)
