a3d12b2133b032a2a1e25582d3290fcb6611d826
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-19 19:01:54 +0530

    changes

--- Problems/Minimum Falling Path Sum/solution.py
+++ Problems/Minimum Falling Path Sum/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        pass
+        n=len(matrix)
+        m=len(matrix[0])
+        for r in range(n):
+            for c in range(m):
+                if r>0 and 0<=c-1 and c+1<m:
+                    matrix[r][c]=min(matrix[r][c]+matrix[r-1][c],matrix[r][c]+matrix[r-1][c+1],matrix[r][c]+matrix[r-1][c-1])
+                elif r>0 and c-1==-1:
+                    matrix[r][c]=min(matrix[r][c]+matrix[r-1][c],matrix[r][c]+matrix[r-1][c+1])
+                elif r>0 and c+1==m:
+                    matrix[r][c]=min(matrix[r][c]+matrix[r-1][c],matrix[r][c]+matrix[r-1][c-1])
+
+        return min(matrix[n-1])
 
 obj = Solution()
 #data = obj.minFallingPathSum(matrix = [[2,1,3],[6,5,4],[7,8,9]])
