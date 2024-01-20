edd9d4949e3c861608cdb93121ca51e8cf6996f8
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-20 17:33:44 +0530

    Sum of Subarray Minimums

---
+++ Problems/Sum of Subarray Minimums/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.sumSubarrayMins(arr = [3,1,2,4])
+data = obj.sumSubarrayMins(arr = [11,81,94,43,3])
+print(data)
