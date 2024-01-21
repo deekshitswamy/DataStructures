839fa165a8d50ba30fbcdd6763b6d69ef4e1ec3d
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-21 18:57:42 +0530

    House Robber

---
+++ Problems/House Robber/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.testfunc(nums = [1,2,3,1])
+data = obj.testfunc(nums = [2,7,9,3,1])
+print(data)
