61a761f1513a6deaa7af02c1921abe3541351d4a
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-22 18:51:08 +0530

    changes

--- Problems/Set Mismatch/solution.py
+++ Problems/Set Mismatch/solution.py
@@ -2,7 +2,11 @@ import io
 from typing import List
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        pass
+        n = len(nums)
+        sumtoN = n*(n+1)//2
+        missingNum = sumtoN - sum(set(nums))
+        duplicateNum = sum(nums) - (sumtoN - missingNum)
+        return [duplicateNum,missingNum]
 
 obj = Solution()
 #data = obj.findErrorNums(nums = [1,2,2,4])
