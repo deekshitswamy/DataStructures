92f162c1b274ff68188c99481e5bf0a791822422
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-22 18:49:58 +0530

    Set Mismatch

---
+++ Problems/Set Mismatch/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.findErrorNums(nums = [1,2,2,4])
+data = obj.findErrorNums(nums = [1,1])
+print(data)
