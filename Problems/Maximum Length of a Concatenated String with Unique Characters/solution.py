398bf115a5837489c62a8db1510af58af2280359
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-23 20:11:44 +0530

    Maximum Length of a Concatenated String with Unique Characters

---
+++ Problems/Maximum Length of a Concatenated String with Unique Characters/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxLength(arr = ["un","iq","ue"])
+#data = obj.maxLength(arr = ["cha","r","act","ers"])
+data = obj.maxLength(arr = ["abcdefghijklmnopqrstuvwxyz"])
+print(data)
