e004c474ee2ca2b0fa5638636286684f91f455fa
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-25 18:26:38 +0530

    Longest Common Subsequence

---
+++ Problems/Longest Common Subsequence/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.longestCommonSubsequence(text1 = "abcde", text2 = "ace")
+#data = obj.longestCommonSubsequence(text1 = "abc", text2 = "abc")
+data = obj.longestCommonSubsequence(text1 = "abc", text2 = "def")
+print(data)
