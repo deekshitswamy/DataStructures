7be0f4434900374dd5ddcaa9a2bd9a51ffcb5014
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-26 09:31:20 +0530

    changes

---
+++ Problems/Out of Boundary Paths/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findPaths(m = 2, n = 2, maxMove = 2, startRow = 0, startColumn = 0)
+data = obj.findPaths(m = 1, n = 3, maxMove = 3, startRow = 0, startColumn = 1)
+print(data)
