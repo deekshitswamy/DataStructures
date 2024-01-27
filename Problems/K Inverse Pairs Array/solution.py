2c41dfb3aadb13c21885c04d4c8cfcfdce787a39
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-27 11:03:58 +0530

    K Inverse Pairs Array

---
+++ Problems/K Inverse Pairs Array/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def kInversePairs(self, n: int, k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.kInversePairs(n = 3, k = 0)
+data = obj.kInversePairs(n = 3, k = 1)
+print(data)
