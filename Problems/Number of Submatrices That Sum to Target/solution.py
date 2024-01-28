e5805a634da740d2cb18925aaa1d02ec1c204994
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-28 13:42:11 +0530

    Number of Submatrices That Sum to Target

---
+++ Problems/Number of Submatrices That Sum to Target/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def numSubmatrixSumTarget(self, matrix: List[List[int]], target: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numSubmatrixSumTarget(matrix = [[0,1,0],[1,1,1],[0,1,0]], target = 0)
+#data = obj.numSubmatrixSumTarget(matrix = [[1,-1],[-1,1]], target = 0)
+data = obj.numSubmatrixSumTarget(matrix = [[904]], target = 0)
+print(data)
