8f52148306456c46dad954b9b4ac2cb7457af2e8
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-29 22:13:48 +0530

    Implement Queue using Stacks

--- Problems/Implement Queue using Stacks/solution.py
+++ Problems/Implement Queue using Stacks/solution.py
@@ -3,21 +3,26 @@ from typing import List
 class MyQueue:
 
     def __init__(self):
-        pass
+        self.s1=[]
+        self.s2=[]
 
     def push(self, x: int) -> None:
-        pass
-        
+        while self.s1:
+            self.s2.append(self.s1.pop())
+        self.s1.append(x)    
+        while self.s2:
+            self.s1.append(self.s2.pop())
+
 
     def pop(self) -> int:
-        pass
-        
+        return self.s1.pop()
+
 
     def peek(self) -> int:
-        pass
+        return self.s1[-1]
 
     def empty(self) -> bool:
-        pass
+        return not self.s1
 
 
 # Your MyQueue object will be instantiated and called as such:
