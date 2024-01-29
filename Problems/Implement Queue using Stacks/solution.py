38755d7fde363ed071cbdab9c0f122b32d420fc5
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-29 22:12:41 +0530

    changes

---
+++ Problems/Implement Queue using Stacks/solution.py
@@ -0,0 +1,30 @@
+import io
+from typing import List
+class MyQueue:
+
+    def __init__(self):
+        pass
+
+    def push(self, x: int) -> None:
+        pass
+        
+
+    def pop(self) -> int:
+        pass
+        
+
+    def peek(self) -> int:
+        pass
+
+    def empty(self) -> bool:
+        pass
+
+
+# Your MyQueue object will be instantiated and called as such:
+input_command = ["MyQueue", "push", "push", "peek", "pop", "empty"]
+data = [[], [1], [2], [], [], []]
+# obj = MyQueue()
+# obj.push()
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
