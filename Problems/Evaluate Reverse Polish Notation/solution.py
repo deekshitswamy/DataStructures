2ff4b5156bd96b8c9f73b6d5d9aad01732e0df3d
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-30 18:28:55 +0530

    Evaluate Reverse Polish Notation

---
+++ Problems/Evaluate Reverse Polish Notation/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.evalRPN(tokens = ["2","1","+","3","*"])
+#data = obj.evalRPN(tokens = ["4","13","5","/","+"])
+data = obj.evalRPN(tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"])
+print(data)
