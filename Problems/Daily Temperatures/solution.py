93ec5e598d9c5c244e20793e574abcae7062433e
Deeku-Zeus <deekuzeus21@gmail.com>
2024-01-31 18:16:10 +0530

    Daily Temperatures

---
+++ Problems/Daily Temperatures/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.dailyTemperatures(n = 1)
+#data = obj.dailyTemperatures(n = 2)
+data = obj.dailyTemperatures(n = 2)
+print(data)
