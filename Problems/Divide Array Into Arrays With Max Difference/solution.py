76c46393e65f2596e16e881845216e65cfbf8e6a
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-01 19:42:25 +0530

    Divide Array Into Arrays With Max Difference

---
+++ Problems/Divide Array Into Arrays With Max Difference/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
+        pass
+
+obj = Solution()
+#data = obj.divideArray(nums = [1,3,4,8,7,9,3,5,1], k = 2)
+data = obj.divideArray(nums = [1,3,3,2,7,3], k = 3)
+print(data)
