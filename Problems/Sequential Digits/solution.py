486a40ca5700b5c4e123db59fd17d04e18dbcd0b
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-02 20:04:44 +0530

    Sequential Digits

---
+++ Problems/Sequential Digits/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.sequentialDigitslow = 100, high = 300)
+data = obj.sequentialDigits(low = 1000, high = 13000)
+print(data)
