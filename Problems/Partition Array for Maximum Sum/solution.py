864bbc070e01ad04b19fc462b9852f65ab928f0a
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-03 19:06:48 +0530

    changes

--- Problems/Partition Array for Maximum Sum/solution.py
+++ Problems/Partition Array for Maximum Sum/solution.py
@@ -2,7 +2,14 @@ import io
 from typing import List
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
-        pass
+        n=len(arr)
+        dp=[0]*(n+1)
+        for i in range(1,n+1):
+            curmax=0
+            for x in range(1,min(k,i)+1):
+                curmax=max(curmax,arr[i-x])
+                dp[i]=max(dp[i],dp[i-x]+curmax*x)
+        return dp[n]
 
 obj = Solution()
 #data = obj.maxSumAfterPartitioning(arr = [1,15,7,9,2,5,10], k = 3)
