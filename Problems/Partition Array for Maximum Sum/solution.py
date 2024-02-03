347f2ccd99d0d8994793b7ebee55e26349cb0018
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-03 19:05:11 +0530

    Partition Array for Maximum Sum

---
+++ Problems/Partition Array for Maximum Sum/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxSumAfterPartitioning(arr = [1,15,7,9,2,5,10], k = 3)
+#data = obj.maxSumAfterPartitioning(arr = [1,4,1,5,7,3,6,1,9,9,3], k = 4)
+data = obj.maxSumAfterPartitioning(arr = [1], k = 1)
+print(data)
