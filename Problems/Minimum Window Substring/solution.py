0f0eadd0432ad5b83739ed53b5c2e85d47582de1
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-04 15:41:51 +0530

    changes

--- Problems/Minimum Window Substring/solution.py
+++ Problems/Minimum Window Substring/solution.py
@@ -2,7 +2,41 @@ import io
 from typing import List
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        pass
+        m, n = len(s), len(t)
+        mp = {}
+
+        ans = float('inf')
+        start = 0
+
+        for x in t:
+            mp[x] = mp.get(x, 0) + 1
+
+        count = len(mp)
+
+        i = 0
+        j = 0
+
+        while j < len(s):
+            mp[s[j]] = mp.get(s[j], 0) - 1
+            if mp[s[j]] == 0:
+                count -= 1
+
+            if count == 0:
+                while count == 0:
+                    if ans > j - i + 1:
+                        ans = j - i + 1
+                        start = i
+                    mp[s[i]] = mp.get(s[i], 0) + 1
+                    if mp[s[i]] > 0:
+                        count += 1
+
+                    i += 1
+            j += 1
+
+        if ans != float('inf'):
+            return s[start:start + ans]
+        else:
+            return ""
 
 obj = Solution()
 #data = obj.minWindow(s = "ADOBECODEBANC", t = "ABC")
