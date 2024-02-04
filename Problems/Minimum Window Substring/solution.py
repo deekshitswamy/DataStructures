350d5a703944c1228fcf33675d47330de7e3a18c
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-04 15:40:57 +0530

    Minimum Window Substring

---
+++ Problems/Minimum Window Substring/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.minWindow(s = "ADOBECODEBANC", t = "ABC")
+#data = obj.minWindow(s = "a", t = "a")
+data = obj.minWindow(s = "a", t = "aa")
+print(data)
