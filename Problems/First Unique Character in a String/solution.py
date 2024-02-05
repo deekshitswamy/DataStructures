e33e23e77cd7acec85f536afb41667c9bd3fc98c
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-05 20:23:44 +0530

    changes

--- Problems/First Unique Character in a String/solution.py
+++ Problems/First Unique Character in a String/solution.py
@@ -2,7 +2,11 @@ import io
 from typing import List
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        pass
+        n=len(s)
+        for i in range(n):
+            if s[i] not in s[:i] and s[i] not in s[i+1:]:
+                return i
+        return -1
 
 obj = Solution()
 #data = obj.firstUniqChar(s = "leetcode")
