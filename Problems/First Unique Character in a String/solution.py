f7cc1fcd94cdacd96812fe1333782a90394bb989
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-05 20:23:00 +0530

    First Unique Character in a String

---
+++ Problems/First Unique Character in a String/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.firstUniqChar(s = "leetcode")
+#data = obj.firstUniqChar(s = "loveleetcode")
+data = obj.firstUniqChar(s = "aabb")
+print(data)
