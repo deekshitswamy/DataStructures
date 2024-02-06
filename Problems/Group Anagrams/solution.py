f7ad7b4a360ef44075fd7f25bea055b783e2142e
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-06 19:38:52 +0530

    changes

--- Problems/Group Anagrams/solution.py
+++ Problems/Group Anagrams/solution.py
@@ -2,7 +2,21 @@ import io
 from typing import List
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        pass
+        mp  = {}
+        for i in range(len(strs)):
+            string = strs[i]
+            new = " ".join(sorted(string))
+            
+            if new not in mp:
+                mp[new] = [string]
+            else:
+                mp[new].append(string)
+
+        ans = []
+        for j in mp:
+            ans.append(mp[j])
+        
+        return ans
 
 obj = Solution()
 #data = obj.groupAnagrams(strs = ["eat","tea","tan","ate","nat","bat"])
