0f44be78d8538de6ce69316b43fb906e2f0135ea
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-06 19:37:52 +0530

    Group Anagrams

---
+++ Problems/Group Anagrams/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        pass
+
+obj = Solution()
+#data = obj.groupAnagrams(strs = ["eat","tea","tan","ate","nat","bat"])
+#data = obj.groupAnagrams(strs = [""])
+data = obj.groupAnagrams(strs = ["a"])
+print(data)
