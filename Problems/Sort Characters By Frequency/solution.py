b21295596c7825c53fc5b27e37a06748c88c4bd6
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-07 19:00:16 +0530

    changes

--- Problems/Sort Characters By Frequency/solution.py
+++ Problems/Sort Characters By Frequency/solution.py
@@ -2,7 +2,11 @@ import io
 from typing import List
 class Solution:
     def frequencySort(self, s: str) -> str:
-        pass
+        freq=Counter(s)
+        ans=''
+        for i,j in sorted(freq.items(),key=lambda x:x[1],reverse=True):
+            ans=ans+i*j
+        return ans
 
 obj = Solution()
 #data = obj.frequencySort(s = "tree")
