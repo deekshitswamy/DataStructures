0be213e4182ea3b0bfc71010adda3a0c27db4f4e
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-08 18:56:11 +0530

    Perfect Squares

---
+++ Problems/Perfect Squares/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def numSquares(self, n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numSquares(n = 12)
+data = obj.numSquares(n = 13)
+print(data)
