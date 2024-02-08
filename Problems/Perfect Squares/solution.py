3b528ab69554824d17f9f1875f8bd84c6069c338
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-08 18:58:58 +0530

    changes

--- Problems/Perfect Squares/solution.py
+++ Problems/Perfect Squares/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def numSquares(self, n: int) -> int:
-        pass
+        q = deque()
+        sqrs = [i*i for i in range(1, int(n ** 0.5) + 1)][::-1]
+        q.append((n, 0))
+        vis = set()
+
+        while q:
+            p, step = q.popleft()
+            if p == 0:
+                return step
+            for sq in sqrs:
+                if (p - sq) >= 0 and (p - sq) not in vis:
+                    q.append((p - sq, step + 1))
+                    vis.add(p - sq)
+        return 0
 
 obj = Solution()
 #data = obj.numSquares(n = 12)
