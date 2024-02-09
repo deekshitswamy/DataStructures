cea34d187e131063706bf2c1340017089984e135
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-09 16:17:21 +0530

    Largest Divisible Subset

---
+++ Problems/Largest Divisible Subset/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.largestDivisibleSubset(nums = [1,2,3])
+data = obj.largestDivisibleSubset(nums = [1,2,4,8])
+print(data)
