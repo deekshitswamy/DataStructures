6c05de2bebcaaac11d8ab77f084f88f935a9adb5
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-09 16:17:55 +0530

    changes

--- Problems/Largest Divisible Subset/solution.py
+++ Problems/Largest Divisible Subset/solution.py
@@ -2,7 +2,16 @@ import io
 from typing import List
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-        pass
+        nums.sort()
+        n=len(nums)
+        if n<2:
+            return nums
+        ans=[ [num] for num in nums]   
+        for i in range(n):
+            for j in range(i):
+                if nums[i]%nums[j]==0 and len(ans[i])<len(ans[j])+1:
+                    ans[i]=ans[j]+[nums[i]]
+        return max(ans,key=lambda x:len(x))
 
 obj = Solution()
 #data = obj.largestDivisibleSubset(nums = [1,2,3])
