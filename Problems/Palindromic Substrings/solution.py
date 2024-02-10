605d388b9a901bef78a112d00583f9214abd6fa0
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-10 10:33:17 +0530

    changes

--- Problems/Palindromic Substrings/solution.py
+++ Problems/Palindromic Substrings/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        pass
+        n=len(s)
+        ans=0
+        for a in range(n):
+            i,j=a,a
+            while 0<=i<n and 0<=j<n and s[i]==s[j]:
+                ans+=1
+                i-=1
+                j+=1
+            i,j=a,a+1    
+            while 0<=i<n and 0<=j<n and s[i]==s[j]:
+                ans+=1
+                i-=1
+                j+=1
+        return ans
 
 obj = Solution()
 #data = obj.countSubstrings(s = "abc")
