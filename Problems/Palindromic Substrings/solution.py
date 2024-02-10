4b442095a6437a32cc910c81c1ab6009bbece2e3
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-10 10:32:31 +0530

    Palindromic Substrings

---
+++ Problems/Palindromic Substrings/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countSubstrings(s = "abc")
+data = obj.countSubstrings(s = "aaa")
+print(data)
