14d947e54d63f22c9cb3d827e2a7b3ca7c09a464
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-12 07:23:37 +0530

    changes

--- Problems/Majority Element/solution.py
+++ Problems/Majority Element/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        pass
+        #Moore's Voting Algorithm
+        count=0
+        result=0
+        for n in nums:
+            if count==0:
+                result=n
+            if n==result:
+                count+=1
+            else:
+                count-=1
+        return result
 
 obj = Solution()
 #data = obj.majorityElement(nums = [3,2,3])
