4d7733e20e563181648e206394c58fe37ca0b930
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-12 07:18:49 +0530

    changes

--- Problems/Cherry Pickup II/solution.py
+++ Problems/Cherry Pickup II/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
-        pass
+        m=len(grid)
+        n=len(grid[0])
+        @lru_cache(None)
+        def dp(i,j,k):
+            if not 0<=j<=k<n:
+                return -inf
+            if i==m:
+                return 0
+            ans=grid[i][j]+(j!=k)*grid[i][k]
+            return ans+max(dp(i+1,y,z) for y in (j-1,j,j+1) for z in (k-1,k,k+1))
+        return dp(0,0,n-1)
 
 obj = Solution()
 #data = obj.cherryPickup(grid = [[3,1,1],[2,5,1],[1,5,5],[2,1,1]])
