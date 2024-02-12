e45b9edda8ebf05c383b1726a522ae88e70fceaa
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-12 07:17:56 +0530

    Cherry Pickup II

---
+++ Problems/Cherry Pickup II/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def cherryPickup(self, grid: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.cherryPickup(grid = [[3,1,1],[2,5,1],[1,5,5],[2,1,1]])
+data = obj.cherryPickup(grid = [[1,0,0,0,0,0,1],[2,0,0,0,0,3,0],[2,0,9,0,0,0,0],[0,3,0,5,4,0,0],[1,0,2,3,0,0,6]])
+print(data)
