a219169baa8b08ca127ce21dfa9bc357bc75ac34
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-13 08:04:59 +0530

    Find First Palindromic String in the Array

---
+++ Problems/Find First Palindromic String in the Array/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        pass
+
+obj = Solution()
+#data = obj.firstPalindrome(words = ["abc","car","ada","racecar","cool"])
+#data = obj.firstPalindrome(words = ["notapalindrome","racecar"])
+data = obj.firstPalindrome(words = ["def","ghi"])
+print(data)
