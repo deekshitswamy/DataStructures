5e5dca771ed10464958074e713711c353e5e23cb
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-14 20:44:34 +0530

    Rearrange Array Elements by Sign

---
+++ Problems/Rearrange Array Elements by Sign/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.rearrangeArray(nums = [3,1,-2,-5,2,-4])
+data = obj.rearrangeArray(nums = [-1,1])
+print(data)
