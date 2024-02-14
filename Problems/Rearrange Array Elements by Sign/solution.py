1ac3c8ccbfa75f010613c7d6779946b689adeab5
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-14 20:45:21 +0530

    changes

--- Problems/Rearrange Array Elements by Sign/solution.py
+++ Problems/Rearrange Array Elements by Sign/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        pass
+        pos=[]
+        neg=[]
+        for num in nums:
+            if num<0:
+                neg.append(num)
+            else:
+                pos.append(num)
+        ans=[]
+        for a,b in zip(pos,neg):
+            ans.append(a)
+            ans.append(b)
+        return ans
 
 obj = Solution()
 #data = obj.rearrangeArray(nums = [3,1,-2,-5,2,-4])
