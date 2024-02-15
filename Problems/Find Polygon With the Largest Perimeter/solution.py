17e09a82e6ceb92817a02ba2cb1648ba88dd8fdd
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-15 22:02:21 +0530

    changes

---
+++ Problems/Find Polygon With the Largest Perimeter/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.largestPerimeter(nums = [5,5,5])
+#data = obj.largestPerimeter(nums = [1,12,1,2,5,50,3])
+data = obj.largestPerimeter(nums = [5,5,50])
+print(data)
