64647384f32281369a60e142a378365c764524f4
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-16 19:06:34 +0530

    Least Number of Unique Integers after K Removals

---
+++ Problems/Least Number of Unique Integers after K Removals/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findLeastNumOfUniqueInts(arr = [5,5,4], k = 1)
+data = obj.findLeastNumOfUniqueInts(arr = [4,3,1,1,3,3,2], k = 3)
+print(data)
