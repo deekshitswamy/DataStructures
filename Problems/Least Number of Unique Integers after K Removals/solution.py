fdb2e98c135e0b59c579f8c57ed39ac139970439
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-16 19:09:23 +0530

    changes

--- Problems/Least Number of Unique Integers after K Removals/solution.py
+++ Problems/Least Number of Unique Integers after K Removals/solution.py
@@ -2,7 +2,16 @@ import io
 from typing import List
 class Solution:
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
-        pass
+        freq_map = Counter(arr)
+        frequencies = list(freq_map.values())
+        frequencies.sort()
+
+        elements_removed = 0
+        for i in range(len(frequencies)):
+            elements_removed += frequencies[i]
+            if elements_removed > k:
+                return len(frequencies) - i
+        return 0
 
 obj = Solution()
 #data = obj.findLeastNumOfUniqueInts(arr = [5,5,4], k = 1)
