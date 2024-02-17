b4e538569d92d9e845235b3662a9a7ebbc24ffc5
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-17 12:07:14 +0530

    changes

--- Problems/Furthest Building You Can Reach/solution.py
+++ Problems/Furthest Building You Can Reach/solution.py
@@ -1,5 +1,6 @@
 import io
 from typing import List
+from heapq import heappop, heappush
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
         N = len(heights)
