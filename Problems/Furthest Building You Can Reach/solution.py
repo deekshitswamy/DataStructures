52e10cee41c93f634fc8cfcda232e7922ede8ed4
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-17 12:05:24 +0530

    Furthest Building You Can Reach

---
+++ Problems/Furthest Building You Can Reach/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.furthestBuilding(heights = [4,2,7,6,9,14,12], bricks = 5, ladders = 1)
+#data = obj.furthestBuilding(heights = [4,12,2,7,3,18,20,3,19], bricks = 10, ladders = 2)
+data = obj.furthestBuilding(heights = [14,3,19,3], bricks = 17, ladders = 0)
+print(data)
