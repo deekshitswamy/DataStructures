59983cbb0c507c22ba156a5486e6a2ac6341bb5f
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-17 12:06:23 +0530

    changes

--- Problems/Furthest Building You Can Reach/solution.py
+++ Problems/Furthest Building You Can Reach/solution.py
@@ -2,7 +2,25 @@ import io
 from typing import List
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
-        pass
+        N = len(heights)
+        jumps = []
+        bricks_used = 0
+
+        for i in range(N-1):
+            if heights[i] < heights[i+1]:
+                diff = heights[i+1] - heights[i]
+                if len(jumps) < ladders:
+                    heappush(jumps, diff)
+                else:
+                    if ladders > 0 and jumps[0] < diff:
+                        bricks_used += heappop(jumps)
+                        heappush(jumps, diff)
+                    else:
+                        bricks_used += diff
+                    if bricks_used > bricks:
+                        return i
+            
+        return N-1
 
 obj = Solution()
 #data = obj.furthestBuilding(heights = [4,2,7,6,9,14,12], bricks = 5, ladders = 1)
