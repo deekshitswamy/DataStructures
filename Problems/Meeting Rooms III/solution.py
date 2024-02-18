821e00bcff4df45331c4a7806efbceec4bf15d88
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-18 19:57:31 +0530

    Meeting Rooms III

---
+++ Problems/Meeting Rooms III/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.mostBooked(n = 2, meetings = [[0,10],[1,5],[2,7],[3,4]])
+data = obj.mostBooked(n = 3, meetings = [[1,20],[2,10],[3,5],[4,9],[6,8]])
+print(data)
