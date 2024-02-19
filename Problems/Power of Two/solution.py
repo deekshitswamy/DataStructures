a871c2f801b0b3de15a59fe6aaf6262c9297aa9d
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-19 18:12:36 +0530

    Power of Two

---
+++ Problems/Power of Two/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.isPowerOfTwo(n = 1)
+#data = obj.isPowerOfTwo(n = 16)
+data = obj.isPowerOfTwo(n = 2)
+print(data)
