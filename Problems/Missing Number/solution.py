38b3febdaca1dccbfb38211b00beb36716d9e849
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-20 20:02:47 +0530

    Missing Number

---
+++ Problems/Missing Number/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.missingNumber(nums = [3,0,1])
+#data = obj.missingNumber(nums = [0,1])
+data = obj.missingNumber(nums = [9,6,4,2,3,5,7,0,1])
+print(data)
