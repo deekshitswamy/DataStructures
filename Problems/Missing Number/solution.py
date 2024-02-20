8ae22cc3e8990caabe02930311668122a594d2bd
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-20 20:03:32 +0530

    changes

--- Problems/Missing Number/solution.py
+++ Problems/Missing Number/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        pass
+        left,right=0,len(nums)
+        nums.sort()
+        while left<=right:
+            mid=(left+right)//2
+            if mid>len(nums)-1:
+                return mid
+            elif mid!=nums[mid] and mid<len(nums):
+                right=mid-1
+            elif mid==nums[mid]:
+                left=mid+1
+        return left
 
 obj = Solution()
 #data = obj.missingNumber(nums = [3,0,1])
