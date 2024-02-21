a37c9b1f6a02860ef8fb358f9023e8804a9b16aa
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-21 19:21:27 +0530

    Bitwise AND of Numbers Range

---
+++ Problems/Bitwise AND of Numbers Range/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.rangeBitwiseAnd(left = 5, right = 7)
+#data = obj.rangeBitwiseAnd(left = 0, right = 0)
+data = obj.rangeBitwiseAnd(left = 1, right = 2147483647)
+print(data)
