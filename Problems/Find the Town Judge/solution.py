8f619d44b340a259c943d0aac172d841bb530327
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-22 18:35:29 +0530

    changes

--- Problems/Find the Town Judge/solution.py
+++ Problems/Find the Town Judge/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        pass
+        trusts_received = defaultdict(int)
+        trusts_given = defaultdict(int)
+        
+        for t in trust:
+            trusts_received[t[1]] += 1 
+            trusts_given[t[0]] += 1    
+
+        for person in range(1, n+1):
+            if trusts_received[person] == n - 1 and trusts_given[person] == 0:
+                return person
+        return -1
 
 obj = Solution()
 #data = obj.findJudge(n = 2, trust = [[1,2]])
