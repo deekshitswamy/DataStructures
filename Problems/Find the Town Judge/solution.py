e63c37b92c15774657936204d38825828ce38f2a
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-22 18:33:57 +0530

    Find the Town Judge

---
+++ Problems/Find the Town Judge/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findJudge(n = 2, trust = [[1,2]])
+#data = obj.findJudge(n = 3, trust = [[1,3],[2,3]])
+data = obj.findJudge(n = 3, trust = [[1,3],[2,3],[3,1]])
+print(data)
