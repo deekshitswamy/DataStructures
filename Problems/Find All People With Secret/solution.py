2400a96172e0dd6be887611fd5ce5c748efe6847
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-24 19:51:12 +0530

    changes

--- Problems/Find All People With Secret/solution.py
+++ Problems/Find All People With Secret/solution.py
@@ -2,7 +2,28 @@ import io
 from typing import List
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-        pass
+        meetings.sort(key=lambda x:x[2])
+        groups=itertools.groupby(meetings,key=lambda x:x[2])
+        ans={0,firstPerson}
+        for key,grp in groups:
+            res=set()
+            graph=defaultdict(list)
+            for a,b,t in grp:
+                graph[a].append(b)
+                graph[b].append(a)
+                if a in ans:
+                    res.add(a)
+                if b in ans:
+                    res.add(b)
+
+            seen=list(res)[::]
+            while seen:
+                node=seen.pop()
+                for i in graph[node]:
+                    if i not in ans:
+                        ans.add(i)
+                        seen.append(i)
+        return list(ans)
 
 obj = Solution()
 #data = obj.findAllPeople(n = 6, meetings = [[1,2,5],[2,3,8],[1,5,10]], firstPerson = 1)
