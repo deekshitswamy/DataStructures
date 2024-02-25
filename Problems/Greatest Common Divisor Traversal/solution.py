62e6c0d78569f0a828ad8776315da74fd32b3c60
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-25 16:28:52 +0530

    Greatest Common Divisor Traversal

---
+++ Problems/Greatest Common Divisor Traversal/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def canTraverseAllPairs(self, nums: List[int]) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.canTraverseAllPairs(nums = [2,3,6])
+#data = obj.canTraverseAllPairs(nums = [3,9,5])
+data = obj.canTraverseAllPairs(nums = [4,3,12,8])
+print(data)
