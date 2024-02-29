59731d42149a1c5d38fcd31214918d9ba97112c4
Deeku-Zeus <deekuzeus21@gmail.com>
2024-02-29 17:26:54 +0530

    changes

--- Problems/Even Odd Tree/solution.py
+++ Problems/Even Odd Tree/solution.py
@@ -8,7 +8,26 @@ class TreeNode:
         self.right = right
 class Solution:
     def isEvenOddTree(self, root: TreeNode) -> bool:
-        pass
+        level=0
+        curlevel=[root]
+        nextlevel=[]
+        while curlevel:
+            for i in range(len(curlevel)):
+                curr=curlevel[i]
+                if curr.left:
+                    nextlevel.append(curr.left)
+                if curr.right:
+                    nextlevel.append(curr.right)
+                if curr.val%2==level%2:
+                    return False
+                if i>0 and level%2==0 and curr.val<=curlevel[i-1].val:
+                    return False
+                if i>0 and level%2!=0 and curr.val>=curlevel[i-1].val:
+                    return False
+            curlevel=nextlevel
+            nextlevel=[]
+            level+=1                        
+        return True
 
 obj = Solution()
 #data = obj.isEvenOddTree(root = [1,10,4,3,null,7,9,12,8,6,null,null,2])
