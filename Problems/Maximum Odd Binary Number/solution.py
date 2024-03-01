fc3db4be0b3495e00799d10f90ef291a4b9d5fa8
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-01 17:18:50 +0530

    changes

--- Problems/Maximum Odd Binary Number/solution.py
+++ Problems/Maximum Odd Binary Number/solution.py
@@ -2,7 +2,15 @@ import io
 from typing import List
 class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
-        pass
+        one = 0
+        for i in s:
+            if(i is '1'):
+                one+=1
+
+        length = len(s)
+        remaining_len = length - one
+        result = '1'*(one-1) + '0'*remaining_len + '1'
+        return result
 
 obj = Solution()
 #data = obj.maximumOddBinaryNumber(s = "010")
