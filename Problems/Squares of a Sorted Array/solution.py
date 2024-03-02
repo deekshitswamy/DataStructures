fb5b1b9855ffa556c605eae1fe483b5a23f1fdfe
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-02 19:59:59 +0530

    Squares of a Sorted Array

---
+++ Problems/Squares of a Sorted Array/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.sortedSquares(nums = [-4,-1,0,3,10])
+data = obj.sortedSquares(nums = [-7,-3,2,3,11])
+print(data)
