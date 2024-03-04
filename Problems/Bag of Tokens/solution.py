0689f98e17f99bde972c3f92c59c0b2b50fc3902
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-04 19:18:21 +0530

    Bag of Tokens

---
+++ Problems/Bag of Tokens/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.bagOfTokensScore(tokens = [100], power = 50)
+#data = obj.bagOfTokensScore(tokens = [200,100], power = 150)
+data = obj.bagOfTokensScore(tokens = [100,200,300,400], power = 200)
+print(data)
