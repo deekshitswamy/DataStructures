874370b55b155292a13ab82cce8157111dac2a73
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-04 19:19:29 +0530

    changes

--- Problems/Bag of Tokens/solution.py
+++ Problems/Bag of Tokens/solution.py
@@ -2,7 +2,22 @@ import io
 from typing import List
 class Solution:
     def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
-        pass
+        q=deque(sorted(tokens))
+        cur=0
+        ans=0
+        while q:
+            if q[0]<=power:
+                power-=q.popleft()
+                cur+=1
+                ans=max(cur,ans)
+
+            elif cur>0:
+                power+=q.pop()
+                cur-=1
+
+            else:
+                break
+        return ans
 
 obj = Solution()
 #data = obj.bagOfTokensScore(tokens = [100], power = 50)
