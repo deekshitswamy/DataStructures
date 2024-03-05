4f8479dc85ebd5137852621394394039bd206543
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-05 23:03:47 +0530

    Minimum Length of String After Deleting Similar Ends

---
+++ Problems/Minimum Length of String After Deleting Similar Ends/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minimumLength(s = "ca")
+#data = obj.minimumLength(s = "cabaabac")
+data = obj.minimumLength(s = "aabccabba")
+print(data)
