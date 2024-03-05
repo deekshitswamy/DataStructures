cdabaa0b222569c9bdece5b380adb58cd3e83c93
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-05 23:04:23 +0530

    changes

--- Problems/Minimum Length of String After Deleting Similar Ends/solution.py
+++ Problems/Minimum Length of String After Deleting Similar Ends/solution.py
@@ -2,7 +2,14 @@ import io
 from typing import List
 class Solution:
     def minimumLength(self, s: str) -> int:
-        pass
+        ans=collections.deque(s)
+        while len(ans)>1 and ans[0]==ans[-1]:
+            cur=ans[0]
+            while len(ans)>0 and ans[0]==cur:
+                ans.popleft()
+            while len(ans)>0 and ans[-1]==cur:
+                ans.pop()
+        return len(ans)
 
 obj = Solution()
 #data = obj.minimumLength(s = "ca")
