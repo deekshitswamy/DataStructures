b1ecd48f22bd30614e64417f11ea4703656f9196
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-08 11:42:07 +0530

    Count Elements With Maximum Frequency

---
+++ Problems/Count Elements With Maximum Frequency/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxFrequencyElements(nums = [1,2,2,3,1,4])
+data = obj.maxFrequencyElements(nums = [1,2,3,4,5])
+print(data)
