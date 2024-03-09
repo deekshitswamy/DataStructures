fe9ac83102da699f48dbbf52c3784ad22c3279e4
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-09 19:53:23 +0530

    Minimum Common Value

---
+++ Problems/Minimum Common Value/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.getCommon(nums1 = [1,2,3], nums2 = [2,4])
+data = obj.getCommon(nums1 = [1,2,3,6], nums2 = [2,3,4,5])
+print(data)
