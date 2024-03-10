31c9ebaa75a79b52eb4bf96cb0cf30736054b377
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-10 19:04:04 +0530

    changes

--- Problems/Intersection of Two Arrays/solution.py
+++ Problems/Intersection of Two Arrays/solution.py
@@ -2,7 +2,27 @@ import io
 from typing import List
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return list(set(nums1) & set(nums2))
+        nums1.sort()
+        nums2.sort()
+
+        result = list()
+
+        n1_ptr = n2_ptr = 0
+        while n1_ptr < len(nums1) and n2_ptr < len(nums2):
+            if nums1[n1_ptr] < nums2[n2_ptr]:
+                n1_ptr += 1
+
+            elif nums1[n1_ptr] > nums2[n2_ptr]:
+                n2_ptr += 1
+
+            else:
+                if nums1[n1_ptr] not in result:
+                    result.append(nums1[n1_ptr])
+
+                n1_ptr += 1
+                n2_ptr += 1
+        
+        return result
 
 obj = Solution()
 #data = obj.intersection(n = 1)
