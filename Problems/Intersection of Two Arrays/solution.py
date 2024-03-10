c59829a94f62f83cfa8a98f7970a2e2dd0bd96e5
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-10 19:02:47 +0530

    Intersection of Two Arrays

---
+++ Problems/Intersection of Two Arrays/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.intersection(n = 1)
+#data = obj.intersection(n = 2)
+data = obj.intersection(n = 2)
+print(data)
