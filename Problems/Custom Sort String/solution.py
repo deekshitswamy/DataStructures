06f93c34ef694392c7d12c888cc7e869e57d4713
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-11 18:13:17 +0530

    changes

--- Problems/Custom Sort String/solution.py
+++ Problems/Custom Sort String/solution.py
@@ -2,7 +2,12 @@ import io
 from typing import List
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        pass
+        ans=defaultdict(int)
+        i=0
+        for c in order:
+            ans[c]=i
+            i+=1
+        return "".join(sorted(s,key=lambda x:ans.get(x,100)))
 
 obj = Solution()
 #data = obj.customSortString(order = "cba", s = "abcd")
