c0e1ab801a8ccedad1caf62132be1b9d5c44ba50
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-11 18:12:33 +0530

    Custom Sort String

---
+++ Problems/Custom Sort String/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.customSortString(order = "cba", s = "abcd")
+data = obj.customSortString(order = "bcafg", s = "abcd")
+print(data)
