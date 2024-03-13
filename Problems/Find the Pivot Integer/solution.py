4b971698d7991c61cbcf2ed93788d464195dfa2c
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-13 18:53:23 +0530

    Find the Pivot Integer

---
+++ Problems/Find the Pivot Integer/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.pivotInteger(n = 8)
+#data = obj.pivotInteger(n = 1)
+data = obj.pivotInteger(n = 4)
+print(data)
