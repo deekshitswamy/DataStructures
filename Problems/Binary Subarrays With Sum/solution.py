f0d619a8c405162db08f3be570d3c151a4a81170
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-14 18:18:55 +0530

    Binary Subarrays With Sum

---
+++ Problems/Binary Subarrays With Sum/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numSubarraysWithSum(nums = [1,0,1,0,1], goal = 2)
+data = obj.numSubarraysWithSum(nums = [0,0,0,0,0], goal = 0)
+print(data)
