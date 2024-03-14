314eccd75e767455d54f666c5c401380e0ba8fa2
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-14 18:19:40 +0530

    changes

--- Problems/Binary Subarrays With Sum/solution.py
+++ Problems/Binary Subarrays With Sum/solution.py
@@ -2,7 +2,14 @@ import io
 from typing import List
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        pass
+        sumsMap,s,cnt={0:1},0,0
+        for a in nums:
+            s+=a
+            if s-goal in sumsMap:
+                cnt+=sumsMap[s-goal]
+            sumsMap[s]=sumsMap.get(s,0)+1
+            
+        return cnt
 
 obj = Solution()
 #data = obj.numSubarraysWithSum(nums = [1,0,1,0,1], goal = 2)
