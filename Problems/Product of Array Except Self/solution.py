753d09fbd303bed8c5e777262ba36cc141123bfe
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-16 00:42:04 +0530

    changes

--- Problems/Product of Array Except Self/solution.py
+++ Problems/Product of Array Except Self/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        pass
+        n=len(nums)
+        ans=[1]*n
+        prefix=1
+        for i in  range(n):
+            ans[i]=prefix
+            prefix*=nums[i]
+        postfix=1
+        for i in range(n-1,-1,-1):
+            ans[i]*=postfix
+            postfix*=nums[i]
+        return ans
 
 obj = Solution()
 #data = obj.productExceptSelf(nums = [1,2,3,4])
