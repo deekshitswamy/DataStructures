0432fb34effa6bcd66ed65235cbe9a259daf9f9e
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-16 00:41:29 +0530

    Product of Array Except Self

---
+++ Problems/Product of Array Except Self/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.productExceptSelf(nums = [1,2,3,4])
+data = obj.productExceptSelf(nums = [-1,1,0,-3,3])
+print(data)
