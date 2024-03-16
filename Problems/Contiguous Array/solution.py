a89365a2a5c4aff465a3146f72ac2d1932235e75
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-16 18:30:55 +0530

    Contiguous Array

---
+++ Problems/Contiguous Array/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findMaxLength(nums = [0,1])
+data = obj.findMaxLength(nums = [0,1,0])
+print(data)
