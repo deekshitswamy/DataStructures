dd7fcc7acf8f05a6c35f30f235cce6e534dc3dd9
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-16 18:31:33 +0530

    changes

--- Problems/Contiguous Array/solution.py
+++ Problems/Contiguous Array/solution.py
@@ -2,7 +2,22 @@ import io
 from typing import List
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        pass
+        lookup={0:-1}
+        ans=0 #max_len
+        cur_sum=0
+        n=len(nums)
+        for i in range(n):
+            if nums[i]==0:
+                cur_sum-=1
+            else:
+                cur_sum+=1  
+            if cur_sum==0:
+                ans=i+1
+            elif cur_sum in lookup:
+                ans=max(ans,i-lookup[cur_sum])
+            else:
+                lookup[cur_sum]=i
+        return ans
 
 obj = Solution()
 #data = obj.findMaxLength(nums = [0,1])
