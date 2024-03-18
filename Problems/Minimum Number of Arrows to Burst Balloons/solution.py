485a473cd33eb5b86e50b569a94cbd67f3ce107d
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-18 20:43:58 +0530

    Minimum Number of Arrows to Burst Balloons

---
+++ Problems/Minimum Number of Arrows to Burst Balloons/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findMinArrowShots(points = [[10,16],[2,8],[1,6],[7,12]])
+#data = obj.findMinArrowShots(points = [[1,2],[3,4],[5,6],[7,8]])
+data = obj.findMinArrowShots(points = [[1,2],[2,3],[3,4],[4,5]])
+print(data)
