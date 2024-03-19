c849ee7e44bf4caf8ef6de74157bad04bf85832b
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-19 21:21:04 +0530

    Task Scheduler

---
+++ Problems/Task Scheduler/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.leastInterval(tasks = ["A","A","A","B","B","B"], n = 2)
+#data = obj.leastInterval(tasks = ["A","C","A","B","D","B"], n = 1)
+data = obj.leastInterval(tasks = ["A","A","A", "B","B","B"], n = 3)
+print(data)
