6cf1d4c2500868cfde56c6fa71458b47fab58221
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-19 21:22:28 +0530

    changes

--- Problems/Task Scheduler/solution.py
+++ Problems/Task Scheduler/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        pass
+        ans=[0]*26
+        for task in tasks:
+            ans[ord(task)-ord('A')]+=1
+        ans.sort()
+        chunk=ans[25]-1
+        idle=chunk*n
+        for i in range(24,-1,-1):
+            idle-=min(chunk,ans[i])
+        if idle>=0:
+            return len(tasks)+idle
+        else:
+            return len(tasks)
 
 obj = Solution()
 #data = obj.leastInterval(tasks = ["A","A","A","B","B","B"], n = 2)
