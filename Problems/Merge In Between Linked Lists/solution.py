8501c3bedf4c5d171ac22ff403c61b77d134b9ca
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-20 07:38:48 +0530

    changes

--- Problems/Merge In Between Linked Lists/solution.py
+++ Problems/Merge In Between Linked Lists/solution.py
@@ -7,7 +7,27 @@ class ListNode:
         self.next = next
 class Solution:
     def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
-        pass
+        curr=list1
+        t=0
+        var=None
+        while curr!=None:
+            t+=1
+            if t==b+1:
+                break
+            if a==t:
+                pp=curr.next
+                var=curr
+                var.next=list2
+                # curr.next=None
+                curr=pp
+            curr=curr.next
+        temp=list1
+        while temp!=None:
+            if temp.next==None:
+                break
+            temp=temp.next
+        temp.next=curr
+        return list1
 
 
 obj = Solution()
