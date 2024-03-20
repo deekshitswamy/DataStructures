8651108051441ae7a767aedc33ea8d080a1ab9f5
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-20 07:38:00 +0530

    Merge In Between Linked Lists

---
+++ Problems/Merge In Between Linked Lists/solution.py
@@ -0,0 +1,16 @@
+import io
+from typing import List
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
+        pass
+
+
+obj = Solution()
+#data = obj.testfunc(list1 = [10,1,13,6,9,5], a = 3, b = 4, list2 = [1000000,1000001,1000002])
+data = obj.testfunc(list1 = [0,1,2,3,4,5,6], a = 2, b = 5, list2 = [1000000,1000001,1000002,1000003,1000004])
+print(data)
