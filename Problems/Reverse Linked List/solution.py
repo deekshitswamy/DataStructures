f13949d519168af66358d26424872a9cec185d43
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-21 21:41:03 +0530

    Reverse Linked List

---
+++ Problems/Reverse Linked List/solution.py
@@ -0,0 +1,16 @@
+import io
+from typing import List
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        pass
+
+obj = Solution()
+#data = obj.reverseList(head = [1,2,3,4,5])
+#data = obj.reverseList(head = [1,2])
+data = obj.reverseList(head = [])
+print(data)
