1afbd69862154419037058fc1f823721466fa9fd
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-23 18:30:39 +0530

    Reorder List

---
+++ Problems/Reorder List/solution.py
@@ -0,0 +1,15 @@
+import io
+from typing import List
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        pass
+
+obj = Solution()
+#data = obj.reorderList(head = [1,2,3,4])
+data = obj.reorderList(head = [1,2,3,4,5])
+print(data)
