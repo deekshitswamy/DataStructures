c7df5d2f16047aa8005cc106e6d74565cf9def79
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-25 20:42:41 +0530

    Find All Duplicates in an Array

---
+++ Problems/Find All Duplicates in an Array/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.findDuplicates(nums = [4,3,2,7,8,2,3,1])
+#data = obj.findDuplicates(nums = [1,1,2])
+data = obj.findDuplicates(nums = [1])
+print(data)
