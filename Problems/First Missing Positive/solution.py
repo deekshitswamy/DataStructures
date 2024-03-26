f3f76af9ffaecf0fe082ccdcf455296a60fc9a9a
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-26 22:27:25 +0530

    changes

--- Problems/First Missing Positive/solution.py
+++ Problems/First Missing Positive/solution.py
@@ -2,7 +2,13 @@ import io
 from typing import List
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        pass
+        nums=set(nums)
+        n=len(nums)
+
+        for i in range(1,n+1):
+            if i not in nums:
+                return i
+        return n+1
 
 obj = Solution()
 #data = obj.firstMissingPositive(nums = [1,2,0])
