0d35e150c03606c4829743daf5d0e992f2194303
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-27 19:28:44 +0530

    Subarray Product Less Than K

---
+++ Problems/Subarray Product Less Than K/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numSubarrayProductLessThanK(nums = [10,5,2,6], k = 100)
+data = obj.numSubarrayProductLessThanK(nums = [1,2,3], k = 0)
+print(data)
