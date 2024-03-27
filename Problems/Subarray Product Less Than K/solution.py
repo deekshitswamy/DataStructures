990596bf51e2a5cfe88480b56ba52e1cd4e99af4
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-27 19:30:05 +0530

    changes

--- Problems/Subarray Product Less Than K/solution.py
+++ Problems/Subarray Product Less Than K/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        pass
+        if k<=1:
+            return 0
+        product=1
+        ans=0
+        left=0
+        n=len(nums)
+        for right in range(n):
+            product*=nums[right]
+            if product>=k:
+                while product>=k and left<=right:
+                    product/=nums[left]
+                    left+=1
+            ans+=right-left+1
+        return ans
 
 obj = Solution()
 #data = obj.numSubarrayProductLessThanK(nums = [10,5,2,6], k = 100)
