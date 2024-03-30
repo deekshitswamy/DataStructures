239fe98b9c66ffdb3143921e62da8aaf2e5fdb29
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-30 12:04:31 +0530

    Subarrays with K Different Integers

---
+++ Problems/Subarrays with K Different Integers/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.subarraysWithKDistinct(nums = [1,2,1,2,3], k = 2)
+data = obj.subarraysWithKDistinct(nums = [1,2,1,3,4], k = 3)
+print(data)
