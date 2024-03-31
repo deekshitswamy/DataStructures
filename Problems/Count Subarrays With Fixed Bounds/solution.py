f114869816115ced2ff420d47cff32c670c7512d
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-31 18:25:56 +0530

    Count Subarrays With Fixed Bounds

---
+++ Problems/Count Subarrays With Fixed Bounds/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countSubarrays(nums = [1,3,5,2,7,5], minK = 1, maxK = 5)
+data = obj.countSubarrays(nums = [1,1,1,1], minK = 1, maxK = 1)
+print(data)
