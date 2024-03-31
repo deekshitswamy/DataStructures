4285c7b8273fa7ab74084e94fbec76635d9a13fa
Deeku-Zeus <deekuzeus21@gmail.com>
2024-03-31 18:28:11 +0530

    changes

--- Problems/Count Subarrays With Fixed Bounds/solution.py
+++ Problems/Count Subarrays With Fixed Bounds/solution.py
@@ -2,7 +2,31 @@ import io
 from typing import List
 class Solution:
     def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
-        pass
+        cnt = 0
+        last_minK_pos, last_maxK_pos = None, None
+        last_violating_position = -1
+
+        for i, num in enumerate(nums):
+            if num > maxK or num < minK:
+                last_minK_pos, last_maxK_pos = None, None
+                last_violating_position = i
+            elif minK < num and num < maxK:
+                if last_minK_pos is None or last_maxK_pos is None:
+                    pass
+                else:
+                    last = min(last_minK_pos, last_maxK_pos) - last_violating_position
+                    cnt += last
+            else:
+                if num == maxK:
+                    last_maxK_pos = i
+                if num == minK:
+                    last_minK_pos = i
+
+                if last_minK_pos is not None and last_maxK_pos is not None:
+                    last = min(last_minK_pos, last_maxK_pos) - last_violating_position
+                    cnt += last
+ 
+        return cnt
 
 obj = Solution()
 #data = obj.countSubarrays(nums = [1,3,5,2,7,5], minK = 1, maxK = 5)
