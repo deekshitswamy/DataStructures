c9a5a5ef293f9942b55cddc8a16cd591f3e2c579
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-01 19:05:46 +0530

    Length of Last Word

---
+++ Problems/Length of Last Word/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.lengthOfLastWord(s = "Hello World")
+#data = obj.lengthOfLastWord(s = "   fly me   to   the moon  ")
+data = obj.lengthOfLastWord(s = "luffy is still joyboy")
+print(data)
