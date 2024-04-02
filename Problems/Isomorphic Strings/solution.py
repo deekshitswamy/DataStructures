b8de1804ad7dfc346a0c87a5010253e296581e2c
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-02 20:01:32 +0530

    changes

--- Problems/Isomorphic Strings/solution.py
+++ Problems/Isomorphic Strings/solution.py
@@ -2,7 +2,10 @@ import io
 from typing import List
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        pass
+        n=len(set(s))
+        m=len(set(t))
+        z=len(set(zip(s,t)))
+        return n==m==z
 
 obj = Solution()
 #data = obj.isIsomorphic(s = "egg", t = "add")
