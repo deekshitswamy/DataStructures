d032cd2c1313b94fb35857c6a6f1e22d36e752b6
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-02 20:00:32 +0530

    Isomorphic Strings

---
+++ Problems/Isomorphic Strings/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.isIsomorphic(s = "egg", t = "add")
+#data = obj.isIsomorphic(s = "foo", t = "bar")
+data = obj.isIsomorphic(s = "paper", t = "title")
+print(data)
