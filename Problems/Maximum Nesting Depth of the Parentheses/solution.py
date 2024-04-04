bfa1be259b228f65df9023139a4f05438ecfa6f2
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-04 22:49:26 +0530

    changes

--- Problems/Maximum Nesting Depth of the Parentheses/solution.py
+++ Problems/Maximum Nesting Depth of the Parentheses/solution.py
@@ -2,7 +2,15 @@ import io
 from typing import List
 class Solution:
     def maxDepth(self, s: str) -> int:
-        pass
+        ans=0
+        depth=0
+        for x in s:
+            if x=="(":
+                depth+=1
+                ans=max(depth,ans)
+            elif x==")":
+                depth-=1
+        return ans
 
 obj = Solution()
 #data = obj.maxDepth(s = "(1+(2*3)+((8)/4))+1")
