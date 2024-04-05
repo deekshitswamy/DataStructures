c9009fc88a90a3d1ecaf9d4644d051a04f25a506
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-05 21:48:21 +0530

    Make The String Great

---
+++ Problems/Make The String Great/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def makeGood(self, s: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.makeGood(s = "leEeetcode")
+#data = obj.makeGood(s = "leEeetcode")
+data = obj.makeGood(s = "s")
+print(data)
