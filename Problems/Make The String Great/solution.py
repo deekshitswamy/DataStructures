3023d09e17a2e38601868728785d1f195da22145
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-05 21:50:33 +0530

    changes

--- Problems/Make The String Great/solution.py
+++ Problems/Make The String Great/solution.py
@@ -2,7 +2,21 @@ import io
 from typing import List
 class Solution:
     def makeGood(self, s: str) -> str:
-        pass
+        change=True
+        sr=list(s)
+        
+        while change and len(sr):
+            i=0
+            c=0
+            while i<len(sr)-1 :
+                if ord(sr[i])==ord(sr[i+1])+32 or ord(sr[i])==ord(sr[i+1])-32 :
+                    del sr[i:i+2]
+                    c+=1
+                i+=1
+            if c==0:
+                change =False
+                    
+        return ''.join(sr)
 
 obj = Solution()
 #data = obj.makeGood(s = "leEeetcode")
