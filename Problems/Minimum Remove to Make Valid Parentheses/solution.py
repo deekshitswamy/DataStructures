916ca80d545405143d1ba713e05cde7b79a67dd5
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-06 23:27:13 +0530

    changes

--- Problems/Minimum Remove to Make Valid Parentheses/solution.py
+++ Problems/Minimum Remove to Make Valid Parentheses/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        pass
+        ans=[]
+        n=len(s)
+        S=list(s)
+        for i in range(n):
+            if S[i]=="(":
+                ans.append(i)
+            elif S[i]==")":
+                if ans:
+                    ans.pop()
+                else:
+                    S[i]=""
+        for j in ans:
+            S[j]=""
+        return "".join(S)
 
 obj = Solution()
 #data = obj.minRemoveToMakeValid(s = "lee(t(c)o)de)")
