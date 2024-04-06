476406ab3a1698dcc94cc056f5706e59248a221c
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-06 23:26:38 +0530

    Minimum Remove to Make Valid Parentheses

---
+++ Problems/Minimum Remove to Make Valid Parentheses/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.minRemoveToMakeValid(s = "lee(t(c)o)de)")
+#data = obj.minRemoveToMakeValid(s = "a)b(c)d")
+data = obj.minRemoveToMakeValid(s = "))((")
+print(data)
