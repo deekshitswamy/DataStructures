c67676bbf874d2cc3224bd4a58d356addc943948
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-07 20:56:58 +0530

    Valid Parenthesis String

---
+++ Problems/Valid Parenthesis String/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.checkValidString(s = "()")
+#data = obj.checkValidString(s = "(*)")
+data = obj.checkValidString(s = "(*))")
+print(data)
