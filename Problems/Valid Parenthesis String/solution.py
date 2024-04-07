8b539a5233a6dbe96da36f16b425d867116e5a3b
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-07 20:57:37 +0530

    changes

--- Problems/Valid Parenthesis String/solution.py
+++ Problems/Valid Parenthesis String/solution.py
@@ -2,7 +2,22 @@ import io
 from typing import List
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        pass
+        leftmin=leftmax=0
+        for c in s:
+            if c=="(":
+                leftmin+=1
+                leftmax+=1
+            elif c==")":
+                leftmin-=1
+                leftmax-=1
+            else:
+                leftmin-=1
+                leftmax+=1
+            if leftmax<0:
+                return False
+            if leftmin<0:
+                leftmin=0
+        return leftmin==0
 
 obj = Solution()
 #data = obj.checkValidString(s = "()")
