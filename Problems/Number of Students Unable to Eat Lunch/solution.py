f57d354afd5a67509a5d3c05f8a954917d61e7d8
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-08 23:54:36 +0530

    changes

--- Problems/Number of Students Unable to Eat Lunch/solution.py
+++ Problems/Number of Students Unable to Eat Lunch/solution.py
@@ -2,7 +2,16 @@ import io
 from typing import List
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        pass
+        ans=0
+        while len(students)>ans:
+            if students[0]==sandwiches[0]:
+                sandwiches.pop(0)
+                ans=0
+            else:
+                students.append(students[0])
+                ans+=1
+            students.pop(0)
+        return len(students)
 
 obj = Solution()
 #data = obj.countStudents(students = [1,1,0,0], sandwiches = [0,1,0,1])
