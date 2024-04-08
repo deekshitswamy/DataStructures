129685edcec866fe061510203f426cb9201af9d1
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-08 23:53:32 +0530

    Number of Students Unable to Eat Lunch

---
+++ Problems/Number of Students Unable to Eat Lunch/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countStudents(students = [1,1,0,0], sandwiches = [0,1,0,1])
+data = obj.countStudents(students = [1,1,1,0,0,1], sandwiches = [1,0,0,0,1,1])
+print(data)
