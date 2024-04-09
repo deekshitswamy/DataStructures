0604074fa08ec93d2e128da141f06a6ab5ab7b09
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-09 22:41:57 +0530

    changes

--- Problems/Time Needed to Buy Tickets/solution.py
+++ Problems/Time Needed to Buy Tickets/solution.py
@@ -2,7 +2,15 @@ import io
 from typing import List
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        pass
+        total=0
+        for i,x in enumerate(tickets):
+            if i<=k:
+                total+=min(tickets[i],tickets[k])
+                
+            else:
+                total+=min(tickets[i],tickets[k]-1)
+                
+        return total
 
 obj = Solution()
 #data = obj.timeRequiredToBuy(tickets = [2,3,2], k = 2)
