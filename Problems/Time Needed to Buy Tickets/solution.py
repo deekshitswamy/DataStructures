17384551bbd383997417ec9107ea4d68411596ff
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-09 22:41:07 +0530

    Time Needed to Buy Tickets

---
+++ Problems/Time Needed to Buy Tickets/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.timeRequiredToBuy(tickets = [2,3,2], k = 2)
+data = obj.timeRequiredToBuy(tickets = [5,1,1,1], k = 0)
+print(data)
