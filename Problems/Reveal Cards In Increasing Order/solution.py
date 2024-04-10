17c699e02543a7c741e4284d5793802a84e57a04
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-10 23:50:06 +0530

    Reveal Cards In Increasing Order

---
+++ Problems/Reveal Cards In Increasing Order/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.deckRevealedIncreasing(deck = [17,13,11,2,3,5,7])
+data = obj.deckRevealedIncreasing(deck = [1,1000])
+print(data)
