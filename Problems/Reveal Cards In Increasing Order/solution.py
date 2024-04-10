d210f17c5cce6eca4710937083efe9d7a78d5efe
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-10 23:51:12 +0530

    changes

--- Problems/Reveal Cards In Increasing Order/solution.py
+++ Problems/Reveal Cards In Increasing Order/solution.py
@@ -2,7 +2,11 @@ import io
 from typing import List
 class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
-        pass
+        ans=[]
+        deck.sort(reverse=True)
+        for i in deck:
+            ans=[i]+ans[-1:]+ans[:-1]
+        return ans
 
 obj = Solution()
 #data = obj.deckRevealedIncreasing(deck = [17,13,11,2,3,5,7])
