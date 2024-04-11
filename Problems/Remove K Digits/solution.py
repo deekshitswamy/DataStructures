fb6f7d2b17a641a6cabfc980ab6e4748e8e04a0d
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-11 18:31:07 +0530

    changes

--- Problems/Remove K Digits/solution.py
+++ Problems/Remove K Digits/solution.py
@@ -2,7 +2,19 @@ import io
 from typing import List
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
-        pass
+        ans=[]
+        for i in num:
+            while k>0 and ans and ans[-1]>i:
+                
+                ans.pop()
+                k-=1
+            ans.append(i)
+        while k>0:
+            ans.pop()
+            k-=1    
+        
+        res="".join(ans).lstrip("0")
+        return res if res else "0"
 
 obj = Solution()
 #data = obj.removeKdigits(num = "1432219", k = 3)
