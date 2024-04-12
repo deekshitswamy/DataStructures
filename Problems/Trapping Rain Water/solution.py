82ece3d6562684511029ffeedb3f264f47c7c81a
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-12 20:34:31 +0530

    changes

--- Problems/Trapping Rain Water/solution.py
+++ Problems/Trapping Rain Water/solution.py
@@ -2,7 +2,21 @@ import io
 from typing import List
 class Solution:
     def trap(self, height: List[int]) -> int:
-        pass
+        if not height:
+            return 0
+        l,r=0,len(height)-1
+        leftmax , rightmax =height[l],height[r]
+        water=0
+        while l<r:
+            if leftmax<rightmax:
+                l+=1
+                leftmax=max(leftmax,height[l])
+                water+=leftmax-height[l]
+            else:
+                r-=1
+                rightmax=max(rightmax,height[r])
+                water+=rightmax-height[r]
+        return water
 
 obj = Solution()
 #data = obj.trap(height = [0,1,0,2,1,0,1,3,2,1,2,1])
