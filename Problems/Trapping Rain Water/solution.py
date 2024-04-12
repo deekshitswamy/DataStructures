888597fa53207e678e13e4125f9f53d677d270dd
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-12 20:33:37 +0530

    Trapping Rain Water

---
+++ Problems/Trapping Rain Water/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.trap(height = [0,1,0,2,1,0,1,3,2,1,2,1])
+data = obj.trap(height = [4,2,0,3,2,5])
+print(data)
