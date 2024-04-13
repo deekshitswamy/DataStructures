787173b063958280af5a3eee90b810ec52c5644f
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-13 21:15:15 +0530

    Maximal Rectangle

---
+++ Problems/Maximal Rectangle/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maximalRectangle(matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]])
+#data = obj.maximalRectangle(matrix = [["0"]])
+data = obj.maximalRectangle(matrix = [["1"]])
+print(data)
