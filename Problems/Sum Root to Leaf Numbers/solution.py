0f29de914dadf9faef9dbdf5c9c3db3a2f71febf
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-15 16:28:14 +0530

    changes

--- Problems/Sum Root to Leaf Numbers/solution.py
+++ Problems/Sum Root to Leaf Numbers/solution.py
@@ -10,7 +10,17 @@ class TreeNode:
 
 class Solution:
     def sumNumbers(self, root: TreeNode) -> int:
-        pass
+        def dfs(cur,num):
+            if not cur:return 0
+            
+            num=num*10+cur.val
+            
+            if not cur.left and not cur.right:
+                return num
+            
+            return dfs(cur.left,num)+dfs(cur.right,num)
+        
+        return dfs(root,0)
 
 obj = Solution()
 #data = obj.sumNumbers(root = [1,2,3])
