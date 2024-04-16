6c085607e1ef3b5357115e73c366052f1da3dad4
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-16 19:50:06 +0530

    changes

--- Problems/Add One Row to Tree/solution.py
+++ Problems/Add One Row to Tree/solution.py
@@ -9,7 +9,25 @@ class TreeNode:
 
 class Solution:
     def addOneRow(self, root: TreeNode, val: int, depth: int) -> TreeNode:
-        pass
+        if depth ==1:
+            r = root
+            root = TreeNode(val,left=r)
+            return root
+            
+        q = [(root,1)]
+        while q:
+            node,cur_depth = q.pop(0)
+            if cur_depth == depth-1:
+                left = node.left
+                right=node.right
+                node.left = TreeNode(val,left=left)
+                node.right = TreeNode(val,right=right)
+            else:
+                if node.left:
+                    q.append((node.left,cur_depth+1))
+                if node.right:
+                    q.append((node.right,cur_depth+1))
+        return root
 
 obj = Solution()
 #data = obj.addOneRow(root = [4,2,6,3,1,5], val = 1, depth = 2)
