6d26630ad1a701f86c6f2d912fe0d4d07330a132
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-17 18:15:10 +0530

    changes

--- Problems/Smallest String Starting From Leaf/solution.py
+++ Problems/Smallest String Starting From Leaf/solution.py
@@ -9,7 +9,18 @@ class TreeNode:
 
 class Solution:
     def smallestFromLeaf(self, root: TreeNode) -> str:
-        pass
+        self.ans="~"
+        self.traverse(root,[])
+        return self.ans
+    def traverse(self,root,path):
+        if not root:
+            return 
+        path.append(chr(root.val+97))
+        if not root.left and not root.right:
+            self.ans=min(self.ans,"".join(reversed(path)))
+        self.traverse(root.left,path)
+        self.traverse(root.right,path)
+        path.pop()
 
 obj = Solution()
 #data = obj.smallestFromLeaf(root = [0,1,2,3,4,3,4])
