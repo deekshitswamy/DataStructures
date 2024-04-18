a2cdc1a397e18f0bb951bc46cb5e9ef817855649
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-18 21:46:24 +0530

    Island Perimeter

---
+++ Problems/Island Perimeter/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.islandPerimeter(grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]])
+#data = obj.islandPerimeter(grid = [[1]])
+data = obj.islandPerimeter(grid = [[1,0]])
+print(data)
