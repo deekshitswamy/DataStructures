3df797bc0452882103b296ff15046c6a37a8ea41
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-20 22:17:20 +0530

    changes

--- Problems/Find All Groups of Farmland/solution.py
+++ Problems/Find All Groups of Farmland/solution.py
@@ -2,7 +2,26 @@ import io
 from typing import List
 class Solution:
     def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
-        pass
+        n=len(land)
+        m=len(land[0])
+        ans=[]
+        for i in range(n):
+            for j in range(m):
+                if land[i][j]:
+                    min_i,min_j=i,j
+                    max_i,max_j=i,j
+                    stack=[(i,j)]
+                    land[i][j]=0
+                    while stack:
+                        i,j=stack.pop()
+                        for x,y in (i-1,j),(i,j-1),(i,j+1),(i+1,j):
+                            if 0<=x<n and 0<=y<m and land[x][y]:
+                                stack.append((x,y))
+                                land[x][y]=0
+                                max_i=max(max_i,x)
+                                max_j=max(max_j,y)
+                    ans.append([min_i,min_j,max_i,max_j])
+        return ans
 
 obj = Solution()
 #data = obj.findFarmland(land = [[1,0,0],[0,1,1],[0,1,1]])
