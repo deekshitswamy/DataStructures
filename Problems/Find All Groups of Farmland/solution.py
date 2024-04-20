28f954104096fb26977de7dfabd906864b445ed8
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-20 22:16:41 +0530

    Find All Groups of Farmland

---
+++ Problems/Find All Groups of Farmland/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
+        pass
+
+obj = Solution()
+#data = obj.findFarmland(land = [[1,0,0],[0,1,1],[0,1,1]])
+#data = obj.findFarmland(land = [[1,1],[1,1]])
+data = obj.findFarmland(land = [[0]])
+print(data)
