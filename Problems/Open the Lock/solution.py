4910ff6ce240543345289a1bfa10e62c9b48a39f
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-22 20:16:44 +0530

    Open the Lock

---
+++ Problems/Open the Lock/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.openLock(deadends = ["0201","0101","0102","1212","2002"], target = "0202")
+#data = obj.openLock(deadends = ["8888"], target = "0009")
+data = obj.openLock(deadends = ["8887","8889","8878","8898","8788","8988","7888","9888"], target = "8888")
+print(data)
