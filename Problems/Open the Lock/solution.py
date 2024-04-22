8b10c74597d5048026c39b32a739d6911a4eb473
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-22 20:17:24 +0530

    changes

--- Problems/Open the Lock/solution.py
+++ Problems/Open the Lock/solution.py
@@ -2,7 +2,22 @@ import io
 from typing import List
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
-        pass
+        deadends=set(deadends)
+        if "0000" in deadends:
+            return -1
+        q=deque([("0000",0)])
+        visited=set()
+        while q:
+            cand,steps=q.popleft()
+            if cand==target:
+                return steps
+            for i in range(4):
+                for digit in [((int(cand[i])+1)%10),((int(cand[i])-1)%10)]:
+                    nx=cand[:i]+str(digit)+cand[1+i:]
+                    if nx not in deadends and nx not in visited:
+                        visited.add(nx)
+                        q.append((nx,steps+1))
+        return -1
 
 obj = Solution()
 #data = obj.openLock(deadends = ["0201","0101","0102","1212","2002"], target = "0202")
