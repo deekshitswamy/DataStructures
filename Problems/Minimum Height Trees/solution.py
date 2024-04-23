95af6767a08db156d0dc23326df62aaa51ad56b7
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-23 20:02:45 +0530

    Minimum Height Trees

---
+++ Problems/Minimum Height Trees/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.findMinHeightTrees(n = 4, edges = [[1,0],[1,2],[1,3]])
+data = obj.findMinHeightTrees(n = 6, edges = [[3,0],[3,1],[3,2],[3,4],[5,4]])
+print(data)
