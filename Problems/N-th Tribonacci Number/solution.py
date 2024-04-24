eec120ccc9714cbe98771875c6741ff1a84c9fed
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-24 20:04:39 +0530

    N-th Tribonacci Number

---
+++ Problems/N-th Tribonacci Number/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.tribonacci(n = 4)
+data = obj.tribonacci(n = 25)
+print(data)
