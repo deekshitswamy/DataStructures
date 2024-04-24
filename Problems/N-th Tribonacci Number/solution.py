505593bc0dc5968bcbdf38e15d65ed241f549f63
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-24 20:05:34 +0530

    changes

--- Problems/N-th Tribonacci Number/solution.py
+++ Problems/N-th Tribonacci Number/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def tribonacci(self, n: int) -> int:
-        pass
+        a=0
+        b=1
+        c=1
+        if n==0 or n==1:
+            return n
+        for i in range(2,n):
+            d=a+b+c
+            a=b
+            b=c
+            c=d
+        return c
 
 obj = Solution()
 #data = obj.tribonacci(n = 4)
