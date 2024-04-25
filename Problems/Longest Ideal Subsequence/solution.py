a4043f14292d19cb602daeef4565f5743a98e26e
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-25 19:44:43 +0530

    changes

--- Problems/Longest Ideal Subsequence/solution.py
+++ Problems/Longest Ideal Subsequence/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def longestIdealString(self, s: str, k: int) -> int:
-        pass
+        n=len(s)
+        best=[0]*26
+        orda=ord('a')
+        nums=list(ord(a)-orda for a in s)
+        best[nums[0]]=1
+        for i in range(1,n):
+            best[nums[i]]+=1
+            for j in range(26):
+                if abs(j-nums[i])<=k and j!=nums[i]:
+                    best[nums[i]]=max(best[j]+1,best[nums[i]])
+        return max(best)
 
 obj = Solution()
 #data = obj.longestIdealString(s = "acfgbd", k = 2)
