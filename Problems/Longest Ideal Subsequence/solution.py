3158dd3faa855a9283d5ea6e6e28dca85caf9003
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-25 19:44:07 +0530

    Longest Ideal Subsequence

---
+++ Problems/Longest Ideal Subsequence/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def longestIdealString(self, s: str, k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.longestIdealString(s = "acfgbd", k = 2)
+data = obj.longestIdealString(s = "abcd", k = 3)
+print(data)
