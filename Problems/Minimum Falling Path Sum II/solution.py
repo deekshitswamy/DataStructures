febc86398f4e203f0a98b820583f5f82855443fb
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-26 23:49:37 +0530

    Minimum Falling Path Sum II

---
+++ Problems/Minimum Falling Path Sum II/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minFallingPathSum(self, grid: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minFallingPathSum(grid = [[1,2,3],[4,5,6],[7,8,9]])
+data = obj.minFallingPathSum(grid = [[7]])
+print(data)
