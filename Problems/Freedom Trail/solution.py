b378688daacb0fd289ca000d8d69f480d9b3b733
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-27 16:17:13 +0530

    Freedom Trail

---
+++ Problems/Freedom Trail/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findRotateSteps(self, ring: str, key: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findRotateSteps(ring = "godding", key = "gd")
+data = obj.findRotateSteps(ring = "godding", key = "godding")
+print(data)
