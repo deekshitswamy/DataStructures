7b5aebfba55a7f3e065d37881aeed1ddffa33eaf
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-28 15:10:26 +0530

    Sum of Distances in Tree

---
+++ Problems/Sum of Distances in Tree/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.sumOfDistancesInTree(n = 6, edges = [[0,1],[0,2],[2,3],[2,4],[2,5]])
+#data = obj.sumOfDistancesInTree(n = 1, edges = [])
+data = obj.sumOfDistancesInTree(n = 2, edges = [[1,0]])
+print(data)
