1a48d6e1a5a8460109ef37740ef6694ae1f3d11c
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-29 22:26:53 +0530

    changes

--- Problems/Minimum Number of Operations to Make Array XOR Equal to K/solution.py
+++ Problems/Minimum Number of Operations to Make Array XOR Equal to K/solution.py
@@ -2,7 +2,12 @@ import io
 from typing import List
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        pass
+        ans=0
+        for i in nums:
+            ans^=i
+            
+        ans=ans^k
+        return bin(ans).count('1')
 
 obj = Solution()
 #data = obj.minOperations(nums = [2,1,3,4], k = 1)
