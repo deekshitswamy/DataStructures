a0b675bf8f9cf2034fb9dd628e95df7dc38d81d3
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-30 16:32:04 +0530

    changes

--- Problems/Number of Wonderful Substrings/solution.py
+++ Problems/Number of Wonderful Substrings/solution.py
@@ -2,7 +2,19 @@ import io
 from typing import List
 class Solution:
     def wonderfulSubstrings(self, word: str) -> int:
-        pass
+        count=collections.defaultdict(int)
+        total=0
+        count[0]=1
+        cur=0
+        for idx,c in enumerate(word):
+            ci=ord(c)-ord('a')
+            cur^=(1<<ci)
+            total+=count[cur]
+            count[cur]+=1
+            for i in range(10):
+                delta=cur^(1<<i)
+                total+=count[delta]
+        return total
 
 obj = Solution()
 #data = obj.wonderfulSubstrings(word = "aba")
