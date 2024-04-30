0371bfba5388ab4a64c24a5525325b3b6ec1f913
Deeku-Zeus <deekuzeus21@gmail.com>
2024-04-30 16:31:18 +0530

    Number of Wonderful Substrings

---
+++ Problems/Number of Wonderful Substrings/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def wonderfulSubstrings(self, word: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.wonderfulSubstrings(word = "aba")
+#data = obj.wonderfulSubstrings(word = "aabb")
+data = obj.wonderfulSubstrings(word = "he")
+print(data)
