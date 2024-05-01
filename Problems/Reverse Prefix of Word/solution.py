6e60e4677d187020532e12add3f585bb6d7b9e15
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-01 23:10:26 +1000

    Reverse Prefix of Word

---
+++ Problems/Reverse Prefix of Word/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.reversePrefix(word = "abcdefd", ch = "d")
+#data = obj.reversePrefix(word = "xyxzxe", ch = "z")
+data = obj.reversePrefix(word = "abcd", ch = "z")
+print(data)
