6d6fc1b81be074061ebb549c01285097d1ea39cd
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-02 22:10:49 +0530

    changes

--- Problems/Largest Positive Integer That Exists With Its Negative/solution.py
+++ Problems/Largest Positive Integer That Exists With Its Negative/solution.py
@@ -2,7 +2,16 @@ import io
 from typing import List
 class Solution:
     def findMaxK(self, nums: List[int]) -> int:
-        pass
+        l,r = 0,len(nums) - 1
+        nums.sort()
+        while l < r:
+            if -nums[l] == nums[r]:
+                return nums[r]
+            if abs(nums[l]) > abs(nums[r]):
+                l+=1
+            else:
+                r-=1     
+        return -1
 
 obj = Solution()
 #data = obj.findMaxK(nums = [-1,2,-3,3])
