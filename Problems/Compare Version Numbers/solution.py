684b59629b0158a6626a903982ef668cd737d79d
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-03 16:52:00 +0530

    changes

--- Problems/Compare Version Numbers/solution.py
+++ Problems/Compare Version Numbers/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        pass
+        v1_l = list(map(int, version1.split(".")))
+        v2_l = list(map(int, version2.split(".")))
+        l1 = len(v1_l)
+        l2 = len(v2_l)
+
+        for i in range(max(l1, l2)):
+            p1 = v1_l[i] if i<l1 else 0
+            p2 = v2_l[i] if i<l2 else 0
+            if p1 > p2:
+                return 1
+            elif p2 > p1:
+                return -1
+        
+        return 0
 
 obj = Solution()
 #data = obj.compareVersion(version1 = "1.01", version2 = "1.001")
