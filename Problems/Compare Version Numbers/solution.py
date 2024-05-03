bf0cf48b3e6ccf2e618ab77c67053e5f7a554231
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-03 16:51:06 +0530

    Compare Version Numbers

---
+++ Problems/Compare Version Numbers/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.compareVersion(version1 = "1.01", version2 = "1.001")
+#data = obj.compareVersion(version1 = "1.0", version2 = "1.0.0")
+data = obj.compareVersion(version1 = "0.1", version2 = "1.1")
+print(data)
