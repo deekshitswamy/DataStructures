2ebaf5713e48ae3626e42e1a6a6af5614d531272
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-04 18:26:22 +0530

    Boats to Save People

---
+++ Problems/Boats to Save People/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numRescueBoats(people = [1,2], limit = 3)
+#data = obj.numRescueBoats(people = [3,2,2,1], limit = 3)
+data = obj.numRescueBoats(people = [3,5,3,4], limit = 5)
+print(data)
