99e327225717df9b0c9953d9753dd654b1a8084b
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-04 18:27:41 +0530

    changes

--- Problems/Boats to Save People/solution.py
+++ Problems/Boats to Save People/solution.py
@@ -2,7 +2,15 @@ import io
 from typing import List
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        pass
+        people.sort()
+        l, r = 0, len(people) - 1
+        boats = 0
+        while l < r:
+            if people[l] + people[r] <= limit:
+                l += 1
+            r -= 1
+            boats += 1
+        return boats + int(l == r)
 
 obj = Solution()
 #data = obj.numRescueBoats(people = [1,2], limit = 3)
