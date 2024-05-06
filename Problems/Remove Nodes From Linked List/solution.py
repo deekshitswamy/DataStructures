c2dae742a6680a573e85db90e3a6ea0b8d2bb831
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-06 18:28:50 +0530

    changes

--- Problems/Remove Nodes From Linked List/solution.py
+++ Problems/Remove Nodes From Linked List/solution.py
@@ -7,7 +7,31 @@ class ListNode:
         self.next = next
 class Solution:
     def removeNodes(self, head: ListNode) -> ListNode:
-        pass
+        cur=head
+        temp=None
+        #Reversing linkedlist
+        while cur:
+            cur.next,cur,temp=temp,cur.next,cur
+        head=temp
+        maxn=head.val
+        prev=head
+        cur=head.next
+        #Checking maxn cur val is lesser than cur max or not
+        while cur:
+            if maxn>cur.val:
+                maxn=max(maxn,cur.val)
+                prev.next=cur.next
+                cur=prev.next
+            else:
+                maxn=max(maxn,cur.val)
+                prev=prev.next
+                cur=cur.next
+        cur=head
+        temp=None
+        #Reversing the linkedlist
+        while cur:
+            cur.next,cur,temp=temp,cur.next,cur
+        return temp
 
 obj = Solution()
 #data = obj.removeNodes(head = [5,2,13,3,8])
