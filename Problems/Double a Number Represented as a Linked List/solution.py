68c440407be20510a92e3901e96f045b48aaf1a8
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-07 21:38:08 +0530

    changes

--- Problems/Double a Number Represented as a Linked List/solution.py
+++ Problems/Double a Number Represented as a Linked List/solution.py
@@ -7,7 +7,15 @@ class ListNode:
         self.next = next
 class Solution:
     def doubleIt(self, head: ListNode) -> ListNode:
-        pass
+        if head.val>4:
+            head=ListNode(0,head)
+        node=head
+        while node:
+            node.val=(node.val*2)%10
+            if node.next and node.next.val>4:
+                node.val+=1
+            node=node.next
+        return head
 
 obj = Solution()
 #data = obj.testfunc(n = 1)
