bdb5d0c94ca427f1f8b7b4aecd011014d5dd64b4
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-08 22:18:58 +0530

    Relative Ranks

---
+++ Problems/Relative Ranks/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        pass
+
+obj = Solution()
+#data = obj.findRelativeRanks(score = [5,4,3,2,1])
+data = obj.findRelativeRanks(score = [10,3,8,9,4])
+print(data)
