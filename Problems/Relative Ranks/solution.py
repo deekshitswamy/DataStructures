2cebc7cc3afd89329a039cd939557cbede6cd991
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-08 22:19:37 +0530

    changes

--- Problems/Relative Ranks/solution.py
+++ Problems/Relative Ranks/solution.py
@@ -2,7 +2,22 @@ import io
 from typing import List
 class Solution:
     def findRelativeRanks(self, score: List[int]) -> List[str]:
-        pass
+        lst=sorted(score,reverse=True)
+        n=len(lst)
+        ans={}
+        for i in range(n):
+            if i==0:
+                ans[lst[i]]="Gold Medal"
+            elif i==1:
+                ans[lst[i]]="Silver Medal"
+            elif i==2:
+                ans[lst[i]]="Bronze Medal"
+            else:
+                ans[lst[i]]=str(i+1)
+        res=[]
+        for i in score:
+            res.append(ans[i])
+        return res
 
 obj = Solution()
 #data = obj.findRelativeRanks(score = [5,4,3,2,1])
