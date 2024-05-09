48e7755b31112ac7f4ade6311e3989a9b858c6aa
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-09 19:13:04 +0530

    changes

--- Problems/Maximize Happiness of Selected Children/solution.py
+++ Problems/Maximize Happiness of Selected Children/solution.py
@@ -2,7 +2,16 @@ import io
 from typing import List
 class Solution:
     def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
-        pass
+        happiness.sort(reverse=True)
+        selected=0
+        sum_happiness_value=0
+        for happiness_value in happiness:
+            if selected==k:
+                return sum_happiness_value
+            sum_happiness_value+=max(0,happiness_value-selected)
+            selected+=1
+      
+        return  sum_happiness_value
 
 obj = Solution()
 #data = obj.maximumHappinessSum(happiness = [1,2,3], k = 2)
