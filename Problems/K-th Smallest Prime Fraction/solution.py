80cf66bd53d976142d8689668800d871e2a38e22
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-10 20:39:00 +0530

    changes

--- Problems/K-th Smallest Prime Fraction/solution.py
+++ Problems/K-th Smallest Prime Fraction/solution.py
@@ -2,7 +2,30 @@ import io
 from typing import List
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        pass
+        n=len(arr)
+        ans=[0,1]
+        left=0
+        right=1
+        while True:
+            mid=(left+right)/2
+            ans[0]=0
+            count=0
+            j=1
+            for i in range(n):
+                while j<n and arr[i]>mid*arr[j]:
+                    j+=1
+                count+=n-j
+                if j==n:
+                    break
+                if ans[0]*arr[j]<ans[1]*arr[i]:
+                    ans[0]=arr[i]
+                    ans[1]=arr[j]
+            if count<k:
+                left=mid
+            elif count>k:
+                right=mid
+            else:
+                return ans
 
 obj = Solution()
 #data = obj.kthSmallestPrimeFraction(arr = [1,2,3,5], k = 3)
