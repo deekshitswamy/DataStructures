e33eb173ca9a60fba6cc0e277b18be6c707c0020
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-11 19:52:07 +0530

    Minimum Cost to Hire K Workers

---
+++ Problems/Minimum Cost to Hire K Workers/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
+        pass
+
+obj = Solution()
+#data = obj.mincostToHireWorkers(quality = [10,20,5], wage = [70,50,30], k = 2)
+data = obj.mincostToHireWorkers(quality = [3,1,10,10,1], wage = [4,8,2,2,7], k = 3)
+print(data)
