4256ebbf0e753644f12efcb7a4cd297683e37520
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-11 19:52:52 +0530

    changes

--- Problems/Minimum Cost to Hire K Workers/solution.py
+++ Problems/Minimum Cost to Hire K Workers/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
-        pass
+        ans=inf
+        rsm=0
+        stack=[]
+        for q,w in sorted(zip(quality,wage),key=lambda x:x[1]/x[0]):
+            rsm+=q
+            heappush(stack,-q)
+            if len(stack)>k:
+                rsm+=heappop(stack)
+            if len(stack)==k:
+                ans=min(ans,rsm*w/q)
+        return ans
 
 obj = Solution()
 #data = obj.mincostToHireWorkers(quality = [10,20,5], wage = [70,50,30], k = 2)
