60ac2afab85ca170f6d26906541dce5d7b979c66
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-12 18:51:44 +0530

    changes

--- Problems/Largest Local Values in a Matrix/solution.py
+++ Problems/Largest Local Values in a Matrix/solution.py
@@ -2,7 +2,15 @@ import io
 from typing import List
 class Solution:
     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
-        pass
+        n=len(grid)
+        ans=[[0]*(n-2) for _ in range(n-2)]
+        for i in range(n-2):
+            for j in range(n-2):
+                ans[i][j]=grid[i][j]
+                for di in range(3):
+                    for dj in range(3):
+                        ans[i][j]=max(ans[i][j],grid[i+di][j+dj])
+        return ans
 
 obj = Solution()
 #data = obj.largestLocal(grid = [[9,9,8,1],[5,6,2,6],[8,2,6,4],[6,2,2,2]])
