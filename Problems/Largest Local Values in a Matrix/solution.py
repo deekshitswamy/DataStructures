c008324bad5a93ffd11a1bf364698dba4aeb1fe2
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-12 18:50:43 +0530

    Largest Local Values in a Matrix

---
+++ Problems/Largest Local Values in a Matrix/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        pass
+
+obj = Solution()
+#data = obj.largestLocal(grid = [[9,9,8,1],[5,6,2,6],[8,2,6,4],[6,2,2,2]])
+data = obj.largestLocal(grid = [[1,1,1,1,1],[1,1,1,1,1],[1,1,2,1,1],[1,1,1,1,1],[1,1,1,1,1]])
+print(data)
