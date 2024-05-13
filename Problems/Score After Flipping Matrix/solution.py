01d2bd9de5c1840ccf1659c5089b7d40ef6aa14a
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-13 22:13:40 +0530

    Score After Flipping Matrix

---
+++ Problems/Score After Flipping Matrix/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def matrixScore(self, grid: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.matrixScore(grid = [[0,0,1,1],[1,0,1,0],[1,1,0,0]])
+data = obj.matrixScore(grid = [[0]])
+print(data)
