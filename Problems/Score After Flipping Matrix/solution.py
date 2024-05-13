148cd0e7c89ea2789960948da7870d2126483489
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-13 22:14:23 +0530

    changes

--- Problems/Score After Flipping Matrix/solution.py
+++ Problems/Score After Flipping Matrix/solution.py
@@ -2,7 +2,13 @@ import io
 from typing import List
 class Solution:
     def matrixScore(self, grid: List[List[int]]) -> int:
-        pass
+        n=len(grid)
+        m=len(grid[0])
+        ans=0
+        for j in range(m):
+            col=sum(grid[i][j]==grid[i][0] for i in range(n))
+            ans+=max(col,n-col)*2**(m-1-j)
+        return ans
 
 obj = Solution()
 #data = obj.matrixScore(grid = [[0,0,1,1],[1,0,1,0],[1,1,0,0]])
