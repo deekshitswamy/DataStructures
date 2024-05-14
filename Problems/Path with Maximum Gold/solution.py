00a08293226b3a0e33e01de3c8ff3eff0293a341
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-14 18:35:52 +0530

    Path with Maximum Gold

---
+++ Problems/Path with Maximum Gold/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def getMaximumGold(self, grid: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.getMaximumGold(grid = [[0,6,0],[5,8,7],[0,9,0]])
+data = obj.getMaximumGold(grid = [[1,0,7],[2,0,6],[3,4,5],[0,3,0],[9,0,20]])
+print(data)
