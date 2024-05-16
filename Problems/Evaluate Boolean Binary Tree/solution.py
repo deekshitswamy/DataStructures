2224cc4378668151916772d5d23dce80ac2749b9
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-16 18:58:55 +0530

    Evaluate Boolean Binary Tree

---
+++ Problems/Evaluate Boolean Binary Tree/solution.py
@@ -0,0 +1,17 @@
+import io
+from typing import List
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def evaluateTree(self, root: TreeNode) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.evaluateTree(root = [2,1,3,null,null,0,1])
+data = obj.evaluateTree(root = [0])
+print(data)
