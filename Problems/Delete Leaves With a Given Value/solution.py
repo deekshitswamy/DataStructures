7aaa9c3cc3cb740b40dfafa95fe7ac95a497e0e9
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-17 17:01:12 +0530

    changes

--- Problems/Delete Leaves With a Given Value/solution.py
+++ Problems/Delete Leaves With a Given Value/solution.py
@@ -9,7 +9,13 @@ class TreeNode:
 
 class Solution:
     def removeLeafNodes(self, root: TreeNode, target: int) -> TreeNode:
-        pass
+        if root:
+            root.left=self.removeLeafNodes(root.left,target)
+            root.right=self.removeLeafNodes(root.right,target)
+            if root.val==target and not root.left and not root.right:
+                root=None
+
+        return root
 
 obj = Solution()
 #data = obj.removeLeafNodes(root = [1,2,3,2,null,2,4], target = 2)
