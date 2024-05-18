0cc79fb6bb8d9472f8e90b66bd2a77a6e003f6a8
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-18 21:31:49 +0530

    Distribute Coins in Binary Tree

---
+++ Problems/Distribute Coins in Binary Tree/solution.py
@@ -0,0 +1,17 @@
+import io
+from typing import List
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def distributeCoins(self, root: TreeNode) -> int:
+        pass
+
+obj = Solution()
+#data = obj.distributeCoins(root = [3,0,0])
+data = obj.distributeCoins(root = [0,3,0])
+print(data)
