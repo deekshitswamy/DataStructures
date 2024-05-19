48ca04965b1152a3625aa643bf56b027e5d1efb8
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-19 19:38:54 +0530

    Find the Maximum Sum of Node Values

---
+++ Problems/Find the Maximum Sum of Node Values/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maximumValueSum(nums = [1,2,1], k = 3, edges = [[0,1],[0,2]])
+#data = obj.maximumValueSum(nums = [2,3], k = 7, edges = [[0,1]])
+data = obj.maximumValueSum(nums = [7,7,7,7,7,7], k = 3, edges = [[0,1],[0,2],[0,3],[0,4],[0,5]])
+print(data)
