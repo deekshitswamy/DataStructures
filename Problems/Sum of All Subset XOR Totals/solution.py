7b5abc8586ae8e461d5b65de1a4f1b930d2bdaed
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-20 21:48:41 +0530

    Sum of All Subset XOR Totals

---
+++ Problems/Sum of All Subset XOR Totals/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.subsetXORSum(nums = [1,3])
+#data = obj.subsetXORSum(nums = [5,1,6])
+data = obj.subsetXORSum(nums = [3,4,5,6,7,8])
+print(data)
