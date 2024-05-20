392305e81c9d6b8cc03edcb4bb482972af724261
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-20 21:49:20 +0530

    changes

--- Problems/Sum of All Subset XOR Totals/solution.py
+++ Problems/Sum of All Subset XOR Totals/solution.py
@@ -2,7 +2,15 @@ import io
 from typing import List
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        pass
+        n=len(nums)
+        ans=0
+        for i in range(1<<n):
+            cur=0
+            for x in range(n):
+                if (i &(1<<x))>0:
+                    cur^=nums[x]
+            ans+=cur
+        return ans
 
 obj = Solution()
 #data = obj.subsetXORSum(nums = [1,3])
