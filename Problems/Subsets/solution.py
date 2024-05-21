e65ba12246d494b8bb99af07d894357aa7353082
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-21 21:05:11 +0530

    changes

--- Problems/Subsets/solution.py
+++ Problems/Subsets/solution.py
@@ -2,7 +2,19 @@ import io
 from typing import List
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        pass
+        n=len(nums)
+        ans=[]
+        subset=[]
+        def dfs(i):
+            if i>=n:
+                ans.append(subset.copy())
+                return
+            subset.append(nums[i])
+            dfs(i+1)
+            subset.pop()
+            dfs(i+1)
+        dfs(0)
+        return ans
 
 obj = Solution()
 #data = obj.subsets(nums = [1,2,3])
