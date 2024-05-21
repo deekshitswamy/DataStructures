0eb4eb380ddd882aa9e950c08d8bfe80fad6b0ab
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-21 21:04:27 +0530

    Subsets

---
+++ Problems/Subsets/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        pass
+
+obj = Solution()
+#data = obj.subsets(nums = [1,2,3])
+data = obj.subsets(nums = [0])
+print(data)
