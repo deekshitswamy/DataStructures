2fb7cb29160546e469b4a36b5b8df21ee84d030b
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-22 21:21:32 +0530

    Palindrome Partitioning

---
+++ Problems/Palindrome Partitioning/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        pass
+
+obj = Solution()
+#data = obj.partition(s = "aab")
+data = obj.partition(s = "a")
+print(data)
