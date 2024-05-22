f009e176dc72327391aa401702bfdc670dee0781
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-22 21:22:49 +0530

    changes

--- Problems/Palindrome Partitioning/solution.py
+++ Problems/Palindrome Partitioning/solution.py
@@ -2,7 +2,23 @@ import io
 from typing import List
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        pass
+        def valid(ip):
+            if ip==ip[-1::-1]:
+                return True
+            return False
+        
+        def solve(ip,op):
+            if ip=='':
+                ans.append(op[:])
+                return
+            for i in range(1,len(ip)+1):
+                if valid(ip[:i]):
+                    op.append(ip[:i])
+                    solve(ip[i:],op)
+                    op.pop()
+        ans=[]
+        solve(s,[])
+        return ans
 
 obj = Solution()
 #data = obj.partition(s = "aab")
