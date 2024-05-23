e83cbba47204fc9996fa9f335ec2aae4cc72a6c0
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-23 23:19:43 +0530

    The Number of Beautiful Subsets

---
+++ Problems/The Number of Beautiful Subsets/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def beautifulSubsets(self, n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.beautifulSubsets(nums = [2,4,6], k = 2)
+data = obj.beautifulSubsets(nums = [1], k = 1)
+print(data)
