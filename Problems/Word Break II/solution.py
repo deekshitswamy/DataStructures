f41245d1c199e36c6f18633ae511bd5e2c9ea953
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-25 21:11:48 +0530

    changes

--- Problems/Word Break II/solution.py
+++ Problems/Word Break II/solution.py
@@ -2,7 +2,27 @@ import io
 from typing import List
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        pass
+        memo={}
+        def beakTrack(s):
+            if not s:
+                return [[]]
+            if s in memo:
+                return memo[s]
+            cur_word_breaks=[]
+            
+            for i in range(len(s)):
+                new_word=s[:i+1]
+                
+                if new_word in wordDict:
+                    rest_word_breaks=beakTrack(s[i+1:])
+                    for word_break in rest_word_breaks:
+                        cur_word_breaks.append([new_word]+word_break)
+                        
+            memo[s]=cur_word_breaks
+            return cur_word_breaks
+        wordSet=set(wordDict)
+        word_breaks=beakTrack(s)
+        return [" ".join(word_break) for word_break in word_breaks]
 
 obj = Solution()
 #data = obj.wordBreak(s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"])
