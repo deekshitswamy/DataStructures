fcc8875cad590d28e39a23e1fc2b7abb6afaac22
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-26 20:55:48 +0530

    Student Attendance Record II

---
+++ Problems/Student Attendance Record II/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def checkRecord(self, n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.checkRecord(n = 1)
+#data = obj.checkRecord(n = 2)
+data = obj.checkRecord(n = 10101)
+print(data)
