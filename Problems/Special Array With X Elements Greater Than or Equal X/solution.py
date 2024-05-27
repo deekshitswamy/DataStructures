13a3d39edcc0eca8caf6d57be357ff336a7d925d
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-27 18:58:26 +0530

    Special Array With X Elements Greater Than or Equal X

---
+++ Problems/Special Array With X Elements Greater Than or Equal X/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.specialArray(nums = [3,5])
+#data = obj.specialArray(nums = [0,0])
+data = obj.specialArray(nums = [0,4,3,0,4])
+print(data)
