9ca03d7fd451ba7a7a4fa90c90118e8feb9f251c
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-28 19:38:23 +0530

    Get Equal Substrings Within Budget

---
+++ Problems/Get Equal Substrings Within Budget/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.equalSubstring(s = "abcd", t = "bcdf", maxCost = 3)
+#data = obj.equalSubstring(s = "abcd", t = "cdef", maxCost = 3)
+data = obj.equalSubstring(s = "abcd", t = "acde", maxCost = 0)
+print(data)
