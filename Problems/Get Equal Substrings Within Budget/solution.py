9926b1755c8b317ebcc15106109ba9419c9d27d4
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-28 19:39:12 +0530

    changes

--- Problems/Get Equal Substrings Within Budget/solution.py
+++ Problems/Get Equal Substrings Within Budget/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-        pass
+        start = 0
+        current_cost = 0
+        max_length = 0
+
+        for end in range(len(s)):
+            current_cost += abs(ord(s[end]) - ord(t[end]))
+
+            while current_cost > maxCost:
+                current_cost -= abs(ord(s[start]) - ord(t[start]))
+                start += 1
+
+            max_length = max(max_length, end - start + 1)
+        
+        return max_length
 
 obj = Solution()
 #data = obj.equalSubstring(s = "abcd", t = "bcdf", maxCost = 3)
