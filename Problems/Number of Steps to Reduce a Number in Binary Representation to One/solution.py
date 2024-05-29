bad9b1b5dd787ebd97f234690f6474a40a31d7a7
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-29 18:35:10 +0530

    Number of Steps to Reduce a Number in Binary Representation to One

---
+++ Problems/Number of Steps to Reduce a Number in Binary Representation to One/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def numSteps(self, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numSteps(s = "1101")
+#data = obj.numSteps(s = "10")
+data = obj.numSteps(s = "1")
+print(data)
