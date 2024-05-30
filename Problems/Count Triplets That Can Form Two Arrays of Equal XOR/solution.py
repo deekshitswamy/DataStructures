2ef24020df9be3027d043ca7a2bb447f7c6f4b16
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-30 20:07:15 +0530

    Count Triplets That Can Form Two Arrays of Equal XOR

---
+++ Problems/Count Triplets That Can Form Two Arrays of Equal XOR/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def countTriplets(self, arr: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countTriplets(arr = [2,3,1,6,7])
+data = obj.countTriplets(arr = [1,1,1,1,1])
+print(data)
