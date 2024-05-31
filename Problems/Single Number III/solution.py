69906579dd06a9584967b1be2b2133c91759e59f
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-31 21:51:13 +0530

    Single Number III

---
+++ Problems/Single Number III/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.singleNumber(nums = [1,2,1,3,2,5])
+#data = obj.singleNumber(nums = [-1,0])
+data = obj.singleNumber(nums = [0,1])
+print(data)
