39cd592442ac1da34a5380f51c706fcdf8cd43d9
Deeku-Zeus <deekuzeus21@gmail.com>
2024-05-31 21:51:53 +0530

    changes

--- Problems/Single Number III/solution.py
+++ Problems/Single Number III/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        pass
+        x=0
+        for num in nums:
+            x^=num
+        mask=x&-x
+        ans1,ans2=0,0
+        for num in nums:
+            if num & mask:
+                ans1^=num
+            else:
+                ans2^=num
+        return [ans1,ans2]
 
 obj = Solution()
 #data = obj.singleNumber(nums = [1,2,1,3,2,5])
