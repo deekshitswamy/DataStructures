5e616fcf6622a0f3939104f0269bc50c5a94bd36
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-01 13:59:58 +0530

    Score of a String

---
+++ Problems/Score of a String/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.scoreOfString(s = "hello")
+data = obj.scoreOfString(s = "zaz")
+print(data)
