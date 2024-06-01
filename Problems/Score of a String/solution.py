bf4d0840396aa0f0d8ad0fa2f70f845e4c12ccfa
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-01 14:05:04 +0530

    changes

--- Problems/Score of a String/solution.py
+++ Problems/Score of a String/solution.py
@@ -2,7 +2,10 @@ import io
 from typing import List
 class Solution:
     def scoreOfString(self, s: str) -> int:
-        pass
+        res = 0
+        for x in range(0,len(s)-1):
+            res = res + abs(ord(s[x])-ord(s[x+1]))
+        return res
 
 obj = Solution()
 #data = obj.scoreOfString(s = "hello")
