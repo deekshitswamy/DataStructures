9eda6cc23eb579cee2bb3704e9c401ad21ab9108
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-02 20:24:23 +0530

    changes

--- Problems/Reverse String/solution.py
+++ Problems/Reverse String/solution.py
@@ -2,7 +2,9 @@ import io
 from typing import List
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        pass
+        for i in range(0,len(s)//2):
+            s[i], s[-i-1] = s[-i-1],s[i]
+        return s
 
 obj = Solution()
 #data = obj.reverseString(s = ["h","e","l","l","o"])
