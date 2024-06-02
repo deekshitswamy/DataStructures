b4e361c60461cfb030cd71f05ced8630f2dc1ea4
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-02 20:17:52 +0530

    Reverse String

---
+++ Problems/Reverse String/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        pass
+
+obj = Solution()
+#data = obj.reverseString(s = ["h","e","l","l","o"])
+data = obj.reverseString(s = ["H","a","n","n","a","h"])
+print(data)
