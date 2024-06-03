66e605277f6820f83cd4f8756916999679931c36
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-03 20:50:32 +0530

    changes

--- Problems/Append Characters to String to Make Subsequence/solution.py
+++ Problems/Append Characters to String to Make Subsequence/solution.py
@@ -2,7 +2,23 @@ import io
 from typing import List
 class Solution:
     def appendCharacters(self, s: str, t: str) -> int:
-        pass
+        s_index = 0
+        t_index = 0
+        nomatch = False
+        for i in range(t_index, len(t)):
+            #print("t=====>",t[i],end=",")
+            for j in range(s_index, len(s)):
+                #print("\ns=>",s[j],end="")
+                nomatch = True
+                if t[i] == s[j]:
+                    t_index = i+1
+                    s_index = j+1
+                    nomatch = False
+                    break
+            if nomatch:
+                break
+            print()
+        return len(t[t_index:])
 
 obj = Solution()
 #data = obj.appendCharacters(s = "coaching", t = "coding")
