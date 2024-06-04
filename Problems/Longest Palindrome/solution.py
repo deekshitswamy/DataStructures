72de36ed9063d81aad04aeff15b7603f8241d627
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-04 18:06:20 +0530

    changes

--- Problems/Longest Palindrome/solution.py
+++ Problems/Longest Palindrome/solution.py
@@ -2,7 +2,21 @@ import io
 from typing import List
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        pass
+        residue_text = []
+        len_palindrome = 0
+        s = ''.join(sorted(s))
+        print(s)
+        for i in s:
+            if i in residue_text:
+                residue_text.pop()
+            else:
+                residue_text.append(i)
+        str(residue_text)
+        if len(residue_text):
+            len_palindrome = len(s) - len(residue_text) + 1
+        else:
+            len_palindrome = len(s)
+        return len_palindrome
 
 obj = Solution()
 #data = obj.longestPalindrome(s = "abccccdd")
