6bbdb4be18d54b7d2b9b08f1b0bbc83c475e9ea0
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-05 18:41:54 +0530

    Find Common Characters

---
+++ Problems/Find Common Characters/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        pass
+
+obj = Solution()
+#data = obj.commonChars(words = ["bella","label","roller"])
+data = obj.commonChars(words = ["cool","lock","cook"])
+print(data)
