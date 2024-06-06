30e2b4dbdb1a7db6e62505a3e3f85de1bd1a45fd
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-06 19:19:32 +0530

    Hand of Straights

---
+++ Problems/Hand of Straights/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.isNStraightHand(hand = [1,2,3,4,5], groupSize = 4)
+data = obj.isNStraightHand(hand = [1,2,3,4,5], groupSize = 4)
+print(data)
