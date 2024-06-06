3725d03d172405d0fb7029ded99997e2be806966
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-06 20:45:37 +0530

    changes

--- Problems/Hand of Straights/solution.py
+++ Problems/Hand of Straights/solution.py
@@ -2,7 +2,21 @@ import io
 from typing import List
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        pass
+        if len(hand) % groupSize != 0:
+            return False
+
+        hand.sort()
+        count = Counter(hand)
+        
+        for card in hand:
+            if count[card] == 0:
+                continue
+            for i in range(groupSize):
+                if count[card + i] > 0:
+                    count[card + i] -= 1
+                else:
+                    return False
+        return True
 
 obj = Solution()
 #data = obj.isNStraightHand(hand = [1,2,3,6,2,3,4,7,8], groupSize = 3)
