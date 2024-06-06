8e4bd7362fb5cd1fcf919c1e69729569dd6537dd
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-06 20:45:51 +0530

    changes

--- Problems/Hand of Straights/solution.py
+++ Problems/Hand of Straights/solution.py
@@ -1,5 +1,6 @@
 import io
 from typing import List
+from collections import Counter
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         if len(hand) % groupSize != 0:
