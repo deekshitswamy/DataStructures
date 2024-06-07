f713682d31ae94b51f2ddc72be23e0d4288f9fbf
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-07 18:52:21 +0530

    Replace Words

---
+++ Problems/Replace Words/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.replaceWords(dictionary = ["cat","bat","rat"], sentence = "the cattle was rattled by the battery")
+data = obj.replaceWords(dictionary = ["a","b","c"], sentence = "aadsfasf absbs bbab cadsfafs")
+print(data)
