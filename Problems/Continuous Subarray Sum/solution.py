635f8f773a7b1ae1450641f0bfa64769b4c409b7
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-08 19:46:07 +0530

    changes

--- Problems/Continuous Subarray Sum/solution.py
+++ Problems/Continuous Subarray Sum/solution.py
@@ -2,7 +2,25 @@ import io
 from typing import List
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        pass
+        '''
+        Sliding window solution will not work here 
+        This solution reqired different point of view to solve the problem 
+        Using hashmap to store the reminder and making the count for the inside
+        to handle the edge case have an 0 key will value of -1 since the length of the 
+        hashmap should be 2
+        '''
+
+        hash_map = { 0 : -1}
+        total = 0
+        for i ,value in enumerate(nums):
+            total +=value
+            reminder = total % k 
+
+            if reminder not in hash_map:
+                hash_map[reminder] = i
+            elif i - hash_map[reminder]>1:
+                return True
+        return False
 
 obj = Solution()
 #data = obj.checkSubarraySum(nums = [23,2,4,6,7], k = 6)
