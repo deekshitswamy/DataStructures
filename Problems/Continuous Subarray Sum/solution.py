478b5a0bf2c38c4188adeccd49251c13e8fbc357
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-08 19:00:39 +0530

    Continuous Subarray Sum

---
+++ Problems/Continuous Subarray Sum/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.checkSubarraySum(nums = [23,2,4,6,7], k = 6)
+#data = obj.checkSubarraySum(nums = [23,2,6,4,7], k = 6)
+data = obj.checkSubarraySum(nums = [23,2,6,4,7], k = 13)
+print(data)
