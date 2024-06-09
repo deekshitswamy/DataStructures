7b43bf98fc401b4ce58fedbf1a2e2325e45237f0
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-09 21:02:36 +0530

    Subarray Sums Divisible by K

---
+++ Problems/Subarray Sums Divisible by K/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.subarraysDivByK(nums = [4,5,0,-2,-3,1], k = 5)
+data = obj.subarraysDivByK(nums = [5], k = 9)
+print(data)
