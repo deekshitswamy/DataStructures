de19feedb331e170febe007778625a3d6119b501
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-10 18:34:38 +0530

    Height Checker

---
+++ Problems/Height Checker/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.heightChecker(heights = [1,1,4,2,1,3])
+#data = obj.heightChecker(heights = [5,1,2,3,4])
+data = obj.heightChecker(heights = [1,2,3,4,5])
+print(data)
