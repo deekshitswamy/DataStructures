ad67791e449de57b74e069b4fe9c21fb3d3fe531
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-11 22:39:20 +0530

    changes

--- Problems/Relative Sort Array/solution.py
+++ Problems/Relative Sort Array/solution.py
@@ -2,7 +2,28 @@ import io
 from typing import List
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        pass
+        bucket_indices = {}
+
+        for i, e in enumerate(arr2):
+            bucket_indices[e] = i
+
+        buckets = [[] for _ in range(len(arr2))]
+        end = []
+
+        for e in arr1:
+            if e not in bucket_indices:
+                end.append(e)
+            else:
+                i = bucket_indices[e]
+                buckets[i].append(e)
+        
+        end.sort()
+        buckets_flatten = []
+        
+        for bucket in buckets:
+            buckets_flatten += bucket
+        
+        return buckets_flatten + end
 
 obj = Solution()
 #data = obj.relativeSortArray(arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6])
