962c5eba3352306d1268e9d5d7d32d24d4940e5a
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-11 22:38:05 +0530

    Relative Sort Array

---
+++ Problems/Relative Sort Array/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.relativeSortArray(arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6])
+data = obj.relativeSortArray(arr1 = [28,6,22,8,44,17], arr2 = [22,28,8,6])
+print(data)
