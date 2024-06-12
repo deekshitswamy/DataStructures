6eff50a09baf7f0ce56937c02e6df2cedbca92a9
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-12 22:55:03 +0530

    Sort Colors

---
+++ Problems/Sort Colors/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        pass
+
+obj = Solution()
+#data = obj.testfunc(n = 1)
+#data = obj.testfunc(n = 2)
+data = obj.testfunc(n = 2)
+print(data)
