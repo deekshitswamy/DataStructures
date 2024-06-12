4c22af7d11b6db21d0cb758242f3678404659bc0
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-12 22:57:11 +0530

    changes

--- Problems/Sort Colors/solution.py
+++ Problems/Sort Colors/solution.py
@@ -2,7 +2,19 @@ import io
 from typing import List
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        pass
+        #Dutch national flag problem (red, white, blue)
+        l, m, r = 0, 0, len(nums) - 1  # pointers to partition
+
+        while m <= r:
+            if nums[m] == 0:
+                nums[m], nums[l] = nums[l], nums[m]
+                l += 1
+                m += 1
+            elif nums[m] == 1:
+                m += 1
+            else:  # nums[m] == 2
+                nums[m], nums[r] = nums[r], nums[m]
+                r -= 1
 
 obj = Solution()
 #data = obj.sortColors(nums = [2,0,2,1,1,0])
