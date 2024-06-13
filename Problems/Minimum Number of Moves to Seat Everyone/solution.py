c3f78b1f84dea2dc2c9a842e2b9627c9ce24d8fa
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-13 23:20:11 +0530

    Minimum Number of Moves to Seat Everyone

---
+++ Problems/Minimum Number of Moves to Seat Everyone/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minMovesToSeat(seats = [3,1,5], students = [2,7,4])
+#data = obj.minMovesToSeat(seats = [4,1,5,9], students = [1,3,2,6])
+data = obj.minMovesToSeat(seats = [2,2,6,6], students = [1,3,2,6])
+print(data)
