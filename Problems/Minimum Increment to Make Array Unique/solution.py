ff51453151fd29a8a8b760ebde09826e978b5a23
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-14 21:34:06 +0530

    changes

--- Problems/Minimum Increment to Make Array Unique/solution.py
+++ Problems/Minimum Increment to Make Array Unique/solution.py
@@ -2,7 +2,13 @@ import io
 from typing import List
 class Solution:
     def minIncrementForUnique(self, nums: List[int]) -> int:
-        pass
+        nums.sort()
+        move=0
+        ans=0
+        for i in nums:
+            ans+=max(move-i,0)
+            move=max(move+1,i+1)
+        return ans 
 
 obj = Solution()
 #data = obj.minIncrementForUnique(nums = [1,2,2])
