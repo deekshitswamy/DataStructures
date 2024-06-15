8d755ea42eab000562b47da721284b61d8ee3984
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-15 20:18:51 +0530

    IPO

---
+++ Problems/IPO/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findMaximizedCapital(k = 2, w = 0, profits = [1,2,3], capital = [0,1,1])
+data = obj.findMaximizedCapital(k = 3, w = 0, profits = [1,2,3], capital = [0,1,2])
+print(data)
