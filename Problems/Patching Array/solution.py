508b25c9aea478b431e1cb5cbe83c8286819c0eb
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-16 20:21:51 +0530

    changes

--- Problems/Patching Array/solution.py
+++ Problems/Patching Array/solution.py
@@ -2,7 +2,19 @@ import io
 from typing import List
 class Solution:
     def minPatches(self, nums: List[int], n: int) -> int:
-        pass
+        count = 0
+        missing = 1
+        i = 0
+        
+        while missing <= n:
+            if i < len(nums) and nums[i] <= missing:
+                missing += nums[i]
+                i += 1
+            else:
+                missing += missing
+                count += 1
+        
+        return count
 
 obj = Solution()
 #data = obj.minPatches(nums = [1,3], n = 6)
