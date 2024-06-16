bf997dcf62c0568d8ec0e8cf90a4028e2142c1fa
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-16 20:03:07 +0530

    Patching Array

---
+++ Problems/Patching Array/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minPatches(nums = [1,3], n = 6)
+#data = obj.minPatches(nums = [1,5,10], n = 20)
+data = obj.minPatches(nums = [1,2,2], n = 5)
+print(data)
