5e610e675ca0cb38f56eb177077dc954a32a292e
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-17 19:46:14 +0530

    Sum of Square Numbers

---
+++ Problems/Sum of Square Numbers/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.judgeSquareSum(c = 5)
+data = obj.judgeSquareSum(c = 3)
+print(data)
