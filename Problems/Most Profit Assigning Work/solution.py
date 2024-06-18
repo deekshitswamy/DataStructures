b89b32c9c395d35b4bd349d9a9f538608dcd5d4e
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-18 18:31:59 +0530

    Most Profit Assigning Work

---
+++ Problems/Most Profit Assigning Work/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxProfitAssignment(difficulty = [2,4,6,8,10], profit = [10,20,30,40,50], worker = [4,5,6,7])
+data = obj.maxProfitAssignment(difficulty = [85,47,57], profit = [24,66,99], worker = [40,25,25])
+print(data)
