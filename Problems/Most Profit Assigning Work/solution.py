37a14a1be6910ed15fe2f5be762f92249ce7c181
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-18 19:03:27 +0530

    changes

--- Problems/Most Profit Assigning Work/solution.py
+++ Problems/Most Profit Assigning Work/solution.py
@@ -2,7 +2,19 @@ import io
 from typing import List
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
-        pass
+        sorted_worker=sorted(worker)
+        jobs=zip(difficulty,profit)
+        sorted_jobs=sorted(jobs)
+        n=len(sorted_jobs)
+        best_profit=0
+        total_best_profit=0
+        i=0
+        for skill in sorted_worker:
+            while i<n and skill>=sorted_jobs[i][0]:
+                best_profit=max(best_profit, sorted_jobs[i][1])
+                i+=1
+            total_best_profit+=best_profit
+        return total_best_profit
 
 obj = Solution()
 #data = obj.maxProfitAssignment(difficulty = [2,4,6,8,10], profit = [10,20,30,40,50], worker = [4,5,6,7])
