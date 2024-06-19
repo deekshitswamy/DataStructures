ac1d4db5ad8e701b91b2119d6a3268812720784e
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-19 19:41:32 +0530

    changes

--- Problems/Minimum Number of Days to Make m Bouquets/solution.py
+++ Problems/Minimum Number of Days to Make m Bouquets/solution.py
@@ -2,7 +2,30 @@ import io
 from typing import List
 class Solution:
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
-        pass
+        if len(bloomDay) < m * k:
+            return -1
+        
+        def feasible(day: int) -> bool:
+            bouquets = 0
+            size = 0
+            for bloom in bloomDay:
+                if bloom <= day:
+                    size += 1
+                    if size == k:
+                        bouquets += 1
+                        size = 0
+                else:
+                    size = 0
+            return bouquets >= m
+
+        shortest, longest = min(bloomDay), max(bloomDay)
+        while shortest < longest:
+            day = shortest + (longest - shortest) // 2
+            if feasible(day):
+                longest = day
+            else:
+                shortest = day + 1
+        return shortest
 
 obj = Solution()
 #data = obj.minDays(bloomDay = [1,10,3,10,2], m = 3, k = 1)
