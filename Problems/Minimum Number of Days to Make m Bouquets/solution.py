c7bb3e878bea5f39a14a8a685e11fe936e6b98a5
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-19 18:56:41 +0530

    Minimum Number of Days to Make m Bouquets

---
+++ Problems/Minimum Number of Days to Make m Bouquets/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minDays(bloomDay = [1,10,3,10,2], m = 3, k = 1)
+#data = obj.minDays(bloomDay = [1,10,3,10,2], m = 3, k = 2)
+data = obj.minDays(bloomDay = [7,7,7,7,12,7,7], m = 2, k = 3)
+print(data)
