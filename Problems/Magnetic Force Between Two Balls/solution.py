39735b189dbdc3cae222eb54d7d066f6db201ef9
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-20 21:43:10 +0530

    Magnetic Force Between Two Balls

---
+++ Problems/Magnetic Force Between Two Balls/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxDistance(self, position: List[int], m: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxDistance(position = [1,2,3,4,7], m = 3)
+data = obj.maxDistance(position = [5,4,3,2,1,1000000000], m = 2)
+print(data)
