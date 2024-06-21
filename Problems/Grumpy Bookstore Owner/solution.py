260ce01c28d3e3e1226c89d2735a9d30f385684e
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-21 19:14:51 +0530

    changes

--- Problems/Grumpy Bookstore Owner/solution.py
+++ Problems/Grumpy Bookstore Owner/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
-        pass
+        n=len(customers)
+        ans=mx=tamp=0
+        for i in range(n):
+            if not grumpy[i]:
+                ans+=customers[i]
+                customers[i]=0
+            else:
+                tamp+=customers[i]
+            if i>=minutes:
+                tamp-=customers[i-minutes] 
+            mx=max(mx,tamp)
+        return ans+mx
 
 obj = Solution()
 #data = obj.maxSatisfied(customers = [1,0,1,2,1,1,7,5], grumpy = [0,1,0,1,0,1,0,1], minutes = 3)
