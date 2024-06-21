e23d15774155bb964b9dccf11dbb763a4ffe0823
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-21 19:06:02 +0530

    Grumpy Bookstore Owner

---
+++ Problems/Grumpy Bookstore Owner/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxSatisfied(customers = [1,0,1,2,1,1,7,5], grumpy = [0,1,0,1,0,1,0,1], minutes = 3)
+data = obj.maxSatisfied(customers = [1], grumpy = [0], minutes = 1)
+print(data)
