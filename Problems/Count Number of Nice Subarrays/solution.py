55341f7dc111769f9b424961c83fcc284b012120
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-22 19:06:47 +0530

    Count Number of Nice Subarrays

---
+++ Problems/Count Number of Nice Subarrays/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numberOfSubarrays(nums = [1,1,2,1,1], k = 3)
+#data = obj.numberOfSubarrays(nums = [2,4,6], k = 1)
+data = obj.numberOfSubarrays(nums = [2,2,2,1,2,2,1,2,2,2], k = 2)
+print(data)
