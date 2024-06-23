4e4ff1ff490962ed147bb0ecb7bd4f54bdd1d419
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-23 20:36:16 +0530

    changes

--- Problems/Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/solution.py
+++ Problems/Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/solution.py
@@ -2,7 +2,15 @@ import io
 from typing import List
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        pass
+        n=len(nums)
+        i=0
+        ans=[]
+        for j in range(n):
+            bisect.insort(ans,nums[j])
+            if ans[-1]-ans[0]>limit:
+                ans.pop(bisect.bisect(ans,nums[i])-1)
+                i+=1
+        return j-i+1
 
 obj = Solution()
 #data = obj.longestSubarray(nums = [8,2,4,7], limit = 4)
