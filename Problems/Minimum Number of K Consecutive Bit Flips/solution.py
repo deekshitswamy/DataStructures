2df27696e6b36c962c62e610f1f79104a77d4585
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-24 21:27:43 +0530

    Minimum Number of K Consecutive Bit Flips

---
+++ Problems/Minimum Number of K Consecutive Bit Flips/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minKBitFlips(self, nums: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minKBitFlips(nums = [0,1,0], k = 1)
+#data = obj.minKBitFlips(nums = [1,1,0], k = 2)
+data = obj.minKBitFlips(nums = [0,0,0,1,0,1,1,0], k = 3)
+print(data)
