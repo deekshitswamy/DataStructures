da07b3d91118f7efa489ecef7f91d28c9a62fd3a
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-27 19:02:41 +0530

    changes

--- Problems/Find Center of Star Graph/solution.py
+++ Problems/Find Center of Star Graph/solution.py
@@ -2,7 +2,7 @@ import io
 from typing import List
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        pass
+        return (set(edges[0]) & set(edges[1])).pop()
 
 obj = Solution()
 #data = obj.findCenter(edges = [[1,2],[2,3],[4,2]])
