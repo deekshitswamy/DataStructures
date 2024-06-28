5b98b2a242c0b8dd4c9a99935b0362b283065d16
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-29 03:05:35 +0530

    changes

--- Problems/Maximum Total Importance of Roads/solution.py
+++ Problems/Maximum Total Importance of Roads/solution.py
@@ -2,7 +2,16 @@ import io
 from typing import List
 class Solution:
     def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
-        pass
+        degrees=[0]*n
+        for u,v in roads:
+            degrees[u]+=1
+            degrees[v]+=1
+        degrees.sort()
+        degrees.reverse()  
+        ans=0
+        for i,j in enumerate(degrees):
+            ans+=(n-i)*j
+        return ans
 
 obj = Solution()
 #data = obj.maximumImportance(n = 5, roads = [[0,1],[1,2],[2,3],[0,2],[1,3],[2,4]])
