c6f561931dfb2bd04947c6b3d3c1bde90835b1fb
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-29 03:04:19 +0530

    Maximum Total Importance of Roads

---
+++ Problems/Maximum Total Importance of Roads/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maximumImportance(n = 5, roads = [[0,1],[1,2],[2,3],[0,2],[1,3],[2,4]])
+data = obj.maximumImportance(n = 5, roads = [[0,3],[2,4],[1,3]])
+print(data)
