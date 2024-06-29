dca7c2faa9dee931c3190f2e362c8673abf776b8
Deeku-Zeus <deekuzeus21@gmail.com>
2024-06-29 20:07:55 +0530

    chanegs

--- Problems/All Ancestors of a Node in a Directed Acyclic Graph/solution.py
+++ Problems/All Ancestors of a Node in a Directed Acyclic Graph/solution.py
@@ -2,7 +2,28 @@ import io
 from typing import List
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
-        pass
+        graph=[[] for _ in range(n)]
+        indegrees=[0]*n
+        for edge in edges:
+            src=edge[0]
+            dest=edge[1]
+            graph[src].append(dest)
+            indegrees[dest]+=1
+        queue=deque()
+        ans=[set() for _ in range(n)]
+        for i in range(len(indegrees)):
+            if indegrees[i]==0:
+                queue.append(i)
+        while queue:
+            cur=queue.pop()
+            for neig in graph[cur]:
+                ans[neig].add(cur)
+                ans[neig].update(ans[cur])
+                indegrees[neig]-=1
+                if indegrees[neig]==0:
+                    queue.append(neig)
+        ans=[(sorted(list(s))) for s in ans]
+        return ans
 
 obj = Solution()
 #data = obj.getAncestors(n = 8, edgeList = [[0,3],[0,4],[1,3],[2,4],[2,7],[3,5],[3,6],[3,7],[4,6]])
