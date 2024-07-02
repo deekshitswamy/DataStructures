5359979c34812469f15fe191752fd0010e15a70e
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-02 20:22:44 +0530

    changes

--- Problems/Intersection of Two Arrays II/solution.py
+++ Problems/Intersection of Two Arrays II/solution.py
@@ -2,7 +2,22 @@ import io
 from typing import List
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        pass
+        i,j=0,0
+        ans=[]
+        nums1.sort()
+        nums2.sort()
+        n1=len(nums1)
+        n2=len(nums2)
+        while i<n1 and j<n2:
+            if nums1[i]<nums2[j]:
+                i+=1
+            elif nums1[i]>nums2[j]:
+                j+=1
+            else:
+                ans.append(nums1[i])
+                i+=1
+                j+=1
+        return ans
 
 obj = Solution()
 #data = obj.intersect(nums1 = [1,2,2,1], nums2 = [2,2])
