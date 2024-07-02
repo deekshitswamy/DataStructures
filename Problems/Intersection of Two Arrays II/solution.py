9bfe0e632a6017945de28ef2389f1e370d2c440e
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-02 20:19:56 +0530

    Intersection of Two Arrays II

---
+++ Problems/Intersection of Two Arrays II/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.intersect(nums1 = [1,2,2,1], nums2 = [2,2])
+data = obj.intersect(nums1 = [4,9,5], nums2 = [9,4,9,8,4])
+print(data)
