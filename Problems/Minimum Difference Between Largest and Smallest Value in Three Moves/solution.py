52ea14dd2204b9ddcb520facb137452ada3e5186
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-03 18:14:45 +0530

    changes

--- Problems/Minimum Difference Between Largest and Smallest Value in Three Moves/solution.py
+++ Problems/Minimum Difference Between Largest and Smallest Value in Three Moves/solution.py
@@ -2,7 +2,12 @@ import io
 from typing import List
 class Solution:
     def minDifference(self, nums: List[int]) -> int:
-        pass
+        n=len(nums)
+        if n<=4:
+            return 0
+        nums.sort()    
+        ans=min(nums[n-1]-nums[3],nums[n-2]-nums[2],nums[n-3]-nums[1],nums[n-4]-nums[0])
+        return ans
 
 obj = Solution()
 #data = obj.minDifference(nums = [5,3,2,4])
