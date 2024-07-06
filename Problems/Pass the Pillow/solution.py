b458573609393836a8614d3b8ed81887b83cb874
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-06 16:54:50 +0530

    Pass the Pillow

---
+++ Problems/Pass the Pillow/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.passThePillow(n = 4, time = 5)
+data = obj.passThePillow(n = 3, time = 2)
+print(data)
