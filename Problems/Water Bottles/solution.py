7afc80f9a1a36b828c5f07a04e5b2c5a0331a605
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-07 23:30:50 +0530

    Water Bottles

---
+++ Problems/Water Bottles/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def numWaterBottles(numBottles: int, numExchange: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numWaterBottles(numBottles = 9, numExchange = 3)
+data = obj.numWaterBottles(numBottles = 15, numExchange = 4)
+print(data)
