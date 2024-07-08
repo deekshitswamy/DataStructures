ba2afb8585157b9f8d5b3c9d395562a9a0b86d5f
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-08 20:45:00 +0530

    changes

--- Problems/Find the Winner of the Circular Game/solution.py
+++ Problems/Find the Winner of the Circular Game/solution.py
@@ -2,7 +2,13 @@ import io
 from typing import List
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        pass
+        vecArr = list(range(1, n + 1))
+        start = 0
+        while len(vecArr) > 1:
+            start = (start + k - 1) % len(vecArr)
+            vecArr.pop(start)
+        
+        return vecArr[0]
 
 obj = Solution()
 #data = obj.findTheWinner(n = 5, k = 2)
