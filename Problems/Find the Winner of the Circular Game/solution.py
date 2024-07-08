b9bd4dea10f5e72a51e296dc3741983ef0c23491
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-08 20:42:52 +0530

    Find the Winner of the Circular Game

---
+++ Problems/Find the Winner of the Circular Game/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findTheWinner(n = 5, k = 2)
+data = obj.findTheWinner(n = 6, k = 5)
+print(data)
