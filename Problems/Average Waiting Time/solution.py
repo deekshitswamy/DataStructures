45ba381d3c9207b73aa0405575c5200375dcd90d
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-09 19:48:04 +0530

    Average Waiting Time

---
+++ Problems/Average Waiting Time/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        pass
+
+obj = Solution()
+#data = obj.averageWaitingTime(customers = [[1,2],[2,5],[4,3]])
+data = obj.averageWaitingTime(customers = [[5,2],[5,4],[10,3],[20,1]])
+print(data)
