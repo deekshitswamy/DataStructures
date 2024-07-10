1a64998cb5eba344791dd4e5439fc5419748555c
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-10 20:53:45 +0530

    changes

--- Problems/Crawler Log Folder/solution.py
+++ Problems/Crawler Log Folder/solution.py
@@ -2,7 +2,16 @@ import io
 from typing import List
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
-        pass
+        pointer = 0
+        for log in logs:
+            if log == '../':
+                if pointer>0:
+                    pointer-=1
+            elif log =='./':
+                continue
+            else:
+                pointer+=1
+        return pointer
 
 obj = Solution()
 #data = obj.minOperations(logs = ["d1/","d2/","../","d21/","./"])
