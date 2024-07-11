70b39c5a4d8a9039d228daf6f230b248ec12669d
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-11 23:19:31 +0530

    Reverse Substrings Between Each Pair of Parentheses

---
+++ Problems/Reverse Substrings Between Each Pair of Parentheses/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.reverseParentheses(s = "(abcd)")
+#data = obj.reverseParentheses(s = "(u(love)i)")
+data = obj.reverseParentheses(s = "(ed(et(oc))el)")
+print(data)
