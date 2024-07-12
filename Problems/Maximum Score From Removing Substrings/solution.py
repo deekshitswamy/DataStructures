b040bd727ece1d37c87c5e514788ad1e3a8d4f10
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-12 18:40:28 +0530

    changes

--- Problems/Maximum Score From Removing Substrings/solution.py
+++ Problems/Maximum Score From Removing Substrings/solution.py
@@ -2,7 +2,37 @@ import io
 from typing import List
 class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
-        pass
+        if x < y: #swap a, b
+            s = s.replace('a', '_')
+            s = s.replace('b', 'a')
+            s = s.replace('_', 'b')
+            x, y = y, x
+        
+        # x >= y
+        ss = ''.join([x if x == 'a' or x == 'b' else '_' for x in s])
+        ss = ss.split('_')
+        print(ss)
+
+        ans = 0
+        for s in ss:
+            totalChange = min(s.count('a'), s.count('b')) # z
+            # print(s, totalChange)
+            numAs = 0 
+            numBs = 0
+
+            cur = 0 # v
+            for c in s:
+                if c == 'a':
+                    numAs += 1
+                elif c == 'b':
+                    if numAs > 0:
+                        cur += 1 # counting the number of pairings
+                        numAs -= 1
+            
+            ans += cur*x
+            ans += (totalChange - cur)* y
+        
+        return ans
 
 obj = Solution()
 #data = obj.maximumGain(s = "cdbcbbaaabab", x = 4, y = 5)
