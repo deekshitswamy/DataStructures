a5af8905e46b6b9d993ddecaa9d348aeadc66223
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-12 18:38:41 +0530

    Maximum Score From Removing Substrings

---
+++ Problems/Maximum Score From Removing Substrings/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maximumGain(self, s: str, x: int, y: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maximumGain(s = "cdbcbbaaabab", x = 4, y = 5)
+data = obj.maximumGain(s = "aabbaaxybbaabb", x = 5, y = 4)
+print(data)
