609bc53d0d584526158576b9006643f30bd9c090
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-14 15:57:41 +0530

    Number of Atoms

---
+++ Problems/Number of Atoms/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def countOfAtoms(self, formula: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.countOfAtoms(formula = "H2O")
+#data = obj.countOfAtoms(formula = "Mg(OH)2")
+data = obj.countOfAtoms(formula = "K4(ON(SO3)2)2")
+print(data)
