4ab97d533572efe034d9a80b055277ed4f986e24
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-15 21:54:25 +0530

    changes

--- Problems/Create Binary Tree From Descriptions/solution.py
+++ Problems/Create Binary Tree From Descriptions/solution.py
@@ -9,7 +9,21 @@ class TreeNode:
 
 class Solution:
     def createBinaryTree(self, descriptions: List[List[int]]) -> TreeNode:
-        pass
+        ans={}
+        seen=set()
+        for x,y,left in descriptions:
+            if x not in ans:
+                ans[x]=TreeNode(x)
+            if y not in ans:
+                ans[y]=TreeNode(y)
+            if left:
+                ans[x].left=ans[y]
+            else:
+                ans[x].right=ans[y]
+            seen.add(y)
+        for x,_,_ in descriptions:
+            if x not in seen:
+                return ans[x]
 
 obj = Solution()
 #data = obj.createBinaryTree(descriptions = [[20,15,1],[20,17,0],[50,20,1],[50,80,0],[80,19,1]])
