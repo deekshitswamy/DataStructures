12be568d42f1447b613ae263f2369e8a481d5b4b
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-17 20:06:36 +0530

    changes

--- Problems/Delete Nodes And Return Forest/solution.py
+++ Problems/Delete Nodes And Return Forest/solution.py
@@ -9,7 +9,26 @@ class TreeNode:
 
 class Solution:
     def delNodes(self, root: TreeNode, to_delete: List[int]) -> List[TreeNode]:
-        pass
+        result = []
+        def dfs(parent: TreeNode, node: TreeNode):
+            nonlocal result
+            if not node:
+                return
+            if node.val in to_delete:
+                if parent:
+                    if parent.left == node:
+                        parent.left = None
+                    else:
+                        parent.right = None
+                dfs(None, node.left)
+                dfs(None, node.right)
+            else:
+                if not parent:
+                    result.append(node)
+                dfs(node, node.left)
+                dfs(node, node.right)
+        dfs(None, root)
+        return result
 
 obj = Solution()
 #data = obj.delNodes(root = [1,2,3,4,5,6,7], to_delete = [3,5])
