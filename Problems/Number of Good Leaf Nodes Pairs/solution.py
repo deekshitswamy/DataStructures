1334718a5add291bc30deefa45efd78b838ec677
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-18 18:17:12 +0530

    changes

--- Problems/Number of Good Leaf Nodes Pairs/solution.py
+++ Problems/Number of Good Leaf Nodes Pairs/solution.py
@@ -9,7 +9,19 @@ class TreeNode:
 
 class Solution:
     def countPairs(self, root: TreeNode, distance: int) -> int:
-        pass
+        self.ans = 0
+        def dfs(root):
+            if not root: return []
+            if not root.left and not root.right: return [1]
+            left = dfs(root.left)
+            right = dfs(root.right)
+            for i in left:
+                for j in right:
+                    if i+j <= distance:
+                        self.ans += 1
+            return [i+1 for i in left+right if i+1 < distance]
+        dfs(root)
+        return self.ans
 
 obj = Solution()
 #data = obj.countPairs(root = [1,2,3,None,4], distance = 3)
