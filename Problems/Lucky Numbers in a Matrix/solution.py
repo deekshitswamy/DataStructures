93173b3ea657cf90f54f6895fda7b4ceb7395f89
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-19 18:36:19 +0530

    changes

--- Problems/Lucky Numbers in a Matrix/solution.py
+++ Problems/Lucky Numbers in a Matrix/solution.py
@@ -2,7 +2,13 @@ import io
 from typing import List
 class Solution:
     def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
-        pass
+        mn=[min(i) for i in matrix]
+        mx,ans=[],[]
+        for i in range(len(matrix[0])):mx.append(max(matrix[j][i] for j in range(len(matrix))))
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j]==mn[i]==mx[j]:ans.append(matrix[i][j])
+        return ans
 
 obj = Solution()
 #data = obj.luckyNumbers(matrix = [[3,7,8],[9,11,13],[15,16,17]])
