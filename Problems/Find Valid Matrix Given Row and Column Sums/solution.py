8ce1fa823a286e37f981a0dc7e588a086c1af13e
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-20 12:19:43 +0530

    changes

--- Problems/Find Valid Matrix Given Row and Column Sums/solution.py
+++ Problems/Find Valid Matrix Given Row and Column Sums/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
-        pass
+        r, c=len(rowSum), len(colSum)
+        arr=[[0]*c for _ in range(r)]
+        i, j=0, 0
+        while i<r and j<c:
+            x=min(rowSum[i], colSum[j])
+            arr[i][j]=x
+            rowSum[i]-=x
+            colSum[j]-=x
+            i+=(rowSum[i]==0)
+            j+=(colSum[j]==0)
+        return arr
 
 obj = Solution()
 #data = obj.restoreMatrix(rowSum = [3,8], colSum = [4,7])
