0a27c42aaf5fb590aeb527939f8899d1802207a6
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-22 19:49:27 +0530

    Sort the People

---
+++ Problems/Sort the People/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        pass
+
+obj = Solution()
+#data = obj.sortPeople(names = ["Mary","John","Emma"], heights = [180,165,170])
+data = obj.sortPeople(names = ["Alice","Bob","Bob"], heights = [155,185,150])
+print(data)
