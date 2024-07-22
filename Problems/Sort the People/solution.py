efd791fb8253357ec5d4ae43f5511b5c1719c507
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-22 19:50:17 +0530

    changes

--- Problems/Sort the People/solution.py
+++ Problems/Sort the People/solution.py
@@ -2,7 +2,13 @@ import io
 from typing import List
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        pass
+        arr = []
+        final = []
+        for val1, val2 in zip(names, heights):
+            obj = (val2, val1)
+            arr.append(obj)
+        arr.sort(reverse=True)
+        return [item[1] for item in arr]
 
 obj = Solution()
 #data = obj.sortPeople(names = ["Mary","John","Emma"], heights = [180,165,170])
