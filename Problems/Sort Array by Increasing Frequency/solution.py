5baa26c1dad9dc3905513ad77a3076b7aa93d9f0
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-23 18:47:24 +0530

    Sort Array by Increasing Frequency

---
+++ Problems/Sort Array by Increasing Frequency/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.frequencySort(nums = [1,1,2,2,2,3])
+#data = obj.frequencySort(nums = [2,3,1,3,2])
+data = obj.frequencySort(nums = [-1,1,-6,4,5,-6,1,4,1])
+print(data)
