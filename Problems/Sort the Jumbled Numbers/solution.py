b3a16eacdba0f031df6704b0c779e4fd670601e6
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-24 19:12:54 +0530

    Sort the Jumbled Numbers

---
+++ Problems/Sort the Jumbled Numbers/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.sortJumbled(mapping = [8,9,4,0,2,1,3,5,7,6], nums = [991,338,38])
+data = obj.sortJumbled(mapping = [0,1,2,3,4,5,6,7,8,9], nums = [789,456,123])
+print(data)
