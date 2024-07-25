10582a030a919634b2281022258e83401327d9e9
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-25 20:38:57 +0530

    Sort an Array

---
+++ Problems/Sort an Array/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.sortArray(nums = [5,2,3,1])
+data = obj.sortArray(nums = [5,1,1,2,0,0])
+print(data)
