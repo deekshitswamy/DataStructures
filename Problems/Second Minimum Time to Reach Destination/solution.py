6b6c1fd992fb8ee8762384b30a89e890aadbacbb
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-28 20:05:36 +0530

    Second Minimum Time to Reach Destination

---
+++ Problems/Second Minimum Time to Reach Destination/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def secondMinimum(self, n: int, edges: List[List[int]], time: int, change: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.secondMinimum(n = 5, edges = [[1,2],[1,3],[1,4],[3,4],[4,5]], time = 3, change = 5)
+data = obj.secondMinimum(n = 2, edges = [[1,2]], time = 3, change = 2)
+print(data)
