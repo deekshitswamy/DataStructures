cff33912a801fa8ce8822ee3507866f42f7983d0
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-29 18:11:01 +0530

    changes

--- Problems/Count Number of Teams/solution.py
+++ Problems/Count Number of Teams/solution.py
@@ -2,7 +2,27 @@ import io
 from typing import List
 class Solution:
     def numTeams(self, rating: List[int]) -> int:
-        pass
+        n = len(rating)
+        count = 0
+        
+        for j in range(n):
+            left_smaller = left_larger = right_smaller = right_larger = 0
+            
+            for i in range(j):
+                if rating[i] < rating[j]:
+                    left_smaller += 1
+                elif rating[i] > rating[j]:
+                    left_larger += 1
+            
+            for k in range(j + 1, n):
+                if rating[k] < rating[j]:
+                    right_smaller += 1
+                elif rating[k] > rating[j]:
+                    right_larger += 1
+            
+            count += left_smaller * right_larger + left_larger * right_smaller
+        
+        return count
 
 obj = Solution()
 #data = obj.numTeams(rating = [2,5,3,4,1])
