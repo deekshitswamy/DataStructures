12f5f070c9e4c71db73eccbe4780485a87d3e273
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-29 18:10:04 +0530

    Count Number of Teams

---
+++ Problems/Count Number of Teams/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def numTeams(self, rating: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numTeams(rating = [2,5,3,4,1])
+#data = obj.numTeams(rating = [2,1,3])
+data = obj.numTeams(rating = [1,2,3,4])
+print(data)
