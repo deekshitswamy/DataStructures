54699f7ec0df7ba131f642b2232ec75502226e99
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-31 18:00:47 +0530

    Filling Bookcase Shelves

---
+++ Problems/Filling Bookcase Shelves/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minHeightShelves(books = [[1,1],[2,3],[2,3],[1,1],[1,1],[1,1],[1,2]], shelfWidth = 4)
+data = obj.minHeightShelves(books = [[1,3],[2,4],[3,2]], shelfWidth = 6)
+print(data)
