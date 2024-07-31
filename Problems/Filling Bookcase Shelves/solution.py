8479a033456bcb2f4c38dd3e44e424757798c4b1
Deeku-Zeus <deekuzeus21@gmail.com>
2024-07-31 18:02:53 +0530

    changes

--- Problems/Filling Bookcase Shelves/solution.py
+++ Problems/Filling Bookcase Shelves/solution.py
@@ -2,7 +2,21 @@ import io
 from typing import List
 class Solution:
     def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
-        pass
+        n = len(books)
+        dp = [float('inf')] * (n + 1)
+        dp[0] = 0
+
+        for i in range(1, n + 1):
+            width = 0
+            height = 0
+            for j in range(i, 0, -1):
+                width += books[j - 1][0]
+                if width > shelfWidth:
+                    break
+                height = max(height, books[j - 1][1])
+                dp[i] = min(dp[i], dp[j - 1] + height)
+
+        return dp[n]
 
 obj = Solution()
 #data = obj.minHeightShelves(books = [[1,1],[2,3],[2,3],[1,1],[1,1],[1,1],[1,2]], shelfWidth = 4)
