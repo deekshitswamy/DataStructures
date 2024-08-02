91c35dbd725f171f2d4e4cb2af9fc6a68f38d817
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-02 17:40:44 +0530

    Minimum Swaps to Group All 1's Together II

---
+++ Problems/Minimum Swaps to Group All 1's Together II/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minSwaps(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minSwaps(nums = [0,1,0,1,1,0,0])
+#data = obj.minSwaps(nums = [0,1,1,1,0,0,1,1,0])
+data = obj.minSwaps(nums = [1,1,0,0,1])
+print(data)
