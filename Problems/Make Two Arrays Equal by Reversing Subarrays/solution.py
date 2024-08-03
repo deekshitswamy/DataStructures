f18de0a9a1c560c1629ddc112d0fe87f29596278
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-03 18:03:28 +0530

    Make Two Arrays Equal by Reversing Subarrays

---
+++ Problems/Make Two Arrays Equal by Reversing Subarrays/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.canBeEqual(target = [1,2,3,4], arr = [2,4,1,3])
+#data = obj.canBeEqual(target = [7], arr = [7])
+data = obj.canBeEqual(target = [3,7,9], arr = [3,7,11])
+print(data)
