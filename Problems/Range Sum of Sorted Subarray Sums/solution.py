cc8985c2f5ce00db76485ae94c6c47de98bf7390
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-04 20:36:30 +0530

    changes

--- Problems/Range Sum of Sorted Subarray Sums/solution.py
+++ Problems/Range Sum of Sorted Subarray Sums/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
-        pass
+        minHeap = [(n, i) for i, n in enumerate(nums)]
+        res = 0
+        MOD = 10 ** 9 + 7
+        heapq.heapify(minHeap)
+
+        for i in range(right):
+            num, index = heapq.heappop(minHeap)
+            if i >= left - 1:
+                res = (res + num) % MOD
+            if index + 1 < n:
+                next = (num + nums[index+1], index + 1)
+                heapq.heappush(minHeap, next)
+
+        return res
 
 obj = Solution()
 #data = obj.rangeSum(nums = [1,2,3,4], n = 4, left = 1, right = 5)
