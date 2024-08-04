23867c003ee1a3865252b951d98f3f7c1ba8c36b
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-04 20:35:45 +0530

    Range Sum of Sorted Subarray Sums

---
+++ Problems/Range Sum of Sorted Subarray Sums/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.rangeSum(nums = [1,2,3,4], n = 4, left = 1, right = 5)
+#data = obj.rangeSum(nums = [1,2,3,4], n = 4, left = 3, right = 4)
+data = obj.rangeSum(nums = [1,2,3,4], n = 4, left = 1, right = 10)
+print(data)
