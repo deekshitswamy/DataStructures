bc8bcaabcf2c342d91667df13cdb39abf6c7ea32
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-05 18:49:11 +0530

    Kth Distinct String in an Array

---
+++ Problems/Kth Distinct String in an Array/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        pass
+
+obj = Solution()
+#data = obj.kthDistinct(arr = ["d","b","c","b","c","a"], k = 2)
+#data = obj.kthDistinct(arr = ["aaa","aa","a"], k = 1)
+data = obj.kthDistinct(arr = ["a","b","a"], k = 3)
+print(data)
