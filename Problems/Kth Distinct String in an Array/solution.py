84ffec35b8afb1fa88a446b113d7e2da91b05e40
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-05 18:50:00 +0530

    changes

--- Problems/Kth Distinct String in an Array/solution.py
+++ Problems/Kth Distinct String in an Array/solution.py
@@ -2,7 +2,27 @@ import io
 from typing import List
 class Solution:
     def kthDistinct(self, arr: List[str], k: int) -> str:
-        pass
+        distinct = set()
+        nonDistinct = set()
+        
+        for s in arr:
+            if s not in nonDistinct:
+                if s in distinct:
+                    distinct.remove(s)
+                    nonDistinct.add(s)
+                else:
+                    distinct.add(s)
+        
+        if len(distinct) < k:
+            return ""
+        
+        for s in arr:
+            if s in distinct:
+                k -= 1
+                if k == 0:
+                    return s
+        
+        return ""
 
 obj = Solution()
 #data = obj.kthDistinct(arr = ["d","b","c","b","c","a"], k = 2)
