4de9239c3cd8333184ccee2e3e1f24ee6d0ca80d
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-07 18:20:17 +0530

    Minimum Number of Pushes to Type Word II

---
+++ Problems/Minimum Number of Pushes to Type Word II/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minimumPushes(word = "abcde")
+#data = obj.minimumPushes(word = "xyzxyzxyzxyz")
+data = obj.minimumPushes(word = "aabbccddeeffgghhiiiiii")
+print(data)
