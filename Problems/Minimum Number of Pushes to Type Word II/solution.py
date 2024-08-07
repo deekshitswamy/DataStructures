dbf8ea2b8aadd7fc3f527dea1af69f3bd17ba0c0
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-07 18:20:58 +0530

    changes

--- Problems/Minimum Number of Pushes to Type Word II/solution.py
+++ Problems/Minimum Number of Pushes to Type Word II/solution.py
@@ -2,7 +2,22 @@ import io
 from typing import List
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        pass
+        d={}
+        for i in set(word):
+            d[i]=word.count(i)
+        print(d)
+        dd=sorted(d.items(),key=lambda x:x[1],reverse=True)
+        print(dd)
+        c=0
+        j=1
+        x=1
+        for i in dd:
+            c+=i[1]*x
+            j+=1
+            if j>8:
+                j=1
+                x+=1
+        return c
 
 obj = Solution()
 #data = obj.minimumPushes(word = "abcde")
