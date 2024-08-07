eb3885cb61b2e769a31486223a9c39fbaf5ca635
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-07 18:18:36 +0530

    changes

--- Problems/Integer to English Words/solution.py
+++ Problems/Integer to English Words/solution.py
@@ -2,7 +2,50 @@ import io
 from typing import List
 class Solution:
     def numberToWords(self, num: int) -> str:
-        pass
+        less_than_twenty = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+        ten_places = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+
+        if num == 0:
+            return "Zero"
+
+        def two_digit(num):
+            if num < 20:
+                return less_than_twenty[num]
+            else:
+                tens = num // 10
+                ones = num % 10
+                return ten_places[tens] + ('' if ones == 0 else ' ' + less_than_twenty[ones])
+
+        def three_digit(num):
+            if not num: return ''
+            if not num//100: return two_digit(num)
+            return less_than_twenty[num//100] + ' ' +'Hundred' + (' ' + two_digit(num%100) if num%100 else '')
+
+        billion = num // 1000000000
+        million = (num // 1000000) % 1000
+        thousand = (num // 1000) % 1000
+        hundred = num % 1000
+
+        res = ''
+        if billion:
+            res += three_digit(billion) + ' Billion'
+        
+        if million:
+            if res:
+                res += ' '
+            res += three_digit(million) + ' Million'
+        
+        if thousand:
+            if res:
+                res += ' '
+            res += three_digit(thousand) + ' Thousand'
+        
+        if hundred:
+            if res:
+                res += ' '
+            res += three_digit(hundred)
+
+        return res.strip()
 
 obj = Solution()
 #data = obj.numberToWords(num = 123)
