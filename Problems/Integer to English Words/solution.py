596de74e388c3e7fe91b5dc92943c0efeb196894
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-07 18:17:50 +0530

    Integer to English Words

---
+++ Problems/Integer to English Words/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def numberToWords(self, num: int) -> str:
+        pass
+
+obj = Solution()
+#data = obj.numberToWords(num = 123)
+#data = obj.numberToWords(num = 12345)
+data = obj.numberToWords(num = 1234567)
+print(data)
