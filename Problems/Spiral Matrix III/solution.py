17b04f4243ef228c53a077861ea3ae04bc254317
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-08 19:42:12 +0530

    Spiral Matrix III

---
+++ Problems/Spiral Matrix III/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
+        pass
+
+obj = Solution()
+#data = obj.spiralMatrixIII(rows = 1, cols = 4, rStart = 0, cStart = 0)
+data = obj.spiralMatrixIII(rows = 5, cols = 6, rStart = 1, cStart = 4)
+print(data)
