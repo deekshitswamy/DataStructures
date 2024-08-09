d1c028da77f85cc3c4608f9492a87bab4f18590f
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-09 18:48:16 +0530

    Magic Squares In Grid

---
+++ Problems/Magic Squares In Grid/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numMagicSquaresInside(grid = [[4,3,8,4],[9,5,1,9],[2,7,6,2]])
+data = obj.numMagicSquaresInside(grid = [[8]])
+print(data)
