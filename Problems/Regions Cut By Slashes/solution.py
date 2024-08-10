093b54e34623a35b00d9ebf088bad0693ba09f1b
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-10 21:40:52 +0530

    Regions Cut By Slashes

---
+++ Problems/Regions Cut By Slashes/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def regionsBySlashes(self, grid: List[str]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.regionsBySlashes(grid = [" /","/ "])
+#data = obj.regionsBySlashes(grid = [" /","  "])
+data = obj.regionsBySlashes(grid = ["/\\","\\/"])
+print(data)
