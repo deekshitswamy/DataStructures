4b9a0163bc3f11ffbc19a095fe45ce0a4a3a89c8
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-11 17:58:21 +0530

    Minimum Number of Days to Disconnect Island

---
+++ Problems/Minimum Number of Days to Disconnect Island/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minDays(self, grid: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minDays(grid = [[0,1,1,0],[0,1,1,0],[0,0,0,0]])
+data = obj.minDays(grid = [[1,1]])
+print(data)
