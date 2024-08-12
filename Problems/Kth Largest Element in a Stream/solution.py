fd45bbb9811fe7f073722064ddd328b69f2918ca
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-13 00:44:40 +0530

    Kth Largest Element in a Stream

---
+++ Problems/Kth Largest Element in a Stream/solution.py
@@ -0,0 +1,15 @@
+import io
+from typing import List
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        pass
+
+    def add(self, val: int) -> int:
+        pass
+
+
+# Your KthLargest object will be instantiated and called as such:
+obj = KthLargest(3, [4, 5, 8, 2])
+param_1 = obj.add(3)
+print(param_1)
