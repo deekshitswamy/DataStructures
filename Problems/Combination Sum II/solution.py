e9334722256dbd1d36af51c6a793acd5b41b4dbe
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-13 19:29:29 +0530

    Combination Sum II

--- Problems/Combination Sum II/solution.py
+++ Problems/Combination Sum II/solution.py
@@ -2,7 +2,24 @@ import io
 from typing import List
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        pass
+        res, curr = [], []
+        n = len(candidates)
+        candidates.sort()
+
+        def backtrack(index, total):
+            if total == target:
+                res.append(curr.copy())
+            if total >= target or index == n:
+                return
+            for i in range(index, n):
+                if i > index and candidates[i] == candidates[i-1]:
+                    continue
+                curr.append(candidates[i])
+                backtrack(i + 1, total + candidates[i])
+                curr.pop()
+        
+        backtrack(0, 0)
+        return res
 
 obj = Solution()
 #data = obj.combinationSum2(candidates = [10,1,2,7,6,1,5], target = 8)
