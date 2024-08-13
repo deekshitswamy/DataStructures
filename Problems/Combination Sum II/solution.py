04cfa581249064986a274c5890521287a255c16b
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-13 19:28:59 +0530

    Combination Sum II

---
+++ Problems/Combination Sum II/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        pass
+
+obj = Solution()
+#data = obj.combinationSum2(candidates = [10,1,2,7,6,1,5], target = 8)
+data = obj.combinationSum2(candidates = [2,5,2,1,2], target = 5)
+print(data)
