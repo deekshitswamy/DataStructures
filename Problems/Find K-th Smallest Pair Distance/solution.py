168a475610442e6e0e4ff28aa6872c2451554326
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-14 17:22:29 +0530

    Find K-th Smallest Pair Distance

---
+++ Problems/Find K-th Smallest Pair Distance/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def smallestDistancePair(self, nums: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.smallestDistancePair(nums = [1,3,1], k = 1)
+#data = obj.smallestDistancePair(nums = [1,1,1], k = 2)
+data = obj.smallestDistancePair(nums = [1,6,1], k = 3)
+print(data)
