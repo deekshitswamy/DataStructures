795421e26b3def9ee049e67830c52cec51d1ad44
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-14 17:23:01 +0530

    changes

--- Problems/Find K-th Smallest Pair Distance/solution.py
+++ Problems/Find K-th Smallest Pair Distance/solution.py
@@ -2,7 +2,28 @@ import io
 from typing import List
 class Solution:
     def smallestDistancePair(self, nums: List[int], k: int) -> int:
-        pass
+        def countPairs(nums, mid):
+            count = 0
+            left = 0
+            for right in range(len(nums)):
+                while nums[right] - nums[left] > mid:
+                    left += 1
+                count += right - left
+            return count
+        
+        nums.sort()
+        left, right = 0, nums[-1] - nums[0]
+
+        while left < right:
+            mid = (left + right) // 2
+            count = countPairs(nums, mid)
+
+            if count < k:
+                left = mid + 1
+            else:
+                right = mid
+
+        return left
 
 obj = Solution()
 #data = obj.smallestDistancePair(nums = [1,3,1], k = 1)
