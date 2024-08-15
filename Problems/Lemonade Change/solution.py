54c5ca1f5748b835d800f1c78b7c98e764def0bb
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-16 01:51:40 +0530

    Lemonade Change

---
+++ Problems/Lemonade Change/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.lemonadeChange(bills = [5,5,5,10,20])
+data = obj.lemonadeChange(bills = [5,5,10,10,20])
+print(data)
