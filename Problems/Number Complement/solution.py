c944acecbde4c439788391eaaff55fdcaada8803
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-22 20:11:41 +0530

    Number Complement

---
+++ Problems/Number Complement/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def findComplement(self, num: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findComplement(n = 1)
+#data = obj.findComplement(n = 2)
+data = obj.findComplement(n = 2)
+print(data)
