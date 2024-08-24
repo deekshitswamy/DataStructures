bc707e9b04a20e2e2d8f659f87934e8457e5d716
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-24 19:47:14 +0530

    Find the Closest Palindrome

---
+++ Problems/Find the Closest Palindrome/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def nearestPalindromic(self, n: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.nearestPalindromic(n = "123")
+data = obj.nearestPalindromic(n = "1")
+print(data)
