afe2cf81dacd1c39679ea09feedcb666651aa2a0
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-28 23:46:14 +0530

    Count Sub Islands

---
+++ Problems/Count Sub Islands/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countSubIslands(grid1 = [[1,1,1,0,0],[0,1,1,1,1],[0,0,0,0,0],[1,0,0,0,0],[1,1,0,1,1]], grid2 = [[1,1,1,0,0],[0,0,1,1,1],[0,1,0,0,0],[1,0,1,1,0],[0,1,0,1,0]])
+data = obj.countSubIslands(grid1 = [[1,0,1,0,1],[1,1,1,1,1],[0,0,0,0,0],[1,1,1,1,1],[1,0,1,0,1]], grid2 = [[0,0,0,0,0],[1,1,1,1,1],[0,1,0,1,0],[0,1,0,1,0],[1,0,0,0,1]])
+print(data)
