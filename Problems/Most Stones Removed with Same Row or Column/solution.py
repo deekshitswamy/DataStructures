048e7ced90fb242a6f560c06628bd280964bd976
Deeku-Zeus <deekuzeus21@gmail.com>
2024-08-29 19:53:08 +0530

    Most Stones Removed with Same Row or Column

---
+++ Problems/Most Stones Removed with Same Row or Column/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.removeStones(stones = [[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]])
+#data = obj.removeStones(stones = [[0,0],[0,2],[1,1],[2,0],[2,2]])
+data = obj.removeStones(stones = [[0,0]])
+print(data)
