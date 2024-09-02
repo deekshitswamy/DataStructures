13964f3d301b66872754bcc78ef7ac3ceadafab3
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-02 23:46:14 +0530

    Find the Student that Will Replace the Chalk

---
+++ Problems/Find the Student that Will Replace the Chalk/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def chalkReplacer(self, chalk: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.chalkReplacer(chalk = [5,1,5], k = 22)
+data = obj.chalkReplacer(chalk = [3,4,1,2], k = 25)
+print(data)
