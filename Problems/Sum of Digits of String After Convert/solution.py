26f703039895c6cd96d9994943f6537e16b22566
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-04 22:07:16 +0530

    Sum of Digits of String After Convert

---
+++ Problems/Sum of Digits of String After Convert/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.getLucky(s = "iiii", k = 1)
+#data = obj.getLucky(s = "leetcode", k = 2)
+data = obj.getLucky(s = "zbax", k = 2)
+print(data)
