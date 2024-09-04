60648285037f5258dc1e833f2e0433ab719046a4
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-04 22:04:06 +0530

    Walking Robot Simulation

---
+++ Problems/Walking Robot Simulation/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.robotSim(commands = [4,-1,3], obstacles = [])
+#data = obj.robotSim(commands = [4,-1,4,-2,4], obstacles = [[2,4]])
+data = obj.robotSim(commands = [6,-1,-1,6], obstacles = [])
+print(data)
