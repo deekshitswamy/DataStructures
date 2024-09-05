871a6034e4d94161964d2217f10722f4e54e4076
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-05 23:53:58 +0530

    Find Missing Observations

---
+++ Problems/Find Missing Observations/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.missingRolls(rolls = [3,2,4,3], mean = 4, n = 2)
+#data = obj.missingRolls(rolls = [1,5,6], mean = 3, n = 4)
+data = obj.missingRolls(rolls = [1,2,3,4], mean = 6, n = 4)
+print(data)
