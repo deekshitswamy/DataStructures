a43e81e32f9c88594bd22a92245c25a18a596b2a
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-05 23:55:01 +0530

    changes

--- Problems/Find Missing Observations/solution.py
+++ Problems/Find Missing Observations/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
-        pass
+        m=len(rolls)
+        totalsum=mean*(n+m)
+        missingsum=totalsum-sum(rolls)
+        if missingsum>6*n or missingsum<n:
+            return []
+        res=[]
+        while n:
+            curr=min(6,missingsum-n+1)
+            res.append(curr)
+            missingsum-=curr
+            n-=1
+        return res
 
 obj = Solution()
 #data = obj.missingRolls(rolls = [3,2,4,3], mean = 4, n = 2)
