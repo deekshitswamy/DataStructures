afab609730ac0d62713809cf97719c52d392d72e
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-10 21:16:43 +0530

    Insert Greatest Common Divisors in Linked List

---
+++ Problems/Insert Greatest Common Divisors in Linked List/solution.py
@@ -0,0 +1,15 @@
+import io
+from typing import List
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def insertGreatestCommonDivisors(self, head: ListNode) -> ListNode:
+        pass
+
+obj = Solution()
+#data = obj.insertGreatestCommonDivisors(head = [18,6,10,3])
+data = obj.insertGreatestCommonDivisors(head = [7])
+print(data)
