878ee9dfc7ab83885f75b718ed5a5031a5b40919
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-11 21:08:56 +0530

    Minimum Bit Flips to Convert Number

---
+++ Problems/Minimum Bit Flips to Convert Number/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minBitFlips(start = 10, goal = 7)
+data = obj.minBitFlips(start = 3, goal = 4)
+print(data)
