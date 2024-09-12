3e77045302bd24b048da6268985bef4350b0b1ea
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-12 19:24:42 +0530

    changes

--- Problems/Count the Number of Consistent Strings/solution.py
+++ Problems/Count the Number of Consistent Strings/solution.py
@@ -2,7 +2,14 @@ import io
 from typing import List
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
-        pass
+        allowed_set = set(allowed)  # Convert allowed string to a set for O(1) lookups
+        consistent_count = 0
+        
+        for word in words:
+            if all(char in allowed_set for char in word):
+                consistent_count += 1
+        
+        return consistent_count
 
 obj = Solution()
 #data = obj.countConsistentStrings(allowed = "ab", words = ["ad","bd","aaab","baa","badab"])
