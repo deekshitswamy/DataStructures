f2aa684fbe990566879c5c1abf60d18e2c2fd6a3
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-13 18:29:16 +0530

    XOR Queries of a Subarray

---
+++ Problems/XOR Queries of a Subarray/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.xorQueries(arr = [1,3,4,8], queries = [[0,1],[1,2],[0,3],[3,3]])
+data = obj.xorQueries(arr = [4,8,2,10], queries = [[2,3],[1,3],[0,0],[0,3]])
+print(data)
