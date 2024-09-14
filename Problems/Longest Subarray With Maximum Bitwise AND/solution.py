b376aebb1ee7fa6e64ce91b44f6114c8d11e1308
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-14 20:50:40 +0530

    Longest Subarray With Maximum Bitwise AND

---
+++ Problems/Longest Subarray With Maximum Bitwise AND/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.longestSubarray(nums = [1,2,3,3,2,2])
+data = obj.longestSubarray(nums = [1,2,3,4])
+print(data)
