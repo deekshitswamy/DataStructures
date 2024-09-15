41af215cd43c002bb8d12dc6e020ce5e709e99d9
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-15 19:18:51 +0530

    Find the Longest Substring Containing Vowels in Even Counts

---
+++ Problems/Find the Longest Substring Containing Vowels in Even Counts/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def findTheLongestSubstring(self, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findTheLongestSubstring(s = "eleetminicoworoep")
+#data = obj.findTheLongestSubstring(s = "leetcodeisgreat")
+data = obj.findTheLongestSubstring(s = "bcbcbc")
+print(data)
