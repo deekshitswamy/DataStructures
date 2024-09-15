1f01b7c8b291befb465db9d401d75cb93e1c5278
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-15 19:19:52 +0530

    changes

--- Problems/Find the Longest Substring Containing Vowels in Even Counts/solution.py
+++ Problems/Find the Longest Substring Containing Vowels in Even Counts/solution.py
@@ -2,7 +2,30 @@ import io
 from typing import List
 class Solution:
     def findTheLongestSubstring(self, s: str) -> int:
-        pass
+        mask = 0
+        first_occurrence = {0: -1}  # base case for mask=0
+        max_len = 0
+        
+        for i, char in enumerate(s):
+            # Update the mask based on the current character
+            if char == 'a':
+                mask ^= 1
+            elif char == 'e':
+                mask ^= 2
+            elif char == 'i':
+                mask ^= 4
+            elif char == 'o':
+                mask ^= 8
+            elif char == 'u':
+                mask ^= 16
+
+            # Check if this mask was seen before
+            if mask in first_occurrence:
+                max_len = max(max_len, i - first_occurrence[mask])
+            else:
+                first_occurrence[mask] = i  # store first occurrence of this mask
+        
+        return max_len
 
 obj = Solution()
 #data = obj.findTheLongestSubstring(s = "eleetminicoworoep")
