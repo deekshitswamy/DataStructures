c6e75485a9387acf0b994ccb6ae099ab7495b944
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-16 22:09:00 +0530

    Minimum Time Difference

---
+++ Problems/Minimum Time Difference/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findMinDifference(self, timePoints: List[str]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findMinDifference(timePoints = ["23:59","00:00"])
+data = obj.findMinDifference(timePoints = ["00:00","23:59","00:00"])
+print(data)
