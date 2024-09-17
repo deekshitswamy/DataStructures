3f135fb803b12fdf7409d5822d8dbe0489394e43
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-17 20:54:07 +0530

    Uncommon Words from Two Sentences

---
+++ Problems/Uncommon Words from Two Sentences/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        pass
+
+obj = Solution()
+#data = obj.uncommonFromSentences(s1 = "this apple is sweet", s2 = "this apple is sour")
+data = obj.uncommonFromSentences(s1 = "apple apple", s2 = "banana")
+print(data)
