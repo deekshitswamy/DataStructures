44b11d5fb33616344b75fd54f2cd36f98fb14760
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-18 20:51:37 +0530

    changes

--- Problems/Largest Number/solution.py
+++ Problems/Largest Number/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        pass
+        res = []
+        if set(nums) == {0}:
+            return "0"
+        for i in range(len(nums)):
+            c = nums[i] / ( 10 ** len(str(nums[i]))-1)
+            res.append((nums[i],c))
+        res.sort(key= lambda x:x[1],reverse=True)
+
+        result = ''
+        for num,den in res:
+            result += str(num)
+        return result
 
 obj = Solution()
 #data = obj.largestNumber(nums = [10,2])
