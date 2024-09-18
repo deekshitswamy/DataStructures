0ac16fa30d55c051c8a0d33b6f5257c2be7d0cef
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-18 20:50:22 +0530

    Largest Number

---
+++ Problems/Largest Number/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        pass
+
+obj = Solution()
+#data = obj.largestNumber(nums = [10,2])
+data = obj.largestNumber(nums = [3,30,34,5,9])
+print(data)
