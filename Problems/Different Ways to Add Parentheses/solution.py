883ebf60e61ef2469513e258b1e8a78e4cbdead9
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-19 22:29:28 +0530

    Different Ways to Add Parentheses

---
+++ Problems/Different Ways to Add Parentheses/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.diffWaysToCompute(expression = "2-1-1")
+data = obj.diffWaysToCompute(expression = "2*3-4*5")
+print(data)
