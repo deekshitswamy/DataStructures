03d00dadbb4b78f23c4793032d01536106546a66
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-20 21:57:22 +0530

    changes

--- Problems/Shortest Palindrome/solution.py
+++ Problems/Shortest Palindrome/solution.py
@@ -2,7 +2,16 @@ import io
 from typing import List
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
-        pass
+        i=len(s)-1
+        a=""
+        if s==s[::-1]:
+            return s
+        while i!=0:
+            a+=s[i]
+            if a+s==(a+s)[::-1]:
+                return a+s  
+            i-=1  
+        return a+s
 
 obj = Solution()
 #data = obj.shortestPalindrome(s = "aacecaaa")
