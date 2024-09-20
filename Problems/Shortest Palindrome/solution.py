05073e27763bc71ce4f9ea121b48b234f64a6b87
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-20 21:55:44 +0530

    Shortest Palindrome

---
+++ Problems/Shortest Palindrome/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.shortestPalindrome(s = "aacecaaa")
+data = obj.shortestPalindrome(s = "abcd")
+print(data)
