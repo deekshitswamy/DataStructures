d7cb1b35803caee364770cf4eba7e4ad115b1ac1
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-21 18:02:10 +0530

    changes

--- Problems/Lexicographical Numbers/solution.py
+++ Problems/Lexicographical Numbers/solution.py
@@ -2,7 +2,16 @@ import io
 from typing import List
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        pass
+        number = {}
+        s = str(n)
+        length = len(s) 
+        for i in range(1, n + 1):
+            number[i] = i * (10 ** (length - len(str(i))))
+        d = sorted(number.items(), key=lambda number: number[1])
+        result = []
+        for key, value in d:
+            result.append(key)
+        return result
 
 obj = Solution()
 #data = obj.lexicalOrder(n = 13)
