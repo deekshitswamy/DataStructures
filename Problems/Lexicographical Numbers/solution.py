ace7d7073cb620490cb32496bba1d6ee1ddd0ad8
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-21 18:01:38 +0530

    Lexicographical Numbers

---
+++ Problems/Lexicographical Numbers/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.lexicalOrder(n = 13)
+data = obj.lexicalOrder(n = 2)
+print(data)
