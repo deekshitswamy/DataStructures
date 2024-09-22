f6c2934a147b63da4e10800b2c6be695560bd098
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-22 18:16:11 +0530

    K-th Smallest in Lexicographical Order

---
+++ Problems/K-th Smallest in Lexicographical Order/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findKthNumber(self, n: int, k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findKthNumber(n = 13, k = 2)
+data = obj.findKthNumber(n = 1, k = 1)
+print(data)
