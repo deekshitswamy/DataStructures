36058ddaf20353f97c396988538620b0d68bb9c3
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-24 20:34:19 +0530

    Find the Length of the Longest Common Prefix

---
+++ Problems/Find the Length of the Longest Common Prefix/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.longestCommonPrefix(arr1 = [1,10,100], arr2 = [1000])
+data = obj.longestCommonPrefix(arr1 = [1,2,3], arr2 = [4,4,4])
+print(data)
