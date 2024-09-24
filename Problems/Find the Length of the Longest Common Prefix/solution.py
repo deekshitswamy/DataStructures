6425ae92158d81002f0d1b78b37a97968b2378eb
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-24 20:35:25 +0530

    changes

--- Problems/Find the Length of the Longest Common Prefix/solution.py
+++ Problems/Find the Length of the Longest Common Prefix/solution.py
@@ -2,7 +2,24 @@ import io
 from typing import List
 class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
-        pass
+        def prefix(a, b):
+            a, b = str(a), str(b)
+            length = min(len(a), len(b))
+            for i in range(length):
+                if a[i] != b[i]:
+                    return i  
+            return length  
+        result = 0
+        d1 = sorted(map(str, set(arr1)))
+        d2 = sorted(map(str, set(arr2)))
+        i = j = 0
+        while i < len(d1) and j < len(d2):
+            result = max(result, prefix(d1[i], d2[j]))
+            if d1[i] < d2[j]:
+                i += 1
+            else:
+                j += 1
+        return result
 
 obj = Solution()
 #data = obj.longestCommonPrefix(arr1 = [1,10,100], arr2 = [1000])
