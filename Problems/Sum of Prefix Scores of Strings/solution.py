23bc7546e0be3a2a81540e3242771a61fff263c2
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-25 22:13:49 +0530

    Sum of Prefix Scores of Strings

---
+++ Problems/Sum of Prefix Scores of Strings/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.sumPrefixScores(words = ["abc","ab","bc","b"])
+data = obj.sumPrefixScores(words = ["abcd"])
+print(data)
