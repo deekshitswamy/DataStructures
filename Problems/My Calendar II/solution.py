80242b6962f16b4a80f56462767f233e5be3fd31
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-27 19:32:26 +0530

    My Calendar II

---
+++ Problems/My Calendar II/solution.py
@@ -0,0 +1,16 @@
+import io
+from typing import List
+class MyCalendarTwo:
+
+    def __init__(self):
+        pass
+        
+
+    def book(self, start: int, end: int) -> bool:
+        pass
+
+obj = MyCalendarTwo()
+#data = obj.book(10, 20)
+#data = obj.book(50, 60)
+data = obj.book(10, 40)
+print(data)
