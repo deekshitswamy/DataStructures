bd63c87efd90b149c5e4a28114611d7d1b869f15
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-27 19:33:04 +0530

    changes

--- Problems/My Calendar II/solution.py
+++ Problems/My Calendar II/solution.py
@@ -3,11 +3,22 @@ from typing import List
 class MyCalendarTwo:
 
     def __init__(self):
-        pass
-        
+        self.cal = []
+        self.intrscs = []
 
     def book(self, start: int, end: int) -> bool:
-        pass
+        for x in self.intrscs:
+            if (start < x[0] and end > x[0]) or (start >= x[0] and start < x[1]):
+                return False
+
+        for x in self.cal:
+            if (start < x[0] and end > x[0]) or (start >= x[0] and start < x[1]):
+                cs = max(x[0], start)
+                ce = min(x[1], end)
+                self.intrscs.append([cs, ce])
+
+        self.cal.append([start, end])
+        return True
 
 obj = MyCalendarTwo()
 #data = obj.book(10, 20)
