f0e06070542d3ca13bed2ca8e1892d4f0d0f4b5c
Deeku-Zeus <deekuzeus21@gmail.com>
2024-09-29 22:35:54 +0530

    All O`one Data Structure

---
+++ Problems/All O`one Data Structure/solution.py
@@ -0,0 +1,27 @@
+import io
+from typing import List
+class AllOne:
+
+    def __init__(self):
+        pass
+
+    def inc(self, key: str) -> None:
+        pass
+
+    def dec(self, key: str) -> None:
+        pass
+
+    def getMaxKey(self) -> str:
+        pass
+
+    def getMinKey(self) -> str:
+        pass
+        
+
+
+# Your AllOne object will be instantiated and called as such:
+obj = AllOne()
+obj.inc("hello")
+obj.dec("hello")
+param_3 = obj.getMaxKey()
+param_4 = obj.getMinKey()
