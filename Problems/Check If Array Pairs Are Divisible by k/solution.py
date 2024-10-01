dc91de61edc8f1c441eb9211360cf2c33a3d57d6
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-01 23:16:28 +0530

    Check If Array Pairs Are Divisible by k

---
+++ Problems/Check If Array Pairs Are Divisible by k/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.canArrange(arr = [1,2,3,4,5,10,6,7,8,9], k = 5)
+#data = obj.canArrange(arr = [1,2,3,4,5,6], k = 7)
+data = obj.canArrange(arr = [1,2,3,4,5,6], k = 10)
+print(data)
