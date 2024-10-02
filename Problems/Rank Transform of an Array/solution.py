635b3a9a03f2c7eca127e6b6e3e50a7df2566b55
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-02 19:01:43 +0530

    Rank Transform of an Array

---
+++ Problems/Rank Transform of an Array/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.arrayRankTransform(arr = [40,10,20,30])
+#data = obj.arrayRankTransform(arr = [100,100,100])
+data = obj.arrayRankTransform(arr = [37,12,28,9,100,56,80,5,12])
+print(data)
