9f1a43b59d1a1db4bc487218458573bdddde0019
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-04 01:38:39 +0530

    Make Sum Divisible by P

---
+++ Problems/Make Sum Divisible by P/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minSubarray(self, nums: List[int], p: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minSubarray(nums = [3,1,4,2], p = 6)
+#data = obj.minSubarray(nums = [6,3,5,2], p = 9)
+data = obj.minSubarray(nums = [1,2,3], p = 3)
+print(data)
