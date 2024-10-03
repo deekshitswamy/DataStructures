25bd54a4cdcbf6bb90b2e03c29953f25ba8c5ee0
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-04 01:39:34 +0530

    changes

--- Problems/Make Sum Divisible by P/solution.py
+++ Problems/Make Sum Divisible by P/solution.py
@@ -2,7 +2,28 @@ import io
 from typing import List
 class Solution:
     def minSubarray(self, nums: List[int], p: int) -> int:
-        pass
+        total_sum = sum(nums)
+        remainder = total_sum % p
+        if remainder == 0:
+            return 0
+
+        currSumMod = 0
+        res = len(nums)
+        prefix_map = {0: -1}
+
+        for i in range(0, len(nums)):
+            currSumMod = (currSumMod + nums[i]) % p
+            needMod = (currSumMod - remainder + p) % p
+
+            if needMod in prefix_map:
+                res = min(res, i - prefix_map[needMod])
+
+            prefix_map[currSumMod % p] = i
+
+        if res < len(nums):
+            return res
+        else:
+            return -1
 
 obj = Solution()
 #data = obj.minSubarray(nums = [3,1,4,2], p = 6)
