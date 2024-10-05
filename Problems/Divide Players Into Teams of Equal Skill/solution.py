1444e1991f2bedd8603563881b261636e3a934ba
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-05 23:38:27 +0530

    Divide Players Into Teams of Equal Skill

---
+++ Problems/Divide Players Into Teams of Equal Skill/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.dividePlayers(skill = [3,2,5,1,3,4])
+#data = obj.dividePlayers(skill = [3,4])
+data = obj.dividePlayers(skill = [1,1,2,3])
+print(data)
