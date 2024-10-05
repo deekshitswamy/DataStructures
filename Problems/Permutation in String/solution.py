f8035ff3f21833562817b6286f43bad2edd21e2f
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-05 23:42:39 +0530

    Permutation in String

---
+++ Problems/Permutation in String/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.checkInclusion(s1 = "ab", s2 = "eidbaooo")
+data = obj.checkInclusion(s1 = "ab", s2 = "eidboaoo")
+print(data)
