f8ff2419d992f3c2e73298d1073f0e73f278dcc6
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-05 23:40:39 +0530

    changes

--- Problems/Divide Players Into Teams of Equal Skill/solution.py
+++ Problems/Divide Players Into Teams of Equal Skill/solution.py
@@ -2,30 +2,22 @@ import io
 from typing import List
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
-        tsum = sum(skill)
-        tsum = (tsum / len(skill)) * 2
-
-        mp = {}
-        for x in skill:
-            mp[x] = mp.get(x, 0) + 1
-
-        chem = 0
-        for key in list(mp.keys()):
-            val = mp[key]
-            complement = tsum - key
-
-            if complement not in mp or val != mp[complement]:
+        sum_ = sum(skill)
+        n = len(skill)
+        if n == 2:
+            return skill[0] * skill[1]
+        if sum_ % (n//2) != 0:
+            return -1
+        skill.sort()
+        score = skill[0] + skill[-1]
+        pairs = []
+        result = 0
+        for i in range(n // 2):
+            l, r = skill[i], skill[n - 1 - i]
+            if l + r != score:
                 return -1
-
-            if key == complement:
-                chem += (key * complement) * (val / 2)
-            else:
-                chem += (key * complement) * val
-
-            del mp[key]
-            del mp[complement]
-
-        return chem
+            result += l * r
+        return result
 
 obj = Solution()
 #data = obj.dividePlayers(skill = [3,2,5,1,3,4])
