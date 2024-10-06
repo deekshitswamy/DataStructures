4816240d8df10c7d302314e841254d113d03c479
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-06 21:15:10 +0530

    Sentence Similarity III

---
+++ Problems/Sentence Similarity III/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.areSentencesSimilar(sentence1 = "My name is Haley", sentence2 = "My Haley")
+#data = obj.areSentencesSimilar(sentence1 = "of", sentence2 = "A lot of words")
+data = obj.areSentencesSimilar(sentence1 = "Eating right now", sentence2 = "Eating")
+print(data)
