8014e5d4eb247e8401100bf4a3410ef8fe512398
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-07 22:36:16 +0530

    changes

--- Problems/Minimum String Length After Removing Substrings/solution.py
+++ Problems/Minimum String Length After Removing Substrings/solution.py
@@ -2,7 +2,15 @@ import io
 from typing import List
 class Solution:
     def minLength(self, s: str) -> int:
-        pass
+        s = list(s)
+        top = -1
+        for i in range(len(s)):
+            if top >= 0 and s[top] + s[i] in ("AB","CD"):
+                top -= 1
+            else:
+                top += 1
+                s[top] = s[i]
+        return top + 1
 
 obj = Solution()
 #data = obj.minLength(s = "ABFCACDB")
