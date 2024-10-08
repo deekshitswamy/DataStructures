fdbdf7e933e0bfe018ba7c303c580ed078117801
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-08 21:00:18 +0530

    Minimum Number of Swaps to Make the String Balanced

---
+++ Problems/Minimum Number of Swaps to Make the String Balanced/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minSwaps(s = "][][")
+#data = obj.minSwaps(s = "]]][[[")
+data = obj.minSwaps(s = "[]")
+print(data)
