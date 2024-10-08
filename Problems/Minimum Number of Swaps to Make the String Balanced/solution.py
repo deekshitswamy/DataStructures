5f19a6c7a1a65818ebccba260b729e0dc5efef3d
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-08 21:00:58 +0530

    changes

--- Problems/Minimum Number of Swaps to Make the String Balanced/solution.py
+++ Problems/Minimum Number of Swaps to Make the String Balanced/solution.py
@@ -1,8 +1,21 @@
 import io
+import math
 from typing import List
 class Solution:
     def minSwaps(self, s: str) -> int:
-        pass
+        st = [] # Stack to track unbalanced parentheses
+        
+        for i in s:
+            if i == '[':
+                st.append(i) # Push opening bracket to the stack
+            elif st and st[-1] == '[':
+                st.pop() # Excluding balanced pairs
+            else:
+                st.append(i) # Push closing bracket to the stack
+        
+        unbalancedPairs = len(st) // 2
+        swaps = math.ceil(unbalancedPairs / 2.0)
+        return swaps
 
 obj = Solution()
 #data = obj.minSwaps(s = "][][")
