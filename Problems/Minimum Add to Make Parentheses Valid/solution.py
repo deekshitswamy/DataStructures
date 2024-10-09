11baa615fde930a8ed8cc591c5f8f2f2a139fef4
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-09 23:27:03 +0530

    changes

--- Problems/Minimum Add to Make Parentheses Valid/solution.py
+++ Problems/Minimum Add to Make Parentheses Valid/solution.py
@@ -2,7 +2,13 @@ import io
 from typing import List
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        pass
+        count, ans = 0, 0
+
+        for ch in s:
+            if ch =='(': count += 1
+            elif count: count -= 1
+            else: ans += 1
+        return ans + count
 
 obj = Solution()
 #data = obj.minAddToMakeValid(s = "())")
