c3987a0bba3c641291c8a004e0c12047e9000523
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-09 23:26:28 +0530

    Minimum Add to Make Parentheses Valid

---
+++ Problems/Minimum Add to Make Parentheses Valid/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minAddToMakeValid(s = "())")
+data = obj.minAddToMakeValid(s = "(((")
+print(data)
