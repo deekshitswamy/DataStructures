fefcdf8364bd4cf2e0a45b66aad3bc8473bbad50
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-10 20:39:01 +0530

    Maximum Width Ramp

---
+++ Problems/Maximum Width Ramp/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxWidthRamp(nums = [6,0,8,2,1,5])
+data = obj.maxWidthRamp(nums = [9,8,1,0,1,9,4,0,4,1])
+print(data)
