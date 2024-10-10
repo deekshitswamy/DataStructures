567942c6a198ef43f85afdd7bc9eab2963bff52f
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-10 20:39:45 +0530

    changes

--- Problems/Maximum Width Ramp/solution.py
+++ Problems/Maximum Width Ramp/solution.py
@@ -2,7 +2,21 @@ import io
 from typing import List
 class Solution:
     def maxWidthRamp(self, nums: List[int]) -> int:
-        pass
+        dec = [0]
+
+        # Decreasing stack
+        for i in range(1, len(nums)):
+            if nums[i] < nums[dec[-1]]:
+                dec.append(i)
+
+        max_ramp = 0
+
+        # Loop from right to left
+        for i in range(len(nums) - 1, -1, -1):
+            while dec and nums[i] >= nums[dec[-1]]:
+                max_ramp = max(max_ramp, i - dec.pop())
+
+        return max_ramp
 
 obj = Solution()
 #data = obj.maxWidthRamp(nums = [6,0,8,2,1,5])
