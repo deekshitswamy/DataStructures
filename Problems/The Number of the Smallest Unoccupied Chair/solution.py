46f60d1f60847120221cb526d8b2bdaac6d0c778
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-11 22:55:16 +0530

    changes

--- Problems/The Number of the Smallest Unoccupied Chair/solution.py
+++ Problems/The Number of the Smallest Unoccupied Chair/solution.py
@@ -2,7 +2,24 @@ import io
 from typing import List
 class Solution:
     def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
-        pass
+        n = len(times)
+        timeline = [] # stores (eventTime, isArrivingEvent, friendNumber)
+        for i, (arrive, leaving) in enumerate(times):
+            timeline.append((arrive, 1, i)) 
+            timeline.append((leaving, 0, i)) # handle leaving events before arriving
+        timeline.sort()
+
+        heap = [i for i in range(n)] # stores the empty seat numbers as a heap, so we can always get smallest number
+        rooms = [-1] * n # the seat number that each friend is occupying
+        for _, isArrive, friend in timeline:
+            if friend == targetFriend:
+                return heap[0]
+            if isArrive: # when friend arrive, assign it smallest chair
+                room = heappop(heap)
+                rooms[friend] = room
+            else: # when friend leave, push the chair back to heap
+                room = rooms[friend]
+                heappush(heap, room)
 
 obj = Solution()
 #data = obj.smallestChair(times = [[1,4],[2,3],[4,6]], targetFriend = 1)
