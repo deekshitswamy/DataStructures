9875d21131b52001a102dc660426cc0f362f679c
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-11 22:54:14 +0530

    The Number of the Smallest Unoccupied Chair

---
+++ Problems/The Number of the Smallest Unoccupied Chair/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.smallestChair(times = [[1,4],[2,3],[4,6]], targetFriend = 1)
+data = obj.smallestChair(times = [[3,10],[1,5],[2,6]], targetFriend = 0)
+print(data)
