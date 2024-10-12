2a122bc0ea172bbbbea9ec70b7463012488d28f7
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-12 22:55:37 +0530

    changes

--- Problems/Divide Intervals Into Minimum Number of Groups/solution.py
+++ Problems/Divide Intervals Into Minimum Number of Groups/solution.py
@@ -2,7 +2,19 @@ import io
 from typing import List
 class Solution:
     def minGroups(self, intervals: List[List[int]]) -> int:
-        pass
+        start_times_sorted = sorted(interval[0] for interval in intervals)
+        end_times_sorted = sorted(interval[1] for interval in intervals)
+        
+        end_index, group_counter = 0, 0
+
+        # Traverse through sorted start times to calculate required groups
+        for current_start in start_times_sorted:
+            if current_start > end_times_sorted[end_index]:
+                end_index += 1
+            else:
+                group_counter += 1
+
+        return group_counter
 
 obj = Solution()
 #data = obj.testfunc(n = 1)
