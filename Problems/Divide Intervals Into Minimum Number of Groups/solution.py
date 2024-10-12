c2daafd99c92304153a21bdbf40bb922a0c0c31e
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-12 22:54:59 +0530

    Divide Intervals Into Minimum Number of Groups

---
+++ Problems/Divide Intervals Into Minimum Number of Groups/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minGroups(self, intervals: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.testfunc(n = 1)
+#data = obj.testfunc(n = 2)
+data = obj.testfunc(n = 2)
+print(data)
