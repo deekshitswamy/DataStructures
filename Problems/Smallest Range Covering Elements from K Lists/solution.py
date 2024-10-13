8f3e59b9c109fba87c324d7ac5877e8135c916ce
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-13 19:42:44 +0530

    Smallest Range Covering Elements from K Lists

---
+++ Problems/Smallest Range Covering Elements from K Lists/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def smallestRange(self, nums: List[List[int]]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.smallestRange(nums = [[4,10,15,24,26],[0,9,12,20],[5,18,22,30]])
+data = obj.smallestRange(nums = [[1,2,3],[1,2,3],[1,2,3]])
+print(data)
