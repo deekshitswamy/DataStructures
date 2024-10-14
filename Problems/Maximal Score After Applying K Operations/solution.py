c650c992b09cf63431ecf39a8e6ed815ad095410
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-14 20:11:57 +0530

    changes

--- Problems/Maximal Score After Applying K Operations/solution.py
+++ Problems/Maximal Score After Applying K Operations/solution.py
@@ -1,8 +1,19 @@
 import io
+import math
+import heapq
 from typing import List
 class Solution:
     def maxKelements(self, nums: List[int], k: int) -> int:
-        pass
+        ans=0
+        maxi=[-n for n in nums]
+        heapq.heapify(maxi)
+        while k:
+            n=-heapq.heappop(maxi)
+            ans+=n
+            k-=1
+            p=-math.ceil(n/3)
+            heapq.heappush(maxi,p)
+        return ans
 
 obj = Solution()
 #data = obj.maxKelements(nums = [10,10,10,10,10], k = 5)
