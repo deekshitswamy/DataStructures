f117a3f018dfab08d4811ae0c8e012bf07a531db
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-14 20:10:34 +0530

    Maximal Score After Applying K Operations

---
+++ Problems/Maximal Score After Applying K Operations/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maxKelements(self, nums: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxKelements(n = 1)
+#data = obj.maxKelements(n = 2)
+data = obj.maxKelements(n = 2)
+print(data)
