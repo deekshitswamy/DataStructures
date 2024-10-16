63f9130597c3eba2b1e27ad904b2d4fc1eaf2962
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-16 21:37:40 +0530

    Longest Happy String

---
+++ Problems/Longest Happy String/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        pass
+
+obj = Solution()
+#data = obj.longestDiverseString(a = 1, b = 1, c = 7)
+data = obj.longestDiverseString(a = 7, b = 1, c = 0)
+print(data)
