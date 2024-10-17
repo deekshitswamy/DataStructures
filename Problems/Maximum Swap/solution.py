4ebe2d0842c1edb3d7b915bcb844dc772584a2b8
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-17 19:57:24 +0530

    Maximum Swap

---
+++ Problems/Maximum Swap/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maximumSwap(n = 1)
+#data = obj.maximumSwap(n = 2)
+data = obj.maximumSwap(n = 2)
+print(data)
