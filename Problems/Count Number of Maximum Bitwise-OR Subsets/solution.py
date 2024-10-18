189477b64e515987d86e2b234ebc2d55374ff32c
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-18 22:49:12 +0530

    Count Number of Maximum Bitwise-OR Subsets

---
+++ Problems/Count Number of Maximum Bitwise-OR Subsets/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countMaxOrSubsets(nums = [3,1])
+#data = obj.countMaxOrSubsets(nums = [2,2,2])
+data = obj.countMaxOrSubsets(nums = [3,2,1,5])
+print(data)
