5b496e0e240289ea32d81d29aef0058f936f1f83
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-19 22:10:01 +0530

    chnages

--- Problems/Find Kth Bit in Nth Binary String/solution.py
+++ Problems/Find Kth Bit in Nth Binary String/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
-        pass
+        if n == 1:
+            return '0'
+
+        mid = (1 << (n - 1)) - 1  
+
+        if k <= mid:
+            return self.findKthBit(n - 1, k)
+        elif k == mid + 1:
+            return '1'
+        else:
+            return '1' if self.findKthBit(n - 1, (1 << n) - k) == '0' else '0'
 
 obj = Solution()
 #data = obj.findKthBit(n = 3, k = 1)
