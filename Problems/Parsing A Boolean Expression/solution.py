6af0761416445d977e465862da174a8f5cec0075
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-20 23:36:24 +0530

    changes

--- Problems/Parsing A Boolean Expression/solution.py
+++ Problems/Parsing A Boolean Expression/solution.py
@@ -2,7 +2,21 @@ import io
 from typing import List
 class Solution:
     def parseBoolExpr(self, expression: str) -> bool:
-        pass
+        stk = []
+        for ch in expression:
+            if ch == ',': continue
+            if ch == ')':
+                t = f = False
+                while stk[-1] != '(':
+                    top = stk.pop()
+                    t |= top == 't'
+                    f |= top == 'f'
+                stk.pop()  
+                op = stk.pop()  
+                stk.append('t' if (op == '|' and t) or (op == '&' and not f) else 'f')
+                if op == '!': stk[-1] = 'f' if t else 't'
+            else: stk.append(ch)
+        return stk[-1] == 't'
 
 obj = Solution()
 #data = obj.parseBoolExpr(expression = "&(|(f))")
