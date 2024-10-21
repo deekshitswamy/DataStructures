3adcf9c75bc9a07000965a0ce45c4c7bc5784001
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-21 21:10:09 +0530

    Split a String Into the Max Number of Unique Substrings

---
+++ Problems/Split a String Into the Max Number of Unique Substrings/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maxUniqueSplit(self, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxUniqueSplit(s = "ababccc")
+#data = obj.maxUniqueSplit(s = "aba")
+data = obj.maxUniqueSplit(s = "aa")
+print(data)
