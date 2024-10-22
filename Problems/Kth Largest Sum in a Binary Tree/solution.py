0fea7fb198cf9ff0fde0ed71315a326293929301
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-22 21:54:00 +0530

    changes

--- Problems/Kth Largest Sum in a Binary Tree/solution.py
+++ Problems/Kth Largest Sum in a Binary Tree/solution.py
@@ -1,4 +1,5 @@
 import io
+import heapq
 from typing import List
 # Definition for a binary tree node.
 class TreeNode:
@@ -9,7 +10,30 @@ class TreeNode:
 
 class Solution:
     def kthLargestLevelSum(self, root: TreeNode, k: int) -> int:
-        pass
+        max_heap = []
+    
+        def dfs(childArr):
+            if not childArr:
+                return
+            sum_val = 0
+            nextChildArr = []
+            for child in childArr:
+                sum_val += child.val
+                if child.left:
+                    nextChildArr.append(child.left)
+                if child.right:
+                    nextChildArr.append(child.right)
+            heapq.heappush(max_heap, -sum_val)  # Using a min-heap with negative values to simulate max-heap
+            dfs(nextChildArr)
+        
+        dfs([root])
+        
+        top = -1
+        while k:
+            top = -heapq.heappop(max_heap)
+            k -= 1
+            
+        return top if top != -1 else -1
 
 obj = Solution()
 #data = obj.kthLargestLevelSum(root = [5,8,9,2,1,3,7,4,6], k = 2)
