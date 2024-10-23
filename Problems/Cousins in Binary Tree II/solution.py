e5174b0e5a2962975e90b9e5ff3c291fe79c0fad
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-23 19:50:49 +0530

    changes

--- Problems/Cousins in Binary Tree II/solution.py
+++ Problems/Cousins in Binary Tree II/solution.py
@@ -9,7 +9,25 @@ class TreeNode:
 
 class Solution:
     def replaceValueInTree(self, root: TreeNode) -> TreeNode:
-        pass
+        queue1 = deque([(1, None, root)])
+        queue2 = []
+        values = defaultdict(int)
+        parents = defaultdict(int)
+        parents[None] = root.val
+        while queue1:           
+            level, _, node = tree_node = queue1.popleft()    
+            queue2.append(tree_node)       
+            values[level] += node.val
+            if node.left:
+                parents[node] += node.left.val
+                queue1.append((level + 1, node, node.left))
+            if node.right:
+                parents[node] += node.right.val
+                queue1.append((level + 1, node, node.right))
+        
+        for level, parent, node in queue2:
+            node.val = values[level] - parents[parent]
+        return root
 
 obj = Solution()
 #data = obj.replaceValueInTree(root = [5,4,9,1,10,None,7])
