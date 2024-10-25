8d5862adc2d34681455ddb66fd3f79cd41ad05ee
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-25 20:12:05 +0530

    changes

--- Problems/Remove Sub-Folders from the Filesystem/solution.py
+++ Problems/Remove Sub-Folders from the Filesystem/solution.py
@@ -2,7 +2,16 @@ import io
 from typing import List
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
-        pass
+        folder.sort()
+        ans, prev = [folder[0]], ''.join((folder[0],'/'))
+    
+        for path in folder[1:]:
+            if  path.startswith(prev): continue
+
+            ans.append(path)
+            prev = ''.join((path,'/'))
+
+        return ans
 
 obj = Solution()
 #data = obj.removeSubfolders(folder = ["/a","/a/b","/c/d","/c/d/e","/c/f"])
