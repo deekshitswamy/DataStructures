02e053efd174336d07f3add8f4582956bcee131f
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-27 23:41:48 +0530

    changes

--- Problems/Count Square Submatrices with All Ones/solution.py
+++ Problems/Count Square Submatrices with All Ones/solution.py
@@ -1,8 +1,31 @@
 import io
 from typing import List
 class Solution:
+    def window(self, size, rows, cols, matrix):
+        total_ones = 0
+        for row_index in range(rows - size + 1):
+            for col_index in range(cols - size + 1):
+                all_ones = True
+                for i in range(size):
+                    for j in range(size):
+                        if matrix[row_index + i][col_index + j] != 1:
+                            all_ones = False
+                            break
+                    if not all_ones:
+                        break
+                if all_ones:
+                    total_ones += 1
+        return total_ones
+
     def countSquares(self, matrix: List[List[int]]) -> int:
-        pass
+        total = 0
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        for size in range(1, min(rows, cols) + 1):
+            total += self.window(size, rows, cols, matrix)
+        
+        return total
 
 obj = Solution()
 '''
