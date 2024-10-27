a892efa97a7616216a1a56a1aaf564608fe2bb21
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-27 23:40:49 +0530

    Count Square Submatrices with All Ones

---
+++ Problems/Count Square Submatrices with All Ones/solution.py
@@ -0,0 +1,22 @@
+import io
+from typing import List
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+'''
+data = obj.countSquares(matrix =
+[
+  [0,1,1,1],
+  [1,1,1,1],
+  [0,1,1,1]
+])
+'''
+data = obj.countSquares(matrix = 
+[
+  [1,0,1],
+  [1,1,0],
+  [1,1,0]
+])
+print(data)
