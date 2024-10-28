56cc50e037dffa96ccedf17a430fa27f04ce0ca6
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-28 22:44:08 +0530

    Longest Square Streak in an Array

---
+++ Problems/Longest Square Streak in an Array/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def longestSquareStreak(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.longestSquareStreak(nums = [4,3,6,16,8,2])
+data = obj.longestSquareStreak(nums = [2,3,5,6,7])
+print(data)
