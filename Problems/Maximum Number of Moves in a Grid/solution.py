538a81a1db52f008f319d616c261ddcc25f61f8b
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-29 23:34:29 +0530

    Maximum Number of Moves in a Grid

---
+++ Problems/Maximum Number of Moves in a Grid/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxMoves(self, grid: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxMoves(grid = [[3,2,4],[2,1,9],[1,1,7]])
+data = obj.maxMoves(grid = [[3,2,4],[2,1,9],[1,1,7]])
+print(data)
