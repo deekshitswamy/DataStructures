ddef4814c799f0610e260a4e9cf230e026a043b4
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-30 19:14:26 +0530

    changes

---
+++ Problems/Minimum Number of Removals to Make Mountain Array/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minimumMountainRemovals(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minimumMountainRemovals(nums = [1,3,1])
+data = obj.minimumMountainRemovals(nums = [2,1,1,5,6,2,3,1])
+print(data)
