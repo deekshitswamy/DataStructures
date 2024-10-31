a2acc7bc36163852410497cc2abfb96d67c7687a
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-31 22:07:19 +0530

    Minimum Total Distance Traveled

---
+++ Problems/Minimum Total Distance Traveled/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minimumTotalDistance(self, robot: List[int], factory: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minimumTotalDistance(robot = [0,4,6], factory = [[2,2],[6,2]])
+data = obj.minimumTotalDistance(robot = [1,-1], factory = [[-2,1],[2,1]])
+print(data)
