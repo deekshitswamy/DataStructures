a704888bf96f316f78fef6d88da7233ded7f1c9f
Deeku-Zeus <deekuzeus21@gmail.com>
2024-10-31 22:09:07 +0530

    changes

--- Problems/Minimum Total Distance Traveled/solution.py
+++ Problems/Minimum Total Distance Traveled/solution.py
@@ -2,7 +2,27 @@ import io
 from typing import List
 class Solution:
     def minimumTotalDistance(self, robot: List[int], factory: List[List[int]]) -> int:
-        pass
+        robot.sort()
+        factory.sort()
+        memo = {}
+
+        def helper(currRobot, currFact, usedCapacity):
+            if currRobot == len(robot):
+                return 0
+            if currFact == len(factory):
+                return float('inf')
+            key = (currRobot, currFact, usedCapacity)
+            if key in memo:
+                return memo[key]
+
+            minDist = helper(currRobot, currFact+1, 0)
+            position, capacity = factory[currFact]
+            if usedCapacity < capacity:
+                dist = abs(robot[currRobot] - position)
+                minDist = min(minDist, dist + helper(currRobot+1, currFact, usedCapacity + 1))
+            memo[key] = minDist
+            return minDist
+        return helper(0,0,0)
 
 obj = Solution()
 #data = obj.minimumTotalDistance(robot = [0,4,6], factory = [[2,2],[6,2]])
