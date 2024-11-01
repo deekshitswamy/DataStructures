e032437238fbd43f13a52e4511e58c5a3d4e9826
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-01 23:40:23 +0530

    Delete Characters to Make Fancy String

---
+++ Problems/Delete Characters to Make Fancy String/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.makeFancyString(s = "leeetcode")
+#data = obj.makeFancyString(s = "aaabaaaa")
+data = obj.makeFancyString(s = "aab")
+print(data)
