ed831c85290a628f5f5646c7d2b3434874f56c5f
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-02 22:51:41 +0530

    Circular Sentence

---
+++ Problems/Circular Sentence/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.isCircularSentence(sentence = "leetcode exercises sound delightful")
+#data = obj.isCircularSentence(sentence = "eetcode")
+data = obj.isCircularSentence(sentence = "Leetcode is cool")
+print(data)
