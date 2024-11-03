287ca8290b35d4d593f379ba694eac5862af7964
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-03 22:32:02 +0530

    Rotate String

---
+++ Problems/Rotate String/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.rotateString(s = "abcde", goal = "cdeab")
+data = obj.rotateString(s = "abcde", goal = "abced")
+print(data)
