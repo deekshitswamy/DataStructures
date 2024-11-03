c9e0881d073836973e6b40336e740cef85f45aa2
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-03 22:33:01 +0530

    changes

--- Problems/Rotate String/solution.py
+++ Problems/Rotate String/solution.py
@@ -2,7 +2,16 @@ import io
 from typing import List
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        pass
+        if len(s) != len(goal):
+            return False
+        if s == goal:
+            return True
+        else:
+            for i in range(len(s)):
+                s = s[1:] + s[0]
+                if s == goal:
+                    return True
+        return False
 
 obj = Solution()
 #data = obj.rotateString(s = "abcde", goal = "cdeab")
