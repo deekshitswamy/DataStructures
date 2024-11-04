cfe2743d64f94215651b25236d70a2a86476678d
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-05 00:20:20 +0530

    String Compression III

---
+++ Problems/String Compression III/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def compressedString(self, word: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.compressedString(word = "abcde")
+data = obj.compressedString(word = "aaaaaaaaaaaaaabb")
+print(data)
