d3e183850fb2814a3cf07f8f0708f46da02f2340
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-05 00:21:01 +0530

    changes

--- Problems/String Compression III/solution.py
+++ Problems/String Compression III/solution.py
@@ -2,7 +2,28 @@ import io
 from typing import List
 class Solution:
     def compressedString(self, word: str) -> str:
-        pass
+        dic = defaultdict(int)
+        res = []
+        prev = ""
+
+        for i in word: 
+            dic[i] += 1
+            if prev == i and dic[i] < 9 : 
+                continue
+            else:
+                if prev == i and dic[i] == 9: 
+                    res.append("9"+i)
+                    dic[i] = 0 
+                if prev != i and dic[prev] != 0: 
+                    val = str(dic[prev])
+                    res.append(val+prev)
+                    dic = defaultdict(int)
+                    dic[i] += 1
+            prev = i
+        if dic[word[-1]] < 9 and dic[word[-1]] != 0: 
+            s = str(dic[word[-1]])
+            res.append(s+word[-1])
+        return "".join(res)
 
 obj = Solution()
 #data = obj.compressedString(word = "abcde")
