383823cae0f7e0f1946be53ee02454d5d5da946f
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-05 21:12:31 +0530

    Minimum Number of Changes to Make Binary String Beautiful

---
+++ Problems/Minimum Number of Changes to Make Binary String Beautiful/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minChanges(self, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minChanges(s = "1001")
+#data = obj.minChanges(s = "10")
+data = obj.minChanges(s = "0000")
+print(data)
