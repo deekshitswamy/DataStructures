c48a024530cb6178511e0d99391bf3aff2d9e7cd
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-06 20:00:55 +0530

    Find if Array Can Be Sorted

---
+++ Problems/Find if Array Can Be Sorted/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def canSortArray(self, nums: List[int]) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.canSortArray(nums = [8,4,2,30,15])
+#data = obj.canSortArray(nums = [1,2,3,4,5])
+data = obj.canSortArray(nums = [3,16,8,4,2])
+print(data)
