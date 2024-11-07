d909903c401bfab1610a8402081e6d42624090b7
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-07 23:28:44 +0530

    changes

--- Problems/Largest Combination With Bitwise AND Greater Than Zero/solution.py
+++ Problems/Largest Combination With Bitwise AND Greater Than Zero/solution.py
@@ -2,7 +2,15 @@ import io
 from typing import List
 class Solution:
     def largestCombination(self, candidates: List[int]) -> int:
-        pass
+        res = [0] * 30
+
+        for i in candidates:
+            for j in range(30):
+                if i & (1 << j):
+                    res[j] += 1
+
+        max_count = max(res)
+        return max_count
 
 obj = Solution()
 #data = obj.largestCombination(candidates = [16,17,71,62,12,24,14])
