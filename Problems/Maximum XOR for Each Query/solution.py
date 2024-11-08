b70b64f80f5c7159c90d131d14975b18814d5a07
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-08 23:20:17 +0530

    Maximum XOR for Each Query

---
+++ Problems/Maximum XOR for Each Query/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.getMaximumXor(nums = [0,1,1,3], maximumBit = 2)
+#data = obj.getMaximumXor(nums = [2,3,4,7], maximumBit = 3)
+data = obj.getMaximumXor(nums = [0,1,2,2,5,7], maximumBit = 3)
+print(data)
