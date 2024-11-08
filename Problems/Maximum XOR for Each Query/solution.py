2f810c0320dcaef6e609049f6543dea3a4ae86d4
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-08 23:21:26 +0530

    changes

--- Problems/Maximum XOR for Each Query/solution.py
+++ Problems/Maximum XOR for Each Query/solution.py
@@ -2,7 +2,15 @@ import io
 from typing import List
 class Solution:
     def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
-        pass
+        answer = [0] * len(nums)
+        totalXor = 0
+        maxNum = (1 << maximumBit) - 1  # maxNum with all bits set up to maximumBit
+        
+        for i in range(len(nums)):
+            totalXor ^= nums[i]  # Update cumulative XOR
+            answer[len(nums) - i - 1] = totalXor ^ maxNum  # Calculate k and store in reverse order
+            
+        return answer
 
 obj = Solution()
 #data = obj.getMaximumXor(nums = [0,1,1,3], maximumBit = 2)
