839c47642f87e131fe2bc05887c4cc9f60fd0eaa
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-09 20:41:10 +0530

    changes

--- Problems/Minimum Array End/solution.py
+++ Problems/Minimum Array End/solution.py
@@ -2,7 +2,19 @@ import io
 from typing import List
 class Solution:
     def minEnd(self, n: int, x: int) -> int:
-        pass
+        X = bin(x)[2:].zfill(64)
+        N = bin(n - 1)[2:].zfill(64)
+        result = ['0'] * 64
+        j = 0
+
+        for i in range(64):
+            if X[-1 - i] == '1':
+                result[-1 - i] = '1'
+            else:
+                result[-1 - i] = N[-1 - j]
+                j += 1
+
+        return int(''.join(result), 2)
 
 obj = Solution()
 #data = obj.minEnd(n = 3, x = 4)
