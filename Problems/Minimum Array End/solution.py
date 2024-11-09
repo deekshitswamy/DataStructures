c28f8bc85e638dd090a00324d3d8e3bea0e854e6
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-09 20:40:33 +0530

    Minimum Array End

---
+++ Problems/Minimum Array End/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minEnd(self, n: int, x: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minEnd(n = 3, x = 4)
+data = obj.minEnd(n = 2, x = 7)
+print(data)
