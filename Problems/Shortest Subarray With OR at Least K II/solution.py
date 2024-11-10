0654fd0229791de3fbef281029c2826a9fc97158
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-10 21:55:34 +0530

    Shortest Subarray With OR at Least K II

---
+++ Problems/Shortest Subarray With OR at Least K II/solution.py
@@ -0,0 +1,11 @@
+import io
+from collections import deque
+from typing import List
+class Solution:
+    def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
+        pass
+obj = Solution()
+#data = obj.minimumSubarrayLength(nums = [1,2,3], k = 2)
+#data = obj.minimumSubarrayLength(nums = [2,1,8], k = 10)
+data = obj.minimumSubarrayLength(nums = [1,2], k = 0)
+print(data)
