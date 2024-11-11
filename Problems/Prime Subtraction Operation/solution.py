af4e3d392d4d4763cc55dbbed0e36bc69525eee7
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-11 23:27:13 +0530

    Prime Subtraction Operation

---
+++ Problems/Prime Subtraction Operation/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def primeSubOperation(self, nums: List[int]) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.primeSubOperation(nums = [4,9,6,10])
+#data = obj.primeSubOperation(nums = [6,8,11,12])
+data = obj.primeSubOperation(nums = [5,8,3])
+print(data)
