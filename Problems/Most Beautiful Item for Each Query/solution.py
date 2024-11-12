d115998f10eb09facf94d2a7d4bd15202505b446
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-12 22:33:57 +0530

    Most Beautiful Item for Each Query

---
+++ Problems/Most Beautiful Item for Each Query/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.maximumBeauty(items = [[1,2],[3,2],[2,4],[5,6],[3,5]], queries = [1,2,3,4,5,6])
+#data = obj.maximumBeauty(items = [[1,2],[1,2],[1,3],[1,4]], queries = [1])
+data = obj.maximumBeauty(items = [[10,1000]], queries = [5])
+print(data)
