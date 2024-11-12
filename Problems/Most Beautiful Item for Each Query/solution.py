686e77365b89190a03c2b541c513a87c89f3c5b7
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-12 22:34:30 +0530

    changes

--- Problems/Most Beautiful Item for Each Query/solution.py
+++ Problems/Most Beautiful Item for Each Query/solution.py
@@ -2,7 +2,23 @@ import io
 from typing import List
 class Solution:
     def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
-        pass
+        items.sort()
+        beauty = []
+        maxbeauty = items[0][1]
+        beauty.append(maxbeauty)
+
+        for i in range(1, len(items)):
+            maxbeauty = max(items[i][1], maxbeauty)
+            beauty.append(maxbeauty)
+
+        result = []
+        for query in queries:
+            index = bisect.bisect_right(items, [query, float('inf')]) - 1
+            if index >= 0:
+                result.append(beauty[index])
+            else:
+                result.append(0)
+        return result
 
 obj = Solution()
 #data = obj.maximumBeauty(items = [[1,2],[3,2],[2,4],[5,6],[3,5]], queries = [1,2,3,4,5,6])
