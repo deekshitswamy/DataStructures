f21c31a6547f17b410cafc51cf669c165179a8ea
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-13 22:19:27 +0530

    Count the Number of Fair Pairs

---
+++ Problems/Count the Number of Fair Pairs/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countFairPairs(nums = [0,1,7,4,4,5], lower = 3, upper = 6)
+data = obj.countFairPairs(nums = [1,7,9,2,5], lower = 11, upper = 11)
+print(data)
