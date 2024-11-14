93483e245f7bc573c5373b9372c44fcc5efbeb59
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-14 23:01:16 +0530

    Minimized Maximum of Products Distributed to Any Store

---
+++ Problems/Minimized Maximum of Products Distributed to Any Store/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minimizedMaximum(self, n: int, quantities: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minimizedMaximum(n = 6, quantities = [11,6])
+#data = obj.minimizedMaximum(n = 7, quantities = [15,10,10])
+data = obj.minimizedMaximum(n = 1, quantities = [100000])
+print(data)
