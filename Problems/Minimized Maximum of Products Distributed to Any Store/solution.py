9de8f8db5e119195c55182519076a778c948174f
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-14 23:04:04 +0530

    changes

--- Problems/Minimized Maximum of Products Distributed to Any Store/solution.py
+++ Problems/Minimized Maximum of Products Distributed to Any Store/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def minimizedMaximum(self, n: int, quantities: List[int]) -> int:
-        pass
+        def isPossible(x: int, quantities: list[int], n: int) -> bool:
+            sum = 0
+            for u in quantities:
+                sum += -(-u // x)  # Equivalent to ceil(u / x)
+            return sum > n
+
+        left, right = 1, 100000
+        while left < right:
+            mid = (left + right) // 2
+            if isPossible(mid, quantities, n):
+                left = mid + 1
+            else:
+                right = mid
+        return left
 
 obj = Solution()
 #data = obj.minimizedMaximum(n = 6, quantities = [11,6])
