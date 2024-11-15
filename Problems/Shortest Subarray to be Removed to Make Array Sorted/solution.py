1b4f35af8f1f5152a1036c2f858d1a608587cdfb
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-15 23:05:04 +0530

    Shortest Subarray to be Removed to Make Array Sorted

---
+++ Problems/Shortest Subarray to be Removed to Make Array Sorted/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findLengthOfShortestSubarray(arr = [1,2,3,10,4,2,3,5])
+#data = obj.findLengthOfShortestSubarray(arr = [5,4,3,2,1])
+data = obj.findLengthOfShortestSubarray(arr = [1,2,3])
+print(data)
