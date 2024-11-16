dce473961d13621be3bee3b461a245ca2cbd488a
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-16 23:44:49 +0530

    changes

--- Problems/Find the Power of K-Size Subarrays I/solution.py
+++ Problems/Find the Power of K-Size Subarrays I/solution.py
@@ -2,7 +2,36 @@ import io
 from typing import List
 class Solution:
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
-        pass
+        count = 0
+        max_val = nums[0]
+        
+        for i in range(k - 1):
+            if nums[i] == nums[i + 1] - 1:
+                max_val += 1
+            else:
+                count = i + 1
+                max_val = nums[i + 1]
+
+        res = []
+        if count > 0:
+            res.append(-1)
+        else:
+            res.append(max_val)
+
+        for i in range(k, len(nums)):
+            if nums[i] == nums[i - 1] + 1:
+                max_val += 1
+                count -= 1
+            else:
+                count = k - 1
+                max_val = nums[i]
+
+            if count > 0:
+                res.append(-1)
+            else:
+                res.append(max_val)
+        
+        return res
 
 obj = Solution()
 #data = obj.resultsArray(nums = [1,2,3,4,3,2,5], k = 3)
