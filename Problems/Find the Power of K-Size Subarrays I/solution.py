9cd33ea451e467a1a484e0ab881da8b61d50fbab
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-16 23:44:00 +0530

    Find the Power of K-Size Subarrays I

---
+++ Problems/Find the Power of K-Size Subarrays I/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def resultsArray(self, nums: List[int], k: int) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.resultsArray(nums = [1,2,3,4,3,2,5], k = 3)
+#data = obj.resultsArray(nums = [2,2,2,2,2], k = 4)
+data = obj.resultsArray(nums = [3,2,3,2,3,2], k = 2)
+print(data)
