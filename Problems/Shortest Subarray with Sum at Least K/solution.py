ab5f60110cb0b1c9ca47cf827951f2c1cccf3e2e
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-17 23:57:18 +0530

    Shortest Subarray with Sum at Least K

---
+++ Problems/Shortest Subarray with Sum at Least K/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.shortestSubarray(nums = [1], k = 1)
+#data = obj.shortestSubarray(nums = [1,2], k = 4)
+data = obj.shortestSubarray(nums = [2,-1,2], k = 3)
+print(data)
