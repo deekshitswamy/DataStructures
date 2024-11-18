43f0bdd3b18b4a88038ba7239b4111ee650a0991
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-18 23:43:42 +0530

    Defuse the Bomb

---
+++ Problems/Defuse the Bomb/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.decrypt(code = [5,7,1,4], k = 3)
+#data = obj.decrypt(code = [1,2,3,4], k = 0)
+data = obj.decrypt(code = [2,4,9,3], k = -2)
+print(data)
