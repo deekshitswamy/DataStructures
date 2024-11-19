6f35d156143a95525007e4595ee89c562db65727
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-19 23:29:58 +0530

    changes

--- Problems/Maximum Sum of Distinct Subarrays With Length K/solution.py
+++ Problems/Maximum Sum of Distinct Subarrays With Length K/solution.py
@@ -2,7 +2,31 @@ import io
 from typing import List
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        pass
+        n = len(nums)
+        nums_set = set()
+        current_sum = max_sum = begin = 0
+
+        for end in range(n):
+            if nums[end] not in nums_set:
+                current_sum += nums[end]
+                nums_set.add(nums[end])
+
+                if end - begin + 1 == k:
+                    if current_sum > max_sum:
+                        max_sum = current_sum
+
+                    current_sum -= nums[begin]
+                    nums_set.remove(nums[begin])
+                    begin += 1
+            else:
+                while nums[begin] != nums[end]:
+                    current_sum -= nums[begin]
+                    nums_set.remove(nums[begin])
+                    begin += 1
+
+                begin += 1
+
+        return max_sum
 
 obj = Solution()
 #data = obj.maximumSubarraySum(nums = [1,5,4,2,9,9,9], k = 3)
