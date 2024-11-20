ab10e78b13a2d4c68a1937604cf7f809610e9e54
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-20 22:41:57 +0530

    changes

--- Problems/Take K of Each Character From Left and Right/solution.py
+++ Problems/Take K of Each Character From Left and Right/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def takeCharacters(self, s: str, k: int) -> int:
-        pass
+        limits = {c: s.count(c) - k for c in 'abc'}
+        if any(x < 0 for x in limits.values()):
+            return -1
+
+        cnts = {c: 0 for c in 'abc'}
+        ans = l = 0
+        for r, c in enumerate(s):
+            cnts[c] += 1
+            while cnts[c] > limits[c]:
+                cnts[s[l]] -= 1
+                l += 1
+            ans = max(ans, r - l + 1)
+
+        return len(s) - ans
 
 obj = Solution()
 #data = obj.takeCharacters(s = "aabaaaacaabc", k = 2)
