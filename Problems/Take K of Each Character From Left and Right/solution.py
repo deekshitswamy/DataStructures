eb21d3fc79c64edec1a7581a739f52696b519c3a
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-20 22:40:51 +0530

    Take K of Each Character From Left and Right

---
+++ Problems/Take K of Each Character From Left and Right/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def takeCharacters(self, s: str, k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.takeCharacters(s = "aabaaaacaabc", k = 2)
+data = obj.takeCharacters(s = "a", k = 1)
+print(data)
