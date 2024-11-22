c213e2f398dc1877c0c21d102385b2232daa7310
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-22 23:02:57 +0530

    changes

--- Problems/Flip Columns For Maximum Number of Equal Rows/solution.py
+++ Problems/Flip Columns For Maximum Number of Equal Rows/solution.py
@@ -2,7 +2,13 @@ import io
 from typing import List
 class Solution:
     def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
-        pass
+        pattern_count = defaultdict(int)
+        
+        for row in matrix:
+            pattern = ''.join('1' if (row[i] ^ row[0]) else '0' for i in range(len(row)))
+            pattern_count[pattern] += 1
+        
+        return max(pattern_count.values())
 
 obj = Solution()
 #data = obj.maxEqualRowsAfterFlips(matrix = [[0,1],[1,1]])
