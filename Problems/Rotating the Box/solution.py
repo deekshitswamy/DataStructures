d100b723334541b0ef56f0b932d3dab660c99e88
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-23 23:39:15 +0530

    changes

--- Problems/Rotating the Box/solution.py
+++ Problems/Rotating the Box/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
-        pass
+        rows, cols = len(box), len(box[0])
+        ans = list(box)
+
+        for i, row in enumerate(box):
+            start = 0
+            for j, value in enumerate(row):
+                if value != '#':
+                    start = j + 1
+                else:
+                    if j + 1 < cols and box[i][j + 1] == '.':
+                        ans[i][start], ans[i][j + 1] = '.', '#'
+                        start += 1
+
+        return [[ans[i][j] for i in range(rows - 1, -1, -1)] for j in range(cols)]
 
 obj = Solution()
 #data = obj.rotateTheBox(box = [["#",".","#"]])
