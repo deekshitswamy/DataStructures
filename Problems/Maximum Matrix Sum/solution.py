0447de2229a2de42ffaf08a62dfb49425ccce151
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-24 23:13:48 +0530

    Maximum Matrix Sum

---
+++ Problems/Maximum Matrix Sum/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxMatrixSum(self, matrix: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxMatrixSum(matrix = [[1,-1],[-1,1]])
+data = obj.maxMatrixSum(matrix = [[1,2,3],[-1,-2,-3],[1,2,3]])
+print(data)
