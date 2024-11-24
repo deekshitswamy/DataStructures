a2cebb3817c6b647e91f878b5202a70977c1abff
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-24 23:14:43 +0530

    changes

--- Problems/Maximum Matrix Sum/solution.py
+++ Problems/Maximum Matrix Sum/solution.py
@@ -2,7 +2,26 @@ import io
 from typing import List
 class Solution:
     def maxMatrixSum(self, matrix: List[List[int]]) -> int:
-        pass
+        n = len(matrix)
+        def absSmall(matr):
+            small = abs(matr[0][0])
+            for i in range(n):
+                for j in range(n):
+                    small = min(small, abs(matr[i][j]))
+            return small
+        
+        countNegative = 0
+        totalSum = 0
+        for i in range(n):
+            for j in range(n):
+                if matrix[i][j] <0:
+                    countNegative +=1
+                totalSum += abs(matrix[i][j])
+        small = absSmall(matrix)
+        if countNegative %2 == 0:
+            return totalSum
+        else:
+            return totalSum - 2*small
 
 obj = Solution()
 #data = obj.maxMatrixSum(matrix = [[1,-1],[-1,1]])
