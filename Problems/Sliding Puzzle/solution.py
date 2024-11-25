2ca8e151abbd1f623746407b1ded5387008ec58b
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-25 22:12:22 +0530

    Sliding Puzzle

---
+++ Problems/Sliding Puzzle/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def slidingPuzzle(self, board: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.slidingPuzzle(board = [[1,2,3],[4,0,5]])
+#data = obj.slidingPuzzle(board = [[1,2,3],[5,4,0]])
+data = obj.slidingPuzzle (board = [[4,1,2],[5,0,3]])
+print(data)
