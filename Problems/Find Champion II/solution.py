21da3df337d6273360b61639128612a0f4730746
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-27 19:10:08 +0530

    Find Champion II

---
+++ Problems/Find Champion II/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findChampion(self, n: int, edges: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findChampion(n = 3, edges = [[0,1],[1,2]])
+data = obj.findChampion(n = 4, edges = [[0,2],[1,3],[1,2]])
+print(data)
