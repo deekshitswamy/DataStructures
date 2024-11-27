e67ea0f3aa67ff7576a68d455a78f729c8750ab3
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-27 19:13:56 +0530

    changes

--- Problems/Shortest Distance After Road Addition Queries I/solution.py
+++ Problems/Shortest Distance After Road Addition Queries I/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
-        pass
+        dic = {end: [end-1] for end in range(1, n)} # add the original 1-step arrows
+        res = []
+        for j in range(len(queries)):
+            dp = [0 if i == 0 else n for i in range(n)]
+            dic[queries[j][1]].append(queries[j][0])
+            # below, each query will be visited at most once so it's not really a "nested" loop
+            for i in range(1, n):
+                for start in dic[i]:
+                    dp[i] = min(dp[i], dp[start]+1)
+            res.append(dp[-1])
+        return res
 
 
 obj = Solution()
