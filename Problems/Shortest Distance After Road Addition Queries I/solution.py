3d1cc4e4b334ce30cc5c5242ebdf933552c15156
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-27 19:12:35 +0530

    changes

---
+++ Problems/Shortest Distance After Road Addition Queries I/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.shortestDistanceAfterQueries(n = 5, queries = [[2,4],[0,2],[0,4]])
+data = obj.shortestDistanceAfterQueries(n = 4, queries = [[0,3],[0,2]])
+print(data)
