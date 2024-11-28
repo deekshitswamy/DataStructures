9beb15babfd271e47b7384d0da78d723699e40a8
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-28 20:45:52 +0530

    changes

--- Problems/Minimum Obstacle Removal to Reach Corner/solution.py
+++ Problems/Minimum Obstacle Removal to Reach Corner/solution.py
@@ -2,7 +2,27 @@ import io
 from typing import List
 class Solution:
     def minimumObstacles(self, grid: List[List[int]]) -> int:
-        pass
+        m,n=len(grid), len(grid[0])
+        dir=[(0,1),(1,0),(-1,0),(0,-1)]
+
+        q=deque([(0,0,0)])
+        visit=[[False] *n for i in range(m)]
+        visit[0][0]=True
+        while q:
+            x,y,cost=q.popleft()
+
+            if x==m-1 and y==n-1:
+                return cost
+            for dx,dy in dir:
+                newx,newy=x+dx,y+dy
+
+                if 0<=newx <m and 0<=newy<n and not visit[newx][newy]:
+                    visit[newx][newy]=True
+                    if grid[newx][newy]==1:
+                        q.append((newx,newy,cost+1))
+                    else:
+                        q.appendleft((newx,newy,cost))
+        return 0
 
 obj = Solution()
 #data = obj.minimumObstacles(grid = [[0,1,1],[1,1,0],[1,1,0]])
