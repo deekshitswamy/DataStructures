60afbb37b2ec53e06ec323a8766673a7b4fd38ab
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-28 20:45:11 +0530

    Minimum Obstacle Removal to Reach Corner

---
+++ Problems/Minimum Obstacle Removal to Reach Corner/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minimumObstacles(self, grid: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minimumObstacles(grid = [[0,1,1],[1,1,0],[1,1,0]])
+data = obj.minimumObstacles(grid = [[0,1,0,0,0],[0,1,0,1,0],[0,0,0,1,0]])
+print(data)
