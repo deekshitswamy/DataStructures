5a3cf45fc2b7ceeeba52a29faa06fe779ed2f4e1
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-28 20:46:13 +0530

    changes

--- Problems/Minimum Obstacle Removal to Reach Corner/solution.py
+++ Problems/Minimum Obstacle Removal to Reach Corner/solution.py
@@ -1,5 +1,6 @@
 import io
 from typing import List
+from collections import deque
 class Solution:
     def minimumObstacles(self, grid: List[List[int]]) -> int:
         m,n=len(grid), len(grid[0])
