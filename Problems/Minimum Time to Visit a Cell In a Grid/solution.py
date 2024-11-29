d8602e26006726123b5b79c3d4fd6d161cb9cad2
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-29 20:34:33 +0530

    Minimum Time to Visit a Cell In a Grid

---
+++ Problems/Minimum Time to Visit a Cell In a Grid/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minimumTime(self, grid: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minimumTime(grid = [[0,1,3,2],[5,1,2,5],[4,3,8,6]])
+data = obj.minimumTime(grid = [[0,2,4],[3,2,1],[1,0,4]])
+print(data)
