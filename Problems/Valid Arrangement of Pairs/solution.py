9d075f5a763b4e7f1c109f6fc20e6cdd3e69fd93
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-30 21:15:10 +0530

    changes

--- Problems/Valid Arrangement of Pairs/solution.py
+++ Problems/Valid Arrangement of Pairs/solution.py
@@ -2,7 +2,26 @@ import io
 from typing import List
 class Solution:
     def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
-        pass
+        g = defaultdict(list)
+        d = defaultdict(int)
+        for u, v in pairs:
+            g[u].append(v)
+            d[u] += 1
+            d[v] -= 1
+        
+        s = pairs[0][0]
+        for u in d:
+            if d[u] == 1: s = u
+        
+        ans = []
+        def dfs(u: int) -> None:
+            while g[u]:
+                v = g[u].pop()
+                dfs(v)
+                ans.append([u, v])
+    
+        dfs(s)
+        return ans[::-1]
 
 obj = Solution()
 #data = obj.validArrangement(pairs = [[5,1],[4,5],[11,9],[9,4]])
