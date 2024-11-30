95ba237c420864733b31c924abd3e3987f2b5687
Deeku-Zeus <deekuzeus21@gmail.com>
2024-11-30 21:14:07 +0530

    Valid Arrangement of Pairs

---
+++ Problems/Valid Arrangement of Pairs/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
+        pass
+
+obj = Solution()
+#data = obj.validArrangement(pairs = [[5,1],[4,5],[11,9],[9,4]])
+#data = obj.validArrangement(pairs = [[1,3],[3,2],[2,1]])
+data = obj.validArrangement(pairs = [[1,2],[1,3],[2,1]])
+print(data)
