652e7a69df261ffee745f66e375bc87d00b7ce1f
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-01 20:29:46 +0530

    Check If N and Its Double Exist

---
+++ Problems/Check If N and Its Double Exist/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.checkIfExist(arr = [10,2,5,3])
+data = obj.checkIfExist(arr = [3,1,7,11])
+print(data)
