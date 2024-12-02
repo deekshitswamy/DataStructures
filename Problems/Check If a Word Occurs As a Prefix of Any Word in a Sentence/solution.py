a20ab8fa69029fa76c6e052d3a6df0e18dc55b48
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-02 21:28:58 +0530

    changes

--- Problems/Check If a Word Occurs As a Prefix of Any Word in a Sentence/solution.py
+++ Problems/Check If a Word Occurs As a Prefix of Any Word in a Sentence/solution.py
@@ -2,7 +2,11 @@ import io
 from typing import List
 class Solution:
     def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
-        pass
+        words = sentence.split(" ")
+        for i, word in enumerate(words):
+            if word.startswith(searchWord):
+                return i + 1
+        return -1
 
 obj = Solution()
 #data = obj.isPrefixOfWord(sentence = "i love eating burger", searchWord = "burg")
