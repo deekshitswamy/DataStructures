e1c391228e6b01dc3915ee60943cc0c2f6a6c253
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-03 22:14:02 +0530

    changes

--- Problems/Adding Spaces to a String/solution.py
+++ Problems/Adding Spaces to a String/solution.py
@@ -2,7 +2,24 @@ import io
 from typing import List
 class Solution:
     def addSpaces(self, s: str, spaces: List[int]) -> str:
-        pass
+        result = [''] * (len(s) + len(spaces))
+        write_pos = 0
+        read_pos = 0
+        
+        for space_pos in spaces:
+            while read_pos < space_pos:
+                result[write_pos] = s[read_pos]
+                write_pos += 1
+                read_pos += 1
+            result[write_pos] = ' '
+            write_pos += 1
+            
+        while read_pos < len(s):
+            result[write_pos] = s[read_pos]
+            write_pos += 1
+            read_pos += 1
+            
+        return ''.join(result)
 
 obj = Solution()
 #data = obj.addSpaces(s = "LeetcodeHelpsMeLearn", spaces = [8,13,15])
