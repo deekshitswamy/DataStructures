6af5403147bd47993c2f406f0177c18b8c712757
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-04 22:27:48 +0530

    Make String a Subsequence Using Cyclic Increments

---
+++ Problems/Make String a Subsequence Using Cyclic Increments/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def canMakeSubsequence(self, str1: str, str2: str) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.canMakeSubsequence(str1 = "abc", str2 = "ad")
+#data = obj.canMakeSubsequence(str1 = "zc", str2 = "ad")
+data = obj.canMakeSubsequence(str1 = "ab", str2 = "d")
+print(data)
