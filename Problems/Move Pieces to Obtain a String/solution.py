3e6f9aee444086ae2769c2c465d8dc7754547d61
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-05 21:46:37 +0530

    changes

--- Problems/Move Pieces to Obtain a String/solution.py
+++ Problems/Move Pieces to Obtain a String/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def canChange(self, start: str, target: str) -> bool:
-        pass
+        start_li, target_li = [], []
+        for idx, (st_ch, tg_ch) in enumerate(zip(start, target)):
+            if st_ch != "_":
+                start_li.append((idx, st_ch))
+            if tg_ch != "_":
+                target_li.append((idx, tg_ch))
+        if len(start_li) != len(target_li):
+            return False
+        for st, tg in zip(start_li, target_li):
+            if st[1] != tg[1] or (st[1] == "L" and st[0] < tg[0]) or (st[1] == "R" and st[0] > tg[0]):
+                return False
+        return True
 
 obj = Solution()
 #data = obj.canChange(start = "_L__R__R_", target = "L______RR")
