4b5f3040d3c63c64c6b62c13f9d08fc865569cf7
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-05 21:45:45 +0530

    Move Pieces to Obtain a String

---
+++ Problems/Move Pieces to Obtain a String/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def canChange(self, start: str, target: str) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.canChange(start = "_L__R__R_", target = "L______RR")
+#data = obj.canChange(start = "R_L_", target = "__LR")
+data = obj.canChange(start = "_R", target = "R_")
+print(data)
