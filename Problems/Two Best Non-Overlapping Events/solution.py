5c495a760cf25c888eefb3e7a4749381000763a0
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-08 18:19:15 +0530

    changes

--- Problems/Two Best Non-Overlapping Events/solution.py
+++ Problems/Two Best Non-Overlapping Events/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def maxTwoEvents(self, events: List[List[int]]) -> int:
-        pass
+        events.sort()
+        pq = []
+        max_val = 0
+        ans = 0
+
+        for start, end, value in events:
+            while pq and pq[0][0] < start:
+                max_val = max(max_val, heapq.heappop(pq)[1])
+            ans = max(ans, max_val + value)
+            heapq.heappush(pq, (end, value))
+
+        return ans
 
 obj = Solution()
 #data = obj.maxTwoEvents(events = [[1,3,2],[4,5,2],[2,4,3]])
