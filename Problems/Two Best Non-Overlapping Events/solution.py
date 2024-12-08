09ece08c6bf95c6b287e09f290cf46f536298858
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-08 18:12:47 +0530

    Two Best Non-Overlapping Events

---
+++ Problems/Two Best Non-Overlapping Events/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maxTwoEvents(self, events: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxTwoEvents(events = [[1,3,2],[4,5,2],[2,4,3]])
+#data = obj.maxTwoEvents(events = [[1,3,2],[4,5,2],[1,5,5]])
+data = obj.maxTwoEvents(events = [[1,5,3],[1,5,1],[6,6,5]])
+print(data)
