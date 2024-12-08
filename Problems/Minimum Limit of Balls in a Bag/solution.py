24e0d2e0cbceb1ed08b0a015c5550e1885708f42
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-08 18:15:04 +0530

    changes

--- Problems/Minimum Limit of Balls in a Bag/solution.py
+++ Problems/Minimum Limit of Balls in a Bag/solution.py
@@ -2,7 +2,25 @@ import io
 from typing import List
 class Solution:
     def minimumSize(self, nums: List[int], maxOperations: int) -> int:
-        pass
+        left, right = 1, max(nums)
+        ans = right
+
+        while left <= right:
+            mid = (left + right) // 2
+            ops = 0
+
+            for n in nums:
+                ops += (n - 1) // mid
+                if ops > maxOperations:
+                    break
+
+            if ops <= maxOperations:
+                ans = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return ans
 
 obj = Solution()
 #data = obj.minimumSize(nums = [9], maxOperations = 2)
