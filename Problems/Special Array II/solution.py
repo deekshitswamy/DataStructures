0059604a7b14b6ac494425cddacb3173ade5f44a
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-10 02:11:45 +0530

    changes

--- Problems/Special Array II/solution.py
+++ Problems/Special Array II/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
-        pass
+        ans = []
+        id = 0
+        parityIds = [id]
+        for a, b in itertools.pairwise(nums):
+            if a % 2 == b % 2:
+                id += 1
+            parityIds.append(id)
+
+        for _from, to in queries:
+            ans.append(parityIds[_from] == parityIds[to])
+
+        return ans
 
 obj = Solution()
 #data = obj.isArraySpecial(nums = [3,4,1,2,6], queries = [[0,4]])
