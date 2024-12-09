1eff9676a5c823446dc68f26b774aeb72841097e
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-10 02:10:25 +0530

    Special Array II

---
+++ Problems/Special Array II/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+        pass
+
+obj = Solution()
+#data = obj.isArraySpecial(nums = [3,4,1,2,6], queries = [[0,4]])
+data = obj.isArraySpecial(nums = [4,3,1,6], queries = [[0,2],[2,3]])
+print(data)
