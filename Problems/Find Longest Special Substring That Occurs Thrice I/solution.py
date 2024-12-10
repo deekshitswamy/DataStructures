286829207efc4fc7f4a9b0512158f3bef52036c7
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-10 20:36:26 +0530

    Find Longest Special Substring That Occurs Thrice I

---
+++ Problems/Find Longest Special Substring That Occurs Thrice I/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maximumLength(s = "aaaa")
+#data = obj.maximumLength(s = "abcdef")
+data = obj.maximumLength(s = "abcaba")
+print(data)
