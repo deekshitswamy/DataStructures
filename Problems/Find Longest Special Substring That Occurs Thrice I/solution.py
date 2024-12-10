c0c531e37d6f6fdc92ad0e8954d0596a24a24f36
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-10 20:38:57 +0530

    changes

--- Problems/Find Longest Special Substring That Occurs Thrice I/solution.py
+++ Problems/Find Longest Special Substring That Occurs Thrice I/solution.py
@@ -2,7 +2,28 @@ import io
 from typing import List
 class Solution:
     def maximumLength(self, s: str) -> int:
-        pass
+        n = len(s)
+        runningLen = 0
+        prevLetter = '@'
+        counts = [[0] * (n + 1) for _ in range(26)]
+        for c in s:
+            if c == prevLetter:
+                runningLen += 1
+                counts[string.ascii_lowercase.index(c)][runningLen] += 1
+            else:
+                runningLen = 1
+                counts[string.ascii_lowercase.index(c)][runningLen] += 1
+                prevLetter = c
+
+        def getMaxFreq(count: list[int]) -> int:
+            times = 0
+            for freq in range(n, 0, -1):
+                times += count[freq]
+                if times >= 3:
+                    return freq
+            return -1
+
+        return max(getMaxFreq(count) for count in counts)
 
 obj = Solution()
 #data = obj.maximumLength(s = "aaaa")
