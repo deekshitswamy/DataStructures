e538d1d4d7cd852b6d477d8fb33655372ec187a2
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-11 21:03:04 +0530

    changes

--- Problems/Maximum Beauty of an Array After Applying Operation/solution.py
+++ Problems/Maximum Beauty of an Array After Applying Operation/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
-        pass
+        nums.sort()
+        n=len(nums)
+        left=0
+        ans=0
+        for i,num in enumerate(nums):
+            while left<n and nums[left]<num-2*k:
+                left+=1
+
+            ans=max(ans,i-left+1)
+
+        return ans
 
 obj = Solution()
 #data = obj.maximumBeauty(nums = [4,6,1,2], k = 2)
