d7e82e4bfed403dc43a023b150de96083c5491d6
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-12 22:40:21 +0530

    Take Gifts From the Richest Pile

---
+++ Problems/Take Gifts From the Richest Pile/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.pickGifts(gifts = [25,64,9,4,100], k = 4)
+data = obj.pickGifts(gifts = [1,1,1,1], k = 4)
+print(data)
