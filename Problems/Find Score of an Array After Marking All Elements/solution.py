4001ad634867b2d1421feee0884d686a11f6c165
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-13 19:38:28 +0530

    changes

--- Problems/Find Score of an Array After Marking All Elements/solution.py
+++ Problems/Find Score of an Array After Marking All Elements/solution.py
@@ -2,7 +2,16 @@ import io
 from typing import List
 class Solution:
     def findScore(self, nums: List[int]) -> int:
-        pass
+        n, j, s = len(nums), 0, 0
+        while j < n:
+            odd = even = 0
+            while j + 1 < n and nums[j] > nums[j + 1]:
+                if j & 1: odd += nums[j]
+                else: even += nums[j]
+                j += 1
+            s += (odd if j & 1 else even) + nums[j]
+            j += 2
+        return s
 
 obj = Solution()
 #data = obj.findScore(nums = [2,1,3,4,5,2])
