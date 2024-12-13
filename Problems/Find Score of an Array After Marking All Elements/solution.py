b47d71360d7d60d44989bb2dfb85dbcffae43ae3
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-13 19:37:22 +0530

    Find Score of an Array After Marking All Elements

---
+++ Problems/Find Score of an Array After Marking All Elements/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findScore(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findScore(nums = [2,1,3,4,5,2])
+data = obj.findScore(nums = [2,3,5,1,3,2])
+print(data)
