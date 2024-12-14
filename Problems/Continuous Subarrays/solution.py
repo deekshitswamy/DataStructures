ad89944e7e75b3693d9baa6116a11c56c34e32b2
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-14 20:39:16 +0530

    Continuous Subarrays

---
+++ Problems/Continuous Subarrays/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def continuousSubarrays(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.continuousSubarrays(nums = [5,4,2,4])
+data = obj.continuousSubarrays(nums = [1,2,3])
+print(data)
