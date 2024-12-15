4398a77db50d98eba6733c6becf3949feff1399e
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-15 19:52:04 +0530

    changes

--- Problems/Maximum Average Pass Ratio/solution.py
+++ Problems/Maximum Average Pass Ratio/solution.py
@@ -2,7 +2,23 @@ import io
 from typing import List
 class Solution:
     def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
-        pass
+        max_heap = []
+        n = len(classes)
+
+        for p, t in classes:
+            heapq.heappush(max_heap, (p / t - (p + 1) / (t + 1), p, t))
+
+        while extraStudents > 0:
+            _, p, t = heapq.heappop(max_heap)
+            p += 1
+            t += 1
+
+            heapq.heappush(max_heap, (p / t - (p + 1) / (t + 1), p, t))
+            extraStudents -= 1
+
+        result = sum([p / t for _, p, t in max_heap])
+
+        return result / n
 
 obj = Solution()
 #data = obj.maxAverageRatio(classes = [[1,2],[3,5],[2,2]], extraStudents = 2)
