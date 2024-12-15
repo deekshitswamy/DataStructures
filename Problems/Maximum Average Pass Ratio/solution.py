78bf4a647f8c7314a3abe2a2b59fc23e843f67c3
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-15 19:51:18 +0530

    Maximum Average Pass Ratio

---
+++ Problems/Maximum Average Pass Ratio/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
+        pass
+
+obj = Solution()
+#data = obj.maxAverageRatio(classes = [[1,2],[3,5],[2,2]], extraStudents = 2)
+data = obj.maxAverageRatio(classes = [[2,4],[3,9],[4,5],[2,10]], extraStudents = 4)
+print(data)
