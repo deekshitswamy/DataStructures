3da9de19af84bda94b63d2131f20b485e302415f
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-16 21:25:00 +0530

    changes

--- Problems/Final Array State After K Multiplication Operations I/solution.py
+++ Problems/Final Array State After K Multiplication Operations I/solution.py
@@ -2,7 +2,10 @@ import io
 from typing import List
 class Solution:
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
-        pass
+        for i in range(k):
+            j = nums.index(min(nums))
+            nums[j]*=multiplier
+        return nums
 
 obj = Solution()
 #data = obj.getFinalState(nums = [2,1,3,5,6], k = 5, multiplier = 2)
