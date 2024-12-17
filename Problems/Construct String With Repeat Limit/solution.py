5fea45bc884e404bf4002ede79c256f487413f29
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-17 19:55:10 +0530

    Construct String With Repeat Limit

---
+++ Problems/Construct String With Repeat Limit/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
+        pass
+
+obj = Solution()
+#data = obj.repeatLimitedString(s = "cczazcc", repeatLimit = 3)
+data = obj.repeatLimitedString(s = "aababab", repeatLimit = 2)
+print(data)
