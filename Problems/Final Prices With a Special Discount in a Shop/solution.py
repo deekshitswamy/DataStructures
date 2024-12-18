6f9d2ba8f7d8bd404aa534691c931a1146015343
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-18 20:04:21 +0530

    changes

--- Problems/Final Prices With a Special Discount in a Shop/solution.py
+++ Problems/Final Prices With a Special Discount in a Shop/solution.py
@@ -2,7 +2,14 @@ import io
 from typing import List
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        pass
+        for i in range(len(prices)):
+            for j in range(i+1,len(prices)):
+                if prices[i] >= prices[j]:
+                    prices[i] -= prices[j]
+                    break
+                else:
+                    pass
+        return prices
 
 obj = Solution()
 #data = obj.finalPrices(n = 1)
