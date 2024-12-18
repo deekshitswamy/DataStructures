83dc1e776423366f8e40aa15e67e8a85caa4bf16
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-18 20:03:32 +0530

    Final Prices With a Special Discount in a Shop

---
+++ Problems/Final Prices With a Special Discount in a Shop/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.finalPrices(n = 1)
+#data = obj.finalPrices(n = 2)
+data = obj.finalPrices(n = 2)
+print(data)
