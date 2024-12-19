822451395d8d027118a59384e217078a49955441
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-19 07:16:46 +0530

    changes:

--- Problems/Max Chunks To Make Sorted/solution.py
+++ Problems/Max Chunks To Make Sorted/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
-        pass
+        chunk = 0
+        max_val = [0] * len(arr)
+        max_val[0] = arr[0]
+
+        for i in range(1, len(arr)):
+            max_val[i] = max(max_val[i - 1], arr[i])
+
+        for i in range(len(arr)):
+            if max_val[i] == i:
+                chunk += 1
+        
+        return chunk
 
 obj = Solution()
 #data = obj.maxChunksToSorted(arr = [4,3,2,1,0])
