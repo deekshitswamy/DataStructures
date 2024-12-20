f7a9eecda9100a9863c85362f06ef02ec3a40f4a
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-20 21:59:36 +0530

    changes

--- Problems/Reverse Odd Levels of Binary Tree/solution.py
+++ Problems/Reverse Odd Levels of Binary Tree/solution.py
@@ -9,7 +9,41 @@ class TreeNode:
 
 class Solution:
     def reverseOddLevels(self, root: TreeNode) -> TreeNode:
-        pass
+        queue = deque()
+        level = 0
+        queue.append(root)
+
+        while queue:
+            size = len(queue)
+            if level % 2 != 0:
+                nodes_at_level = []
+
+                for _ in range(size):
+                    node = queue.popleft()
+                    nodes_at_level.append(node)
+                    if node.left:
+                        queue.append(node.left)
+                    if node.right:
+                        queue.append(node.right)
+
+                # Reverse the values at the current odd level
+                n = len(nodes_at_level)
+                for i in range(n // 2):
+                    nodes_at_level[i].val, nodes_at_level[n - i - 1].val = (
+                        nodes_at_level[n - i - 1].val, nodes_at_level[i].val
+                    )
+
+            else:
+                for _ in range(size):
+                    node = queue.popleft()
+                    if node.left:
+                        queue.append(node.left)
+                    if node.right:
+                        queue.append(node.right)
+
+            level += 1
+
+        return root
 
 obj = Solution()
 #data = obj.reverseOddLevels(root = [2,3,5,8,13,21,34])
