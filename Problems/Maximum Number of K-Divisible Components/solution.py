67300ef7fdf0c15695250d76488e1d369111bda2
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-21 15:42:38 +0530

    changes

--- Problems/Maximum Number of K-Divisible Components/solution.py
+++ Problems/Maximum Number of K-Divisible Components/solution.py
@@ -2,7 +2,27 @@ import io
 from typing import List
 class Solution:
     def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
-        pass
+        adj = defaultdict(list)
+        src = 0
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        comp = 0
+        visited = set()
+        def dfs(root):
+            nonlocal comp
+            if root in visited:
+                return 0
+            visited.add(root)
+            ans = values[root]
+            for neigh in adj[root]:
+                ans += dfs(neigh)
+            if ans % k == 0:
+                comp += 1
+                return 0
+            return ans % k
+        dfs(src)
+        return comp
 
 obj = Solution()
 #data = obj.maxKDivisibleComponents(n = 5, edges = [[0,2],[1,2],[1,3],[2,4]], values = [1,8,1,4,4], k = 6)
