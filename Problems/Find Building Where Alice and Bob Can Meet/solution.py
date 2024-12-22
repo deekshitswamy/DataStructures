848b6198611b2ff294cda63f79557f3dd400174d
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-22 20:15:15 +0530

    changes

---
+++ Problems/Find Building Where Alice and Bob Can Meet/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def leftmostBuildingQueries(self, heights: List[int], queries: List[List[int]]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.leftmostBuildingQueries(heights = [6,4,8,5,2,7], queries = [[0,1],[0,3],[2,4],[3,4],[2,2]])
+data = obj.leftmostBuildingQueries(heights = [5,3,8,2,6,1,4,6], queries = [[0,7],[3,5],[5,2],[3,0],[1,6]])
+print(data)
