29ec71b5909444fb1c31aba353d17cb19b6c7861
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-23 18:55:14 +0530

    changes

--- Problems/Minimum Number of Operations to Sort a Binary Tree by Level/solution.py
+++ Problems/Minimum Number of Operations to Sort a Binary Tree by Level/solution.py
@@ -9,7 +9,34 @@ class TreeNode:
 
 class Solution:
     def minimumOperations(self, root: TreeNode) -> int:
-        pass
+        def swap(arr, i, j):
+            arr[i], arr[j] = arr[j], arr[i]
+
+        def f(t):
+            n = len(t)
+            m = {v: i for i, v in enumerate(sorted(t))}
+            for i in range(n):
+                t[i] = m[t[i]]
+            ans = 0
+            for i in range(n):
+                while t[i] != i:
+                    swap(t, i, t[i])
+                    ans += 1
+            return ans
+
+        q = deque([root])
+        ans = 0
+        while q:
+            t = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                t.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            ans += f(t)
+        return ans
 
 obj = Solution()
 #data = obj.minimumOperations(root = [1,4,3,7,6,8,5,null,null,null,null,9,null,10])
