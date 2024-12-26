ec26d182eb2fc869f5634813e7f31f9df40b3b0d
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-26 17:54:48 +0530

    Target Sum

---
+++ Problems/Target Sum/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findTargetSumWays(nums = [1,1,1,1,1], target = 3)
+data = obj.findTargetSumWays(nums = [1], target = 1)
+print(data)
