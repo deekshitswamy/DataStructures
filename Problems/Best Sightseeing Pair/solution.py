b8ff69104bfa98a5eebc87584cc37a44c5dcde04
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-27 20:40:53 +0530

    changes

--- Problems/Best Sightseeing Pair/solution.py
+++ Problems/Best Sightseeing Pair/solution.py
@@ -2,7 +2,26 @@ import io
 from typing import List
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        pass
+        n = len(values)
+        left_max = [0] * n
+        right_max = [0] * n
+
+        # Compute left_max array
+        left_max[0] = values[0]
+        for i in range(1, n):
+            left_max[i] = max(left_max[i - 1], values[i] + i)
+
+        # Compute right_max array
+        right_max[n - 1] = values[n - 1] - (n - 1)
+        for i in range(n - 2, -1, -1):
+            right_max[i] = max(right_max[i + 1], values[i] - i)
+
+        # Compute the maximum score
+        maxi = 0
+        for i in range(1, n):
+            maxi = max(maxi, left_max[i - 1] + right_max[i])
+
+        return maxi
 
 obj = Solution()
 #data = obj.maxScoreSightseeingPair(values = [8,1,5,2,6])
