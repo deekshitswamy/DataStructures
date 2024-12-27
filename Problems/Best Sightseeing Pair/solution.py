434f5ddce2985a60b972fb500c01d0f8940ecac5
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-27 20:36:42 +0530

    Best Sightseeing Pair

---
+++ Problems/Best Sightseeing Pair/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxScoreSightseeingPair(values = [8,1,5,2,6])
+data = obj.maxScoreSightseeingPair(values = [1,2])
+print(data)
