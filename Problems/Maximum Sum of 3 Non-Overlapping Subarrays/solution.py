4a583865990004e0ea53e95c5eba9d6e360e8856
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-28 18:06:48 +0530

    Maximum Sum of 3 Non-Overlapping Subarrays

---
+++ Problems/Maximum Sum of 3 Non-Overlapping Subarrays/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxSumOfThreeSubarrays(self, nums: List[int], k: int) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.maxSumOfThreeSubarrays(nums = [1,2,1,2,6,7,5,1], k = 2)
+data = obj.maxSumOfThreeSubarrays(nums = [1,2,1,2,1,2,1,2,1], k = 2)
+print(data)
