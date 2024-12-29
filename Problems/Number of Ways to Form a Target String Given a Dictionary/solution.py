c123dd5c27a320b247fb2128dd87cc3c46957e04
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-29 23:20:39 +0530

    Number of Ways to Form a Target String Given a Dictionary

---
+++ Problems/Number of Ways to Form a Target String Given a Dictionary/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def numWays(self, words: List[str], target: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numWays(words = ["acca","bbbb","caca"], target = "aba")
+data = obj.numWays(words = ["abba","baab"], target = "bab")
+print(data)
