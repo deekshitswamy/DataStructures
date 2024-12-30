4702e31d10dc0f6a2f3480f716c050822de0403d
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-30 22:26:23 +0530

    Count Ways To Build Good Strings

---
+++ Problems/Count Ways To Build Good Strings/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countGoodStrings(low = 3, high = 3, zero = 1, one = 1)
+data = obj.countGoodStrings(low = 2, high = 3, zero = 1, one = 2)
+print(data)
