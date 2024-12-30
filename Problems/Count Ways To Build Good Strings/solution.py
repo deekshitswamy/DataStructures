ede7d50a7877df22ef936186a238c114a6895301
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-30 22:27:57 +0530

    changes

--- Problems/Count Ways To Build Good Strings/solution.py
+++ Problems/Count Ways To Build Good Strings/solution.py
@@ -2,7 +2,26 @@ import io
 from typing import List
 class Solution:
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
-        pass
+        MOD = 10**9 + 7
+        memo = {}
+
+        def rec(length):
+            if length > high:
+                return 0
+            
+            if length in memo:
+                return memo[length]
+            
+            count = 1 if low <= length <= high else 0
+
+            count += rec(length + zero) % MOD
+            count += rec(length + one) % MOD
+            count %= MOD
+            
+            memo[length] = count
+            return count
+
+        return rec(0)
 
 obj = Solution()
 #data = obj.countGoodStrings(low = 3, high = 3, zero = 1, one = 1)
