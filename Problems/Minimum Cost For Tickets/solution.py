52f77e7a3509a5d3b3ed4c9113a644464df0410f
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-31 20:06:01 +0530

    Minimum Cost For Tickets

--- Problems/Minimum Cost For Tickets/solution.py
+++ Problems/Minimum Cost For Tickets/solution.py
@@ -2,7 +2,25 @@ import io
 from typing import List
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        pass
+        a, b, c = costs
+        dp = {}
+
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+            if i >= len(days):
+                return 0
+
+            res = float("inf")
+            for cost, duration in [(a,1),(b,7),(c,30)]:
+                j = i
+                while j < len(days) and days[i] + duration - 1 >= days[j]:
+                    j += 1
+                res = min(res, cost + dfs(j))
+            dp[i] = res
+
+            return dp[i]
+        return dfs(0)
 
 obj = Solution()
 #data = obj.mincostTickets(days = [1,4,6,7,8,20], costs = [2,7,15])
