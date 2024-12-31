0238885a89fc2a1d7976f761e99250289d7b5e21
Deeku-Zeus <deekuzeus21@gmail.com>
2024-12-31 19:42:50 +0530

    Minimum Cost For Tickets

---
+++ Problems/Minimum Cost For Tickets/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.mincostTickets(days = [1,4,6,7,8,20], costs = [2,7,15])
+data = obj.mincostTickets(days = [1,2,3,4,5,6,7,8,9,10,30,31], costs = [2,7,15])
+print(data)
