ea7fe027fe98f450286b79986f25651a780cbbc4
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-01 21:16:43 +0530

    changes

--- Problems/Maximum Score After Splitting a String/solution.py
+++ Problems/Maximum Score After Splitting a String/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def maxScore(self, s: str) -> int:
-        pass
+        n=len(s)
+        ans=0
+        zeroes=0
+        ones=s.count('1')
+        for i in range(n-1):
+            if s[i]=='0':
+                zeroes+=1
+            else:
+                ones-=1
+            ans=max(ans,ones+zeroes)
+        return ans
 
 obj = Solution()
 #data = obj.maxScore(s = "011101")
