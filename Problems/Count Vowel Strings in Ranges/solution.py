aeda53ff5382ee77b1f91e8924d2b402484dcd6c
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-02 20:11:50 +0530

    changes

--- Problems/Count Vowel Strings in Ranges/solution.py
+++ Problems/Count Vowel Strings in Ranges/solution.py
@@ -2,7 +2,24 @@ import io
 from typing import List
 class Solution:
     def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
-        pass
+        arr=[0]*len(words)
+        for i in range(len(words)):
+            if words[i][0] in "aeiou" and words[i][-1] in "aeiou":
+                arr[i]=1
+        prefix=[0]*len(words)
+        prefix[0]=arr[0]
+        for i in range(1,len(words)):
+            prefix[i]=arr[i]+prefix[i-1]
+        ans=[]
+        for i in queries:
+            start=i[0]
+            end=i[1]
+            if start==0:
+                sub=0
+            else:
+                sub=prefix[start-1]
+            ans.append(prefix[end]-sub)
+        return ans
 
 obj = Solution()
 #data = obj.vowelStrings(words = ["aba","bcb","ece","aa","e"], queries = [[0,2],[1,4],[1,1]])
