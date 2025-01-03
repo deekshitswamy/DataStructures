91f39606289e3497ed9e8732978cbf1189e09f96
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-03 19:48:27 +0530

    Number of Ways to Split Array

---
+++ Problems/Number of Ways to Split Array/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.waysToSplitArray(nums = [10,4,-8,7])
+data = obj.waysToSplitArray(nums = [2,3,1,0])
+print(data)
