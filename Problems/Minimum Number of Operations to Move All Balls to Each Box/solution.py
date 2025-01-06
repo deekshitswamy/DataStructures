20d7815cad498306d888b3de5fe5bc8036c79416
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-06 21:32:09 +0530

    Minimum Number of Operations to Move All Balls to Each Box

---
+++ Problems/Minimum Number of Operations to Move All Balls to Each Box/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.minOperations(boxes = "110")
+data = obj.minOperations(boxes = "001011")
+print(data)
