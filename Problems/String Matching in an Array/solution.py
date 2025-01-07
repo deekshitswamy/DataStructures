8cf2107066b450809859a76516e59079d07a0647
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-07 22:41:39 +0530

    String Matching in an Array

---
+++ Problems/String Matching in an Array/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        pass
+
+obj = Solution()
+#data = obj.stringMatching(words = ["mass","as","hero","superhero"])
+#data = obj.stringMatching(words = ["leetcode","et","code"])
+data = obj.stringMatching(words = ["blue","green","bu"])
+print(data)
