e3f8043012a09a82f81c137f1f87777720474eee
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-07 22:43:43 +0530

    changes

--- Problems/String Matching in an Array/solution.py
+++ Problems/String Matching in an Array/solution.py
@@ -2,7 +2,14 @@ import io
 from typing import List
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
-        pass
+        words.sort(key=len)
+        ans=[]
+        for i in range(len(words)):
+            for j in range(i+1,len(words)):
+                if words[i] in words[j]:
+                    ans.append(words[i])
+                    break
+        return ans
 
 obj = Solution()
 #data = obj.stringMatching(words = ["mass","as","hero","superhero"])
