c61260f2e5e107a1e0718a967df36e211127ff57
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-08 23:23:13 +0530

    Count Prefix and Suffix Pairs I

---
+++ Problems/Count Prefix and Suffix Pairs I/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countPrefixSuffixPairs(words = ["a","aba","ababa","aa"])
+#data = obj.countPrefixSuffixPairs(words = ["pa","papa","ma","mama"])
+data = obj.countPrefixSuffixPairs(words = ["abab","ab"])
+print(data)
