8a7d659aecf3d9e079d02e37a4168de29f0f12e4
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-10 01:50:01 +0530

    changes

--- Problems/Counting Words With a Given Prefix/solution.py
+++ Problems/Counting Words With a Given Prefix/solution.py
@@ -2,7 +2,11 @@ import io
 from typing import List
 class Solution:
     def prefixCount(self, words: List[str], pref: str) -> int:
-        pass
+        count = 0
+        for word in words:
+            if word.find(pref) == 0:
+                count += 1
+        return count
 
 obj = Solution()
 #data = obj.prefixCount(words = ["pay","attention","practice","attend"], pref = "at")
