85ff5cb73145781c5643c1164ce50b8b4bc64603
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-10 23:37:39 +0530

    Word Subsets

---
+++ Problems/Word Subsets/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        pass
+
+obj = Solution()
+#data = obj.wordSubsets(words1 = ["amazon","apple","facebook","google","leetcode"], words2 = ["e","o"])
+data = obj.wordSubsets(words1 = ["amazon","apple","facebook","google","leetcode"], words2 = ["l","e"])
+print(data)
