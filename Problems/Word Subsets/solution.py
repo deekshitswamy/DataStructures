80e32eec3e559e930f56b3558a01d963ced28101
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-10 23:38:19 +0530

    changes

--- Problems/Word Subsets/solution.py
+++ Problems/Word Subsets/solution.py
@@ -2,7 +2,21 @@ import io
 from typing import List
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
-        pass
+        map = [0] * 26
+        for x in words2:
+            temp = [0] * 26
+            for c in x:
+                temp[ord(c) - ord('a')] += 1
+            for i in range(26):
+                map[i] = max(map[i], temp[i])
+        ans = []
+        for i in words1:
+            temp = [0] * 26
+            for c in i:
+                temp[ord(c) - ord('a')] += 1
+            if all(temp[j] >= map[j] for j in range(26)):
+                ans.append(i)
+        return ans
 
 obj = Solution()
 #data = obj.wordSubsets(words1 = ["amazon","apple","facebook","google","leetcode"], words2 = ["e","o"])
