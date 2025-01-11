b2e67e3d9b192618f2d9293bfc7e69407c5642b4
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-12 00:57:11 +0530

    changes

--- Problems/Construct K Palindrome Strings/solution.py
+++ Problems/Construct K Palindrome Strings/solution.py
@@ -2,7 +2,9 @@ import io
 from typing import List
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
-        pass
+        freq = Counter(s)
+        odd_count = sum(1 for count in freq.values() if count % 2 != 0)
+        return odd_count <= k <= len(s)
 
 obj = Solution()
 #data = obj.canConstruct(s = "annabelle", k = 2)
