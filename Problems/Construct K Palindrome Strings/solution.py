5be6bff34fc2d9f590cebc527277573db90c37f1
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-12 00:56:16 +0530

    Construct K Palindrome Strings

---
+++ Problems/Construct K Palindrome Strings/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.canConstruct(s = "annabelle", k = 2)
+#data = obj.canConstruct(s = "leetcode", k = 3)
+data = obj.canConstruct(s = "true", k = 4)
+print(data)
