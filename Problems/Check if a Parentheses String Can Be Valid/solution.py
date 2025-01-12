120a35006f60dea99e35446c52d6b5c6ce4109fa
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-12 22:53:29 +0530

    changes

--- Problems/Check if a Parentheses String Can Be Valid/solution.py
+++ Problems/Check if a Parentheses String Can Be Valid/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def canBeValid(self, s: str, locked: str) -> bool:
-        pass
+        n = len(s)
+        if n % 2 == 1:
+            return False
+        b = a = 0
+        for i in range(n):
+            b += 1 if locked[i] == '0' or s[i] == '(' else -1
+            if b < 0:
+                return False
+            a += 1 if locked[n - i - 1] == '0' or s[n - i - 1] == ')' else -1
+            if a < 0:
+                return False
+        return True
 
 obj = Solution()
 #data = obj.canBeValid(s = "))()))", locked = "010100")
