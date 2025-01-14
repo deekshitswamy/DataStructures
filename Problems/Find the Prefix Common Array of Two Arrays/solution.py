b21a9ad65c146f913d704c2e67f9a7ee38fe2a5d
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-14 21:15:47 +0530

    changes

--- Problems/Find the Prefix Common Array of Two Arrays/solution.py
+++ Problems/Find the Prefix Common Array of Two Arrays/solution.py
@@ -2,7 +2,21 @@ import io
 from typing import List
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
-        pass
+        C = [0]*len(A)
+        seen = set()
+        count= 0
+
+        for i in range(len(A)):
+            if A[i] in seen:
+                count+=1
+            seen.add(A[i])
+            if B[i] in seen:
+                count+=1
+            seen.add(B[i])
+
+            C[i] = count
+        
+        return C
 
 obj = Solution()
 #data = obj.findThePrefixCommonArray(A = [1,3,2,4], B = [3,1,2,4])
