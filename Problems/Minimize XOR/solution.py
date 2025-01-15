92b25f65fa32fa46c9a5b060debc19d960667d1a
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-15 22:14:03 +0530

    Minimize XOR

---
+++ Problems/Minimize XOR/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minimizeXor(self, num1: int, num2: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minimizeXor(num1 = 3, num2 = 5)
+data = obj.minimizeXor(num1 = 1, num2 = 12)
+print(data)
