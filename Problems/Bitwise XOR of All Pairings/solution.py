f31caa1a9bc7a6ac4bf35a9419ced381b09662b0
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-16 21:47:18 +0530

    changes

--- Problems/Bitwise XOR of All Pairings/solution.py
+++ Problems/Bitwise XOR of All Pairings/solution.py
@@ -2,7 +2,27 @@ import io
 from typing import List
 class Solution:
     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
-        pass
+        num1=nums1
+        num2=nums2
+        if len(num1)%2==0 and len(num2)%2==0:
+            return 0
+        elif len(num2)%2==0:
+            x=0
+            for i in num2:
+                x^=i
+            return x
+        elif len(num1)%2==0:
+            x=0
+            for i in num1:
+                x^=i
+            return x
+        else:
+            x=0
+            for i in num2:
+                x^=i
+            for i in num1:
+                x^=i
+            return x
 
 obj = Solution()
 #data = obj.xorAllNums(nums1 = [2,1,3], nums2 = [10,2,5,0])
