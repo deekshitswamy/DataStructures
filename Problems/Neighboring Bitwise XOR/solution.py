3b39066db08825ec468f4ccc9932792fec10f9bf
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-17 22:44:13 +0530

    changes

--- Problems/Neighboring Bitwise XOR/solution.py
+++ Problems/Neighboring Bitwise XOR/solution.py
@@ -2,7 +2,12 @@ import io
 from typing import List
 class Solution:
     def doesValidArrayExist(self, derived: List[int]) -> bool:
-        pass
+        a = 0
+        for i in derived:
+            a = a ^ i
+        if not(a):
+            return True
+        return False
 
 obj = Solution()
 #data = obj.doesValidArrayExist(derived = [1,1,0])
