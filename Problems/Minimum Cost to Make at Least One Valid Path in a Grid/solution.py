005da4f942d5144781b8b4733bd76448d8930014
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-19 00:31:37 +0530

    Minimum Cost to Make at Least One Valid Path in a Grid

---
+++ Problems/Minimum Cost to Make at Least One Valid Path in a Grid/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minCost(self, grid: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minCost(grid = [[1,1,1,1],[2,2,2,2],[1,1,1,1],[2,2,2,2]])
+#data = obj.minCost(grid = [[1,1,3],[3,2,2],[1,1,4]])
+data = obj.minCost(grid = [[1,2],[4,3]])
+print(data)
