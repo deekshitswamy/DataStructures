526d4e16ad2792e08d4b27db4511952fbbf160ff
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-19 20:55:00 +0530

    Trapping Rain Water II

---
+++ Problems/Trapping Rain Water II/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def trapRainWater(self, heightMap: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.trapRainWater(heightMap = [[1,4,3,1,3,2],[3,2,1,3,2,4],[2,3,3,2,3,1]])
+data = obj.trapRainWater(heightMap = [[3,3,3,3,3],[3,2,2,2,3],[3,2,1,2,3],[3,2,2,2,3],[3,3,3,3,3]])
+print(data)
