96b4dd2d21bfe7bda87035f4857a96282dacb13e
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-20 20:06:23 +0530

    First Completely Painted Row or Column

---
+++ Problems/First Completely Painted Row or Column/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.firstCompleteIndex(arr = [1,3,4,2], mat = [[1,4],[2,3]])
+data = obj.firstCompleteIndex(arr = [2,8,7,4,1,3,5,6,9], mat = [[3,2,5],[1,4,6],[8,7,9]])
+print(data)
