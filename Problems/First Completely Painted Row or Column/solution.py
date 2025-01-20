6fd06bdc79fcfae61f2b4eea06d1046445ab5c1b
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-20 20:07:52 +0530

    changes

--- Problems/First Completely Painted Row or Column/solution.py
+++ Problems/First Completely Painted Row or Column/solution.py
@@ -2,7 +2,23 @@ import io
 from typing import List
 class Solution:
     def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
-        pass
+        rows, cols = len(mat), len(mat[0])
+        location_map = [None] * (rows * cols + 1)
+
+        for i in range(rows):
+            for j in range(cols):
+                location_map[mat[i][j]] = (i, j)
+
+        row_map = [cols] * rows
+        col_map = [rows] * cols
+        
+        for idx, val in enumerate(arr):
+            i, j = location_map[val]
+            row_map[i] -= 1
+            col_map[j] -= 1
+
+            if row_map[i] == 0 or col_map[j] == 0:
+                return idx
 
 obj = Solution()
 #data = obj.firstCompleteIndex(arr = [1,3,4,2], mat = [[1,4],[2,3]])
