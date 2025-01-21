de754ec11c9967bfae5c6dce2b0a8a0eac89500f
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-21 22:21:43 +0530

    Grid Game

---
+++ Problems/Grid Game/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def gridGame(self, grid: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.gridGame(grid = [[2,5,4],[1,5,1]])
+#data = obj.gridGame(grid = [[3,3,1],[8,5,2]])
+data = obj.gridGame(grid = [[1,3,1,15],[1,3,3,1]])
+print(data)
