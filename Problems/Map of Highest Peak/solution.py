c183bf7484bd6c872170f56b1df672f1795eaf3a
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-22 22:26:59 +0530

    changes

--- Problems/Map of Highest Peak/solution.py
+++ Problems/Map of Highest Peak/solution.py
@@ -2,7 +2,26 @@ import io
 from typing import List
 class Solution:
     def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
-        pass
+        rows=len(isWater)
+        cols=len(isWater[0])
+        inf=10**9
+        dist=[[inf]*cols for _ in range(rows)]
+        q=collections.deque()
+        for x in range(rows):
+            for y in range(cols):
+                if isWater[x][y]==1:
+                    dist[x][y]=0
+                    q.append((0,x,y))
+                    
+        directions=[(1,0),(0,1),(-1,0),(0,-1)]
+        while len(q)>0:
+            d,x,y=q.popleft()
+            for dx,dy in directions:
+                nx,ny=x+dx,y+dy
+                if 0<=nx<rows and 0<=ny<cols and dist[nx][ny]==inf:
+                    dist[nx][ny]=d+1
+                    q.append((d+1,nx,ny))
+        return dist
 
 obj = Solution()
 #data = obj.highestPeak(isWater = [[0,1],[0,0]])
