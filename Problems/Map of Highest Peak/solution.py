e1463b179ad7200347f7b81fb0e9a8091a6a0598
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-22 22:25:58 +0530

    Map of Highest Peak

---
+++ Problems/Map of Highest Peak/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
+        pass
+
+obj = Solution()
+#data = obj.highestPeak(isWater = [[0,1],[0,0]])
+data = obj.highestPeak(isWater = [[0,0,1],[1,0,0],[0,0,0]])
+print(data)
