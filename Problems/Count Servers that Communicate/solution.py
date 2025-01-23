829215b96e5b1665fad7d1cf3706ef1e77d66f96
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-23 23:16:46 +0530

    changews

--- Problems/Count Servers that Communicate/solution.py
+++ Problems/Count Servers that Communicate/solution.py
@@ -2,7 +2,22 @@ import io
 from typing import List
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
-        pass
+        m = len(grid)
+        n = len(grid[0])
+        row = [0]*m
+        col = [0]*n
+        for i in range(m):
+            for j in range(n):
+                row[i] += grid[i][j]
+                col[j] += grid[i][j]
+
+        total = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1 and (row[i]>1 or col[j]>1):
+                    total += 1
+
+        return total
 
 obj = Solution()
 #data = obj.countServers(grid = [[1,0],[0,1]])
