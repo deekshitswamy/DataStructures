103a5371fae250f91b57bbe11388cc73c6379eaa
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-23 23:15:57 +0530

    Count Servers that Communicate

---
+++ Problems/Count Servers that Communicate/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def countServers(self, grid: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countServers(grid = [[1,0],[0,1]])
+#data = obj.countServers(grid = [[1,0],[1,1]])
+data = obj.countServers(grid = [[1,1,0,0],[0,0,1,0],[0,0,1,0],[0,0,0,1]])
+print(data)
