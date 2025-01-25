e8c74a7b907521f68154abd1c70d7cc0044f59e3
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-25 19:32:12 +0530

    Make Lexicographically Smallest Array by Swapping Elements

---
+++ Problems/Make Lexicographically Smallest Array by Swapping Elements/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.lexicographicallySmallestArray(nums = [1,5,3,9,8], limit = 2)
+#data = obj.lexicographicallySmallestArray(nums = [1,7,6,18,2,1], limit = 3)
+data = obj.lexicographicallySmallestArray(nums = [1,7,28,19,10], limit = 3)
+print(data)
