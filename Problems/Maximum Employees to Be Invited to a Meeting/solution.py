8abbbb142d165fa273e4cf23a0a65f3d1165f48c
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-27 00:21:49 +0530

    Maximum Employees to Be Invited to a Meeting

---
+++ Problems/Maximum Employees to Be Invited to a Meeting/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maximumInvitations(self, favorite: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maximumInvitations(favorite = [2,2,1,2])
+#data = obj.maximumInvitations(favorite = [1,2,0])
+data = obj.maximumInvitations(favorite = [3,0,1,4,1])
+print(data)
