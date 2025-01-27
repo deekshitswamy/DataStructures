cd702b974e95eb821eb60554cfe5805d92182e3f
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-27 22:08:07 +0530

    changes

--- Problems/Course Schedule IV/solution.py
+++ Problems/Course Schedule IV/solution.py
@@ -5,7 +5,7 @@ class Solution:
         pass
 
 obj = Solution()
-#data = obj.checkIfPrerequisite(n = 1)
-#data = obj.checkIfPrerequisite(n = 2)
-data = obj.checkIfPrerequisite(n = 2)
+#data = obj.checkIfPrerequisite(numCourses = 2, prerequisites = [[1,0]], queries = [[0,1],[1,0]])
+#data = obj.checkIfPrerequisite(numCourses = 2, prerequisites = [], queries = [[1,0],[0,1]])
+data = obj.checkIfPrerequisite(numCourses = 3, prerequisites = [[1,2],[1,0],[2,0]], queries = [[1,0],[1,2]])
 print(data)
