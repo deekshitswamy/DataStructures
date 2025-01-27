d8330e0f252b55e68c0ef98f09637465df2fd84d
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-27 22:07:27 +0530

    Course Schedule IV

---
+++ Problems/Course Schedule IV/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        pass
+
+obj = Solution()
+#data = obj.checkIfPrerequisite(n = 1)
+#data = obj.checkIfPrerequisite(n = 2)
+data = obj.checkIfPrerequisite(n = 2)
+print(data)
