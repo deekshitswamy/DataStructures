c1f661a815544a90def7f1dbe1e4fa2ece3e4bb9
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-28 19:49:30 +0530

    Maximum Number of Fish in a Grid

---
+++ Problems/Maximum Number of Fish in a Grid/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findMaxFish(self, grid: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findMaxFish(grid = [[0,2,1,0],[4,0,0,3],[1,0,0,4],[0,3,2,0]])
+data = obj.findMaxFish(grid = [[1,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,1]])
+print(data)
