3122584f04f2702a53a95875461802ede5e0d680
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-29 23:28:32 +0530

    changes

--- Problems/Redundant Connection/solution.py
+++ Problems/Redundant Connection/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        pass
+        src = [n for n in range(len(edges) + 1)] 
+
+        def find(x):
+            src[x] = x if src[x] == x else find(src[x])
+            return src[x]
+        
+        for a, b in edges:
+            src_a, src_b = find(a), find(b)
+            if src_a == src_b:
+                return [a,b] 
+                              
+            src[src_b] = src_a
 
 obj = Solution()
 #data = obj.findRedundantConnection(edges = [[1,2],[1,3],[2,3]])
