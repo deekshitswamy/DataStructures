d29d94198f5730c29706be31d95e62d5b8514ea6
Deeku-Zeus <deekuzeus21@gmail.com>
2025-01-30 21:45:43 +0530

    Divide Nodes Into the Maximum Number of Groups

---
+++ Problems/Divide Nodes Into the Maximum Number of Groups/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def magnificentSets(self, n: int, edges: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.magnificentSets(n = 6, edges = [[1,2],[1,4],[1,5],[2,6],[2,3],[4,6]])
+data = obj.magnificentSets(n = 3, edges = [[1,2],[2,3],[3,1]])
+print(data)
