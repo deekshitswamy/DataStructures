581214edc8487755b306c9f20b7f98caf1e4c9da
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-01 21:37:12 +0530

    Special Array I

---
+++ Problems/Special Array I/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.isArraySpecial(nums = [1])
+#data = obj.isArraySpecial(nums = [2,1,4])
+data = obj.isArraySpecial(nums = [4,3,1,6])
+print(data)
