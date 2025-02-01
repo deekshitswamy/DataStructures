2cd043b667239e9fef4ec3cb1978a07a665dccb9
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-01 21:34:28 +0530

    Making A Large Island

---
+++ Problems/Making A Large Island/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def largestIsland(self, grid: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.largestIsland(grid = [[1,0],[0,1]])
+#data = obj.largestIsland(grid = [[1,1],[1,0]])
+data = obj.largestIsland(grid = [[1,1],[1,1]])
+print(data)
