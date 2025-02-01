7c026830aa99db2a1923c5bf63ceaf027ce26577
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-01 21:38:18 +0530

    changes

--- Problems/Special Array I/solution.py
+++ Problems/Special Array I/solution.py
@@ -2,7 +2,13 @@ import io
 from typing import List
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
-        pass
+        pairty = nums[0] % 2
+        for i in range(1, len(nums)):
+            if nums[i] % 2 == pairty:
+                return False
+            pairty = (pairty + 1) % 2
+        
+        return True
 
 obj = Solution()
 #data = obj.isArraySpecial(nums = [1])
