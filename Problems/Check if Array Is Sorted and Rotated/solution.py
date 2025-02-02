fdbe4c066ce4da15d82684d0535936ae9bb6b1a2
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-02 16:54:02 +0530

    Check if Array Is Sorted and Rotated

---
+++ Problems/Check if Array Is Sorted and Rotated/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.check(nums = [3,4,5,1,2])
+#data = obj.check(nums = [2,1,3,4])
+data = obj.check(nums = [1,2,3])
+print(data)
