fcf4e616bfe30041362399969ca3ac2e4c3ea0ca
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-04 21:27:00 +0530

    changes

--- Problems/Maximum Ascending Subarray Sum/solution.py
+++ Problems/Maximum Ascending Subarray Sum/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def maxAscendingSum(self, nums: List[int]) -> int:
-        pass
+        n=len(nums)
+        if(n==0):return 0
+        if(n==1):return nums[0]
+        sum,ans=nums[0],0
+        for i in range(0,n-1):
+            if nums[i+1]>nums[i]:
+                sum+=nums[i+1]
+            else:
+                ans=max(sum,ans)
+                sum=nums[i+1]
+            ans=max(sum,ans)
+        return ans
 
 obj = Solution()
 #data = obj.maxAscendingSum(nums = [10,20,30,5,10,50])
