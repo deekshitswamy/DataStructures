49a79ba0d00d8171b7d48cf2dba66dfe73485c25
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-05 21:11:41 +0530

    changes

--- Problems/Check if One String Swap Can Make Strings Equal/solution.py
+++ Problems/Check if One String Swap Can Make Strings Equal/solution.py
@@ -2,7 +2,19 @@ import io
 from typing import List
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        pass
+        if len(s1) != len(s2):
+            return False
+        not_match, count = -1, 0
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                if count == 2:
+                    return False
+                elif not_match == -1:
+                    not_match = i
+                elif s1[not_match] != s2[i] or s1[i] != s2[not_match]:
+                    return False
+                count += 1
+        return count == 0 or count == 2
 
 obj = Solution()
 #data = obj.areAlmostEqual(s1 = "bank", s2 = "kanb")
