68e90cb032e5c0cdb3302c7b2637d3badd166269
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-06 23:53:29 +0530

    changes

--- Problems/Tuple with Same Product/solution.py
+++ Problems/Tuple with Same Product/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def tupleSameProduct(self, nums: List[int]) -> int:
-        pass
+        n = len(nums)
+        record = {}
+        for i in range(n):
+            for j in range(i+1, n):
+                product = nums[i] * nums[j]
+                record[product] = record.get(product, 0) + 1
+        result = 0
+        for _, value in record.items():
+            if value >= 2:
+                result += value * (value-1) // 2 * 8
+        return result
 
 obj = Solution()
 #data = obj.tupleSameProduct(nums = [2,3,4,6])
