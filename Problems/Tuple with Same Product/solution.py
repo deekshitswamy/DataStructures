5aa21bd1bd56afcc5792eab00c3209013532fcb1
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-06 23:52:39 +0530

    Tuple with Same Product

---
+++ Problems/Tuple with Same Product/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.tupleSameProduct(nums = [2,3,4,6])
+data = obj.tupleSameProduct(nums = [1,2,4,5,10])
+print(data)
