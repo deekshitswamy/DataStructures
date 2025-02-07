77a0540b835da30ca40bc28a1407afb8957b9dff
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-07 21:37:27 +0530

    Find the Number of Distinct Colors Among the Balls

---
+++ Problems/Find the Number of Distinct Colors Among the Balls/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.queryResults(limit = 4, queries = [[1,4],[2,5],[1,3],[3,4]])
+data = obj.queryResults(limit = 4, queries = [[0,1],[1,2],[2,2],[3,4],[4,5]])
+print(data)
