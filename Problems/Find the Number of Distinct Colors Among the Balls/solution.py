0bf62dfd91b5fee39f91beb253321f5afe44a3fe
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-07 21:38:50 +0530

    changes

--- Problems/Find the Number of Distinct Colors Among the Balls/solution.py
+++ Problems/Find the Number of Distinct Colors Among the Balls/solution.py
@@ -2,7 +2,31 @@ import io
 from typing import List
 class Solution:
     def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
-        pass
+        colors={}
+        balls={}
+        cnt=0
+        res=[]
+        for query in queries:
+            x,y = query[0],query[1]
+            if x not in balls:
+                if y not in colors or colors[y]<=0:
+                    colors[y]=0
+                    cnt+=1
+                colors[y]+=1
+            else:
+                old_c=balls[x]
+                colors[old_c]-=1
+                if colors[old_c]==0:
+                    cnt-=1
+                    
+                if y not in colors or colors[y]==0:
+                    colors[y]=0
+                    cnt+=1
+                colors[y]+=1
+
+            balls[x]=y
+            res.append(cnt)
+        return res
 
 obj = Solution()
 #data = obj.queryResults(limit = 4, queries = [[1,4],[2,5],[1,3],[3,4]])
