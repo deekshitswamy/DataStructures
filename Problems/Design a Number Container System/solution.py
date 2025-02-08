b7c81d118c51a66f0de317544848eb7b73967d87
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-09 00:02:43 +0530

    Design a Number Container System

---
+++ Problems/Design a Number Container System/solution.py
@@ -0,0 +1,28 @@
+import io
+from typing import List
+class NumberContainers:
+
+    def __init__(self):
+        pass
+        
+
+    def change(self, index: int, number: int) -> None:
+        pass
+        
+
+    def find(self, number: int) -> int:
+        pass
+        
+
+
+# Your NumberContainers object will be instantiated and called as such:
+obj = NumberContainers()
+# obj.change(["NumberContainers", "find", "change", "change", "change", "change", "find", "change", "find"]
+# [[], [10], [2, 10], [1, 10], [3, 10], [5, 10], [10], [1, 20], [10]])
+# param_2 = obj.find(number)
+
+# obj = Solution()
+# #data = obj.testfunc(n = 1)
+# #data = obj.testfunc(n = 2)
+# data = obj.testfunc(n = 2)
+# print(data)
