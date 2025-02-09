dbf18c48e1b785d34019bfb09bfc3f6bdd69c041
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-09 16:28:56 +0530

    changes

--- Problems/Count Number of Bad Pairs/solution.py
+++ Problems/Count Number of Bad Pairs/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
-        pass
+        m = defaultdict(int)
+
+        for i, num in enumerate(nums):
+            m[i - num] += 1
+        
+        n = len(nums)
+        res = n * (n - 1) // 2
+        
+        for v in m.values():
+            res -= v * (v - 1) // 2
+        
+        return res
 
 obj = Solution()
 #data = obj.countBadPairs(nums = [4,1,3,3])
