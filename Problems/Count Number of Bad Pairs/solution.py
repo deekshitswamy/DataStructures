f7e60382e199bc73155840cbcc8aabe23a3cfa54
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-09 16:28:19 +0530

    Count Number of Bad Pairs

---
+++ Problems/Count Number of Bad Pairs/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def countBadPairs(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countBadPairs(nums = [4,1,3,3])
+data = obj.countBadPairs(nums = [1,2,3,4,5])
+print(data)
