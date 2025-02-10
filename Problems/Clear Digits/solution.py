4fd06ce7f9726142cf43e3dbfb1968e99b12194f
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-10 23:36:06 +0530

    Clear Digits

---
+++ Problems/Clear Digits/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.clearDigits(s = "abc")
+data = obj.clearDigits(s = "cb34")
+print(data)
