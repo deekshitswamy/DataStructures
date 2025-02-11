7d2b47f580dac8160fc1d4c553a813e4425a83b0
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-11 20:19:30 +0530

    Remove All Occurrences of a Substring

---
+++ Problems/Remove All Occurrences of a Substring/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.removeOccurrences(s = "daabcbaabcbc", part = "abc")
+data = obj.removeOccurrences(s = "axxxxyyyyb", part = "xy")
+print(data)
