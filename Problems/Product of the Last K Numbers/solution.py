9d3b59c76676db6be6d0d7021a55f8af69d82e14
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-14 21:39:46 +0530

    Product of the Last K Numbers

---
+++ Problems/Product of the Last K Numbers/solution.py
@@ -0,0 +1,22 @@
+import io
+from typing import List
+class ProductOfNumbers:
+
+    def __init__(self):
+        pass
+        
+
+    def add(self, num: int) -> None:
+        pass
+        
+
+    def getProduct(self, k: int) -> int:
+        pass
+
+        
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)
