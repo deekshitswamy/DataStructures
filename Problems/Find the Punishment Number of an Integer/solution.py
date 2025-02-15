f63ccdfa2ad32e937350e9575f46b6b7986efb24
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-15 17:28:48 +0530

    Find the Punishment Number of an Integer

---
+++ Problems/Find the Punishment Number of an Integer/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def punishmentNumber(self, n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.punishmentNumber(n = 10)
+data = obj.punishmentNumber(n = 37)
+print(data)
