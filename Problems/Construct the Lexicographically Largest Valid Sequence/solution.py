3c13f6b590ebf4aae3fe9b7f0beacf131e0e6250
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-16 22:54:51 +0530

    changes

--- Problems/Construct the Lexicographically Largest Valid Sequence/solution.py
+++ Problems/Construct the Lexicographically Largest Valid Sequence/solution.py
@@ -2,7 +2,31 @@ import io
 from typing import List
 class Solution:
     def constructDistancedSequence(self, n: int) -> List[int]:
-        pass
+        sequence = [0] * (2 * n - 1)
+        seen = set()
+        def dfs(i):
+            if i == len(sequence):
+                return True
+            if sequence[i]:
+                return dfs(i + 1)
+            for j in range(n, 0, - 1):
+                if j in seen:
+                    continue
+                seen.add(j)
+                sequence[i] = j
+                if j == 1:
+                    if dfs(i + 1):
+                        return True
+                elif i + j < len(sequence) and sequence[i + j] == 0:
+                    sequence[i + j] = j
+                    if dfs(i + 1):
+                        return True
+                    sequence[i + j] = 0
+                sequence[i] = 0
+                seen.remove(j)
+            return False
+        dfs(0)
+        return sequence
 
 obj = Solution()
 #data = obj.constructDistancedSequence(n = 5)
