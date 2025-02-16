043d2208e68a70b3ca429b21fca2ec56157f01b7
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-16 22:54:08 +0530

    Construct the Lexicographically Largest Valid Sequence

---
+++ Problems/Construct the Lexicographically Largest Valid Sequence/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def constructDistancedSequence(self, n: int) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.constructDistancedSequence(n = 5)
+data = obj.constructDistancedSequence(n = 3)
+print(data)
