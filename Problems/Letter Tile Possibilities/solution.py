699148b3449f35594f11f05c5d7b770ed36dbc35
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-17 23:09:07 +0530

    Letter Tile Possibilities

---
+++ Problems/Letter Tile Possibilities/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numTilePossibilities(tiles = "AAB")
+#data = obj.numTilePossibilities(tiles = "AAABBC")
+data = obj.numTilePossibilities(tiles = "V")
+print(data)
