c07b78b94e4707208816889a75e3d08a35a9e4ab
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-18 22:29:56 +0530

    Construct Smallest Number From DI String

---
+++ Problems/Construct Smallest Number From DI String/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def smallestNumber(self, pattern: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.smallestNumber(pattern = "IIIDIDDD")
+data = obj.smallestNumber(pattern = "DDD")
+print(data)
