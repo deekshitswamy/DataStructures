37944acbd9d445a456f0dcef0439fd483804c1a3
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-18 22:30:44 +0530

    changes

--- Problems/Construct Smallest Number From DI String/solution.py
+++ Problems/Construct Smallest Number From DI String/solution.py
@@ -2,7 +2,29 @@ import io
 from typing import List
 class Solution:
     def smallestNumber(self, pattern: str) -> str:
-        pass
+        n = len(pattern)
+        used = [False] * (n + 1)
+        num_arr = []
+
+        def dfs(idx):
+            if idx >= n:
+                return True
+            nonlocal num_arr
+            start = 0 if idx == -1 or pattern[idx] == "D" else num_arr[-1]
+            end = n + 1 if idx == -1 or pattern[idx] == "I" else num_arr[-1]
+
+            for i in range(start, end):
+                if used[i]:
+                    continue
+                used[i] = True
+                num_arr.append(i + 1)
+                if dfs(idx + 1):
+                    return True
+                num_arr.pop()
+                used[i] = False
+
+        dfs(-1)
+        return ''.join([str(num) for num in num_arr])
 
 obj = Solution()
 #data = obj.smallestNumber(pattern = "IIIDIDDD")
