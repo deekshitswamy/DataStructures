83aed9a9d9d4852230f6709de7552640313fc1e2
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-19 22:17:38 +0530

    The k-th Lexicographical String of All Happy Strings of Length n

---
+++ Problems/The k-th Lexicographical String of All Happy Strings of Length n/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        pass
+
+obj = Solution()
+#data = obj.getHappyString(n = 1, k = 3)
+#data = obj.getHappyString(n = 1, k = 4)
+data = obj.getHappyString(n = 3, k = 9)
+print(data)
