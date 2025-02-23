2d5bd1f9da259f144948e4a8929f7dd16dd272e4
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-23 22:19:07 +0530

    changes

--- Problems/Construct Binary Tree from Preorder and Postorder Traversal/solution.py
+++ Problems/Construct Binary Tree from Preorder and Postorder Traversal/solution.py
@@ -8,7 +8,19 @@ class TreeNode:
         self.right = right
 class Solution:
     def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> TreeNode:
-        pass
+        positions = {x: i for i, x in enumerate(postorder)}
+        st: List[TreeNode] = []
+        for x in preorder:
+            cur = TreeNode(x)
+            while st and positions[st[-1].val] < positions[x]:
+                st.pop()
+            if st:
+                if st[-1].left is None:
+                    st[-1].left = cur
+                else:
+                    st[-1].right = cur
+            st.append(cur)
+        return st[0]
 
 obj = Solution()
 #data = obj.constructFromPrePost(preorder = [1,2,4,5,3,6,7], postorder = [4,5,2,6,7,3,1])
