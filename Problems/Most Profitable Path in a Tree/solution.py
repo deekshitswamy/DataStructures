f906e564b665c733689014b259b835ca07dafb0e
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-24 22:55:49 +0530

    changes

--- Problems/Most Profitable Path in a Tree/solution.py
+++ Problems/Most Profitable Path in a Tree/solution.py
@@ -2,7 +2,51 @@ import io
 from typing import List
 class Solution:
     def mostProfitablePath(self, edges: List[List[int]], bob: int, amount: List[int]) -> int:
-        pass
+        n = len(amount)
+        adj = [[] for _ in range(n)]
+
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+
+        bob_parent = {}
+        
+        def find_path_to_root(u, p):
+            if u == 0:
+                return True
+            for v in adj[u]:
+                if v != p:
+                    if find_path_to_root(v, u):
+                        bob_parent[u] = v
+                        return True
+            return False
+            
+        find_path_to_root(bob, -1)
+
+        def dfs(a, b, alice_parent):
+            bob_amount = amount[b]
+            alice_amount = 0
+            if a == b:
+                alice_amount = (amount[a] // 2)
+            else:
+                alice_amount = amount[a]
+            
+            if len(adj[a]) == 1 and adj[a][0] == alice_parent:
+                return alice_amount
+
+            amount[b] = 0
+            nxt_b = bob_parent.get(b, 0)
+
+            mx = float('-inf')
+            for nxt_a in adj[a]:
+                if nxt_a != alice_parent:
+                    mx = max(mx, alice_amount + dfs(nxt_a, nxt_b, a))
+
+            amount[b] = bob_amount
+            return mx
+        
+        return dfs(0, bob, -1)
 
 obj = Solution()
 #data = obj.mostProfitablePath(edges = [[0,1],[1,2],[1,3],[3,4]], bob = 3, amount = [-2,4,2,-4,6])
