9efb9598369df7d1a26e3e2a4d8ab38d59d98502
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-24 22:53:32 +0530

    Most Profitable Path in a Tree

---
+++ Problems/Most Profitable Path in a Tree/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def mostProfitablePath(self, edges: List[List[int]], bob: int, amount: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.mostProfitablePath(edges = [[0,1],[1,2],[1,3],[3,4]], bob = 3, amount = [-2,4,2,-4,6])
+data = obj.mostProfitablePath(edges = [[0,1]], bob = 1, amount = [-7280,2350])
+print(data)
