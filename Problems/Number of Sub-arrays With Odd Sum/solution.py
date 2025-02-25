c4c24151592e6b0b8c2281a1169e9bfecfc9769e
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-25 21:59:54 +0530

    Number of Sub-arrays With Odd Sum

---
+++ Problems/Number of Sub-arrays With Odd Sum/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def numOfSubarrays(self, arr: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numOfSubarrays(arr = [1,3,5])
+#data = obj.numOfSubarrays(arr = [2,4,6])
+data = obj.numOfSubarrays(arr = [1,2,3,4,5,6,7])
+print(data)
