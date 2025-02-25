aa7f359f19323a8e54190a2075fec6e2fad4ee17
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-25 22:01:24 +0530

    changes

--- Problems/Number of Sub-arrays With Odd Sum/solution.py
+++ Problems/Number of Sub-arrays With Odd Sum/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def numOfSubarrays(self, arr: List[int]) -> int:
-        pass
+        mod = 10**9 + 7
+        odd_cnt, even_cnt = 0, 1
+        curr_sum, res = 0, 0
+        
+        for num in arr:
+            curr_sum += num
+            if curr_sum % 2 == 0:
+                res += odd_cnt
+                even_cnt += 1
+            else:
+                res += even_cnt
+                odd_cnt += 1
+                
+        return res % mod
 
 obj = Solution()
 #data = obj.numOfSubarrays(arr = [1,3,5])
