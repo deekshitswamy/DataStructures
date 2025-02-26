1fefb6ed16fe442ba815888d3d2f9c3a3dc2a201
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-26 22:28:27 +0530

    Maximum Absolute Sum of Any Subarray

---
+++ Problems/Maximum Absolute Sum of Any Subarray/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxAbsoluteSum(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxAbsoluteSum(nums = [1,-3,2,3,-4])
+data = obj.maxAbsoluteSum(nums = [2,-5,1,-4,3,-2])
+print(data)
