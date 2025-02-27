81cf6f59ccbb50e808f9e279fca3d65d9771a4ac
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-27 23:44:19 +0530

    Length of Longest Fibonacci Subsequence

---
+++ Problems/Length of Longest Fibonacci Subsequence/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def lenLongestFibSubseq(self, arr: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.lenLongestFibSubseq(arr = [1,2,3,4,5,6,7,8])
+data = obj.lenLongestFibSubseq(arr = [1,3,7,11,12,14,18])
+print(data)
