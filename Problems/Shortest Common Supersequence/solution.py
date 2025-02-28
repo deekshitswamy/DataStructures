849359bee4133e1d07ffb91dd1670a4f7253a756
Deeku-Zeus <deekuzeus21@gmail.com>
2025-02-28 20:10:27 +0530

    Shortest Common Supersequence

---
+++ Problems/Shortest Common Supersequence/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.shortestCommonSupersequence(str1 = "abac", str2 = "cab")
+data = obj.shortestCommonSupersequence(str1 = "aaaaaaaa", str2 = "aaaaaaaa")
+print(data)
