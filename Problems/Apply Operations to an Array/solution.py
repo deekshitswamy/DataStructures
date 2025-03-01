a8480ec436577e6853528d07abd268211fda5df9
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-01 19:34:32 +0530

    changes

--- Problems/Apply Operations to an Array/solution.py
+++ Problems/Apply Operations to an Array/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        pass
+        for i in range(len(nums)-1):
+            if nums[i]==nums[i+1]:
+                nums[i]=nums[i]*2
+                nums[i+1]=0
+            else:
+                continue
+        j1=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[j1],nums[i]=nums[i],nums[j1]
+                j1+=1
+        return nums
 
 obj = Solution()
 #data = obj.applyOperations(nums = [1,2,2,1,1,0])
