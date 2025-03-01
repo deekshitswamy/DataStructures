53bf52109e4106b93f7586e58d263672cf238a1f
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-01 19:33:52 +0530

    Apply Operations to an Array

---
+++ Problems/Apply Operations to an Array/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.applyOperations(nums = [1,2,2,1,1,0])
+data = obj.applyOperations(nums = [0,1])
+print(data)
