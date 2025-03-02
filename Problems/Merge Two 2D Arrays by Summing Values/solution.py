91c7e843d0ad8c9eced27ff6237878610d170c1a
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-02 22:40:47 +0530

    changes

--- Problems/Merge Two 2D Arrays by Summing Values/solution.py
+++ Problems/Merge Two 2D Arrays by Summing Values/solution.py
@@ -2,7 +2,13 @@ import io
 from typing import List
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
-        pass
+        nums1 = dict(nums1)
+        nums2 = dict(nums2)
+        ms = nums1.keys() | nums2.keys()
+        lst = []
+        for k in sorted(ms):
+            lst.append([k, nums1.get(k,0)+nums2.get(k,0)])
+        return lst
 
 obj = Solution()
 #data = obj.mergeArrays(nums1 = [[1,2],[2,3],[4,5]], nums2 = [[1,4],[3,2],[4,1]])
