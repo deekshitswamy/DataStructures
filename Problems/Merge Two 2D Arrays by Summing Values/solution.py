cca66fb2ae5f3372dce4d447f7772895077b2264
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-02 22:39:31 +0530

    Merge Two 2D Arrays by Summing Values

---
+++ Problems/Merge Two 2D Arrays by Summing Values/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        pass
+
+obj = Solution()
+#data = obj.mergeArrays(n = 1)
+#data = obj.mergeArrays(n = 2)
+data = obj.mergeArrays(n = 2)
+print(data)
