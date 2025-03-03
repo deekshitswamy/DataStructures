bd310697b906501c05413e9e3393c66c9f2185f2
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-03 21:58:55 +0530

    Partition Array According to Given Pivot

---
+++ Problems/Partition Array According to Given Pivot/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.pivotArray(nums = [9,12,5,10,14,3,10], pivot = 10)
+data = obj.pivotArray(nums = [-3,4,3,2], pivot = 2)
+print(data)
