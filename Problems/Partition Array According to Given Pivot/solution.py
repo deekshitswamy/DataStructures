b08c68855a2ed0334b0ed7dad9e2081e1eddd7ba
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-03 22:00:14 +0530

    changes

--- Problems/Partition Array According to Given Pivot/solution.py
+++ Problems/Partition Array According to Given Pivot/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        pass
+        pivot_count = 0
+        less, equal, more = [], [], []
+
+        for num in nums:
+            if num == pivot:
+                equal.append(num)
+            elif num < pivot:
+                less.append(num)
+            else:
+                more.append(num)
+
+        return (less + equal + more)
 
 obj = Solution()
 #data = obj.pivotArray(nums = [9,12,5,10,14,3,10], pivot = 10)
