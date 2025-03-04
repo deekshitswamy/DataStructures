13b03a60977c5ec2d1b4c0e6bb1c931d5a3ac9bd
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-05 02:36:35 +0530

    Check if Number is a Sum of Powers of Three

---
+++ Problems/Check if Number is a Sum of Powers of Three/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.checkPowersOfThree(n = 12)
+#data = obj.checkPowersOfThree(n = 91)
+data = obj.checkPowersOfThree(n = 21)
+print(data)
