09d15b72dcab6042ce63e4b6cab2bbd5df353bc7
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-05 23:30:54 +0530

    Count Total Number of Colored Cells

---
+++ Problems/Count Total Number of Colored Cells/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def coloredCells(self, n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.coloredCells(n = 1)
+data = obj.coloredCells(n = 2)
+print(data)
