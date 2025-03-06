41a1fe4e84f180b4607967f16ac3c7cbd5466fb0
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-06 21:47:53 +0530

    changes

--- Problems/Find Missing and Repeated Values/solution.py
+++ Problems/Find Missing and Repeated Values/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
-        pass
+        n = len(grid)
+        res = [0, 0]
+        num = [0] * (n * n + 1)
+        for i in range(n):
+            for j in range(n):
+                if num[grid[i][j]] == 1:
+                    res[0] = grid[i][j]
+                else:
+                    num[grid[i][j]] = 1
+        for i in range(1, n * n + 1):
+            if num[i] == 0:
+                res[1] = i
+                break
+        return res
 
 obj = Solution()
 #data = obj.findMissingAndRepeatedValues(grid = [[1,3],[2,2]])
