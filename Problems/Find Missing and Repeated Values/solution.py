cd318012b36ff2fbaaa83849d97e4227e3f28f31
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-06 21:47:05 +0530

    Find Missing and Repeated Values

---
+++ Problems/Find Missing and Repeated Values/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.findMissingAndRepeatedValues(grid = [[1,3],[2,2]])
+data = obj.findMissingAndRepeatedValues(grid = [[9,1,7],[8,9,2],[3,4,6]])
+print(data)
