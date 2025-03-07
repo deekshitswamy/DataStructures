23c0b52d0da990cd418e8fe8d94b8494224a14e2
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-07 23:15:36 +0530

    Closest Prime Numbers in Range

---
+++ Problems/Closest Prime Numbers in Range/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def closestPrimes(self, left: int, right: int) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.closestPrimes(left = 10, right = 19)
+data = obj.closestPrimes(left = 4, right = 6)
+print(data)
