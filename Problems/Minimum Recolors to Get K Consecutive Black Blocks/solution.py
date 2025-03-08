3051d2002169ee263cf9f9fcfd171910a809632b
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-08 23:39:39 +0530

    changes

--- Problems/Minimum Recolors to Get K Consecutive Black Blocks/solution.py
+++ Problems/Minimum Recolors to Get K Consecutive Black Blocks/solution.py
@@ -2,7 +2,22 @@ import io
 from typing import List
 class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
-        pass
+        res, white = 0, 0
+        for i in range(len(blocks)):
+            if i < k:
+                if blocks[i] == 'W':
+                    white += 1
+                if i == k - 1:
+                    res = white
+            else:
+                if blocks[i - k] == 'W':
+                    white -= 1
+                if blocks[i] == 'W':
+                    white += 1
+
+            res = min(res, white)
+
+        return res
 
 obj = Solution()
 #data = obj.minimumRecolors(blocks = "WBBWWBBWBW", k = 7)
