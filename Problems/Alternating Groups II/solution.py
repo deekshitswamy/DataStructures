d114477d3e33e7968d28d5eaf1fd305b5f2e1463
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-09 21:20:30 +0530

    changes

--- Problems/Alternating Groups II/solution.py
+++ Problems/Alternating Groups II/solution.py
@@ -2,7 +2,15 @@ import io
 from typing import List
 class Solution:
     def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
-        pass
+        colors += colors[: k - 1]
+        i, j, res = 0, 1, 0
+        while j < len(colors):
+            if colors[j] == colors[j - 1]: i = j
+            if j - i + 1 == k: 
+                res += 1
+                i += 1
+            j += 1
+        return res
 
 obj = Solution()
 #data = obj.numberOfAlternatingGroups(colors = [0,1,0,1,0], k = 3)
