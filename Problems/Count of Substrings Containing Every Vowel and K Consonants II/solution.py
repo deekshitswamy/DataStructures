c1794d5fdcae3b93d65198758ea4dd7969eb5bd7
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-10 22:27:26 +0530

    Count of Substrings Containing Every Vowel and K Consonants II

---
+++ Problems/Count of Substrings Containing Every Vowel and K Consonants II/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def countOfSubstrings(self, word: str, k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countOfSubstrings(word = "aeioqq", k = 1)
+#data = obj.countOfSubstrings(word = "aeiou", k = 0)
+data = obj.countOfSubstrings(word = "ieaouqqieaouqq", k = 1)
+print(data)
