ec306fc1948dc5c56feb1cca0ba9e1b7a27001b3
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-11 23:36:52 +0530

    changes

--- Problems/Number of Substrings Containing All Three Characters/solution.py
+++ Problems/Number of Substrings Containing All Three Characters/solution.py
@@ -2,7 +2,13 @@ import io
 from typing import List
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        pass
+        ls = [-1, -1, -1]  
+        count = 0  
+        for i in range(len(s)):  
+            ls[ord(s[i]) - ord('a')] = i  
+            if -1 not in ls:  
+                count += 1 + min(ls)  
+        return count
 
 obj = Solution()
 #data = obj.numberOfSubstrings(s = "abcabc")
