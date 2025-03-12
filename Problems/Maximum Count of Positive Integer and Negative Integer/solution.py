80171b50cffd0a930fe92c8e7507baa175fabf0b
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-12 22:00:27 +0530

    Maximum Count of Positive Integer and Negative Integer

---
+++ Problems/Maximum Count of Positive Integer and Negative Integer/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maximumCount(nums = [-2,-1,-1,1,2,3])
+#data = obj.maximumCount(nums = [-3,-2,-1,0,0,1,2])
+data = obj.maximumCount(nums = [5,20,66,1314])
+print(data)
