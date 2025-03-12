32d2bdb4cbfd72922a70001b5f40b538dd2011ec
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-12 22:01:51 +0530

    changes

--- Problems/Maximum Count of Positive Integer and Negative Integer/solution.py
+++ Problems/Maximum Count of Positive Integer and Negative Integer/solution.py
@@ -2,7 +2,11 @@ import io
 from typing import List
 class Solution:
     def maximumCount(self, nums: List[int]) -> int:
-        pass
+        first_neg = bisect_left(nums, 0)
+        first_pos = bisect_left(nums, 1)
+        neg = first_neg
+        pos = len(nums) - first_pos
+        return max(neg, pos)
 
 obj = Solution()
 #data = obj.maximumCount(nums = [-2,-1,-1,1,2,3])
