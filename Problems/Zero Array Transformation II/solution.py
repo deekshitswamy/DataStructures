427f3ff012944ebca52b4f689876134856c5bc90
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-13 22:26:22 +0530

    Zero Array Transformation II

---
+++ Problems/Zero Array Transformation II/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minZeroArray(nums = [2,0,2], queries = [[0,2,1],[0,2,1],[1,1,3]])
+data = obj.minZeroArray(nums = [4,3,2,1], queries = [[1,3,2],[0,2,1]])
+print(data)
