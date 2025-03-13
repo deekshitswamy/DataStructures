d6b4bdd45929091ec63b3c829c5964242beecbb8
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-13 22:27:10 +0530

    changes

--- Problems/Zero Array Transformation II/solution.py
+++ Problems/Zero Array Transformation II/solution.py
@@ -2,7 +2,41 @@ import io
 from typing import List
 class Solution:
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
-        pass
+        n = len(nums)
+        q_len = len(queries)
+        
+        def canZero(k: int) -> bool:
+            diff = [0] * (n + 1)
+            
+            for i in range(k):
+                l, r, val = queries[i]
+                diff[l] += val
+                if r + 1 < n:
+                    diff[r + 1] -= val
+            
+
+            coverage = [0] * n
+            coverage[0] = diff[0]
+            for i in range(1, n):
+                coverage[i] = coverage[i - 1] + diff[i]
+            
+            for i in range(n):
+                if coverage[i] < nums[i]:
+                    return False
+            return True
+        
+        left, right = 0, q_len
+        answer = -1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if canZero(mid):
+                answer = mid
+                right = mid - 1 
+            else:
+                left = mid + 1  
+        
+        return answer
 
 obj = Solution()
 #data = obj.minZeroArray(nums = [2,0,2], queries = [[0,2,1],[0,2,1],[1,1,3]])
