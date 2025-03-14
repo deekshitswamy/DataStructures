2781800483e23b433d3296f260f897bcb5cc0f8e
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-14 22:47:24 +0530

    Maximum Candies Allocated to K Children

---
+++ Problems/Maximum Candies Allocated to K Children/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maximumCandies(self, candies: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maximumCandies(candies = [5,8,6], k = 3)
+data = obj.maximumCandies(candies = [2,5], k = 11)
+print(data)
