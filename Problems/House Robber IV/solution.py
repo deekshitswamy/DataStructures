2b1c77649d833e2380deb75ecc6fdf68269e5d2d
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-15 17:41:48 +0530

    House Robber IV

---
+++ Problems/House Robber IV/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minCapability(self, nums: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minCapability(nums = [2,3,5,9], k = 2)
+data = obj.minCapability(nums = [2,7,9,3,1], k = 2)
+print(data)
