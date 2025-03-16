8cf3450e8011928e485cff94c493eaa98f4420ec
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-16 22:09:36 +0530

    changes

--- Problems/Minimum Time to Repair Cars/solution.py
+++ Problems/Minimum Time to Repair Cars/solution.py
@@ -2,7 +2,24 @@ import io
 from typing import List
 class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
-        pass
+        def repairCount(time):
+            count = 0
+            for r in ranks:
+                count += int(sqrt(time / r))
+            return count
+        
+        l = 1
+        r = ranks[0] * cars * cars
+        res = -1
+        while l <= r:
+            m = (l + r) // 2
+            repaired = repairCount(m)
+            if repaired >= cars:
+                res = m
+                r = m - 1
+            else:
+                l = m + 1
+        return res
 
 obj = Solution()
 #data = obj.repairCars(ranks = [4,2,3,1], cars = 10)
