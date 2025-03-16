d35ccfe18f11901a502dfc49c926ba5fe05af9fc
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-16 22:08:34 +0530

    Minimum Time to Repair Cars

---
+++ Problems/Minimum Time to Repair Cars/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.repairCars(ranks = [4,2,3,1], cars = 10)
+data = obj.repairCars(ranks = [5,1,8], cars = 6)
+print(data)
