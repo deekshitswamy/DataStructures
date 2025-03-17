2181960946250f9a836728d5df75103074764c54
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-17 22:57:27 +0530

    Divide Array Into Equal Pairs

---
+++ Problems/Divide Array Into Equal Pairs/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.divideArray(nums = [3,2,3,2,2,2])
+data = obj.divideArray(nums = [1,2,3,4])
+print(data)
