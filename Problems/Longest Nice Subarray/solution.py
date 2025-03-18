7bbc7ff634fc9e6ddf4e6c8f399aa7d5dcefe109
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-18 21:23:43 +0530

    changes

--- Problems/Longest Nice Subarray/solution.py
+++ Problems/Longest Nice Subarray/solution.py
@@ -2,7 +2,22 @@ import io
 from typing import List
 class Solution:
     def longestNiceSubarray(self, nums: List[int]) -> int:
-        pass
+        last = 0; max_len = 0; count = 0
+        index = 0; i = 0
+
+        while i < len(nums):
+
+            while i < len(nums) and last ^ nums[i] == last + nums[i]:
+                last += nums[i]
+                count += 1
+                i += 1
+    
+            max_len = max(max_len, count)
+            count -= 1
+            last -= nums[index]
+            index += 1
+
+        return max_len
 
 obj = Solution()
 #data = obj.longestNiceSubarray(nums = [1,3,8,48,10])
