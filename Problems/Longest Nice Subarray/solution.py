2ac85a02548525af306af08a5727acb9fff8ea03
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-18 21:23:09 +0530

    Longest Nice Subarray

---
+++ Problems/Longest Nice Subarray/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.longestNiceSubarray(nums = [1,3,8,48,10])
+data = obj.longestNiceSubarray(nums = [3,1,5,11,13])
+print(data)
