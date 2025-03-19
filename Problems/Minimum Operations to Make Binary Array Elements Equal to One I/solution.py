dad71da65b7c15e70a519565993828f8f4d03275
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-19 23:29:11 +0530

    Minimum Operations to Make Binary Array Elements Equal to One I

---
+++ Problems/Minimum Operations to Make Binary Array Elements Equal to One I/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minOperations(nums = [0,1,1,1,0,0])
+data = obj.minOperations(nums = [0,1,1,1])
+print(data)
