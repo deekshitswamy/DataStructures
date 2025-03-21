047ebdacce4e18343161c85187c2e3f3d0580f01
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-21 22:17:39 +0530

    changes

--- Problems/Find All Possible Recipes from Given Supplies/solution.py
+++ Problems/Find All Possible Recipes from Given Supplies/solution.py
@@ -2,7 +2,22 @@ import io
 from typing import List
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
-        pass
+        indeg = defaultdict(int)
+        graph = defaultdict(list)
+        for r, ing in zip(recipes, ingredients): 
+            indeg[r] = len(ing)
+            for i in ing: graph[i].append(r)
+        
+        ans = []
+        queue = deque(supplies)
+        recipes = set(recipes)
+        while queue: 
+            x = queue.popleft()
+            if x in recipes: ans.append(x)
+            for xx in graph[x]: 
+                indeg[xx] -= 1
+                if indeg[xx] == 0: queue.append(xx)
+        return ans
 
 obj = Solution()
 #data = obj.findAllRecipes(recipes = ["bread"], ingredients = [["yeast","flour"]], supplies = ["yeast","flour","corn"])
