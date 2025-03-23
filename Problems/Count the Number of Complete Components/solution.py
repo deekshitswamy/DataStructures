6c6b2187501d03894702ec70ce35da91ca59374b
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-23 23:02:12 +0530

    Count the Number of Complete Components

---
+++ Problems/Count the Number of Complete Components/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countCompleteComponents(n = 6, edges = [[0,1],[0,2],[1,2],[3,4]])
+data = obj.countCompleteComponents(n = 6, edges = [[0,1],[0,2],[1,2],[3,4],[3,5]])
+print(data)
