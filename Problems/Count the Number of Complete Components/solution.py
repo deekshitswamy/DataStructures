b6601017f3310a2abd431e880b9aa4af58f221db
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-23 23:02:46 +0530

    changes

--- Problems/Count the Number of Complete Components/solution.py
+++ Problems/Count the Number of Complete Components/solution.py
@@ -2,7 +2,26 @@ import io
 from typing import List
 class Solution:
     def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
-        pass
+        X = [[] for i in range(n)]
+        for u, v in edges:
+            X[u].append(v)
+            X[v].append(u)
+
+        visited = [0] * n
+        res = 0
+        for i in range(n):
+            if visited[i]:
+                continue
+            bfs = [i]
+            visited[i] = 1
+            for j in bfs:
+                for k in X[j]:
+                    if visited[k] == 0:
+                        bfs.append(k)
+                        visited[k] = 1
+            if all(len(X[j]) == len(bfs) - 1 for j in bfs):
+                res += 1
+        return res
 
 obj = Solution()
 #data = obj.countCompleteComponents(n = 6, edges = [[0,1],[0,2],[1,2],[3,4]])
