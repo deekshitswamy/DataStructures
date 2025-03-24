2a7d9ac56c138736801dd689cccede72bbcc8b6d
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-24 20:53:05 +0530

    changes

--- Problems/Count Days Without Meetings/solution.py
+++ Problems/Count Days Without Meetings/solution.py
@@ -2,7 +2,13 @@ import io
 from typing import List
 class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
-        pass
+        meetings.sort()
+        res, prev = 0, 0
+        for start, end in meetings:
+            res += max(0, start - prev - 1)
+            prev = max(end, prev)
+        res += max(0, days - prev)
+        return res
 
 obj = Solution()
 #data = obj.countDays(days = 10, meetings = [[5,7],[1,3],[9,10]])
