6fe2cd5095bee82dc37c0199e9fb8924239df907
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-24 20:51:57 +0530

    Count Days Without Meetings

---
+++ Problems/Count Days Without Meetings/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countDays(days = 10, meetings = [[5,7],[1,3],[9,10]])
+#data = obj.countDays(days = 5, meetings = [[2,4],[1,3]])
+data = obj.countDays(days = 6, meetings = [[1,6]])
+print(data)
