4ea3824099ebf98a034e6f308673a2786d25a806
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-26 23:02:41 +0530

    Minimum Operations to Make a Uni-Value Grid

---
+++ Problems/Minimum Operations to Make a Uni-Value Grid/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minOperations(self, grid: List[List[int]], x: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minOperations(grid = [[2,4],[6,8]], x = 2)
+#data = obj.minOperations(grid = [[1,5],[2,3]], x = 1)
+data = obj.minOperations(grid = [[1,2],[3,4]], x = 2)
+print(data)
