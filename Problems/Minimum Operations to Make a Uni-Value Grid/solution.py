c2e2041863f70850870efdb6aada180f02c7ce5a
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-26 23:03:42 +0530

    changes

--- Problems/Minimum Operations to Make a Uni-Value Grid/solution.py
+++ Problems/Minimum Operations to Make a Uni-Value Grid/solution.py
@@ -2,7 +2,30 @@ import io
 from typing import List
 class Solution:
     def minOperations(self, grid: List[List[int]], x: int) -> int:
-        pass
+        c = Counter()
+        for line in grid:
+            c.update(line)
+        
+        if len({key % x for key in c}) > 1:
+            return -1
+        
+        data = sorted((k // x, v) for k, v in c.items())
+
+        left_sum = left_cnt = 0
+        right_sum = sum(k * v for k, v in data)
+        right_cnt = len(grid) * len(grid[0])
+
+        result = float('inf')
+        for i, (k, v) in enumerate(data):
+            right_cnt -= v
+            right_sum -= k * v
+            result = min(
+                result,
+                right_sum - left_sum + k * (left_cnt - right_cnt),
+            )
+            left_cnt += v
+            left_sum += k * v
+        return result
 
 obj = Solution()
 #data = obj.minOperations(grid = [[2,4],[6,8]], x = 2)
