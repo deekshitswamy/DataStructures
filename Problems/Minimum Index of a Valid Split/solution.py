53a62cbf92f3fce618ce119ce69f2d47fbd5c8e3
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-27 23:15:40 +0530

    Minimum Index of a Valid Split

---
+++ Problems/Minimum Index of a Valid Split/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minimumIndex(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minimumIndex(nums = [1,2,2,2])
+#data = obj.minimumIndex(nums = [2,1,3,1,1,1,7,1,2,1])
+data = obj.minimumIndex(nums = [3,3,3,3,7,2,2])
+print(data)
