35db1b15ef812d9cd1c59099215557af24af0f08
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-28 20:48:27 +0530

    Maximum Number of Points From Grid Queries

---
+++ Problems/Maximum Number of Points From Grid Queries/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxPoints(self, grid: List[List[int]], queries: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.maxPoints(grid = [[1,2,3],[2,5,7],[3,5,1]], queries = [5,6,2])
+data = obj.maxPoints(grid = [[5,2,1],[1,1,2]], queries = [3])
+print(data)
