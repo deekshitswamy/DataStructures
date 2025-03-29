367153f557baa7a604f7a095538329b93cb9f5e0
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-29 22:57:36 +0530

    Apply Operations to Maximize Score

---
+++ Problems/Apply Operations to Maximize Score/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maximumScore(self, nums: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maximumScore(nums = [8,3,9,3,8], k = 2)
+data = obj.maximumScore(nums = [19,12,14,6,10,18], k = 3)
+print(data)
