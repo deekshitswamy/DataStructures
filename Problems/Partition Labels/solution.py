4cf5eba422eb623c8f180b14e524c2c763465fd4
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-30 22:26:56 +0530

    changes

--- Problems/Partition Labels/solution.py
+++ Problems/Partition Labels/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        pass
+        last_occurrence = {char: index for index, char in enumerate(s)}
+        
+        partitions = []
+        start, end = 0, 0
+
+        for i, char in enumerate(s):
+            last_index = last_occurrence[char]  
+            end = max(end, last_index)  
+            
+            if i == end:  
+                partitions.append(i - start + 1)
+                start = i + 1  
+
+        return partitions
 
 obj = Solution()
 #data = obj.partitionLabels(s = "ababcbacadefegdehijhklij")
