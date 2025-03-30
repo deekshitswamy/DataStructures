f3c073f8827e858bcedb9f906dbd4a592c358124
Deeku-Zeus <deekuzeus21@gmail.com>
2025-03-30 22:25:57 +0530

    Partition Labels

---
+++ Problems/Partition Labels/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.partitionLabels(s = "ababcbacadefegdehijhklij")
+data = obj.partitionLabels(s = "eccbbbbdec")
+print(data)
