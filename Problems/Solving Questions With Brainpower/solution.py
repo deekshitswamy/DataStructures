dd8ad3b3340ef0be7d0771167351ce5807a6b960
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-01 23:12:15 +0530

    Solving Questions With Brainpower

---
+++ Problems/Solving Questions With Brainpower/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.mostPoints(questions = [[3,2],[4,3],[4,4],[2,5]])
+data = obj.mostPoints(questions = [[1,1],[2,2],[3,3],[4,4],[5,5]])
+print(data)
