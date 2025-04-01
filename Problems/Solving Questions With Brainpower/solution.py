4592b9c82e23d749b5345992ce162df483c69e6b
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-01 23:13:20 +0530

    changes

--- Problems/Solving Questions With Brainpower/solution.py
+++ Problems/Solving Questions With Brainpower/solution.py
@@ -2,7 +2,14 @@ import io
 from typing import List
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-        pass
+        dp = [0] * len(questions)
+        for i, (p, b) in enumerate(reversed(questions)):
+            j = i - b - 1
+            if j >= 0:
+                dp[i] = max(dp[i-1], dp[j] + p)
+            else:
+                dp[i] = max(dp[i-1], p)
+        return dp[-1]
 
 obj = Solution()
 #data = obj.mostPoints(questions = [[3,2],[4,3],[4,4],[2,5]])
