93de39fc6ad9afe90759867cde601410b2098c0a
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-03 22:18:32 +0530

    Maximum Value of an Ordered Triplet II

---
+++ Problems/Maximum Value of an Ordered Triplet II/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maximumTripletValue(nums = [12,6,1,2,7])
+#data = obj.maximumTripletValue(nums = [1,10,3,4,19])
+data = obj.maximumTripletValue(nums = [1,2,3])
+print(data)
