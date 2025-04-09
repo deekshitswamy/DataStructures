a27053e8a25333b6e887805e3d71c5aebef371d3
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-09 21:09:36 +0530

    changes

--- Problems/Minimum Operations to Make Array Values Equal to K/solution.py
+++ Problems/Minimum Operations to Make Array Values Equal to K/solution.py
@@ -2,7 +2,13 @@ import io
 from typing import List
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        pass
+        seen = set()
+        for num in nums:
+            if num > k:
+                seen.add(num)
+            elif num < k:
+                return -1
+        return len(seen)
 
 obj = Solution()
 #data = obj.minOperations(nums = [5,2,5,4,5], k = 2)
