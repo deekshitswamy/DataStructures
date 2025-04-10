337fdfa2c1fef3c233e61cfe28610fa9e2cfaad2
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-10 22:38:01 +0530

    Count the Number of Powerful Integers

---
+++ Problems/Count the Number of Powerful Integers/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def numberOfPowerfulInt(self, start: int, finish: int, limit: int, s: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numberOfPowerfulInt(start = 1, finish = 6000, limit = 4, s = "124")
+#data = obj.numberOfPowerfulInt(start = 15, finish = 215, limit = 6, s = "10")
+data = obj.numberOfPowerfulInt(start = 1000, finish = 2000, limit = 4, s = "3000")
+print(data)
