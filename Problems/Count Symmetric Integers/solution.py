d55b754a02300f014eb7d27072dfad26098ab817
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-11 20:50:40 +0530

    Count Symmetric Integers

---
+++ Problems/Count Symmetric Integers/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def countSymmetricIntegers(self, low: int, high: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countSymmetricIntegers(low = 1, high = 100)
+data = obj.countSymmetricIntegers(low = 1200, high = 1230)
+print(data)
