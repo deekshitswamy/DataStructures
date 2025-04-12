ea8163fed260e5bdbd6851cefd889d429dec62f4
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-12 22:22:56 +0530

    Find the Count of Good Integers

---
+++ Problems/Find the Count of Good Integers/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def countGoodIntegers(self, n: int, k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countGoodIntegers(n = 3, k = 5)
+#data = obj.countGoodIntegers(n = 1, k = 4)
+data = obj.countGoodIntegers(n = 5, k = 6)
+print(data)
