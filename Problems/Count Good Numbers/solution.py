34de64c17d7644ade29a8b2fe8afbfd67142e0d4
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-13 22:38:11 +0530

    changes

--- Problems/Count Good Numbers/solution.py
+++ Problems/Count Good Numbers/solution.py
@@ -2,7 +2,19 @@ import io
 from typing import List
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        pass
+        mod = 10**9 + 7 
+        def pow(x, n):
+            if n == 0 : return 1
+            res = 1
+            while n :
+                if n % 2 == 1:
+                    res = (res * x) % mod
+                x = (x*x) % mod
+                n //= 2
+            return res
+        even = ceil(n/2)
+        odd = n//2
+        return (pow(5, even) * pow(4, odd)) % mod
 
 obj = Solution()
 #data = obj.countGoodNumbers(n = 1)
