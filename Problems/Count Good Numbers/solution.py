65a167f050b4bdc3f95d2a8ddba501a3eb3002a8
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-13 22:37:30 +0530

    changes

--- Problems/Count Good Numbers/solution.py
+++ Problems/Count Good Numbers/solution.py
@@ -1,11 +1,11 @@
 import io
 from typing import List
 class Solution:
-    def testfunc(self, n: int) -> int:
+    def countGoodNumbers(self, n: int) -> int:
         pass
 
 obj = Solution()
-#data = obj.testfunc(n = 1)
-#data = obj.testfunc(n = 2)
-data = obj.testfunc(n = 2)
+#data = obj.countGoodNumbers(n = 1)
+#data = obj.countGoodNumbers(n = 2)
+data = obj.countGoodNumbers(n = 2)
 print(data)
