d3e07f8d21c6abd4f99ac66dd969ec39c2c3502a
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-16 08:49:50 +0530

    Count Good Triplets in an Array

---
+++ Problems/Count Good Triplets in an Array/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def goodTriplets(self, nums1: List[int], nums2: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.goodTriplets(n = 1)
+#data = obj.goodTriplets(n = 2)
+data = obj.goodTriplets(n = 2)
+print(data)
