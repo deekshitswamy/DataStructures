c054c22a1100b4f48afedd5ad6a5e961d576c102
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-16 08:48:03 +0530

    changes

--- Problems/Count the Number of Good Subarrays/solution.py
+++ Problems/Count the Number of Good Subarrays/solution.py
@@ -2,7 +2,24 @@ import io
 from typing import List
 class Solution:
     def countGood(self, nums: List[int], k: int) -> int:
-        pass
+        hs = defaultdict(int)
+        n = len(nums)
+        right = -1
+        res = 0
+        pairs = 0
+
+        for left in range(n):
+            while right < n - 1 and pairs < k:
+                right += 1
+                pairs += hs[nums[right]]
+                hs[nums[right]] += 1
+            if pairs >= k:
+                res += n - right
+
+            hs[nums[left]] -= 1
+            pairs -= hs[nums[left]]
+        
+        return res
 
 obj = Solution()
 #data = obj.countGood(nums = [1,1,1,1,1], k = 10)
