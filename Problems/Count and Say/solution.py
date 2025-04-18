36dffb57ad5442670ed48f2c97ccfd4681dad0a8
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-18 22:46:17 +0530

    changes

--- Problems/Count and Say/solution.py
+++ Problems/Count and Say/solution.py
@@ -2,7 +2,19 @@ import io
 from typing import List
 class Solution:
     def countAndSay(self, n: int) -> str:
-        pass
+        res = "1"
+        for _ in range(n-1):
+            temp = ""
+            count = 1
+            for i in range(1, len(res)):
+                if res[i] == res[i-1]:
+                    count += 1
+                else :
+                    temp += str(count) + res[i-1]
+                    count = 1
+            temp += str(count) + res[-1]
+            res = temp
+        return res
 
 obj = Solution()
 #data = obj.countAndSay(n = 1)
