ce5904c7cac3cc5e9e9f2c0a1eded568316dfb90
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-18 22:45:27 +0530

    Count and Say

---
+++ Problems/Count and Say/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        pass
+
+obj = Solution()
+#data = obj.countAndSay(n = 1)
+data = obj.countAndSay(n = 4)
+print(data)
