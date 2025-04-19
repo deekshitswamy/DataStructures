0d5f1abb32b874f65f56caccbd2e2fbe5de5ae94
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-19 21:52:03 +0530

    changes

--- Problems/Count the Number of Fair Pairs/solution.py
+++ Problems/Count the Number of Fair Pairs/solution.py
@@ -3,7 +3,23 @@ from typing import List
 from bisect import bisect_left
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
-        pass
+        nums.sort()
+        res = 0
+        ln = len(nums)
+        
+        def helper(l,target):
+            r = ln - 1
+            ans = 0
+            while l < r:
+                s = nums[l] + nums[r]
+                if s < target:
+                    ans += r-l
+                    l += 1
+                else:
+                    r -= 1
+            return ans
+
+        return helper(0,upper+1) - helper(0,lower)
 
 obj = Solution()
 #data = obj.countFairPairs(nums = [0,1,7,4,4,5], lower = 3, upper = 6)
