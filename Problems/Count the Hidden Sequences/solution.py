ebc7e79b263646f4e92488e7af764bf2a4beeed5
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-21 20:55:51 +0530

    Count the Hidden Sequences

---
+++ Problems/Count the Hidden Sequences/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def numberOfArrays(self, differences: List[int], lower: int, upper: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numberOfArrays(differences = [1,-3,4], lower = 1, upper = 6)
+#data = obj.numberOfArrays(differences = [3,-4,5,1,-2], lower = -4, upper = 5)
+data = obj.numberOfArrays(differences = [4,-7,2], lower = 3, upper = 6)
+print(data)
