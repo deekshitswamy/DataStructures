fc8b59192b2dcca61a4d2f38a9603f9526035f9a
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-21 20:56:30 +0530

    changes

--- Problems/Count the Hidden Sequences/solution.py
+++ Problems/Count the Hidden Sequences/solution.py
@@ -2,7 +2,22 @@ import io
 from typing import List
 class Solution:
     def numberOfArrays(self, differences: List[int], lower: int, upper: int) -> int:
-        pass
+        min_s = 0
+        max_s = 0
+        cur = 0
+        for d in differences:
+            cur += d
+            if cur < min_s:
+                min_s = cur
+            if cur > max_s:
+                max_s = cur
+        
+        low_bound = lower - min_s
+        high_bound = upper - max_s
+        
+        if high_bound < low_bound:
+            return 0
+        return high_bound - low_bound + 1
 
 obj = Solution()
 #data = obj.numberOfArrays(differences = [1,-3,4], lower = 1, upper = 6)
