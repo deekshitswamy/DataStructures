6cf3560af2b8e2c93c0ea1b032af81d44c84edab
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-22 21:34:59 +0530

    changes

--- Problems/Count the Number of Ideal Arrays/solution.py
+++ Problems/Count the Number of Ideal Arrays/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def idealArrays(self, n: int, maxValue: int) -> int:
-        pass
+        MOD = 10 ** 9 + 7
+        ans = maxValue
+        freq = {x: 1 for x in range(1, maxValue + 1)}
+        for k in range(1, n):
+            if not freq:
+                break
+            nxt = collections.defaultdict(int)
+            for x in freq:
+                for m in range(2, maxValue // x + 1):
+                    ans += math.comb(n - 1, k) * freq[x]
+                    nxt[m * x] += freq[x]
+            freq = nxt
+            ans %= MOD
+        return ans
 
 obj = Solution()
 #data = obj.idealArrays(n = 2, maxValue = 5)
