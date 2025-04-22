f93a8f07d1d3b7ca93a8dc2c929d19175ed95bd9
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-22 21:33:00 +0530

    Count the Number of Ideal Arrays

---
+++ Problems/Count the Number of Ideal Arrays/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def idealArrays(self, n: int, maxValue: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.idealArrays(n = 2, maxValue = 5)
+data = obj.idealArrays(n = 5, maxValue = 3)
+print(data)
