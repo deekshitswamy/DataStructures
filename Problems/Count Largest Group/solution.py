8ac688a639dbb244d3b3b5802151b175a8423829
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-23 23:59:02 +0530

    Count Largest Group

---
+++ Problems/Count Largest Group/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def countLargestGroup(self, n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countLargestGroup(n = 13)
+data = obj.countLargestGroup(n = 2)
+print(data)
