a094cecdb3e5363d50fe71f4e557294a5df64ffe
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-24 23:35:23 +0530

    Count Complete Subarrays in an Array

---
+++ Problems/Count Complete Subarrays in an Array/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def countCompleteSubarrays(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countCompleteSubarrays(nums = [1,3,1,2,2])
+data = obj.countCompleteSubarrays(nums = [5,5,5,5])
+print(data)
