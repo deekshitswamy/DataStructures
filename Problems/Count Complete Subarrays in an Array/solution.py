55246a18d7c892b8795a39c4dcb2ddd2923692ca
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-24 23:36:10 +0530

    changes

--- Problems/Count Complete Subarrays in an Array/solution.py
+++ Problems/Count Complete Subarrays in an Array/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
-        pass
+        total = len(set(nums))
+        count = l = 0
+        hash = {}
+
+        for r, num in enumerate(nums):
+            hash[num] = hash.get(num, 0) + 1
+            while len(hash) == total:
+                count += len(nums) - r
+                hash[nums[l]] -= 1
+                if hash[nums[l]] == 0:
+                    del hash[nums[l]]
+                l += 1
+
+        return count
 
 obj = Solution()
 #data = obj.countCompleteSubarrays(nums = [1,3,1,2,2])
