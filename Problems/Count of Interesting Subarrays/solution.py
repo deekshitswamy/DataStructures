9e900b14c376bd1cd759994b57a698e920cb99a6
Deeku-Zeus <deekuzeus21@gmail.com>
2025-04-25 22:45:25 +0530

    changes

--- Problems/Count of Interesting Subarrays/solution.py
+++ Problems/Count of Interesting Subarrays/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def countInterestingSubarrays(self, nums: List[int], modulo: int, k: int) -> int:
-        pass
+        current = 0
+        data = Counter()
+        for elem in nums:
+            if elem % modulo == k:
+                current = (current + 1) % modulo
+            data[current] += 1
+
+        result = current = 0
+        for elem in nums:            
+            result += data[(current + k) % modulo]
+            if elem % modulo == k:
+                current = (current + 1) % modulo
+            data[current] -= 1
+        return result
 
 obj = Solution()
 #data = obj.countInterestingSubarrays(nums = [3,2,4], modulo = 2, k = 1)
