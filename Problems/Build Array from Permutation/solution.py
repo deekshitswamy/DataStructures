c8d53bcf318460a440be17878e5aecf43f290679
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-06 23:31:08 +0530

    Build Array from Permutation

---
+++ Problems/Build Array from Permutation/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.buildArray(nums = [0,2,1,5,3,4])
+data = obj.buildArray(nums = [5,0,1,2,3,4])
+print(data)
