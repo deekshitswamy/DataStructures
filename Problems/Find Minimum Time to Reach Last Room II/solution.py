40c803c844ae4b0bcb863743c1913b371ec30df7
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-08 20:09:06 +0530

    Find Minimum Time to Reach Last Room II

---
+++ Problems/Find Minimum Time to Reach Last Room II/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minTimeToReach(moveTime = [[0,4],[4,4]])
+#data = obj.minTimeToReach(moveTime = [[0,0,0,0],[0,0,0,0]])
+data = obj.minTimeToReach(moveTime = [[0,1],[1,2]])
+print(data)
