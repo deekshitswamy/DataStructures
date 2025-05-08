e86ccadae6718efc85146803e3f19ca011454a1e
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-08 20:10:05 +0530

    changes

--- Problems/Find Minimum Time to Reach Last Room II/solution.py
+++ Problems/Find Minimum Time to Reach Last Room II/solution.py
@@ -2,7 +2,19 @@ import io
 from typing import List
 class Solution:
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
-        pass
+        m, n = len(moveTime) - 1, len(moveTime[0]) - 1
+        heap = [(0, 0, 0, True)]
+        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        visited = set()
+        while heap:
+            time, r, c, check = heapq.heappop(heap)
+            if (r, c) == (m, n): return time
+            new = 1 if check else 2
+            for x, y in directions:
+                row, col = x + r, y + c
+                if row <= m and col <= n and col >= 0 and row >= 0 and (row, col) not in visited:
+                    heapq.heappush(heap, (max(time, moveTime[row][col]) + new, row, col, not check))
+                    visited.add((row, col))
 
 obj = Solution()
 #data = obj.minTimeToReach(moveTime = [[0,4],[4,4]])
