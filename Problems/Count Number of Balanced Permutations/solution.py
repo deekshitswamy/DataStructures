fb6065cc15a6f4033b2a482a5ac2bd6b576703de
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-09 22:16:05 +0530

    Count Number of Balanced Permutations

---
+++ Problems/Count Number of Balanced Permutations/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def countBalancedPermutations(self, num: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countBalancedPermutations(num = "123")
+#data = obj.countBalancedPermutations(num = "112")
+data = obj.countBalancedPermutations(num = "12345")
+print(data)
