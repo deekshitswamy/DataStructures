b7f662ed8fa357ec70d6984f93c1d50ebf0f2057
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-10 19:51:17 +0530

    Minimum Equal Sum of Two Arrays After Replacing Zeros

---
+++ Problems/Minimum Equal Sum of Two Arrays After Replacing Zeros/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minSum(self, nums1: List[int], nums2: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minSum(nums1 = [3,2,0,1,0], nums2 = [6,5,0])
+data = obj.minSum(nums1 = [2,0,2,0], nums2 = [1,4])
+print(data)
