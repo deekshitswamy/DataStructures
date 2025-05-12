a3cd79c6ecc948ad0f50f6170b3e8e2dd661ebb4
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-12 22:22:38 +0530

    Finding 3-Digit Even Numbers

---
+++ Problems/Finding 3-Digit Even Numbers/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def findEvenNumbers(self, digits: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.findEvenNumbers(digits = [2,1,3,0])
+#data = obj.findEvenNumbers(digits = [2,2,8,8,2])
+data = obj.findEvenNumbers(digits = [3,7,5])
+print(data)
