97e728bfdff10ecb0c55b039ef10d25a19229680
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-13 23:31:46 +0530

    Total Characters in String After Transformations I

---
+++ Problems/Total Characters in String After Transformations I/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def lengthAfterTransformations(self, s: str, t: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.lengthAfterTransformations(s = "abcyy", t = 2)
+data = obj.lengthAfterTransformations(s = "azbk", t = 1)
+print(data)
