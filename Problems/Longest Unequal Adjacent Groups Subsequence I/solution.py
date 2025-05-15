770e3e029574f83966fd3d9a6b0c5ca7a1319426
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-15 22:20:50 +0530

    change

--- Problems/Longest Unequal Adjacent Groups Subsequence I/solution.py
+++ Problems/Longest Unequal Adjacent Groups Subsequence I/solution.py
@@ -2,7 +2,12 @@ import io
 from typing import List
 class Solution:
     def getLongestSubsequence(self, words: List[str], groups: List[int]) -> List[str]:
-        pass
+        lis=[]
+        lis.append(words[0])
+        for i in range(len(groups)-1):
+            if groups[i]!=groups[i+1]:
+                lis.append(words[i+1])
+        return lis
 
 obj = Solution()
 #data = obj.getLongestSubsequence(words = ["e","a","b"], groups = [0,0,1])
