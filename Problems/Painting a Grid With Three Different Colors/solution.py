59b1e7f9eda6c860293ed8bdb2ab223083ec3bf2
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-18 23:07:55 +0530

    changes

---
+++ Problems/Painting a Grid With Three Different Colors/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def colorTheGrid(self, m: int, n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.colorTheGrid(m = 1, n = 1)
+#data = obj.colorTheGrid(m = 1, n = 2)
+data = obj.colorTheGrid(m = 5, n = 5)
+print(data)
