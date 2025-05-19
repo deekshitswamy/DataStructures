2a83a36755707ff3be42d7f24b97ba92bc75f977
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-19 22:28:09 +0530

    Type of Triangle

---
+++ Problems/Type of Triangle/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+        pass
+
+obj = Solution()
+#data = obj.triangleType(nums = [3,3,3])
+data = obj.triangleType(nums = [3,4,5])
+print(data)
