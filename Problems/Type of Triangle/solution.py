48f0571a94611441ddfd7a93b01080f07e64f49f
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-19 22:59:14 +0530

    changes

--- Problems/Type of Triangle/solution.py
+++ Problems/Type of Triangle/solution.py
@@ -2,9 +2,17 @@ import io
 from typing import List
 class Solution:
     def triangleType(self, nums: List[int]) -> str:
-        pass
+        nums.sort()
+        if nums[0] + nums[1] <= nums[2]:
+            return "none"
+        elif nums[0] == nums[2]:
+            return "equilateral"
+        elif nums[0] == nums[1] or nums[1] == nums[2]:
+            return "isosceles"
+        else:
+            return "scalene"
 
 obj = Solution()
-#data = obj.triangleType(nums = [3,3,3])
-data = obj.triangleType(nums = [3,4,5])
+data = obj.triangleType(nums = [3,3,3])
+#data = obj.triangleType(nums = [3,4,5])
 print(data)
