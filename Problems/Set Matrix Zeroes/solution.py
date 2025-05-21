5e492a0d0e5e246ff449d1f13967ece30b0d4a07
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-21 16:49:04 +0530

    Set Matrix Zeroes

---
+++ Problems/Set Matrix Zeroes/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        pass
+
+obj = Solution()
+#data = obj.setZeroes(matrix = [[1,1,1],[1,0,1],[1,1,1]])
+data = obj.setZeroes(matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]])
+print(data)
