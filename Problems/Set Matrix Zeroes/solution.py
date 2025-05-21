6478780bf652440027e66a37d96edddf70c491d9
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-21 16:50:16 +0530

    changes

--- Problems/Set Matrix Zeroes/solution.py
+++ Problems/Set Matrix Zeroes/solution.py
@@ -2,7 +2,23 @@ import io
 from typing import List
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        pass
+        found_position = None
+        arr = []
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if matrix[i][j] == 0:
+                    found_position = (i, j)
+                    arr.append(found_position)
+
+        def setzero(arr, matrix):
+            for pos in arr:
+                row, col = pos
+                for j in range(len(matrix[0])):
+                    matrix[row][j] = 0
+                for i in range(len(matrix)):
+                    matrix[i][col] = 0
+
+        setzero(arr, matrix)
 
 obj = Solution()
 #data = obj.setZeroes(matrix = [[1,1,1],[1,0,1],[1,1,1]])
