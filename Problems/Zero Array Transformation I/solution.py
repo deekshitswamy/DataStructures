4ac0cf9ac50f9f15b7f66279bb71a012c2b3aed8
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-21 16:47:13 +0530

    changes

--- Problems/Zero Array Transformation I/solution.py
+++ Problems/Zero Array Transformation I/solution.py
@@ -2,7 +2,21 @@ import io
 from typing import List
 class Solution:
     def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
-        pass
+        n = len(nums)
+        diff_array = [0] * (n+1) 
+
+        for start, end in queries:
+            diff_array[start] += 1
+            if end + 1 < n:
+                diff_array[end + 1] -= 1
+
+        current = 0
+        for index in range(n):
+            current += diff_array[index]
+            if nums[index] > current:
+                return False
+
+        return True
 
 obj = Solution()
 #data = obj.isZeroArray(nums = [1,0,1], queries = [[0,2]])
