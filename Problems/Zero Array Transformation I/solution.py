1fbe7ba8257151cdfd247942ed11c932a441b5a9
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-21 16:46:32 +0530

    Zero Array Transformation I

---
+++ Problems/Zero Array Transformation I/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.isZeroArray(nums = [1,0,1], queries = [[0,2]])
+data = obj.isZeroArray(nums = [4,3,2,1], queries = [[1,3],[0,2]])
+print(data)
