9894df7ef7deea631ae192414eb5eaa6370d0bdc
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-22 20:23:42 +0530

    Zero Array Transformation III

---
+++ Problems/Zero Array Transformation III/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maxRemoval(self, nums: List[int], queries: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxRemoval(nums = [2,0,2], queries = [[0,2],[0,2],[1,1]])
+#data = obj.maxRemoval(nums = [1,1,1,1], queries = [[1,3],[0,2],[1,3],[1,2]])
+data = obj.maxRemoval(nums = [1,2,3,4], queries = [[0,3]])
+print(data)
