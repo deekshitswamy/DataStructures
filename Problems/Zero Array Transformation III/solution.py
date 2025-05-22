c14330d9781c3c0be130f347e6532075ee050526
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-22 20:24:26 +0530

    changes

--- Problems/Zero Array Transformation III/solution.py
+++ Problems/Zero Array Transformation III/solution.py
@@ -2,7 +2,32 @@ import io
 from typing import List
 class Solution:
     def maxRemoval(self, nums: List[int], queries: List[List[int]]) -> int:
-        pass
+        queries.sort(key = lambda x: x[0])
+        preSum = [0] * (len(nums)+1)
+
+        validQueries = []
+        k = 0
+        currentSum = 0
+        userQueries = 0
+
+        for i in range(len(nums)):
+            while k < len(queries) and queries[k][0] <= i:
+                heapq.heappush(validQueries, -queries[k][1])
+                k+=1
+
+            while currentSum + preSum[i] < nums[i]:
+                if len(validQueries) == 0 or -validQueries[0] < i:
+                    return -1
+            
+                right = heapq.heappop(validQueries)
+                right = -right
+
+                preSum[i] += 1
+                preSum[right + 1] -= 1
+                userQueries += 1
+
+            currentSum += preSum[i]
+        return len(queries) - userQueries
 
 obj = Solution()
 #data = obj.maxRemoval(nums = [2,0,2], queries = [[0,2],[0,2],[1,1]])
