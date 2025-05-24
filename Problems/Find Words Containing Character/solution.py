6a1226f104e907e8354bf3a5c9b9bc16f1da1092
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-24 18:34:25 +0530

    Find Words Containing Character

---
+++ Problems/Find Words Containing Character/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.findWordsContaining(words = ["leet","code"], x = "e")
+#data = obj.findWordsContaining(words = ["abc","bcd","aaaa","cbc"], x = "a")
+data = obj.findWordsContaining(words = ["abc","bcd","aaaa","cbc"], x = "z")
+print(data)
