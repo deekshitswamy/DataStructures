bb7ed95a9919c81833872d760adfcce204700c75
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-25 21:10:52 +0530

    Longest Palindrome by Concatenating Two Letter Words

---
+++ Problems/Longest Palindrome by Concatenating Two Letter Words/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def longestPalindrome(self, words: List[str]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.longestPalindrome(words = ["lc","cl","gg"])
+#data = obj.longestPalindrome(words = ["ab","ty","yt","lc","cl","ab"])
+data = obj.longestPalindrome(words = ["cc","ll","xx"])
+print(data)
