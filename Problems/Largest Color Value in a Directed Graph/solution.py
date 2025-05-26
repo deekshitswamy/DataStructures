b2d813f97c30dc20f2afe2772acee64b07102854
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-26 21:46:49 +0530

    Largest Color Value in a Directed Graph

---
+++ Problems/Largest Color Value in a Directed Graph/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.largestPathValue(colors = "abaca", edges = [[0,1],[0,2],[2,3],[3,4]])
+data = obj.largestPathValue(colors = "a", edges = [[0,0]])
+print(data)
