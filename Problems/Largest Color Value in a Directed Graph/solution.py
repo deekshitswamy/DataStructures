8345a2468f47f4ce3d20680a45f12b91ad514c2a
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-26 21:47:52 +0530

    changes

--- Problems/Largest Color Value in a Directed Graph/solution.py
+++ Problems/Largest Color Value in a Directed Graph/solution.py
@@ -2,7 +2,53 @@ import io
 from typing import List
 class Solution:
     def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
-        pass
+        N = len(colors)
+        dp = [defaultdict(int) for _ in range(N)]
+
+        g = [[] for _ in range(N)]
+
+        for u, v in edges:
+            g[u].append(v)
+
+        parents = set()
+
+        visited = set()
+
+        def dfs(s):
+
+            if s in parents:
+                return False
+
+            if s in visited:
+                return True
+            
+            parents.add(s)
+        
+            coldict = defaultdict(int)
+
+            for e in g[s]:
+                if not dfs(e):
+                    return False
+
+                for col in dp[e]:
+                    coldict[col] = max(coldict[col], dp[e][col])
+            
+            for col in coldict:
+                dp[s][col] = coldict[col]
+            
+            dp[s][colors[s]] += 1
+            
+            parents.remove(s)
+
+            visited.add(s)
+
+            return True
+
+        for s in range(N):
+            if s not in visited and not dfs(s):
+                return -1
+
+        return max(max(k.values()) for k in dp)
 
 obj = Solution()
 #data = obj.largestPathValue(colors = "abaca", edges = [[0,1],[0,2],[2,3],[3,4]])
