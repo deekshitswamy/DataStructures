2c30e39e9b35389f1479c42d983a2a808ba7d76d
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-27 21:06:57 +0530

    Divisible and Non-divisible Sums Difference

---
+++ Problems/Divisible and Non-divisible Sums Difference/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.differenceOfSums(n = 10, m = 3)
+#data = obj.differenceOfSums(n = 5, m = 6)
+data = obj.differenceOfSums(n = 5, m = 1)
+print(data)
