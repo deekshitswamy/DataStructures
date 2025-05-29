bcfe7d70abd756033d5da98f9353d09fcef97187
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-29 21:03:47 +0530

    Maximize the Number of Target Nodes After Connecting Trees II

---
+++ Problems/Maximize the Number of Target Nodes After Connecting Trees II/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxTargetNodes(self, edges1: List[List[int]], edges2: List[List[int]]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.maxTargetNodes(edges1 = [[0,1],[0,2],[2,3],[2,4]], edges2 = [[0,1],[0,2],[0,3],[2,7],[1,4],[4,5],[4,6]])
+data = obj.maxTargetNodes(edges1 = [[0,1],[0,2],[0,3],[0,4]], edges2 = [[0,1],[1,2],[2,3]])
+print(data)
