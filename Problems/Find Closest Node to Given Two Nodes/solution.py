d6fc0301fd0f87675a6df6e88882cfe2c9b35c04
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-30 22:53:40 +0530

    Find Closest Node to Given Two Nodes

---
+++ Problems/Find Closest Node to Given Two Nodes/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.closestMeetingNode(edges = [2,2,3,-1], node1 = 0, node2 = 1)
+data = obj.closestMeetingNode(edges = [1,2,-1], node1 = 0, node2 = 2)
+print(data)
