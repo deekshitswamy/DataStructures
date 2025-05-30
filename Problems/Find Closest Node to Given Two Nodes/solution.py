78bb3913b61f22fe3f2080fef826649c9fb54e81
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-30 22:54:51 +0530

    changes

--- Problems/Find Closest Node to Given Two Nodes/solution.py
+++ Problems/Find Closest Node to Given Two Nodes/solution.py
@@ -1,5 +1,6 @@
 import io
 from typing import List
+from collections import deque
 class Solution:
     def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
         n = len(edges)
