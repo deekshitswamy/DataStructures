62b03f3a471182f69015ee1e0a7f4fd25a862fad
Deeku-Zeus <deekuzeus21@gmail.com>
2025-05-31 22:45:59 +0530

    Snakes and Ladders

---
+++ Problems/Snakes and Ladders/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.snakesAndLadders(board = [[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,35,-1,-1,13,-1],[-1,-1,-1,-1,-1,-1],[-1,15,-1,-1,-1,-1]])
+data = obj.snakesAndLadders(board = [[-1,-1],[-1,3]])
+print(data)
