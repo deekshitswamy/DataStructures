9feb5f558a848ad96bf2c61df856ed5686f5fb24
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-01 23:46:15 +0530

    changes

--- Problems/Distribute Candies Among Children II/solution.py
+++ Problems/Distribute Candies Among Children II/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
-        pass
+        def comb2(k):
+            return k * (k - 1) // 2 if k >= 2 else 0
+
+        def f(s):
+            return comb2(s + 2)
+
+        a = f(n)
+        b = f(n - (limit + 1))
+        c = f(n - 2 * (limit + 1))
+        d = f(n - 3 * (limit + 1))
+        return a - 3 * b + 3 * c - d
 
 obj = Solution()
 #data = obj.distributeCandies(n = 5, limit = 2)
