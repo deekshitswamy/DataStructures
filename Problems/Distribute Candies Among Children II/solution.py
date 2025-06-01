b2a1b4c5446a45e02613f7fd0f3f213e66bd788f
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-01 23:45:23 +0530

    Distribute Candies Among Children II

---
+++ Problems/Distribute Candies Among Children II/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def distributeCandies(self, n: int, limit: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.distributeCandies(n = 5, limit = 2)
+data = obj.distributeCandies(n = 3, limit = 3)
+print(data)
