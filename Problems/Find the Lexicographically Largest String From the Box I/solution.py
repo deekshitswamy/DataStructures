aba9ac97969ffbeb796cbe7af9365277ab5e2192
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-04 23:47:51 +0530

    changes

--- Problems/Find the Lexicographically Largest String From the Box I/solution.py
+++ Problems/Find the Lexicographically Largest String From the Box I/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def answerString(self, word: str, numFriends: int) -> str:
-        pass
+        if numFriends == 1: return word
+
+        W = len(word)
+        max_length = W - numFriends + 1
+
+        options = []
+        for i in range(W):
+            options.append(word[i : min(W, i + max_length)])
+
+        options.sort()
+
+        return options[-1]
 
 obj = Solution()
 #data = obj.answerString(word = "dbca", numFriends = 2)
