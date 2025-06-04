b3119be7928651b1d18a9dc35dfdd47c2dc0b066
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-04 23:46:10 +0530

    Find the Lexicographically Largest String From the Box I

---
+++ Problems/Find the Lexicographically Largest String From the Box I/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def answerString(self, word: str, numFriends: int) -> str:
+        pass
+
+obj = Solution()
+#data = obj.answerString(word = "dbca", numFriends = 2)
+data = obj.answerString(word = "gggg", numFriends = 4)
+print(data)
