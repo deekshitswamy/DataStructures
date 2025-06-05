61152bfafe96a77c8e48699cdbab9390bbaa5767
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-05 19:03:26 +0530

    Lexicographically Smallest Equivalent String

---
+++ Problems/Lexicographically Smallest Equivalent String/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.smallestEquivalentString(s1 = "parker", s2 = "morris", baseStr = "parser")
+#data = obj.smallestEquivalentString(s1 = "hello", s2 = "world", baseStr = "hold")
+data = obj.smallestEquivalentString(s1 = "leetcode", s2 = "programs", baseStr = "sourcecode")
+print(data)
