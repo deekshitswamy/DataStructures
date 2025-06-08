819d8b6b11a85adc5a923ed0e94246a42a432eeb
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-08 21:52:31 +0530

    Lexicographically Minimum String After Removing Stars

---
+++ Problems/Lexicographically Minimum String After Removing Stars/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def clearStars(self, s: str) -> str:
+        pass
+
+obj = Solution()
+#data = obj.clearStars(s = "aaba*")
+data = obj.clearStars(s = "abc")
+print(data)
