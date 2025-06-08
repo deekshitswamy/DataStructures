f394b1da70e8aa5cc2ebe8ff5328950240f1f1f3
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-08 21:53:02 +0530

    changes

--- Problems/Lexicographically Minimum String After Removing Stars/solution.py
+++ Problems/Lexicographically Minimum String After Removing Stars/solution.py
@@ -2,7 +2,18 @@ import io
 from typing import List
 class Solution:
     def clearStars(self, s: str) -> str:
-        pass
+        heap=[]
+        for i, c in enumerate(s):
+            if c=='*':
+                heappop(heap)
+            else:
+                heappush(heap,(c,-i))
+        ans = ['']*len(s)
+        while heap:
+            char, i=heappop(heap)
+            ans[-i]=char
+
+        return ''.join(ans)
 
 obj = Solution()
 #data = obj.clearStars(s = "aaba*")
