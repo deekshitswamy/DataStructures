f10776e194963884ea7afb6caedc27aa6f684179
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-08 21:50:30 +0530

    Using a Robot to Print the Lexicographically Smallest String

--- Problems/Using a Robot to Print the Lexicographically Smallest String/solution.py
+++ Problems/Using a Robot to Print the Lexicographically Smallest String/solution.py
@@ -1,11 +1,11 @@
 import io
 from typing import List
 class Solution:
-    def testfunc(self, n: int) -> int:
+    def robotWithString(self, s: str) -> str:
         pass
 
 obj = Solution()
-#data = obj.testfunc(n = 1)
-#data = obj.testfunc(n = 2)
-data = obj.testfunc(n = 2)
+#data = obj.robotWithString(s = "zza")
+#data = obj.robotWithString(s = "bac")
+data = obj.robotWithString(s = "bdda")
 print(data)
