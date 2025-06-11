5fdb58069cef843c14aef5ca4483ec1754af5c8e
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-11 21:21:45 +0530

    Maximum Difference Between Even and Odd Frequency II

---
+++ Problems/Maximum Difference Between Even and Odd Frequency II/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maxDifference(self, s: str, k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxDifference(s = "12233", k = 4)
+#data = obj.maxDifference(s = "1122211", k = 3)
+data = obj.maxDifference(s = "110", k = 3)
+print(data)
