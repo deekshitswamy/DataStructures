43aeb0cc3176794d8a9c4c94b6ef3f69aa11b5c0
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-14 21:33:13 +0530

    Maximum Difference by Remapping a Digit

---
+++ Problems/Maximum Difference by Remapping a Digit/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minMaxDifference(self, num: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minMaxDifference(num = 11891)
+data = obj.minMaxDifference(num = 90)
+print(data)
