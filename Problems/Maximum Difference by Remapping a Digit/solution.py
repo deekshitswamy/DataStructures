25f318e82fa74aa2bb7cb99295095e75e8fe79e9
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-14 21:33:50 +0530

    changes

--- Problems/Maximum Difference by Remapping a Digit/solution.py
+++ Problems/Maximum Difference by Remapping a Digit/solution.py
@@ -2,7 +2,25 @@ import io
 from typing import List
 class Solution:
     def minMaxDifference(self, num: int) -> int:
-        pass
+        num_arr = list(str(num))
+        num_s = 0
+        for i in range(len(num_arr)):
+            if num_arr[i] != "9":
+                num_s = num_arr[i]
+                break
+        for i in range(len(num_arr)):
+            if num_arr[i] == num_s:
+                num_arr[i] = "9"
+
+        max_num = int("".join(num_arr))
+        num_arr = list(str(num))
+        num_s = num_arr[0]
+        for j in range(len(num_arr)):
+            if num_arr[j] == num_s:
+                num_arr[j] = "0"
+
+        min_num = int("".join(num_arr))
+        return max_num - min_num
 
 obj = Solution()
 #data = obj.minMaxDifference(num = 11891)
