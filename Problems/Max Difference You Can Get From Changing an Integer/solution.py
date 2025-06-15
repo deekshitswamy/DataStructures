51a249cc097efdb34eda9e26ad91bd9c9a0bfb19
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-15 21:22:01 +0530

    Max Difference You Can Get From Changing an Integer

---
+++ Problems/Max Difference You Can Get From Changing an Integer/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxDiff(self, num: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxDiff(num = 555)
+data = obj.maxDiff(num = 9)
+print(data)
