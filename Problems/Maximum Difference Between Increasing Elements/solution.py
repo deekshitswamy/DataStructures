903e8c3d14e17e4a5e3511de69d7a0a9d1c84959
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-16 22:53:50 +0530

    changes

--- Problems/Maximum Difference Between Increasing Elements/solution.py
+++ Problems/Maximum Difference Between Increasing Elements/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def maximumDifference(self, nums: List[int]) -> int:
-        pass
+        mini = nums[0]
+        diff = 0
+        for i in range(1,len(nums)):
+            if nums[i] < mini:
+                mini = nums[i]
+            else:
+                diff1 = nums[i] - mini
+                diff = max(diff1,diff)
+        if diff == 0:
+            return -1
+        return diff
 
 obj = Solution()
 #data = obj.maximumDifference(nums = [7,1,5,4])
