f439360b608ff54f217d0d38f0c7171a4b6d5ad1
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-17 21:53:44 +0530

    Count the Number of Arrays with K Matching Adjacent Elements

---
+++ Problems/Count the Number of Arrays with K Matching Adjacent Elements/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def countGoodArrays(self, n: int, m: int, k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countGoodArrays(n = 3, m = 2, k = 1)
+#data = obj.countGoodArrays(n = 4, m = 2, k = 2)
+data = obj.countGoodArrays(n = 5, m = 2, k = 0)
+print(data)
