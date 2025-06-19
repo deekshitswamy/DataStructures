2a435c3a58af9acd9aefa6f84b1da10ba1914c71
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-19 22:46:29 +0530

    Partition Array Such That Maximum Difference Is K

---
+++ Problems/Partition Array Such That Maximum Difference Is K/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def partitionArray(self, nums: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.partitionArray(nums = [3,6,1,2,5], k = 2)
+#data = obj.partitionArray(nums = [1,2,3], k = 1)
+data = obj.partitionArray(nums = [2,2,4,5], k = 0)
+print(data)
