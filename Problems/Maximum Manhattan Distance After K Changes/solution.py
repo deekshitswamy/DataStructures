2b9e90ead0bc6bdb5a320b993ac888aae7d27043
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-21 17:00:34 +0530

    changes

--- Problems/Maximum Manhattan Distance After K Changes/solution.py
+++ Problems/Maximum Manhattan Distance After K Changes/solution.py
@@ -2,7 +2,19 @@ import io
 from typing import List
 class Solution:
     def maxDistance(self, s: str, k: int) -> int:
-        pass
+        ans=0
+        E=0
+        W=0
+        N=0
+        S=0
+        for i in range(len(s)):
+            if(s[i] == 'N') : N+=1
+            if(s[i] == 'W') : W+=1
+            if(s[i] == 'E') : E+=1
+            if(s[i] == 'S'): S+=1
+            base = abs(N - S) + abs(E - W)
+            ans=max(ans,min(base + 2*k , i+1))
+        return ans
 
 obj = Solution()
 #data = obj.maxDistance(s = "NWSE", k = 1)
