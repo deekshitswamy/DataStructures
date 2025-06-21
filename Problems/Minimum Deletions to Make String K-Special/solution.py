e61c9406dbe9dae3b34b2bbf92034adcd7343b2d
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-21 17:02:53 +0530

    Minimum Deletions to Make String K-Special

---
+++ Problems/Minimum Deletions to Make String K-Special/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def minimumDeletions(self, word: str, k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minimumDeletions(word = "aabcaba", k = 0)
+#data = obj.minimumDeletions(word = "dabdcbdcdcd", k = 2)
+data = obj.minimumDeletions(word = "aaabaaa", k = 2)
+print(data)
