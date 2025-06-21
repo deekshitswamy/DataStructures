7e40e0edf715e47f8d5e56b2da9311e5657ff032
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-21 16:57:13 +0530

    Maximum Manhattan Distance After K Changes

---
+++ Problems/Maximum Manhattan Distance After K Changes/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxDistance(self, s: str, k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxDistance(s = "NWSE", k = 1)
+data = obj.maxDistance(s = "NSWWEW", k = 3)
+print(data)
