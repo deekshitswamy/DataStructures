11b9b85669e1ecf5db565f5987d4eb8fffd62bbb
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-22 19:15:12 +0530

    Divide a String Into Groups of Size k

---
+++ Problems/Divide a String Into Groups of Size k/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def divideString(self, s: str, k: int, fill: str) -> List[str]:
+        pass
+
+obj = Solution()
+#data = obj.divideString(s = "abcdefghi", k = 3, fill = "x")
+data = obj.divideString(s = "abcdefghij", k = 3, fill = "x")
+print(data)
