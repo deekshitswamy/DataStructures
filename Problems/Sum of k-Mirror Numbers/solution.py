2ea11695b7002b900cb1989c016fab0d84a243d2
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-23 19:51:54 +0530

    Sum of k-Mirror Numbers

---
+++ Problems/Sum of k-Mirror Numbers/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def kMirror(self, k: int, n: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.kMirror(k = 2, n = 5)
+#data = obj.kMirror(k = 3, n = 7)
+data = obj.kMirror(k = 7, n = 17)
+print(data)
