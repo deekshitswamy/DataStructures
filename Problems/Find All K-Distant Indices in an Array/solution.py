7d45f151aecf6abb21fd950f2a5dff841bbb2347
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-24 19:06:02 +0530

    Find All K-Distant Indices in an Array

---
+++ Problems/Find All K-Distant Indices in an Array/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.findKDistantIndices(nums = [3,4,9,1,3,9,5], key = 9, k = 1)
+data = obj.findKDistantIndices(nums = [2,2,2,2,2], key = 2, k = 2)
+print(data)
