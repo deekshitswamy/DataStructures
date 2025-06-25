6559151db3ac851620a216b398f54dffc6ddbaa6
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-25 22:56:44 +0530

    Kth Smallest Product of Two Sorted Arrays

---
+++ Problems/Kth Smallest Product of Two Sorted Arrays/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def kthSmallestProduct(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.kthSmallestProduct(nums1 = [2,5], nums2 = [3,4], k = 2)
+#data = obj.kthSmallestProduct(nums1 = [-4,-2,0,3], nums2 = [2,4], k = 6)
+data = obj.kthSmallestProduct(nums1 = [-2,-1,0,1,2], nums2 = [-3,-1,2,4,5], k = 3)
+print(data)
