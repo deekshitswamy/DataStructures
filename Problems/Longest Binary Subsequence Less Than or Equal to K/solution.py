118d9e4a5ae72cec35da9b7a442707096335df8e
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-26 22:27:52 +0530

    Longest Binary Subsequence Less Than or Equal to K

---
+++ Problems/Longest Binary Subsequence Less Than or Equal to K/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def longestSubsequence(self, s: str, k: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.longestSubsequence(s = "1001010", k = 5)
+data = obj.longestSubsequence(s = "00101001", k = 1)
+print(data)
