5db91a7e94b8fc313f4125aa3e316f9465b1b410
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-28 00:26:17 +0530

    Longest Subsequence Repeated k Times

---
+++ Problems/Longest Subsequence Repeated k Times/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def longestSubsequenceRepeatedK(self, s: str, k: int) -> str:
+        pass
+
+obj = Solution()
+#data = obj.longestSubsequenceRepeatedK(s = "letsleetcode", k = 2)
+#data = obj.longestSubsequenceRepeatedK(s = "bb", k = 2)
+data = obj.longestSubsequenceRepeatedK(s = "ab", k = 2)
+print(data)
