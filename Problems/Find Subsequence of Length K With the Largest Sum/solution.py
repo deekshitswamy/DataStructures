5cec7df3eb38d38922139f14640917d8844feb4c
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-28 22:33:48 +0530

    Find Subsequence of Length K With the Largest Sum

---
+++ Problems/Find Subsequence of Length K With the Largest Sum/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.maxSubsequence(nums = [2,1,3,3], k = 2)
+#data = obj.maxSubsequence(nums = [-1,-2,3,4], k = 3)
+data = obj.maxSubsequence(nums = [3,4,3,3], k = 2)
+print(data)
