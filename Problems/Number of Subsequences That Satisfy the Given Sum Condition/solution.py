af71af99bdfe9ccd7a4306b68277abe3add19a9e
Deeku-Zeus <deekuzeus21@gmail.com>
2025-06-29 21:53:15 +0530

    Number of Subsequences That Satisfy the Given Sum Condition

---
+++ Problems/Number of Subsequences That Satisfy the Given Sum Condition/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numSubseq(nums = [3,5,6,7], target = 9)
+#data = obj.numSubseq(nums = [3,3,6,8], target = 10)
+data = obj.numSubseq(nums = [2,3,3,4,6,7], target = 12)
+print(data)
