a2eaae2bb795f2041587db76c7b91ca49468df03
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-01 23:16:29 +0530

    changes

--- Problems/Find the Original Typed String I/solution.py
+++ Problems/Find the Original Typed String I/solution.py
@@ -2,7 +2,17 @@ import io
 from typing import List
 class Solution:
     def possibleStringCount(self, word: str) -> int:
-        pass
+        ans=1
+        start=0
+        end=len(word)
+
+        while start<end:
+            idx=start
+            while idx<end and word[idx]==word[start]:
+                idx+=1
+            ans+=(idx-start-1)
+            start=idx
+        return ans
 
 obj = Solution()
 #data = obj.possibleStringCount(word = "abbcccc")
