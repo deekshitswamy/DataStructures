a1dae273661d6b1dc8fa6af3145c9fcd233e9cff
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-01 23:18:27 +0530

    Longest Harmonious Subsequence

---
+++ Problems/Longest Harmonious Subsequence/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findLHS(nums = [1,3,2,2,5,2,3,7])
+#data = obj.findLHS(nums = [1,2,3,4])
+data = obj.findLHS(nums = [1,1,1,1])
+print(data)
