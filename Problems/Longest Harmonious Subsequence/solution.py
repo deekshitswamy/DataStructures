9abe2b042dada4af2044d22e1f629a2f649897dc
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-01 23:19:08 +0530

    changes

--- Problems/Longest Harmonious Subsequence/solution.py
+++ Problems/Longest Harmonious Subsequence/solution.py
@@ -2,7 +2,12 @@ import io
 from typing import List
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
-        pass
+        countOfnums = Counter(nums)
+        max_length = 0
+        for key in countOfnums.keys() :
+            if key + 1 in countOfnums.keys() :
+                max_length = max(max_length, countOfnums[key] + countOfnums[key + 1])
+        return max_length
 
 obj = Solution()
 #data = obj.findLHS(nums = [1,3,2,2,5,2,3,7])
