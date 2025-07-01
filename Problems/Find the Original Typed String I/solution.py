9d1c03c73c5d4fd3b64a3e36e38bf547d2e68afb
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-01 23:15:50 +0530

    Find the Original Typed String I

---
+++ Problems/Find the Original Typed String I/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def possibleStringCount(self, word: str) -> int:
+        pass
+
+obj = Solution()
+#data = obj.possibleStringCount(word = "abbcccc")
+#data = obj.possibleStringCount(word = "abcd")
+data = obj.possibleStringCount(word = "aaaa")
+print(data)
