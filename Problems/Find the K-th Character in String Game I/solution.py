eed6d3b4d2973df92311bfd0230f28530a5997f0
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-03 22:54:49 +0530

    Find the K-th Character in String Game I

---
+++ Problems/Find the K-th Character in String Game I/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def kthCharacter(self, k: int) -> str:
+        pass
+
+obj = Solution()
+#data = obj.kthCharacter(k = 5)
+data = obj.kthCharacter(k = 10)
+print(data)
