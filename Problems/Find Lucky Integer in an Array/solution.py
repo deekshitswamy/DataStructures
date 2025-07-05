f4e052b55c35b0665a1ae2f8a921c781f429fcfd
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-05 17:03:58 +0530

    Find Lucky Integer in an Array

---
+++ Problems/Find Lucky Integer in an Array/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.findLucky(arr = [2,2,3,4])
+#data = obj.findLucky(arr = [1,2,2,3,3,3])
+data = obj.findLucky(arr = [2,2,2,3,3])
+print(data)
