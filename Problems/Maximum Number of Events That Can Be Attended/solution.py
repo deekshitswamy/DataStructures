0fcb3c367d9d89bc797e8b35c59865988e2e6fd7
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-07 22:40:13 +0530

    Maximum Number of Events That Can Be Attended

---
+++ Problems/Maximum Number of Events That Can Be Attended/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxEvents(self, events: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxEvents(events = [[1,2],[2,3],[3,4]])
+data = obj.maxEvents(events= [[1,2],[2,3],[3,4],[1,2]])
+print(data)
