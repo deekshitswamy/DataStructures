a9bb6c21f0f76a7877955130416703199ab3d531
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-09 21:36:20 +0530

    changes

--- Problems/Reschedule Meetings for Maximum Free Time I/solution.py
+++ Problems/Reschedule Meetings for Maximum Free Time I/solution.py
@@ -2,7 +2,16 @@ import io
 from typing import List
 class Solution:
     def maxFreeTime(self, eventTime: int, k: int, startTime: List[int], endTime: List[int]) -> int:
-        pass
+        ans = 0
+        start,end,meetingSum,n = 0,0,0,len(startTime)
+        for i in range(n):
+            meetingSum += endTime[i] - startTime[i]
+            end = startTime[i+1] if i + 1 < n else eventTime
+            if i >= k:
+                meetingSum -= endTime[i-k] - startTime[i-k]
+                start = endTime[i-k]
+            ans = max(ans,end - start - meetingSum)
+        return ans
 
 obj = Solution()
 #data = obj.maxFreeTime(eventTime = 5, k = 1, startTime = [1,3], endTime = [2,5])
