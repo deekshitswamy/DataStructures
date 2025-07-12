11925f30512db9e7f725bfb6734d54a3832a2fa6
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-12 19:15:52 +0530

    The Earliest and Latest Rounds Where Players Compete

---
+++ Problems/The Earliest and Latest Rounds Where Players Compete/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def earliestAndLatest(self, n: int, firstPlayer: int, secondPlayer: int) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.earliestAndLatest(n = 11, firstPlayer = 2, secondPlayer = 4)
+data = obj.earliestAndLatest(n = 5, firstPlayer = 1, secondPlayer = 5)
+print(data)
