5a32fa68178b0d434e58158ddd15cae901ee0985
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-13 22:59:43 +0530

    Maximum Matching of Players With Trainers

---
+++ Problems/Maximum Matching of Players With Trainers/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.matchPlayersAndTrainers(players = [4,7,9], trainers = [8,2,5,8])
+data = obj.matchPlayersAndTrainers(players = [1,1,1], trainers = [10])
+print(data)
