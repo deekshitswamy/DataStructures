8be71a40795039c94e349cc27d07e4e1c4a61ee6
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-15 21:25:04 +0530

    changes

--- Problems/Valid Word/solution.py
+++ Problems/Valid Word/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def isValid(self, word: str) -> bool:
-        pass
+        if len(word) < 3:
+            return False
+        vowels = set("aeiouAEIOU")
+        has_vowels = False
+        has_cons = False
+        for c in word:
+            if not (c.isalpha() or c.isdigit()):
+                return False
+            if c.isalpha():
+                if c in vowels:
+                    has_vowels = True
+                else:
+                    has_cons = True
+        return has_vowels and has_cons
 
 obj = Solution()
 #data = obj.isValid(word = "234Adas")
