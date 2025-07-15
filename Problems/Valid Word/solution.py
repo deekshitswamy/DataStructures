04e728bae3713b77f39a0579732c4ba6716929ab
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-15 21:24:26 +0530

    Valid Word

---
+++ Problems/Valid Word/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def isValid(self, word: str) -> bool:
+        pass
+
+obj = Solution()
+#data = obj.isValid(word = "234Adas")
+#data = obj.isValid(word = "b3")
+data = obj.isValid(word = "a3$e")
+print(data)
