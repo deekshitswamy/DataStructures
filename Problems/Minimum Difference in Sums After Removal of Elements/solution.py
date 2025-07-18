d16cfe5704b6c45bd255d909367da31dddf05533
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-18 22:00:07 +0530

    Minimum Difference in Sums After Removal of Elements

---
+++ Problems/Minimum Difference in Sums After Removal of Elements/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minimumDifference(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minimumDifference(nums = [3,1,2])
+data = obj.minimumDifference(nums = [7,9,5,8,1,3])
+print(data)
