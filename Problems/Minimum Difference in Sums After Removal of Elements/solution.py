659df037287fdeb99141503b3e5f86a1103c732d
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-18 22:01:43 +0530

    changes

--- Problems/Minimum Difference in Sums After Removal of Elements/solution.py
+++ Problems/Minimum Difference in Sums After Removal of Elements/solution.py
@@ -2,7 +2,33 @@ import io
 from typing import List
 class Solution:
     def minimumDifference(self, nums: List[int]) -> int:
-        pass
+        n = len(nums) // 3
+
+        pq_mx = [-nums[i] for i in range(n)]
+        heapq.heapify(pq_mx)
+        mn_ss = -sum(pq_mx)
+        mn_arr = [mn_ss] * (n+1)
+
+        for i in range(n, 2*n):
+            if nums[i] < -pq_mx[0]:
+                mx = heapq.heapreplace(pq_mx, -nums[i])
+                mn_ss += mx + nums[i]
+            
+            mn_arr[i-n + 1] = mn_ss
+
+        pq_mn = [nums[i] for i in range(2*n, 3*n)]
+        heapq.heapify(pq_mn)
+        mx_ss = sum(pq_mn)
+        mx_arr = [mx_ss] * (n+1)
+
+        for i in range(2*n - 1, n-1, -1):
+            if nums[i] > pq_mn[0]:
+                mn = heapq.heapreplace(pq_mn, nums[i])
+                mx_ss += -mn + nums[i]
+            
+            mx_arr[i-n] = mx_ss
+
+        return min(mn-mx for mn, mx in zip(mn_arr, mx_arr))
 
 obj = Solution()
 #data = obj.minimumDifference(nums = [3,1,2])
