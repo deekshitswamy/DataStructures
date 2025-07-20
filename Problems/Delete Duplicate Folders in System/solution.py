bb27e18eb9d041a26cb909e38338d8f0349b0796
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-20 19:59:25 +0530

    changes

--- Problems/Delete Duplicate Folders in System/solution.py
+++ Problems/Delete Duplicate Folders in System/solution.py
@@ -2,7 +2,33 @@ import io
 from typing import List
 class Solution:
     def deleteDuplicateFolder(self, paths: List[List[str]]) -> List[List[str]]:
-        pass
+        def serialize(node):
+            if not node:    return "()"
+            child_s = "".join(child + serialize(child_node) for child, child_node in sorted(node.items()))
+            serial = "(" + child_s + ")"
+            duplicates[serial].append(node)
+            return serial
+
+        def collect_paths(node, path):
+            for child_name, child_node in node.items():
+                if "#" in child_node:   continue
+                new_path = path + [child_name]
+                ret.append(new_path)
+                collect_paths(child_node, new_path)
+
+        tree, ret, duplicates = {}, [], defaultdict(list)
+        for path in paths:
+            node = tree
+            for folder in path:
+                node = node.setdefault(folder, {})
+        serialize(tree)
+        for nodes in duplicates.values():
+            if len(nodes) > 1:
+                for node in nodes:
+                    node.clear()
+                    node["#"] = True
+        collect_paths(tree, [])
+        return ret
 
 obj = Solution()
 #data = obj.deleteDuplicateFolder(paths = [["a"],["c"],["d"],["a","b"],["c","b"],["d","a"]])
