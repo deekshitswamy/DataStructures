053bb13726f64c02c81526b93b962645f9555c5b
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-22 20:33:46 +0530

    Maximum Erasure Value

---
+++ Problems/Maximum Erasure Value/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maximumUniqueSubarray(nums = [4,2,4,5,6])
+data = obj.maximumUniqueSubarray(nums = [5,2,1,2,5,2,1,2,5])
+print(data)
