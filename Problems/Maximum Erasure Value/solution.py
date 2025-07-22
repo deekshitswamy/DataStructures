e8a2ecf146501eb736793addb0df21045f1ae2ea
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-22 20:34:25 +0530

    changes

--- Problems/Maximum Erasure Value/solution.py
+++ Problems/Maximum Erasure Value/solution.py
@@ -2,7 +2,15 @@ import io
 from typing import List
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
-        pass
+        dic = {}
+        s, left, left_s, ans = 0, 0, 0, 0
+        for i, num in enumerate(nums):
+            if num in dic and dic[num][0] > left:
+                left, left_s = dic[num]
+            s += num
+            ans = max(s - left_s , ans)
+            dic[num] = (i+1, s)
+        return ans
 
 obj = Solution()
 #data = obj.maximumUniqueSubarray(nums = [4,2,4,5,6])
