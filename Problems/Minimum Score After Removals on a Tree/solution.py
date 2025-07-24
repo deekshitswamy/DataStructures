dccec2eeb5d28f27fb0a90b175c019dd78b7689f
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-24 21:41:11 +0530

    Minimum Score After Removals on a Tree

---
+++ Problems/Minimum Score After Removals on a Tree/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minimumScore(self, nums: List[int], edges: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minimumScore(nums = [1,5,5,4,11], edges = [[0,1],[1,2],[1,3],[3,4]])
+data = obj.minimumScore(nums = [5,5,2,4,4,2], edges = [[0,1],[1,2],[5,2],[4,3],[1,3]])
+print(data)
