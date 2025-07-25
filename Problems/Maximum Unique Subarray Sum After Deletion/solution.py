496e1449f57f8cf0527db59e506492f2152784e5
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-25 20:15:16 +0530

    changes

--- Problems/Maximum Unique Subarray Sum After Deletion/solution.py
+++ Problems/Maximum Unique Subarray Sum After Deletion/solution.py
@@ -2,7 +2,10 @@ import io
 from typing import List
 class Solution:
     def maxSum(self, nums: List[int]) -> int:
-        pass
+        max_num = max(nums)
+        if max_num <= 0:
+            return max_num
+        return sum(set(num for num in nums if num > 0))
 
 obj = Solution()
 #data = obj.maxSum(nums = [1,2,3,4,5])
