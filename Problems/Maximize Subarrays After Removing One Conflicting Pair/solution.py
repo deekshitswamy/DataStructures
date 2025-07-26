0552b19f30e08e672f5d8147c32060bfaee541cc
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-26 18:36:35 +0530

    Maximize Subarrays After Removing One Conflicting Pair

---
+++ Problems/Maximize Subarrays After Removing One Conflicting Pair/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxSubarrays(self, n: int, conflictingPairs: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxSubarrays(n = 4, conflictingPairs = [[2,3],[1,4]])
+data = obj.maxSubarrays(n = 5, conflictingPairs = [[1,2],[2,5],[3,5]])
+print(data)
