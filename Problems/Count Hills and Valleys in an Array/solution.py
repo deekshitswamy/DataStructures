d2db996ef56ba663e589bc5ee94260b06f9b8a94
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-27 13:45:15 +0530

    Count Hills and Valleys in an Array

---
+++ Problems/Count Hills and Valleys in an Array/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def countHillValley(self, nums: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.countHillValley(nums = [2,4,1,1,6,5])
+data = obj.countHillValley(nums = [6,6,5,5,4,1])
+print(data)
