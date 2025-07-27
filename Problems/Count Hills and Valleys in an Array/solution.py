5a28a9b7335871ede85f22d32d8ae1bcf5ae755a
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-27 13:45:53 +0530

    changes

--- Problems/Count Hills and Valleys in an Array/solution.py
+++ Problems/Count Hills and Valleys in an Array/solution.py
@@ -2,7 +2,22 @@ import io
 from typing import List
 class Solution:
     def countHillValley(self, nums: List[int]) -> int:
-        pass
+        stack = []
+        result = 0
+
+        for num in nums:
+            if stack and stack[-1] == num:
+                continue
+            stack.append(num)
+
+        print(stack)
+        
+        for i in range(1,len(stack)-1):
+            if stack[i-1]< stack[i] > stack[i+1]:
+                result += 1
+            elif stack[i-1] > stack[i] < stack[i+1]:
+                result += 1
+        return result
 
 obj = Solution()
 #data = obj.countHillValley(nums = [2,4,1,1,6,5])
