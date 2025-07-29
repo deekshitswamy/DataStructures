6c997dd3bf5ff2679295e34f43481045d1fba974
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-29 23:26:26 +0530

    Smallest Subarrays With Maximum Bitwise OR

---
+++ Problems/Smallest Subarrays With Maximum Bitwise OR/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def smallestSubarrays(self, nums: List[int]) -> List[int]:
+        pass
+
+obj = Solution()
+#data = obj.smallestSubarrays(nums = [1,0,2,1,3])
+data = obj.smallestSubarrays(nums = [1,2])
+print(data)
