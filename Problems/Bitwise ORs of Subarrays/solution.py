ce3fa27e105362af6c006769dbcecc5a264852c0
Deeku-Zeus <deekuzeus21@gmail.com>
2025-07-31 23:17:36 +0530

    Bitwise ORs of Subarrays

---
+++ Problems/Bitwise ORs of Subarrays/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def subarrayBitwiseORs(self, arr: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.subarrayBitwiseORs(arr = [0])
+#data = obj.subarrayBitwiseORs(arr = [1,1,2])
+data = obj.subarrayBitwiseORs(arr = [1,2,4])
+print(data)
