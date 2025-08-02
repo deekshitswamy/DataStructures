82c856da5cc608260e86d98253784d54b63bbf9e
Deeku-Zeus <deekuzeus21@gmail.com>
2025-08-02 19:42:26 +0530

    Rearranging Fruits

---
+++ Problems/Rearranging Fruits/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def minCost(self, basket1: List[int], basket2: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.minCost(basket1 = [4,2,2,2], basket2 = [1,4,1,2])
+data = obj.minCost(basket1 = [2,3,4,1], basket2 = [3,2,5,1])
+print(data)
