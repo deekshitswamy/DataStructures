a6d4271d3eb015435fac0e79b7e61863e1f619aa
Deeku-Zeus <deekuzeus21@gmail.com>
2025-08-02 19:43:11 +0530

    changes

--- Problems/Rearranging Fruits/solution.py
+++ Problems/Rearranging Fruits/solution.py
@@ -2,7 +2,31 @@ import io
 from typing import List
 class Solution:
     def minCost(self, basket1: List[int], basket2: List[int]) -> int:
-        pass
+        dct = defaultdict(int)
+        mn = inf
+
+        for b in basket1:
+            dct[b] += 1
+            mn = min(mn, b)
+        for b in basket2:
+            dct[b] -= 1
+            mn = min(mn, b)
+        
+        lst = []
+        for k, v in dct.items():
+            if v % 2 != 0:
+                return -1
+            for i in range(0, abs(v) // 2):
+                lst.append(k)
+        
+        lst.sort()
+        n = len(lst)
+        total = 0
+
+        for i in range(0, n // 2):
+            total += min(lst[i], mn * 2)
+        
+        return total
 
 obj = Solution()
 #data = obj.minCost(basket1 = [4,2,2,2], basket2 = [1,4,1,2])
