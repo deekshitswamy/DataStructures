de2416b1305f1f1887542542b21011dbe6e5fa87
Deeku-Zeus <deekuzeus21@gmail.com>
2025-08-05 06:52:05 +0530

    changes

--- Problems/Fruit Into Baskets/solution.py
+++ Problems/Fruit Into Baskets/solution.py
@@ -2,7 +2,23 @@ import io
 from typing import List
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        pass
+        dic = defaultdict(int)
+        maxval = 0
+        l = 0
+        for fruit in fruits:
+            dic[fruit] += 1
+            while len(dic) > 2:
+                subfruit = fruits[l]
+                dic[subfruit] -= 1
+                if dic[subfruit] == 0:
+                    del dic[subfruit]
+                l += 1
+            
+            current = 0
+            for key in dic.keys():
+                current += dic[key]
+            maxval = max(maxval, current)
+        return maxval
 
 obj = Solution()
 #data = obj.totalFruit(fruits = [1,2,1])
