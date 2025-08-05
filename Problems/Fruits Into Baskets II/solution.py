462342adc3345c7921502737482f5205e761a1af
Deeku-Zeus <deekuzeus21@gmail.com>
2025-08-05 06:49:45 +0530

    changes

--- Problems/Fruits Into Baskets II/solution.py
+++ Problems/Fruits Into Baskets II/solution.py
@@ -2,7 +2,20 @@ import io
 from typing import List
 class Solution:
     def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
-        pass
+        c=0
+        f=0
+        for i in range(len(fruits)):
+            for j in range(len(baskets)):
+                if fruits[i]<=baskets[j]:
+                    baskets.pop(j)
+                    c=c+1
+                    break
+            if c==1:
+                c=0
+            else:
+                f=f+1
+                c=0
+        return f
 
 obj = Solution()
 #data = obj.numOfUnplacedFruits(fruits = [4,2,5], baskets = [3,5,4])
