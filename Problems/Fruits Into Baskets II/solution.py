27b9ef2964844eb3bda4c469cd274e9ea6a25169
Deeku-Zeus <deekuzeus21@gmail.com>
2025-08-05 06:49:02 +0530

    Fruits Into Baskets II

---
+++ Problems/Fruits Into Baskets II/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.numOfUnplacedFruits(fruits = [4,2,5], baskets = [3,5,4])
+data = obj.numOfUnplacedFruits(fruits = [3,6,1], baskets = [6,4,7])
+print(data)
