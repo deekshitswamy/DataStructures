1dde0dae003f1f616e67934b94859577d617f1c8
Deeku-Zeus <deekuzeus21@gmail.com>
2025-08-05 06:51:25 +0530

    Fruit Into Baskets

---
+++ Problems/Fruit Into Baskets/solution.py
@@ -0,0 +1,11 @@
+import io
+from typing import List
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.totalFruit(fruits = [1,2,1])
+#data = obj.totalFruit(fruits = [0,1,2,2])
+data = obj.totalFruit(fruits = [1,2,3,2,2])
+print(data)
