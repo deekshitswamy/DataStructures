aa344b24cb23b76f556f3113a1b7ca9fd0bac07d
Deeku-Zeus <deekuzeus21@gmail.com>
2025-08-07 19:59:20 +0530

    Find the Maximum Number of Fruits Collected

---
+++ Problems/Find the Maximum Number of Fruits Collected/solution.py
@@ -0,0 +1,10 @@
+import io
+from typing import List
+class Solution:
+    def maxCollectedFruits(self, fruits: List[List[int]]) -> int:
+        pass
+
+obj = Solution()
+#data = obj.maxCollectedFruits(fruits = [[1,2,3,4],[5,6,8,7],[9,10,11,12],[13,14,15,16]])
+data = obj.maxCollectedFruits(fruits = [[1,1],[1,1]])
+print(data)
